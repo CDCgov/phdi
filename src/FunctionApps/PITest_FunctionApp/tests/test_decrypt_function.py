0ad7ebe7fc8c8abe104b3084a71ced3e843ba16c
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2022-02-01 15:58:53 -0500

    removing testing of blob trigger itself

--- src/FunctionApps/PITest_FunctionApp/tests/test_decrypt_function.py
+++ src/FunctionApps/PITest_FunctionApp/tests/test_decrypt_function.py
@@ -1,34 +1,32 @@
-import os
-from importlib.resources import files
 import json
-from pathlib import Path
 import logging
+import os
+import uuid
+from importlib.resources import files
+from pathlib import Path
 
 import azure.functions as func
 import pytest
+from azure.storage.blob import BlobServiceClient, ContainerClient
 
 from .. import DecryptFunction as dcf
 from ..DecryptFunction.settings import Settings
-from azure.storage.blob import BlobServiceClient, ContainerClient
-
-# Fixtures run before each test and can be passed as arguments to individual tests to enable accessing the variables they define. More info: https://docs.pytest.org/en/latest/fixture.html#fixtures-scope-sharing-and-autouse-autouse-fixtures
-@pytest.fixture(scope="session", autouse=True)
-def initialize_env_vars():
-    # get storage account settings
-    storage_connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
-    container_name = os.environ.get("STORAGE_CONTAINER")
-    return storage_connection_string, container_name
 
 
+# This fixture runs before all tests and can be passed as arguments to individual tests to enable accessing the variables they define. 
+# More info: https://docs.pytest.org/en/latest/fixture.html#fixtures-scope-sharing-and-autouse-autouse-fixtures
 @pytest.fixture(scope="session", autouse=True)
 def local_settings():
+    # Note, we manually parse this file because it is not loaded automatically by the test runner
     local_settings_path = files("DecryptFunction").parent / "local.settings.json"
     local_json_config = json.loads(local_settings_path.read_text())
     local_settings_vals = local_json_config.get("Values")
     settings = Settings()
     settings.private_key = local_settings_vals.get("PRIVATE_KEY")
     settings.private_key_password = local_settings_vals.get("PRIVATE_KEY_PASSWORD")
-    # settings.azure_storage_connection_string = local_settings_vals.get("AZURE_STORAGE_CONNECTION_STRING")
+
+    # Azurite connection string, from here: https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=visual-studio#http-connection-strings
+    settings.azure_storage_connection_string = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
     return settings
 
 
@@ -45,42 +43,3 @@ def test_decrypt_message(local_settings):
     )
     assert result == b"TESTING EICR ENCRYPTION"
 
-
-def test_trigger(local_settings):
-
-    source_container_name = "raw"
-    destination_container_name = "decrypted"
-    source_container_client = ContainerClient.from_connection_string(
-        local_settings.azure_storage_connection_string, source_container_name
-    )
-    destination_container_client = ContainerClient.from_connection_string(
-        local_settings.azure_storage_connection_string, source_container_name
-    )
-
-    source_file_name = "encrypted.txt"
-    destination_file_name = f"{source_file_name}.decrypted"
-    test_file_path = (
-        files("DecryptFunction").parent / "tests" / "assets" / source_file_name
-    )
-
-    with open(test_file_path, "rb") as data:
-        blob_upload_result = source_container_client.upload_blob(
-            name=source_file_name, data=data
-        )
-        properties = blob_upload_result.get_blob_properties()
-        logging.properties(f"Post-source data upload blob Properties: {properties}")
-
-        destination_blob_client = destination_container_client.get_blob_client(
-            name=destination_file_name
-        )
-        while not destination_blob_client.exists():
-            logging.info(
-                f"Waiting for {destination_file_name} to be available in {destination_container_name}..."
-            )
-
-        logging.info(
-            f"{destination_file_name} available in {destination_container_name}. Downloading"
-        )
-
-        destination_data = destination_blob_client.download_blob().readall()
-        assert destination_data == b"TESTING EICR ENCRYPTION"
