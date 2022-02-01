e695334ff7d1464963f9a83440b4d8fc404722c4
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2022-02-01 13:14:21 -0500

    adding test for blob actions

--- src/FunctionApps/PITest_FunctionApp/DecryptFunction/settings.py
+++ src/FunctionApps/PITest_FunctionApp/DecryptFunction/settings.py
@@ -4,6 +4,6 @@ import os
 
 @dataclass
 class Settings:
-    private_key_password = os.getenv("PRIVATE_KEY_PASSWORD")
-    private_key = os.getenv("PRIVATE_KEY")
+    private_key_password = os.getenv("PrivateKeyPassword")
+    private_key = os.getenv("PrivateKey")
     azure_storage_connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
