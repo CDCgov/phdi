9953351e6d4e2aaff40cc92e9d74d134ddad67b2
Brett Maden <brett.m.maden@omb.eop.gov>
2022-02-08 10:09:08 -0500

    [CU-1zbqup9] Update DecryptFunction test to use test key (#27)
    * update DecryptFunction test to use a test key
    * add a failing decryption test

--- src/FunctionApps/PITest_FunctionApp/DecryptFunction/settings.py
+++ src/FunctionApps/PITest_FunctionApp/DecryptFunction/settings.py
@@ -8,12 +8,9 @@ class DecryptSettings:
     When run remotely, settings are passed by environment variables in the function app, 
     which in turn reference KeyVault variables as mentioned here: https://docs.microsoft.com/en-us/azure/app-service/app-service-key-vault-references
 
-    When run locally, these consult a local file, local.settings.json.
-    This can be copied from the Azure portal via:
-        ```func azure functionapp fetch-app-settings pitest-functionapp --output-file local.settings.json
-            func settings decrypt
-        ```
-        to get the values direct from the portal, then edited to set the variables.
+    When tests are run, these settings are read from the local file `test.settings.json`
+    which contains a different pgp key that is only used for tests and is not stored in
+    the KeyVault.
     """
     private_key_password = os.getenv("PrivateKeyPassword")
     private_key = os.getenv("PrivateKey")
