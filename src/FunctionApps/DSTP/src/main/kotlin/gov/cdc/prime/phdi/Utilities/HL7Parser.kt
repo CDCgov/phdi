071bdebf69ae2425f1c6315a402c4a7a7eff2f7f
Bryan <bryan@skylight.digital>
2022-02-07 22:29:23 -0800

    addresses most of the concerns listed in PR#18's comments. Still to come is unit testing, moving .gitignore to top level, and assessing logging options

--- src/FunctionApps/DSTP/src/main/kotlin/gov/cdc/prime/phdi/Utilities/HL7Parser.kt
+++ src/FunctionApps/DSTP/src/main/kotlin/gov/cdc/prime/phdi/Utilities/HL7Parser.kt
@@ -10,12 +10,15 @@ import ca.uhn.hl7v2.validation.ValidationContext;
 import ca.uhn.hl7v2.validation.impl.ValidationContextFactory;
 
 public class HL7Parser() {
-    public fun parse(content: String): Map<String, MutableList<Any>> {
+    data class ProcessedMessages(
+        val valid_messages: List<Message>,
+        val invalid_messages: List<String>
+    )
+
+    public fun parse(content: String): ProcessedMessages {
         val rawMessages = convertBatchMessagesToList(content)
-        val processedMessages = mutableMapOf<String, MutableList<Any>>(
-            "invalid_messages" to mutableListOf(),
-            "valid_messages" to mutableListOf()
-        )
+        val valid_messages = mutableListOf<Message>()
+        val invalid_messages = mutableListOf<String>()
 
         val context = DefaultHapiContext()
         // We will likely replace this with a more custom validation class in the future
@@ -25,12 +28,12 @@ public class HL7Parser() {
         rawMessages.forEach {
             try {
                 val parsedMessage = parser.parse(it)
-                processedMessages["valid_messages"]!!.add(parsedMessage)
+                valid_messages.add(parsedMessage)
             } catch (e: HL7Exception) {
-                processedMessages["invalid_messages"]!!.add(it)
+                invalid_messages.add(it)
             }
         }
-        return processedMessages
+        return ProcessedMessages(valid_messages, invalid_messages)
     }
 
     /* This method was adopted from PRIME ReportStream, which can be found here: 
@@ -42,9 +45,11 @@ public class HL7Parser() {
     ): MutableList<String> {
         var reg = "[\r\n]".toRegex()
         var cleanedMessage: String = reg.replace(content, delimiter)
-        /* These are unicode for vertical tab and file separator, respectively
+        /* 
+           These are unicode for vertical tab and file separator, respectively
            \u000b appears before every MSH segment, and \u001c appears at the
-           end of the message.
+           end of the message in some of the data we've been receiving, so 
+           we're explicitly removing them here.
         */
         reg = "[\\u000b\\u001c]".toRegex()
         cleanedMessage = reg.replace(cleanedMessage, "").trim()
@@ -52,6 +57,25 @@ public class HL7Parser() {
         val nextMessage = StringBuilder()
         val output = mutableListOf<String>()
         
+        /* 
+        FHS is a "File Header Segment", which is used to head a file (group of batches)
+        FTS is a "File Trailer Segment", which defines the end of a file
+        BHS is "Batch Header Segment", which defines the start of a batch
+        BTS is "Batch Trailer Segment", which defines the end of a batch
+        
+        The structure of an HL7 Batch looks like this:
+        [FHS] (file header segment) { [BHS] (batch header segment)
+        { [MSH (zero or more HL7 messages)
+        ....
+        ....
+        ....
+        ] }
+        [BTS] (batch trailer segment)
+        }
+        [FTS] (file trailer segment)
+
+        We ignore lines that start with these since we don't want to include them in a message
+        */
         messageLines.forEach {
             if (it.startsWith("FHS"))
                 return@forEach
@@ -62,16 +86,29 @@ public class HL7Parser() {
             if (it.startsWith("FTS"))
                 return@forEach
 
+            /*
+                If we reach a line that starts with MSH and we have
+                content in nextMessage, then by definition we have 
+                a full message in nextMessage and need to append it
+                to output. This will not trigger the first time we
+                see a line with MSH since nextMessage will be empty
+                at that time.
+            */
             if (nextMessage.isNotBlank() && it.startsWith("MSH")) {
                 output.add(nextMessage.toString())
                 nextMessage.clear()
             }
 
+            // Otherwise, continue to add the line of text to nextMessage
             if (it.isNotBlank()) {
                 nextMessage.append("$it\r")
             }
         }
-        // catch the last message
+        /*
+            Since our loop only adds messages to output when it finds
+            a line that starts with MSH, the last message would never
+            be added. So we explicitly add it here.
+        */
         if (nextMessage.isNotBlank()) {
             output.add(nextMessage.toString())
         }
