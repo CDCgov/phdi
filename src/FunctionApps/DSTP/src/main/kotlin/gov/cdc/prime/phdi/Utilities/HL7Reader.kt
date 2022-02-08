844ed12c9d6ed318dc0d7d6b12b42df0f8b55696
Bryan <bryan@skylight.digital>
2022-02-07 22:29:23 -0800

    addresses most of the concerns listed in PR#18's comments. Still to come is unit testing, moving .gitignore to top level, and assessing logging options

--- src/FunctionApps/DSTP/src/main/kotlin/gov/cdc/prime/phdi/Utilities/HL7Reader.kt
+++ src/FunctionApps/DSTP/src/main/kotlin/gov/cdc/prime/phdi/Utilities/HL7Reader.kt
@@ -7,20 +7,22 @@ import java.io.OutputStream
 import java.nio.charset.StandardCharsets
 
 public class HL7Reader() {
+    @Throws(NotImplementedError::class)
     fun readHL7MessagesFromFile(
         filepath: String
     ): String {
         // reads in a file of one or more HL7 messages and returns the content as a string
-        return "This method is not currently implemented."
+        throw NotImplementedError("This method is not currently implemented.")
     }
 
+    @Throws(NotImplementedError::class)
     fun readHL7MessagesFromBlobStorage(
         client: String,
         bucketName: String,
         filepath: String 
     ): String {
         // given a cloud client, bucket name, and file path, read the content and return it as a string
-        return "This method is not currently implemented."
+        throw NotImplementedError("This method is not currently implemented.")
     }
 
     fun readHL7MessagesFromByteArray(
