ef5c4979291ce8915057cb48bd017eaace3acaf9
Brett Maden <brett.m.maden@omb.eop.gov>
2022-02-14 12:46:47 -0500

    Apply python and terraform formatting (#33)
    * python is formatted with black in it's default configuration
    * terraform is formatted with `terraform fmt`

--- operations/app/sample/infrastructurecheck/__init__.py
+++ operations/app/sample/infrastructurecheck/__init__.py
@@ -8,7 +8,7 @@ Query parameters:
 Headers:
 
 * `Accept` if 'text/plain' we'll return a plaintext response, otherwise it'll be JSON
-"""
+"""  # noqa: E501
 import collections
 import json
 import logging
@@ -25,28 +25,32 @@ from azure.storage.blob import BlobServiceClient
 
 logger = logging.getLogger(__name__)
 
-KEY_VAULT_NAME = 'pitest-app-kv'
-SECRET_NAME = 'test-secret'
-SECRET_VALUE_EXPECTED = 'BurritoTown'
-STORAGE_ACCOUNT_CONNECTION = os.environ.get('APPSETTING_AzureWebJobsStorage')
-STORAGE_FILENAME = '_test.txt'
-STORAGE_HOST = 'pitestdatastorage.privatelink.blob.core.windows.net'
-URL_IP_CHECK = 'https://api.ipify.org/?format=json'
+KEY_VAULT_NAME = "pitest-app-kv"
+SECRET_NAME = "test-secret"
+SECRET_VALUE_EXPECTED = "BurritoTown"
+STORAGE_ACCOUNT_CONNECTION = os.environ.get("APPSETTING_AzureWebJobsStorage")
+STORAGE_FILENAME = "_test.txt"
+STORAGE_HOST = "pitestdatastorage.privatelink.blob.core.windows.net"
+URL_IP_CHECK = "https://api.ipify.org/?format=json"
 
-Check = collections.namedtuple('Check', ['name', 'status', 'message'])
+Check = collections.namedtuple("Check", ["name", "status", "message"])
 
 
 def is_blank(v):
-    return v is None or v.strip() == ''
+    return v is None or v.strip() == ""
 
 
 def verify_blob_storage():
     checks = []
     try:
-        service_client = BlobServiceClient.from_connection_string(STORAGE_ACCOUNT_CONNECTION)
-        checks.append(Check('Storage client', 'ok', 'Created from connection string'))
+        service_client = BlobServiceClient.from_connection_string(
+            STORAGE_ACCOUNT_CONNECTION
+        )
+        checks.append(Check("Storage client", "ok", "Created from connection string"))
     except Exception as e:
-        return [Check('Storage client', 'error', f'Failed to create BlobServiceClient: {e}')]
+        return [
+            Check("Storage client", "error", f"Failed to create BlobServiceClient: {e}")
+        ]
 
     containers = ["bronze", "silver", "gold"]
 
@@ -55,7 +59,11 @@ def verify_blob_storage():
             container_client = service_client.get_container_client(container_name)
         except Exception as e:
             checks.append(
-                Check(f'Container client: {container_name}', 'error', f'Failed to create container client: {e}')
+                Check(
+                    f"Container client: {container_name}",
+                    "error",
+                    f"Failed to create container client: {e}",
+                )
             )
             continue
 
@@ -63,17 +71,23 @@ def verify_blob_storage():
             blob_client = container_client.get_blob_client(STORAGE_FILENAME)
         except Exception as e:
             checks.append(
-                Check(f'Blob client: {container_name}', 'error', f'Failed to create blob client: {e}')
+                Check(
+                    f"Blob client: {container_name}",
+                    "error",
+                    f"Failed to create blob client: {e}",
+                )
             )
             continue
 
         try:
             blob = blob_client.download_blob()
             blob_value = blob.content_as_text()
-            checks.append(Check(f'Blob download: {container_name}', 'ok', ''))
+            checks.append(Check(f"Blob download: {container_name}", "ok", ""))
         except Exception as e:
             checks.append(
-                Check(f'Blob read: {container_name}', 'error', f'Failed to get blob: {e}')
+                Check(
+                    f"Blob read: {container_name}", "error", f"Failed to get blob: {e}"
+                )
             )
             continue
 
@@ -86,10 +100,14 @@ def verify_blob_storage():
             new_count = int(count.strip()) + 1
             new_value = "\n".join([lines[0], f"Count: {new_count}"])
             blob_client.upload_blob(new_value, overwrite=True)
-            checks.append(Check(f'Blob upload: {container_name}', 'ok', ''))
+            checks.append(Check(f"Blob upload: {container_name}", "ok", ""))
         except Exception as e:
             checks.append(
-                Check(f'Blob upload: {container_name}', 'error', f'Failed to uploadblob: {e}')
+                Check(
+                    f"Blob upload: {container_name}",
+                    "error",
+                    f"Failed to uploadblob: {e}",
+                )
             )
 
     return checks
@@ -98,16 +116,26 @@ def verify_blob_storage():
 def verify_dns():
     checks = []
     try:
-        google_ip = socket.gethostbyname('google.com')
-        checks.append(Check('DNS lookup - external', 'ok', f'Found IP "{google_ip}" for google.com'))
+        google_ip = socket.gethostbyname("google.com")
+        checks.append(
+            Check(
+                "DNS lookup - external", "ok", f'Found IP "{google_ip}" for google.com'
+            )
+        )
     except Exception as e:
-        checks.append(Check('DNS lookup - external', 'error', str(e)))
+        checks.append(Check("DNS lookup - external", "error", str(e)))
 
     try:
         storage_ip = socket.gethostbyname(STORAGE_HOST)
-        checks.append(Check('DNS lookup - internal', 'ok', f'Found IP "{storage_ip}" for "{STORAGE_HOST}"'))
+        checks.append(
+            Check(
+                "DNS lookup - internal",
+                "ok",
+                f'Found IP "{storage_ip}" for "{STORAGE_HOST}"',
+            )
+        )
     except Exception as e:
-        checks.append(Check('DNS lookup - internal', 'error', str(e)))
+        checks.append(Check("DNS lookup - internal", "error", str(e)))
 
     return checks
 
@@ -116,59 +144,72 @@ def verify_key_vault_read():
     url = f"https://{KEY_VAULT_NAME}.vault.azure.net"
     try:
         credential = azure.identity.DefaultAzureCredential()
-        client = azure.keyvault.secrets.SecretClient(vault_url=url, credential=credential)
+        client = azure.keyvault.secrets.SecretClient(
+            vault_url=url, credential=credential
+        )
         secret = client.get_secret(SECRET_NAME).value
         if secret == SECRET_VALUE_EXPECTED:
-            return Check('Key vault', 'ok', 'Fetched secret and verified contents')
+            return Check("Key vault", "ok", "Fetched secret and verified contents")
         else:
             return Check(
-                'Key vault', 'error',
-                f'Fetched secret ok but value mismatch (expected {SECRET_VALUE_EXPECTED}, got {secret}')
+                "Key vault",
+                "error",
+                f"Fetched secret ok but value mismatch "
+                f"(expected {SECRET_VALUE_EXPECTED}, got {secret}",
+            )
 
     except Exception as e:
-        return Check('Key vault', 'error', f'Failed to create client or fetch secret: {e}')
+        return Check(
+            "Key vault", "error", f"Failed to create client or fetch secret: {e}"
+        )
 
 
 def verify_my_ip():
     try:
         response = requests.get(URL_IP_CHECK, timeout=10)
-        ip = response.json().get('ip') or 'Unknown IP'
-        return Check('IP Check', 'ok', ip)
+        ip = response.json().get("ip") or "Unknown IP"
+        return Check("IP Check", "ok", ip)
     except Exception as e:
-        return Check('IP Check', 'error', str(e))
+        return Check("IP Check", "error", str(e))
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logger.info('APP: Python HTTP trigger function processing a request.')
+    logger.info("APP: Python HTTP trigger function processing a request.")
     checks = []
 
-    logger.info('Task 1: Verifying DNS')
-    checks.extend(verify_dns())    # task 1: can I lookup IPs? checks whether the DNS from CDC (or Azure) is working
-    logger.info('Task 2: Verifying my IP')
-    checks.append(verify_my_ip())  # task 2: can I reach the internet? if so, what is my IP?
+    logger.info("Task 1: Verifying DNS")
+    checks.extend(
+        verify_dns()
+    )  # task 1: can I lookup IPs? checks whether the DNS from CDC (or Azure) is working
+    logger.info("Task 2: Verifying my IP")
+    checks.append(
+        verify_my_ip()
+    )  # task 2: can I reach the internet? if so, what is my IP?
 
     # ?run_azure=false will skip the storage account and key vault actions
-    run_azure = req.params.get('run_azure', 'true').strip().lower() == 'true'
+    run_azure = req.params.get("run_azure", "true").strip().lower() == "true"
 
     if run_azure:
-        logger.info('Task 3: Verifying blob storage')
+        logger.info("Task 3: Verifying blob storage")
         if is_blank(STORAGE_ACCOUNT_CONNECTION):
-            checks.append(Check('Blob *', 'error', 'No connection string defined'))
+            checks.append(Check("Blob *", "error", "No connection string defined"))
         else:
-            checks.extend(verify_blob_storage())  # task 3: can I read/write to blob storage?
-        logger.info('Task 4: Verifying key vault')
-        checks.append(verify_key_vault_read())    # task 4: can I read from key vault?
+            checks.extend(
+                verify_blob_storage()
+            )  # task 3: can I read/write to blob storage?
+        logger.info("Task 4: Verifying key vault")
+        checks.append(verify_key_vault_read())  # task 4: can I read from key vault?
     else:
-        logger.info('Task 3+4: Skipping on request')
-        checks.append(Check('Blob *', 'skip', 'Instructed to not run azure tasks'))
-        checks.append(Check('Key vault', 'skip', 'Instructed to not run azure tasks'))
+        logger.info("Task 3+4: Skipping on request")
+        checks.append(Check("Blob *", "skip", "Instructed to not run azure tasks"))
+        checks.append(Check("Key vault", "skip", "Instructed to not run azure tasks"))
 
     # just check the first one rather than dealing with q-weights
-    accept_header = re.split(r'\s+,\s+', req.headers.get('Accept', ''))
-    is_text = len(accept_header) > 0 and 'text' in accept_header[0]
+    accept_header = re.split(r"\s+,\s+", req.headers.get("Accept", ""))
+    is_text = len(accept_header) > 0 and "text" in accept_header[0]
 
     if is_text:
-        headers = {'Content-Type': 'text/plain'}
+        headers = {"Content-Type": "text/plain"}
         body = "Checks:\n"
         for c in checks:
             body += f"* {c.name}: {c.status}"
@@ -182,10 +223,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     else:
         response = {
-            'checks': [c._asdict() for c in checks],
-            'env': {k: v for k, v in os.environ.items()},
+            "checks": [c._asdict() for c in checks],
+            "env": {k: v for k, v in os.environ.items()},
         }
         body = json.dumps(response, indent=4, sort_keys=True)
-        headers = {'Content-Type': 'application/json'}
+        headers = {"Content-Type": "application/json"}
 
     return func.HttpResponse(body=body, headers=headers, status_code=200)
