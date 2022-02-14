716cb5ea4dc9b657d986f8724a77e16e239f8561
Brett Maden <brett.m.maden@omb.eop.gov>
2022-02-14 12:46:47 -0500

    Apply python and terraform formatting (#33)
    * python is formatted with black in it's default configuration
    * terraform is formatted with `terraform fmt`

--- operations/app/terraform/modules/log_analytics_workspace/data.tf
+++ operations/app/terraform/modules/log_analytics_workspace/data.tf
@@ -1,33 +1,33 @@
 locals {
   default = {
     "function_infrastructure_app" = {
-      id   = var.function_infrastructure_app_id
-      name = "function_infrastructure_app"
+      id    = var.function_infrastructure_app_id
+      name  = "function_infrastructure_app"
       diags = data.azurerm_monitor_diagnostic_categories.function_infrastructure_app
     },
     "function_app" = {
-      id   = var.function_app_id
-      name = "function_app"
+      id    = var.function_app_id
+      name  = "function_app"
       diags = data.azurerm_monitor_diagnostic_categories.function_app
     },
     "app_service_plan" = {
-      id   = var.app_service_plan_id
-      name = "app_service_plan"
+      id    = var.app_service_plan_id
+      name  = "app_service_plan"
       diags = data.azurerm_monitor_diagnostic_categories.app_service_plan
     },
     "cdc_managed_vnet" = {
-      id   = var.cdc_managed_vnet_id
-      name = "cdc_managed_vnet"
+      id    = var.cdc_managed_vnet_id
+      name  = "cdc_managed_vnet"
       diags = data.azurerm_monitor_diagnostic_categories.cdc_managed_vnet
     },
     "sa_data" = {
-      id   = var.sa_data_id
-      name = "sa_data"
+      id    = var.sa_data_id
+      name  = "sa_data"
       diags = data.azurerm_monitor_diagnostic_categories.sa_data
     },
     "adf" = {
-      id   = var.adf_id
-      name = "adf"
+      id    = var.adf_id
+      name  = "adf"
       diags = data.azurerm_monitor_diagnostic_categories.adf
     }
   }
