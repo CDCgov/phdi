09b1229f0b2e218f2d81f52ce3e4fe0b9478f60c
Graham Smith <graham@skylight.digital>
2022-02-17 11:42:13 -0500

    Spike: Use Cosmos DB as a serverless cache

--- src/FunctionApps/CachedGeocode/Geocode/geocode_test.py
+++ src/FunctionApps/CachedGeocode/Geocode/geocode_test.py
@@ -1,17 +1,17 @@
 import json
 
 import azure.functions as func
+
 import pytest
 from unittest import mock
 
 from Geocode import main
 from Geocode import geocode
-from Geocode import CACHE_EXPIRATION
 
 
 @mock.patch("Geocode.geocode")
-@mock.patch("redis.StrictRedis")
-def test_main(mock_redis, mock_geocode):
+@mock.patch("pymongo.MongoClient")
+def test_main(mock_mongo, mock_geocode):
     # Empty requests should be a 400
     resp = main(func.HttpRequest(method="GET", url="/", body="", params={}))
     assert resp.status_code == 400
@@ -39,8 +39,8 @@ def test_main(mock_redis, mock_geocode):
     )
     assert resp.status_code == 500
 
-    # Check what happens when we can't connect to redis
-    mock_redis.side_effect = Exception("probably security groups")
+    # Check what happens when we can't connect to mongodb
+    mock_mongo.side_effect = Exception("probably security groups")
     resp = main(func.HttpRequest(method="GET", url="/", body=""))
 
     assert resp.status_code == 400  # Should attempt to geocode anyway
@@ -49,10 +49,10 @@ def test_main(mock_redis, mock_geocode):
 @mock.patch("requests.get")
 def test_geocode_cache_hit(mock_get):
     cache = mock.Mock()
-    cache.get.return_value = json.dumps({"lng": -75.123, "lat": 43.234})
+    cache.find_one.return_value = {"lng": -75.123, "lat": 43.234}
 
     resp = geocode("123 F@ke St", cache)
-    assert cache.get.called_with("geocode::123FKEST")
+    assert cache.find_one.called_with("123FKEST")
     assert -75.123 == pytest.approx(resp.get("lng"))
     assert 43.234 == pytest.approx(resp.get("lat"))
     assert mock_get.not_called()
@@ -61,7 +61,7 @@ def test_geocode_cache_hit(mock_get):
 @mock.patch("requests.get")
 def test_geocode_cache_miss(mock_get):
     cache = mock.Mock()
-    cache.get.return_value = None
+    cache.find_one.return_value = None
 
     mresp = mock.Mock()
     mresp.status_code = 200
@@ -75,29 +75,27 @@ def test_geocode_cache_miss(mock_get):
     mock_get.assert_called()
     assert 1.0 == pytest.approx(resp.get("lng"))
     assert 2.0 == pytest.approx(resp.get("lat"))
-    cache.setex.assert_called_with(
-        "geocode::123FKEST", CACHE_EXPIRATION, json.dumps(resp)
-    )
+    cache.insert_one.assert_called_with({"key": "123FKEST", "lng": 1.0, "lat": 2.0})
 
 
 @mock.patch("requests.get")
 def test_geocode_failure(mock_get):
     cache = mock.Mock()
-    cache.get.return_value = None
+    cache.find_one.return_value = None
 
     mock_get.return_value = mock.Mock(status_code=500)
 
     with pytest.raises(Exception):
         geocode("123 Fake St", cache)
 
-    cache.get.assert_called()
-    cache.setex.assert_not_called()
+    cache.find_one.assert_called()
+    cache.insert_one.assert_not_called()
 
 
 @mock.patch("requests.get")
 def test_geocode_empty(mock_get):
     cache = mock.Mock()
-    cache.get.return_value = None
+    cache.find_one.return_value = None
 
     mresp = mock.Mock()
     mresp.status_code = 200
@@ -108,5 +106,5 @@ def test_geocode_empty(mock_get):
     with pytest.raises(Exception):
         geocode("123 Fake St", cache)
 
-    cache.get.assert_called()
-    cache.setex.assert_not_called()
+    cache.find_one.assert_called()
+    cache.insert_one.assert_not_called()
