b79df09609bff2aa848925ad222e99aa3ad21a84
Bryan <bryan@skylight.digital>
2022-02-18 11:12:15 -0800

    Initial commit to implement conversion to FHIR using FHIR server endpoint

--- src/FunctionApps/DSTP/src/test/kotlin/gov/cdc/prime/phdi/Utilities/HelperFunctionTest.kt
+++ src/FunctionApps/DSTP/src/test/kotlin/gov/cdc/prime/phdi/Utilities/HelperFunctionTest.kt
@@ -1,47 +1,57 @@
-package gov.cdc.prime.phdi.Utilities
+package gov.cdc.prime.phdi.utilities
 
 import kotlin.test.assertEquals
 import kotlin.test.assertSame
 import org.junit.jupiter.api.Test
-import gov.cdc.prime.phdi.Utilities.HL7Parser
 
-class HL7ParserTest {
+class HelperFunctonTest {
     val singleMessage: String = javaClass.getResource("/singleMessage.hl7").readText()
     val multipleMessages: String = javaClass.getResource("/multipleMessages.hl7").readText()
     val mixedMessages: String = javaClass.getResource("/mixedMessages.hl7").readText()
     val batchFileSingleMessage: String = javaClass.getResource("/batchFileSingleMessage.hl7").readText()
     val batchFileMultipleMessages: String = javaClass.getResource("/batchFileMultipleMessages.hl7").readText()
     val batchFileNoMessage: String = javaClass.getResource("/batchFileNoMessage.hl7").readText()
+    val singleInvalidMessage: String = javaClass.getResource("/singleInvalidMessage.hl7").readText()
     val loremIpsum: String = javaClass.getResource("/loremIpsum.txt").readText()
-    val parser = HL7Parser()
 
+    // TEST READING FUNCTIONALITY
+    @Test
+    fun testReadHL7MessagesFromByteArray() {
+        val multipleMessages: ByteArray = javaClass.getResource("/multipleMessages.hl7").readBytes()
+        val content = readHL7MessagesFromByteArray(multipleMessages)
+
+        // assert that the reader is able to read the bytes and return a string
+        assertSame(String::class.java, content::class.java)
+    }
+
+    // TEST PARSING FUNCTIONALITY
     @Test
     fun testConvertBatchMessagesToList() {
         // test a single message
-        val single = parser.convertBatchMessagesToList(singleMessage)
+        val single = convertBatchMessagesToList(singleMessage)
         assertEquals(1, single.size)
 
         // test multiple messages
-        val multiple = parser.convertBatchMessagesToList(multipleMessages)
+        val multiple = convertBatchMessagesToList(multipleMessages)
         assertEquals(10, multiple.size)
 
         // test invalid messages
         // convertBatchMessagesToList makes no effort to determine if 
         // the data it receives is valid HL7, so it will still chunk
         // messages even if one or more of the messages are invalid
-        val invalid = parser.convertBatchMessagesToList(mixedMessages)
+        val invalid = convertBatchMessagesToList(mixedMessages)
         assertEquals(3, invalid.size)
 
         // test batch file with a single message
-        val batch_single = parser.convertBatchMessagesToList(batchFileSingleMessage)
+        val batch_single = convertBatchMessagesToList(batchFileSingleMessage)
         assertEquals(1, batch_single.size)
 
         // test batch file with multiple messages
-        val batch_multiple = parser.convertBatchMessagesToList(batchFileMultipleMessages)
+        val batch_multiple = convertBatchMessagesToList(batchFileMultipleMessages)
         assertEquals(5, batch_multiple.size)
 
         //test batch file with no messages
-        val batch_empty = parser.convertBatchMessagesToList(batchFileNoMessage)
+        val batch_empty = convertBatchMessagesToList(batchFileNoMessage)
         assertEquals(0, batch_empty.size)
 
         // test pure text file
@@ -50,18 +60,18 @@ class HL7ParserTest {
         // messages even if the text itself is not HL7. This should result
         // in a single element being returned, regardless of how large
         // the text is.
-        val text = parser.convertBatchMessagesToList(loremIpsum)
+        val text = convertBatchMessagesToList(loremIpsum)
         assertEquals(1, text.size)
     }
 
     @Test
     fun testParse() {
-        val processedMessages = parser.parse(mixedMessages)
-        val processedText = parser.parse(loremIpsum)
+        val processedMessages = parse(mixedMessages)
+        val processedText = parse(loremIpsum)
 
         // test that the method is returning the type we expect
-        assertSame(HL7Parser.ProcessedMessages::class.java, processedMessages::class.java)
-        assertSame(HL7Parser.ProcessedMessages::class.java, processedText::class.java)
+        assertSame(ProcessedMessages::class.java, processedMessages::class.java)
+        assertSame(ProcessedMessages::class.java, processedText::class.java)
 
         // test that it returns the correct number of valid messages
         assertEquals(2, processedMessages.valid_messages.size)
@@ -70,5 +80,29 @@ class HL7ParserTest {
         // test that it returns the correct number of invalid messages
         assertEquals(1, processedMessages.invalid_messages.size)
         assertEquals(1, processedText.invalid_messages.size)
+    }
+
+    // need to clean the text to ensure it's working the way it would in production
+    fun cleanText(content: String): String {
+        var reg = "[\r\n]".toRegex()
+        var cleanedMessage: String = reg.replace(content, "\r")
+        reg = "[\\u000b\\u001c]".toRegex()
+        cleanedMessage = reg.replace(cleanedMessage, "")
+        return cleanedMessage
+    }
+    
+    @Test
+    fun testIsValidHL7Message() {
+        // test that it correctly validates a single valid message
+        val cleanedSingleMessage = cleanText(singleMessage)
+        assertEquals(true, isValidHL7Message(cleanedSingleMessage))
+
+        // test that it correctly invalidates a single invalid message
+        val cleanedSingleInvalidMessage = cleanText(singleInvalidMessage)
+        assertEquals(false, isValidHL7Message(cleanedSingleInvalidMessage))
+
+        // test that it correctly invalidates text that is not HL7
+        val cleanedLoremIpsum = cleanText(loremIpsum)
+        assertEquals(false, isValidHL7Message(cleanedLoremIpsum))
     }
 }
