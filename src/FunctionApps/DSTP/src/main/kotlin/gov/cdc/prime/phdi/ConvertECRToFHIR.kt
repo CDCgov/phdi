8756a42ced300a1b765ba66afe48ecd795501a02
Bryan <bryan@skylight.digital>
2022-02-18 11:54:03 -0800

    cleaning up imports and changing paths to reflect prod blog storage structure

--- src/FunctionApps/DSTP/src/main/kotlin/gov/cdc/prime/phdi/ConvertECRToFHIR.kt
+++ src/FunctionApps/DSTP/src/main/kotlin/gov/cdc/prime/phdi/ConvertECRToFHIR.kt
@@ -4,10 +4,6 @@ import java.util.*
 import com.microsoft.azure.functions.*
 import com.microsoft.azure.functions.annotation.*
 
-import ca.uhn.hl7v2.util.Terser
-
-import gov.cdc.prime.phdi.utilities.*
-import gov.cdc.prime.phdi.utilities.*
 import gov.cdc.prime.phdi.utilities.*
 
 class ConvertECRToFHIR {
@@ -16,7 +12,7 @@ class ConvertECRToFHIR {
         @BlobTrigger(
             name = "file",
             dataType = "binary",
-            path = "bronze/ecr/{name}",
+            path = "bronze/decrypted/ecr/{name}",
             connection="AzureWebJobsStorage"
         ) content: ByteArray,
         // @BindingName("name") filename: String,
