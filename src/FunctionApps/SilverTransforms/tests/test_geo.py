dbe0051e096792a2a496f05766ba7ffa39cb0bd0
Graham Smith <graham@skylight.digital>
2022-03-18 15:21:17 -0400

    Refactor to use the transforms library

--- src/FunctionApps/SilverTransforms/tests/test_geo.py
+++ src/FunctionApps/SilverTransforms/tests/test_geo.py
@@ -1,52 +1,6 @@
 from unittest import mock
 
-from smartystreets_python_sdk.us_street.candidate import Candidate
-from smartystreets_python_sdk.us_street.metadata import Metadata
-from smartystreets_python_sdk.us_street.components import Components
-
-from Transform.geo import geocode, cached_geocode
-
-
-def test_geocode():
-    """
-    Make sure to return the correct dict attribs from the SmartyStreets
-    response object on a successful call
-    """
-
-    # SmartyStreets fills in a request object inline, so let's fake that
-    candidate = Candidate({})
-    candidate.delivery_line_1 = "123 FAKE ST"
-    candidate.metadata = Metadata(
-        {"latitude": 45.123, "longitude": -70.234, "county_fips": "36061"}
-    )
-
-    candidate.components = Components(
-        {"zipcode": "10001", "city_name": "New York", "state_abbreviation": "NY"}
-    )
-
-    # Provide a function that adds results to the existing object
-    def fill_in_result(*args, **kwargs):
-        args[0].result = [candidate]
-
-    client = mock.Mock()
-    client.send_lookup.side_effect = fill_in_result
-
-    assert {
-        "address": ["123 FAKE ST"],
-        "city": "New York",
-        "state": "NY",
-        "lat": 45.123,
-        "lng": -70.234,
-        "fips": "36061",
-        "zipcode": "10001",
-    } == geocode(client, "123 Fake St, New York, NY 10001")
-
-    client.send_lookup.assert_called()
-
-
-def test_failed_geocode():
-    """If it doesn't fill in results, return None"""
-    assert geocode(mock.Mock(), "123 Nowhere St, Atlantis GA") is None
+from Transform.geo import cached_geocode
 
 
 @mock.patch("Transform.geo.geocode")
