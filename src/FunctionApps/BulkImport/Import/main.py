02f873ce1ec6d4c39097d2306ece65eb81b906ae
Jesse Skeets <jesse@skylight.digital>
2022-03-25 11:20:23 -0700

    [bulk-import] Posting a single line

--- src/FunctionApps/BulkImport/Import/main.py
+++ src/FunctionApps/BulkImport/Import/main.py
@@ -95,6 +95,24 @@ def read_file(file):
     with open(file) as fp:
         for line in fp:
             json_line = json.loads(line)
-            print(json_line["resourceType"])
+            resource_type = json_line["resourceType"]
+            resource_id   = json_line["id"]
+            post_to_fhir(line,resource_type,resource_id)
+
+def post_to_fhir(line,resource_type,resource_id):
+    # url = os.environ.get("FHIR_URL", "")
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyIsImtpZCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyJ9.eyJhdWQiOiJodHRwczovL3BoZGktcGlsb3QuYXp1cmVoZWFsdGhjYXJlYXBpcy5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9kNDE5ZDhmNy0zYjIwLTQ4NzctYWU2Ny03ZmQxMDBjYTFjZjgvIiwiaWF0IjoxNjQ4MjMxNjAwLCJuYmYiOjE2NDgyMzE2MDAsImV4cCI6MTY0ODIzNTUwMCwiYWlvIjoiRTJaZ1lDamcrUGlpTTlFK2Z0WWV2VHRNUmlmS0FRPT0iLCJhcHBpZCI6ImRmZGJlNjAxLTNkZGEtNDU4ZC1iODQ4LWI3YTA2MGYzMWJkMiIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2Q0MTlkOGY3LTNiMjAtNDg3Ny1hZTY3LTdmZDEwMGNhMWNmOC8iLCJvaWQiOiI3YTQ4YjZlYi00MjlkLTQxNDgtODQ3ZC0zZDJmMGViZjMxOTUiLCJyaCI6IjAuQVZBQTk5Z1oxQ0E3ZDBpdVozX1JBTW9jLU5oNFowX3ZXdHhEb2Ytd2MzSkxsSlZfQUFBLiIsInN1YiI6IjdhNDhiNmViLTQyOWQtNDE0OC04NDdkLTNkMmYwZWJmMzE5NSIsInRpZCI6ImQ0MTlkOGY3LTNiMjAtNDg3Ny1hZTY3LTdmZDEwMGNhMWNmOCIsInV0aSI6Ik5sU1RDcHpqUWtXRTZfcTRkTk96QWciLCJ2ZXIiOiIxLjAifQ.i3Dfux4BcFfnB_fDax520OCCqureWgGVJFHXSa7y0t-uIAdOOfgVpnKmpeydmH7-3VM90Q8sRUfLU0qwjZ56tzgJ9filmSTmEuuousd8uDVPeZDOnL3CnVE727ecce3fzVjPrkWjRylbx9bC8Js0Az_W2liy4qbsbBCQuuu8DfYZV25Moa5DofBg2CE195ysbmOEJKuB2C28uF7x1S7YaELVPZXUlP7UbcY18bIX8D3AoNM4U15ES-aKLQBcmBIWBu4BvaY3x_aweZv01KMi6yZpPn4gTPJ1t272D56A6ayfVFBDk_P0Ww_HrnYFqQXBGLvfXtjonVrv063aRQHMCQ"
+    url = "https://phdi-pilot.azurehealthcareapis.com"
+    print(line)
+    resp = requests.post(
+        f"{url}/{resource_type}",
+        headers={
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/fhir+json",
+            "Content-type": "application/json"
+        },
+        data=line
+    )
+    print(resp)
 
 main()
