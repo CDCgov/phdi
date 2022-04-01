ea0ce3b41af1739ecc52e88031f941546d511c5c
Jesse Skeets <jesse@skylight.digital>
2022-04-01 08:43:59 -0700

    [bulk-import] black formatting

--- src/FunctionApps/BulkImport/Import/main.py
+++ src/FunctionApps/BulkImport/Import/main.py
@@ -10,44 +10,49 @@ from requests.packages.urllib3.util.retry import Retry
 
 unzipped_directory = "./FhirResources"
 
+
 def main(zip_file):
     unzip_input_file(zip_file)
     process_ndjson_files(unzipped_directory)
 
+
 def unzip_input_file(zip_file):
-    with zipfile.ZipFile(zip_file,"r") as zip_ref:
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
         zip_ref.extractall(unzipped_directory)
 
+
 def process_ndjson_files(dir):
-    with multiprocessing.Pool(processes = multiprocessing.cpu_count()-1) as p:
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1) as p:
         directory = os.fsencode(dir)
         for file in os.listdir(directory):
             filename = os.fsdecode(file)
             if filename.endswith(".ndjson"):
-                file_path = (os.path.join(dir, filename))
+                file_path = os.path.join(dir, filename)
                 p.apply_async(read_file, (file_path,))
             else:
                 logging.error(f"File must be ndjson, file given: {filename}")
         p.close()
         p.join()
 
+
 def read_file(file):
     with open(file) as fp:
         for line in fp:
             json_line = json.loads(line)
             resource_type = json_line["resourceType"]
-            resource_id   = json_line["id"]
-            post_to_fhir(line,resource_type,resource_id)
+            resource_id = json_line["id"]
+            post_to_fhir(line, resource_type, resource_id)
 
-def post_to_fhir(line,resource_type,resource_id):
+
+def post_to_fhir(line, resource_type, resource_id):
     try:
         token = get_access_token()
     except Exception:
-        return func.HttpResponse("error getting access token", status_code=401) 
+        return func.HttpResponse("error getting access token", status_code=401)
     retry_strategy = Retry(
-    total=3,
-    status_forcelist=[429, 500, 502, 503, 504],
-    allowed_methods=["HEAD", "POST", "OPTIONS"]
+        total=3,
+        status_forcelist=[429, 500, 502, 503, 504],
+        allowed_methods=["HEAD", "POST", "OPTIONS"],
     )
     adapter = HTTPAdapter(max_retries=retry_strategy)
     http = requests.Session()
@@ -60,16 +65,17 @@ def post_to_fhir(line,resource_type,resource_id):
             headers={
                 "Authorization": f"Bearer {token}",
                 "Accept": "application/fhir+json",
-                "Content-type": "application/json"
+                "Content-type": "application/json",
             },
-            data=line
+            data=line,
         )
         print(f"status={resp.status_code} message={resource_id}")
     except Exception:
-            return func.HttpResponse("ndjson to fhir import failed", status_code=500)
-            logging.error(
-                f"Failed to import ndjson to FHIR server failed status={resp.status_code} message={resp.text}"
-            )
+        return func.HttpResponse("ndjson to fhir import failed", status_code=500)
+        logging.error(
+            f"Failed to import ndjson to FHIR server failed status={resp.status_code} message={resp.text}"
+        )
+
 
 def get_access_token() -> str:
     """Get the access token based on creds in the environment"""
@@ -94,5 +100,5 @@ def get_access_token() -> str:
     raise Exception("access token request failed")
 
 
-if __name__ == '__main__':
-    main("../../../../Archive.zip")
+if __name__ == "__main__":
+    main("../../PITest_FunctionApp/tests/assets/Archive.zip")
