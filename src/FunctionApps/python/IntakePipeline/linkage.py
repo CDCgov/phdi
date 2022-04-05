a56cc49b18f244b0d3d48992a2e999caa18569dc
bamader <brandon@skylight.digital>
2022-04-05 15:11:58 -0400

    Make formatting nice

--- src/FunctionApps/python/IntakePipeline/linkage.py
+++ src/FunctionApps/python/IntakePipeline/linkage.py
@@ -1,7 +1,8 @@
 import hashlib
 
+
 def add_patient_identifier(salt_suffix: str, bundle: dict) -> None:
-    '''
+    """
     Given a FHIR resource bundle (defined as a dictionary of resources
     containing at least one patient resource):
       - identify the patient resource(s) in the bundle
@@ -11,41 +12,43 @@ def add_patient_identifier(salt_suffix: str, bundle: dict) -> None:
         patient resource
     This function assumes data has been standardized already by the
     silver transforms.
-    '''
-    for resource in bundle['entry']:
-        if resource['resource']['resourceType'] == 'Patient':
-            patient = resource['resource']
+    """
+    for resource in bundle["entry"]:
+        if resource["resource"]["resourceType"] == "Patient":
+            patient = resource["resource"]
 
             # Combine given and family name
-            recent_name = patient['name'][0]
-            name_str = '-'.join(recent_name['given']) + '-' + recent_name['family']
+            recent_name = patient["name"][0]
+            name_str = "-".join(recent_name["given"]) + "-" + recent_name["family"]
 
             # Compile one-line address string
-            address = patient['address'][0]
+            address = patient["address"][0]
             address_line = " ".join(address.get("line"))
             address_line += f" {address.get('city')}, {address.get('state')}"
             if "postalCode" in address and address["postalCode"]:
                 address_line += f" {address['postalCode']}"
-            
+
             # Generate and store unique hash code
-            link_str = name_str + '-' + patient['birthDate'] + '-' + address_line
+            link_str = name_str + "-" + patient["birthDate"] + "-" + address_line
             hashcode = generate_hash_str(link_str, salt_suffix)
-            patient['identifier'].append({
-                'value': hashcode,
-                # Note: this system value corresponds to the FHIR specification
-                # for a globally used / generated ID or UUID--the standard here
-                # is to make the use "temporary" even if it's not
-                'system': 'urn:ietf:rfc:3986',
-                'use': 'temp'
-            })
+            patient["identifier"].append(
+                {
+                    "value": hashcode,
+                    # Note: this system value corresponds to the FHIR specification
+                    # for a globally used / generated ID or UUID--the standard here
+                    # is to make the use "temporary" even if it's not
+                    "system": "urn:ietf:rfc:3986",
+                    "use": "temp",
+                }
+            )
 
 
 def generate_hash_str(linking_str: str, salt_str: str) -> str:
-    '''
+    """
     Given a string made of concatenated patient information, generate
     a hash for this string to serve as a "unique" identifier for the
     patient.
-    '''
+    """
     hash_obj = hashlib.sha256()
     to_encode = (linking_str + salt_str).encode("utf-8")
     hash_obj.update(to_encode)
