12e93160c7c9fb2c30081b6a91cd9c622ef71c2c
Graham Smith <graham@skylight.digital>
2022-04-08 11:55:59 -0400

    Add blob store output

--- src/FunctionApps/python/tests/IntakePipeline/fhir_test.py
+++ src/FunctionApps/python/tests/IntakePipeline/fhir_test.py
@@ -3,7 +3,7 @@ import json
 
 from unittest import mock
 
-from IntakePipeline.fhir import read_fhir_bundles, get_blobs
+from IntakePipeline.fhir import read_fhir_bundles, get_blobs, store_bundle
 
 from azure.storage.blob import BlobProperties
 
@@ -44,6 +44,24 @@ def test_get_blobs(mock_get_client):
 
     mock_get_client.assert_called_with("some-url")
     mock_client.list_blobs.assert_called_with(name_starts_with="some-prefix")
+
+
+@mock.patch("IntakePipeline.fhir.get_blob_client")
+@mock.patch("IntakePipeline.fhir.uuid")
+def test_store_bundle(mock_uuid, mock_get_client):
+    mock_uuid.uuid4.return_value = "some-uuid"
+
+    mock_blob = mock.Mock()
+
+    mock_client = mock.Mock()
+    mock_client.get_blob_client.return_value = mock_blob
+
+    mock_get_client.return_value = mock_client
+
+    store_bundle("some-url", "output/path", {"hello": "world"})
+
+    mock_client.get_blob_client.assert_called_with("output/path/some-uuid.fhir")
+    mock_blob.upload_blob.assert_called()
 
 
 @mock.patch("IntakePipeline.fhir.get_blobs")
