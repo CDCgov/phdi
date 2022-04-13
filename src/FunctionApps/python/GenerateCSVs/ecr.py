b631ce164a258c54779c1257b623222776abefde
DanPaseltiner <99684231+DanPaseltiner@users.noreply.github.com>
2022-04-13 15:45:16 -0400

    Dan/patient resource parsing (#81)
    * Initial draft of functions for extracting data from a patient resource and returning a comma deliminated string.
    
    * fix mistake
    
    * Add parsing for the identifier hash.
    
    * Format with black and update doc strings.
    
    * Add testing and tweak to handle missing birthData and gender.
    
    * Add PATIENT_COLUMNS and import with parse_patient_resource() in ecr.py, elr.py, and vxu.py
    
    * Format doc strings and remove preemptive imports to appease flake8
    
    * Resolved flake8 line length issue on line 91.
    
    * Refactor to return lists, move test data into dedicated JSON files, update doc strings, and return race and ethnicity codes instead of display values.
    
    Co-authored-by: DanielPaseltiner <daniel.paseltiner@gmail.com>

--- src/FunctionApps/python/GenerateCSVs/ecr.py
+++ src/FunctionApps/python/GenerateCSVs/ecr.py
@@ -1,7 +1,7 @@
 from typing import List, Any
+from GenerateCSVs.patient import PATIENT_COLUMNS
 
-
-ECR_COLUMNS = []
+ECR_COLUMNS = PATIENT_COLUMNS + []
 
 
 def ecr_to_csv(bundle: dict) -> List[Any]:
--- src/FunctionApps/python/GenerateCSVs/vxu.py
+++ src/FunctionApps/python/GenerateCSVs/vxu.py
@@ -1,7 +1,7 @@
 from typing import List, Any
+from GenerateCSVs.patient import PATIENT_COLUMNS
 
-
-VXU_COLUMNS = []
+VXU_COLUMNS = PATIENT_COLUMNS + []
 
 
 def vxu_to_csv(bundle: dict) -> List[Any]:
