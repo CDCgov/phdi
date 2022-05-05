6e1ea436a628e4c6123cb94560fb27816c0980dd
spencer-skylight <102615834+spencer-skylight@users.noreply.github.com>
2022-05-05 09:42:12 -0600

    Timestamp normalization (#99)
    * Initial commit for timestamp normalization
    
    * Unit testing for timestamp normalization
    
    * pull in changes from main
    
    * Update tests
    
    * Update conversion after unit and functional tests

--- src/FunctionApps/python/IntakePipeline/conversion.py
+++ src/FunctionApps/python/IntakePipeline/conversion.py
@@ -1,18 +1,129 @@
 import logging
 import re
 import requests
+import hl7
 from typing import List, Dict
 
 
-def clean_message(message: str, delimiter: str = "\n") -> str:
-    cleaned_message = re.sub("[\r\n]+", delimiter, message)
+def clean_message(message: str) -> str:
+    """Prepare a message for conversion by adjusting problematic fields
+    to conform to Azure's expectations.
+    * Convert segment terminators from \n to \r
+    * Normalize datetime fields
+    * Convert segment terminators back from \r to \n
+    """
+    parsed_message = ""
+    try:
+        # Conversion from \n to \r EOL characters is needed for hl7
+        # module, and doesn't hurt the Azure converter
+        parsed_message = hl7.parse(message.replace("\n", "\r"))
+
+        # Normalize Dates
+        # MSH-7 - Message date/time
+        normalize_hl7_datetime_segment(parsed_message, "MSH", [7])
+
+        # PID-7 - Date of Birth
+        # PID-29 - Date of Death
+        # PID-33 - Last update date/time
+        normalize_hl7_datetime_segment(parsed_message, "PID", [7, 29, 33])
+
+        # PV1-44 - Admisstion Date
+        # PV1-45 - Discharge Date
+        normalize_hl7_datetime_segment(parsed_message, "PV1", [44, 45])
+
+        # ORC-9 Date/time of transaction
+        # ORC-15 Order effective date/time
+        # ORC-27 Filler's expected availability date/time
+        normalize_hl7_datetime_segment(parsed_message, "ORC", [9, 15, 27])
+
+        # OBR-7 Observation date/time
+        # OBR-8 Observation end date/time
+        # OBR-22 Status change date/time
+        # OBR-36 Scheduled date/time
+        normalize_hl7_datetime_segment(parsed_message, "OBR", [7, 8, 22, 36])
+
+        # OBX-12 Effective date/time of reference range
+        # OBX-14 Date/time of observation
+        # OBX-19 Date/time of analysis
+        normalize_hl7_datetime_segment(parsed_message, "OBX", [12, 14, 19])
+
+        # TQ1-7 Start date/time
+        # TQ1-8 End date/time
+        normalize_hl7_datetime_segment(parsed_message, "TQ1", [7, 8])
+
+        # SPM-18 Specimen received date/time
+        # SPM-19 Specimen expiration date/time
+        normalize_hl7_datetime_segment(parsed_message, "SPM", [18, 19])
+
+        # RXA-3 Date/time start of administration
+        # RXA-4 Date/time end of administration
+        # RXA-16 Substance expiration date
+        # RXA-22 System entry date/time
+        normalize_hl7_datetime_segment(parsed_message, "RXA", [3, 4, 16, 22])
+
+    except Exception:
+        logging.exception(
+            "Exception occurred while cleaning message.  "
+            + "Passing through original message."
+        )
+
+        return message
+
+    return str(parsed_message).replace("\r", "\n")
+
+
+def normalize_hl7_datetime_segment(message: list, segment_id: str, field_list: list):
+    """Utility function to apply datetime normalization
+    to multiple fields in a segment."""
+    try:
+        for segment in message.segments(segment_id):
+            for field_num in field_list:
+                if len(segment) > field_num and segment[field_num][0] != "":
+                    cleansed_datetime = normalize_hl7_datetime(segment[field_num][0])
+                    segment[field_num][0] = cleansed_datetime
+    except KeyError:
+        logging.debug(f"Segment {segment_id} not found in message.")
+
+
+def normalize_hl7_datetime(hl7_datetime: str) -> str:
+    """Break up datetime-formatted fields into the following parts:
+    <integer 8+ digits>[.<integer 1+ digits>][+/-<integer 4+ digits>]
+
+    Each group of integers is truncated to conform to the HL7 specification:
+    first integer group: max 14 digits
+    following decimal point: max 4 digits
+    following +/- (timezone): 4 digits
+    """
+    hl7_datetime_match = re.match(r"(\d{8}\d*)(\.\d+)?([+-]\d+)?", hl7_datetime)
+
+    if not hl7_datetime_match:
+        return hl7_datetime
+
+    hl7_datetime_parts = hl7_datetime_match.groups()
+
+    # Date Base
+    normalized_datetime = hl7_datetime_parts[0][:14]  # First 14 digits
+
+    # Date Decimal
+    if hl7_datetime_parts[1]:
+        normalized_datetime += hl7_datetime_parts[1][:5]  # . plus first 4 digits
+
+    # Date Timezone
+    if hl7_datetime_parts[2] and len(hl7_datetime_parts[2]) >= 5:
+        normalized_datetime += hl7_datetime_parts[2][:5]  # +/- plus 4 digits
+
+    return normalized_datetime
+
+
+def clean_batch(batch: str, delimiter: str = "\n") -> str:
+    cleansed_batch = re.sub("[\r\n]+", delimiter, batch)
 
     # These are unicode for vertical tab and file separator, respectively
     # \u000b appears before every MSH segment, and \u001c appears at the
     # end of the message in some of the data we've been receiving, so
     # we're explicitly removing them here.
-    cleaned_message = re.sub("[\u000b\u001c]", "", cleaned_message).strip()
-    return cleaned_message
+    cleansed_batch = re.sub("[\u000b\u001c]", "", cleansed_batch).strip()
+    return cleansed_batch
 
 
 # This method was adopted from PRIME ReportStream, which can be found here:
@@ -39,8 +150,8 @@ def convert_batch_messages_to_list(content: str, delimiter: str = "\n") -> List[
     them in a message
     """
 
-    cleaned_message = clean_message(content)
-    message_lines = cleaned_message.split(delimiter)
+    cleaned_batch = clean_batch(content)
+    message_lines = cleaned_batch.split(delimiter)
     next_message = ""
     output = []
 
@@ -129,6 +240,9 @@ def convert_message_to_fhir(
     store the original message in a separate container. Otherwise, the FHIR data is
     returned.
 
+    HL7v2 messages are cleaned (minor corrections made) via the clean_message function
+    prior to conversion.
+
     :param message The raw message that needs to be converted to FHIR. Must be HL7
     v2 or HL7 v3
     :param input_data_type The data type of the message. Must be one of Hl7v2 or Ccda
@@ -141,6 +255,9 @@ def convert_message_to_fhir(
     :param access_token A Bearer token used to authenticate with the FHIR server
     :param fhir_url A URL that points to the location of the FHIR server
     """
+    if input_data_type == "Hl7v2":
+        message = clean_message(message)
+
     url = f"{fhir_url}/$convert-data"
     data = {
         "resourceType": "Parameters",
--- src/FunctionApps/python/tests/IntakePipeline/conversion_test.py
+++ src/FunctionApps/python/tests/IntakePipeline/conversion_test.py
@@ -1,26 +1,92 @@
-from unittest import mock
+import hl7
+import pathlib
 import pytest
 
+from unittest import mock
 from IntakePipeline.conversion import (
+    clean_batch,
     clean_message,
     convert_batch_messages_to_list,
     convert_message_to_fhir,
     get_file_type_mappings,
+    normalize_hl7_datetime,
+    normalize_hl7_datetime_segment,
 )
 
 
-def test_clean_message():
+def test_clean_batch():
     TEST_STRING1 = "\r\nHello\nWorld\r\n"
     TEST_STRING2 = "\nHello\r\nW\r\norld"
     TEST_STRING3 = "Hello World"
     TEST_STRING4 = "\u000bHello World\u001c"
     TEST_STRING5 = "\u000bHello\r\nWorld\u001c"
 
-    assert clean_message(TEST_STRING1) == "Hello\nWorld"
-    assert clean_message(TEST_STRING2) == "Hello\nW\norld"
-    assert clean_message(TEST_STRING3) == "Hello World"
-    assert clean_message(TEST_STRING4) == "Hello World"
-    assert clean_message(TEST_STRING5) == "Hello\nWorld"
+    assert clean_batch(TEST_STRING1) == "Hello\nWorld"
+    assert clean_batch(TEST_STRING2) == "Hello\nW\norld"
+    assert clean_batch(TEST_STRING3) == "Hello World"
+    assert clean_batch(TEST_STRING4) == "Hello World"
+    assert clean_batch(TEST_STRING5) == "Hello\nWorld"
+
+
+def test_clean_message():
+    message_1 = open(
+        pathlib.Path(__file__).parent / "assets" / "FileSingleMessageLongDate.hl7"
+    ).read()
+    message_2 = open(
+        pathlib.Path(__file__).parent / "assets" / "FileSingleMessageLongTZ.hl7"
+    ).read()
+
+    assert clean_message(message_1).startswith(
+        "MSH|^~\\&|WIR11.3.2^^|WIR^^||WIRPH^^|20200514010000|"
+        + "|VXU^V04|2020051411020600|P^|2.4^^|||ER\n"
+        + "PID|||3054790^^^^SR^~^^^^PI^||ZTEST^PEDIARIX^^^^^^|"
+        + "HEPB^DTAP^^^^^^|20180808000000|M|||||||||||||||||||||"
+    )
+    assert clean_message(message_2).startswith(
+        "MSH|^~\\&|WIR11.3.2^^|WIR^^||WIRPH^^|20200514010000-0400|"
+        + "|VXU^V04|2020051411020600|P^|2.4^^|||ER"
+    )
+
+
+def test_normalize_hl7_datetime_segment():
+    message_1 = (
+        open(pathlib.Path(__file__).parent / "assets" / "FileSingleMessageLongDate.hl7")
+        .read()
+        .replace("\n", "\r")
+    )
+
+    message_1_parsed = hl7.parse(message_1)
+
+    normalize_hl7_datetime_segment(message_1_parsed, "PID", [7])
+
+    assert str(message_1_parsed).startswith(
+        "MSH|^~\\&|WIR11.3.2^^|WIR^^||WIRPH^^|202005140100001234567890|"
+        + "|VXU^V04|2020051411020600|P^|2.4^^|||ER\r"
+        + "PID|||3054790^^^^SR^~^^^^PI^||ZTEST^PEDIARIX^^^^^^|"
+        + "HEPB^DTAP^^^^^^|20180808000000|M|||||||||||||||||||||"
+    )
+
+
+def test_normalize_hl7_datetime():
+    datetime_0 = ""
+    datetime_1 = "20200514010000"
+    datetime_2 = "202005140100005555"
+    datetime_3 = "20200514"
+    datetime_4 = "20200514.123456"
+    datetime_5 = "20200514+0400000"
+    datetime_6 = "20200514.123456-070000"
+    datetime_7 = "20200514010000.1234-0700"
+    datetime_8 = "not-a-date"
+
+    assert normalize_hl7_datetime(datetime_0) == ""
+    assert normalize_hl7_datetime(datetime_1) == "20200514010000"
+    assert normalize_hl7_datetime(datetime_2) == "20200514010000"
+    assert normalize_hl7_datetime(datetime_3) == "20200514"
+    assert normalize_hl7_datetime(datetime_4) == "20200514.1234"
+    assert normalize_hl7_datetime(datetime_5) == "20200514+0400"
+    assert normalize_hl7_datetime(datetime_6) == "20200514.1234-0700"
+    assert normalize_hl7_datetime(datetime_7) == "20200514010000.1234-0700"
+    assert normalize_hl7_datetime(datetime_8) == "not-a-date"
 
 
 def test_convert_batch_messages_to_list():
@@ -73,10 +139,11 @@ def test_convert_batch_messages_to_list():
 @mock.patch("requests.post")
 def test_convert_message_to_fhir_success(mock_fhir_post):
     mock_fhir_post.return_value = mock.Mock(
-        status_code=200, json=lambda: {"hello": "world"}
+        status_code=200,
+        json=lambda: {"resourceType": "Bundle", "entry": [{"hello": "world"}]},
     )
 
-    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar"
+    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar\n"
     response = convert_message_to_fhir(
         message,
         "some-filename-0",
@@ -104,7 +171,7 @@ def test_convert_message_to_fhir_success(mock_fhir_post):
         headers={"Authorization": "Bearer some-token"},
     )
 
-    assert response == {"hello": "world"}
+    assert response == {"resourceType": "Bundle", "entry": [{"hello": "world"}]}
 
 
 @mock.patch("requests.post")
@@ -114,7 +181,7 @@ def test_convert_message_to_fhir_failure(mock_fhir_post):
         text='{ "resourceType": "Bundle", "entry": [{"hello": "world"}] }',
     )
 
-    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar"
+    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar\n"
     response = convert_message_to_fhir(
         message,
         "some-filename-0",
@@ -151,10 +218,54 @@ def test_convert_message_to_fhir_failure(mock_fhir_post):
 
 @mock.patch("requests.post")
 @mock.patch("logging.error")
+def test_log_fhir_operationoutcome(mock_log, mock_fhir_post):
+    mock_fhir_post.return_value = mock.Mock(
+        status_code=400,
+        text='{ "resourceType": "OperationOutcome", "diagnostics": "some-error" }',
+    )
+
+    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar\n"
+
+    response = convert_message_to_fhir(
+        message,
+        "some-filename-0",
+        "Hl7v2",
+        "VXU_V04",
+        "microsofthealth/fhirconverter:default",
+        "some-token",
+        "some-fhir-url",
+    )
+
+    mock_fhir_post.assert_called_with(
+        url="some-fhir-url/$convert-data",
+        json={
+            "resourceType": "Parameters",
+            "parameter": [
+                {"name": "inputData", "valueString": message},
+                {"name": "inputDataType", "valueString": "Hl7v2"},
+                {
+                    "name": "templateCollectionReference",
+                    "valueString": "microsofthealth/fhirconverter:default",
+                },
+                {"name": "rootTemplate", "valueString": "VXU_V04"},
+            ],
+        },
+        headers={"Authorization": "Bearer some-token"},
+    )
+
+    assert response == {
+        "http_status_code": 400,
+        "response_content": '{ "resourceType": "OperationOutcome", '
+        + '"diagnostics": "some-error" }',
+    }
+
+
+@mock.patch("requests.post")
+@mock.patch("logging.error")
 def test_log_generic_error(mock_log, mock_fhir_post):
     mock_fhir_post.return_value = mock.Mock(status_code=400, text="some-error")
 
-    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar"
+    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar\n"
     convert_message_to_fhir(
         message,
         "some-filename-0",
@@ -192,7 +303,7 @@ def test_log_generic_error(mock_log, mock_fhir_post):
 def test_generic_error(mock_log, mock_fhir_post):
     mock_fhir_post.return_value = mock.Mock(status_code=400, text="some-error")
 
-    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar"
+    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar\n"
 
     response = convert_message_to_fhir(
         message,
@@ -231,7 +342,7 @@ def test_error_with_special_chars(mock_log, mock_fhir_post):
         status_code=400, text="some-error with \" special ' characters \n"
     )
 
-    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar"
+    message = "MSH|blah|foo|test\nPID|some^text|blah\nOBX|foo||||bar^baz&foobar\n"
 
     response = convert_message_to_fhir(
         message,
