360bd3090a5fba2b6d2c9e5e7c4c9f433b37f779
DanPaseltiner <99684231+DanPaseltiner@users.noreply.github.com>
2022-05-25 15:48:50 -0400

    Add updated phone number standardization, supporting functions, and testing (#123)
    * Add updated phone number standardization, supporting functions, and testing
    
    * Add new dependencies to poetry.
    
    * Address comments from code review excluding doc string re-format and fuzzy matching.
    
    * Fix line length issue.
    
    * Replace fuzzy matching with explicit searches by name and official name.
    
    * Remove unused dependency.
    
    * Reformat and update doc strings.
    
    * Clean up conditions in standardize_country and add break to standardize_phone.
    
    * Fix variable name mistakes.
    
    * Reinstate  unit tests that were commented out for debugging purposes.
    
    * Clean flow in initial if block in standardize_country.
    
    Co-authored-by: DanielPaseltiner <daniel.paseltiner@gmail.com>

--- src/lib/phdi-building-blocks/tests/test_standardize.py
+++ src/lib/phdi-building-blocks/tests/test_standardize.py
@@ -2,10 +2,13 @@ import json
 import pathlib
 import copy
 
+
 from phdi_building_blocks.standardize import (
     standardize_name,
     standardize_patient_name,
     standardize_phone,
+    country_extractor,
+    standardize_country,
     standardize_patient_phone,
 )
 
@@ -40,9 +43,36 @@ def test_standardize_patient_name():
 
 
 def test_standardize_phone():
-    assert "0123456789" == standardize_phone("0123456789")
-    assert "0123456789" == standardize_phone("(012)345-6789")
-    assert standardize_phone("345-6789") is None
+    raw_bundle = json.load(
+        open(pathlib.Path(__file__).parent / "assets" / "patient_bundle.json")
+    )
+    patient = raw_bundle["entry"][1]
+    countries = country_extractor(patient)
+
+    assert standardize_phone("+11234567890") == "+11234567890"
+    assert standardize_phone("(123)-456-7890", countries) == "+11234567890"
+    assert standardize_phone("123 456.7890") == "+11234567890"
+
+
+def test_country_extractor():
+    raw_bundle = json.load(
+        open(pathlib.Path(__file__).parent / "assets" / "patient_bundle.json")
+    )
+    patient = raw_bundle["entry"][1]
+    patient["resource"]["address"].append(patient["resource"]["address"][0])
+    patient["resource"]["address"].append(patient["resource"]["address"][0])
+    assert [country for country in country_extractor(patient)] == ["US"] * 3
+    assert [country for country in country_extractor(patient, "alpha_3")] == ["USA"] * 3
+    assert [country for country in country_extractor(patient, "numeric")] == ["840"] * 3
+
+
+def test_standardize_country():
+    assert standardize_country("US") == "US"
+    assert standardize_country("USA") == "US"
+    assert standardize_country("United States of America") == "US"
+    assert standardize_country("United states ") == "US"
+    assert standardize_country("US", "alpha_3") == "USA"
+    assert standardize_country("USA", "numeric") == "840"
 
 
 def test_standardize_patient_phone():
@@ -51,7 +81,7 @@ def test_standardize_patient_phone():
     )
     standardized_bundle = copy.deepcopy(raw_bundle.copy())
     patient = standardized_bundle["entry"][1]["resource"]
-    patient["telecom"][0]["value"] = "1234567890"
+    patient["telecom"][0]["value"] = "+11234567890"
     patient["extension"] = []
     patient["extension"].append(
         {
