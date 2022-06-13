57cfa7c8643cb4bcc640d9e545b46d8be25cfdc2
DanPaseltiner <99684231+DanPaseltiner@users.noreply.github.com>
2022-06-13 17:26:05 -0400

    Dan/initial fhir to parquet (#129)
    * Initial commit with demo FHIR to parquet functionality.
    
    * Update gitignore
    
    * Remove breakpoint.
    
    * Next iteration of querying the FHIR server, applying a schemam, and writing to parquet. Parsing has been generalized, by relying on a more robust schema format.
    
    * Significant changes: Error loging function, standardized on 'selection_criteria', increased generalization of parsing, example schema expanded to 2 resource types and demonstration of expanded fhir_path concept.
    
    * Remove get_next_page. query_fhir_server() has been generalized to suppor this functionality.
    
    * Refactoring for integration with phdi_building_blocks and modularity to support other file formats beyond parquet in the future.
    
    * Small  fix for only closing the writer if one was actually used.
    
    * Correct status_code in log_fhir_server_error().
    
    * Remove code inadvertently included in log_fhir_server_error as the result of a copy/paste error.
    
    * Move FHIR server specific functionality into phdi_building_blocks.fhir.
    
    * Move schema generation functionality into phdi_building_blocks.schemas and add required dependencies to with poetry.
    
    * Update dependencies in read_write_demo.py
    
    * Changes paths in schema to standard FHIRpath format.
    
    * Update poetry.
    
    * Change FHIR resource parsing from hombrewed method to relying on the FHIRpath standard as implemented in fhirpathpy.
    
    * Correct Phone Number FHIR path.
    
    * Remove unused depenencies.
    
    * Update doc strings and add display_head parameter to get_schema_summary to prevent reading entire files unless desired.
    
    * Address Flake8 line length error.
    
    * Add unit testing for load_schema(), apply_selection_criteria(), and apply_schema_to_resource()
    
    * Add test for make_resource_type_table().
    
    * Additional test of make_resource_type_table() for when FHIR server does not return a 200.
    
    * Add FHIR server response example for unit testing.
    
    * Clean up unused local variable.
    
    * Remove additional unsed variable.
    
    * Add test for generate_schema()
    
    * Add tests for write_schema_table.
    
    * Add test for log_fhir_server_error().
    
    * Add test for query_fhir_server().
    
    * Change query_fhir_server() to fhir_server_get(), address comments, update usage and unit testing.
    
    * Move FHIR server response logging into fhir_server_get().
    
    * Updates to log_fhir_server_error.
    
    * Add error handling for FileNotFoundError to load_schema().
    
    * Updates to apply_selection_criteria().
    
    * Remove pandas and import fhirpathpy completely instead of just evaluate.
    
    * Updates to schemas.py
    
    * Move read_write git status
    
    * Rename read_write_demo.py
    
    * Doc string edits in schemas.py
    
    * Updates to while loop in make_resource_type_table().
    
    * Remove AzureFhirserverCredentialManager and update unit tests.
    
    * Fix typos in fhir_server_get().
    
    * Add TODO for generalizing AzureFhirserverCredentialManager.
    
    * Generalizing for multiple tables in a schema. Unit testing fixes to come shortly.
    
    * Update while loop in make_table() and unit tests.
    
    * Fix typo in doc string.
    
    * Update log_fhir_server_error().
    
    * Address Flake8 issues.
    
    * Revert to elif in apply_selection_criteria().
    
    * Change branch to load phdi_building_blocks from to main.
    
    * Add test for print_schema_summary().
    
    * Add 403 handling to log_fhir_server_error() and refactor test_log_fhir_server_error() to use a loop for improved cleanlness and future maintainability.
    
    * Move AzureFhirserverCredentialManager doc string outside class, use asser_not_called() in test_make_table_fail().
    
    Co-authored-by: DanielPaseltiner <daniel.paseltiner@gmail.com>

---
+++ src/lib/phdi-building-blocks/tests/test_schemas.py
@@ -0,0 +1,246 @@
+import json
+import yaml
+import pathlib
+from unittest import mock
+
+from phdi_building_blocks.schemas import (
+    load_schema,
+    apply_selection_criteria,
+    apply_schema_to_resource,
+    make_table,
+    make_schema_tables,
+    write_schema_table,
+    print_schema_summary,
+)
+
+
+def test_load_schema():
+    load_schema(
+        pathlib.Path(__file__).parent / "assets" / "test_schema.yaml"
+    ) == yaml.safe_load(
+        open(pathlib.Path(__file__).parent / "assets" / "test_schema.yaml")
+    )
+
+
+def test_apply_selection_criteria():
+    test_list = ["one", "two", "three"]
+    assert apply_selection_criteria(test_list, "first") == "one"
+    assert apply_selection_criteria(test_list, "last") == "three"
+    assert apply_selection_criteria(test_list, "random") in test_list
+    assert apply_selection_criteria(test_list, "all") == ",".join(test_list)
+
+
+def test_apply_schema_to_resource():
+    resource = json.load(
+        open(pathlib.Path(__file__).parent / "assets" / "patient_bundle.json")
+    )
+
+    resource = resource["entry"][1]["resource"]
+
+    schema = yaml.safe_load(
+        open(pathlib.Path(__file__).parent / "assets" / "test_schema.yaml")
+    )
+    schema = schema["my_table"]
+
+    assert apply_schema_to_resource(resource, schema) == {
+        "patient_id": "some-uuid",
+        "first_name": "John ",
+        "last_name": "doe",
+        "phone_number": "123-456-7890",
+    }
+
+
+@mock.patch("phdi_building_blocks.schemas.write_schema_table")
+@mock.patch("phdi_building_blocks.schemas.fhir_server_get")
+def test_make_table_success(patch_query, patch_write):
+
+    schema = yaml.safe_load(
+        open(pathlib.Path(__file__).parent / "assets" / "test_schema.yaml")
+    )
+
+    output_path = mock.Mock()
+    output_path.__truediv__ = (  # Redefine division operator to prevent failure.
+        lambda x, y: x
+    )
+    output_format = "parquet"
+    fhir_url = "some_fhir_server_url"
+    access_token = "some_access_token"
+
+    fhir_server_responses = json.load(
+        open(
+            pathlib.Path(__file__).parent
+            / "assets"
+            / "FHIR_server_query_response_200_example.json"
+        )
+    )
+
+    query_response_1 = mock.Mock()
+    query_response_1.status_code = fhir_server_responses["status_code_1"]
+    query_response_1.content = json.dumps(fhir_server_responses["content_1"])
+
+    query_response_2 = mock.Mock()
+    query_response_2.status_code = fhir_server_responses["status_code_2"]
+    query_response_2.content = json.dumps(fhir_server_responses["content_2"])
+
+    patch_query.side_effect = [query_response_1, query_response_2]
+
+    make_table(
+        schema["my_table"],
+        output_path,
+        output_format,
+        fhir_url,
+        access_token,
+    )
+
+    assert len(patch_write.call_args_list[0]) == 2
+
+    assert patch_write.call_args_list[0][0] == (
+        [
+            apply_schema_to_resource(
+                fhir_server_responses["content_1"]["entry"][0]["resource"],
+                schema["my_table"],
+            )
+        ],
+        output_path,
+        output_format,
+        None,
+    )
+    assert patch_write.call_args_list[1][0] == (
+        [
+            apply_schema_to_resource(
+                fhir_server_responses["content_2"]["entry"][0]["resource"],
+                schema["my_table"],
+            )
+        ],
+        output_path,
+        output_format,
+        patch_write(
+            (
+                [
+                    apply_schema_to_resource(
+                        fhir_server_responses["content_1"]["entry"][0]["resource"],
+                        schema["my_table"],
+                    )
+                ],
+                output_path,
+                output_format,
+                None,
+            )
+        ),
+    )
+
+
+@mock.patch("phdi_building_blocks.schemas.write_schema_table")
+@mock.patch("phdi_building_blocks.schemas.fhir_server_get")
+def test_make_table_fail(patch_query, patch_write):
+
+    schema = {}
+
+    output_path = mock.Mock()
+    output_path.__truediv__ = (  # Redefine division operator to prevent failure.
+        lambda x, y: x
+    )
+
+    output_format = "parquet"
+
+    fhir_url = "some_fhir_server_url"
+    access_token = "some_access_token"
+
+    response = mock.Mock()
+    response.status_code = 400
+    patch_query.return_value = response
+
+    make_table(
+        schema,
+        output_path,
+        output_format,
+        fhir_url,
+        access_token,
+    )
+    patch_write.assert_not_called()
+
+
+@mock.patch("phdi_building_blocks.schemas.make_table")
+@mock.patch("phdi_building_blocks.schemas.load_schema")
+def test_make_tables_from_schema(patched_load_schema, patched_make_table):
+
+    schema_path = mock.Mock()
+    output_path = mock.Mock()
+    output_path.__truediv__ = (  # Redefine division operator to prevent failure.
+        lambda x, y: x
+    )
+    output_format = "parquet"
+    fhir_url = "some_fhir_url"
+    access_token = "some_access_token"
+
+    schema = yaml.safe_load(
+        open(pathlib.Path(__file__).parent / "assets" / "test_schema.yaml")
+    )
+
+    patched_load_schema.return_value = schema
+
+    make_schema_tables(schema_path, output_path, output_format, fhir_url, access_token)
+
+    patched_make_table.assert_called_with(
+        schema["my_table"],
+        output_path,
+        output_format,
+        fhir_url,
+        access_token,
+    )
+
+
+@mock.patch("phdi_building_blocks.schemas.pq.ParquetWriter")
+@mock.patch("phdi_building_blocks.schemas.pa.Table")
+def test_write_schema_table_no_writer(patched_pa_table, patched_writer):
+
+    data = [{"some_column": "some value", "some_other_column": "some other value"}]
+    output_file_name = mock.Mock()
+    file_format = "parquet"
+
+    write_schema_table(data, output_file_name, file_format)
+    patched_pa_table.from_pylist.assert_called_with(data)
+    table = patched_pa_table.from_pylist(data)
+    patched_writer.assert_called_with(output_file_name, table.schema)
+    patched_writer(output_file_name, table.schema).write_table.assert_called_with(
+        table=table
+    )
+
+
+@mock.patch("phdi_building_blocks.schemas.pq.ParquetWriter")
+@mock.patch("phdi_building_blocks.schemas.pa.Table")
+def test_write_schema_table_with_writer(patched_pa_table, patched_writer):
+
+    data = [{"some_column": "some value", "some_other_column": "some other value"}]
+    output_file_name = mock.Mock()
+    file_format = "parquet"
+    writer = mock.Mock()
+
+    write_schema_table(data, output_file_name, file_format, writer)
+    patched_pa_table.from_pylist.assert_called_with(data)
+    table = patched_pa_table.from_pylist(data)
+    writer.write_table.assert_called_with(table=table)
+    assert len(patched_writer.call_args_list) == 0
+
+
+@mock.patch("phdi_building_blocks.schemas.pq.read_table")
+@mock.patch("phdi_building_blocks.schemas.pq.ParquetFile")
+@mock.patch("phdi_building_blocks.schemas.os.walk")
+def test_print_schema_summary(patched_os_walk, patched_ParquetFile, patched_reader):
+
+    patched_os_walk.return_value = [("some_path", None, ["filename.parquet"])]
+
+    schema_directory = mock.Mock()
+    display_head = False
+    print_schema_summary(schema_directory, display_head)
+    patched_ParquetFile.assert_called_with(
+        pathlib.Path("some_path") / "filename.parquet"
+    )
+    assert len(patched_reader.call_args_list) == 0
+
+    display_head = True
+    print_schema_summary(schema_directory, display_head)
+    patched_ParquetFile.assert_called_with(
+        pathlib.Path("some_path") / "filename.parquet"
+    )
+    patched_reader.assert_called_with(pathlib.Path("some_path") / "filename.parquet")
