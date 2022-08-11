c2b88db3c06b7562588da8f0134767056026d57d
bamader <49412165+bamader@users.noreply.github.com>
2022-08-11 14:07:36 -0400

    Raw Data Harmonization (#8)
    * Non fhir harmonization
    
    * Remove unused mock
    
    * Fhir wrappers and comments
    
    * Remove unused imports
    
    * Clarify todo for logic
    
    * Final comments

---
+++ phdi/fhir/harmonization/standardization.py
@@ -0,0 +1,179 @@
+from copy import copy
+from typing import List, Literal, Union
+from phdi.harmonization import (
+    standardize_name,
+    standardize_country_code,
+    standardize_phone,
+)
+
+
+def standardize_names(
+    data: dict,
+    trim: bool = True,
+    case: Literal["upper", "lower", "title"] = "upper",
+    remove_numbers: bool = True,
+    overwrite: bool = True,
+) -> dict:
+    """
+    Given either a FHIR bundle or a FHIR resource, transform all names
+    contained in any resource in the input.  The default standardization
+    behavior is our defined non-numeric, space-trimming, full
+    capitalization standardization, but other modes may be specified.
+
+    :param data: Either a FHIR bundle or a FHIR-formatted JSON dict
+    :param trim: Whether leading/trailing whitespace should be removed
+    :param: case: The type of casing that should be used
+    :param: remove_numbers: Whether to delete numeric characters
+    :param overwrite: Whether to replace the original names in the input
+      data with the standardized names (default is yes)
+    :return: The bundle or resource with names appropriately standardized
+    """
+    # Copy the data if we don't want to overwrite the original
+    if not overwrite:
+        data = copy.deepcopy(data)
+
+    # Allow users to pass in either a resource or a bundle
+    bundle = data
+    if "entry" not in data:
+        bundle = {"entry": [{"resource": data}]}
+
+    # Handle all resources individually
+    for entry in bundle.get("entry"):
+        resource = entry.get("resource", {})
+        resource = _standardize_names_in_resource(
+            resource, trim, case, remove_numbers, overwrite
+        )
+
+    if "entry" not in data:
+        return bundle.get("entry", [{}])[0].get("resource", {})
+    return bundle
+
+
+def standardize_phones(data: dict, overwrite=True) -> dict:
+    """
+    Given a FHIR bundle or a FHIR resource, standardize all phone
+    numbers contained in any resources in the input data.
+    Standardization is done according to the underlying
+    standardize_phone function in phdi.harmonization, so for more
+    information on country-coding and parsing, see the relevant
+    docstring.
+
+    :param data: A FHIR bundle or FHIR-formatted JSON dict
+    :param overwrite: Whether to replace the original phone numbers
+      in the input data with the standardized versions (default is yes)
+    :return: The bundle or resource with phones appropriately
+      standardized
+    """
+
+    if not overwrite:
+        data = copy.deepcopy(data)
+
+    # Allow users to pass in either a resource or a bundle
+    bundle = data
+    if "entry" not in data:
+        bundle = {"entry": [{"resource": data}]}
+
+    for entry in bundle.get("entry"):
+        resource = entry.get("resource", {})
+        resource = _standardize_phones_in_resource(resource, overwrite)
+
+    if "entry" not in data:
+        return bundle.get("entry", [{}])[0].get("resource", {})
+    return bundle
+
+
+def _standardize_names_in_resource(
+    resource: dict,
+    trim: bool = True,
+    case: Literal["upper", "lower", "title"] = "upper",
+    remove_numbers: bool = True,
+    overwrite: bool = True,
+) -> Union[dict, None]:
+    """
+    Helper method to standardize all found names in a given resource.
+    The resource can be of any type currently supported by the
+    function's logic. At this time, those resources include:
+
+      - Patient
+
+    The parameters to this function match the standardization flags
+    used by the underlying "standardize_name" function found in
+    phdi.harmonization. For more information, see the docstring for
+    that function.
+
+    :param resource: A FHIR-formatted JSON dictionary
+    :param trim: Whether to trim trailing/leading whitespace
+    :param case: What case to employ for the cleaned name
+    :param remove_numbers: Whether to delete numeric characters from
+      names in the resource
+    :param overwrite: Whether to overwrite the input data with the
+      new, standardized value (default is yes)
+    :return: The resource (or a copy thereof) with standardized
+      information in place of the raw
+    """
+
+    if not overwrite:
+        resource = copy.deepcopy(resource)
+
+    if resource.get("resourceType", "") == "Patient":
+        for name in resource.get("name", []):
+
+            # Handle family names
+            if "family" in name:
+                transformed_name = standardize_name(
+                    name.get("family", ""), trim, case, remove_numbers
+                )
+                name["family"] = transformed_name
+
+            # Given names are stored in a list, as there could be multiple,
+            # process them all and take the overall diff for metrics
+            if "given" in name:
+                transformed_names = [
+                    standardize_name(g, trim, case, remove_numbers)
+                    for g in name.get("given", [])
+                ]
+                name["given"] = transformed_names
+    return resource
+
+
+def _standardize_phones_in_resource(
+    resource: dict, overwrite=True
+) -> Union[dict, None]:
+
+    if not overwrite:
+        resource = copy.deepcopy(resource)
+
+    if resource.get("resourceType", "") == "Patient":
+        for telecom in resource.get("telecom", []):
+            if telecom.get("system") == "phone" and "value" in telecom:
+                countries = _extract_countries_from_resource(resource)
+                transformed_phone = standardize_phone(
+                    telecom.get("value", ""), countries
+                )
+                telecom["value"] = transformed_phone
+    return resource
+
+
+def _extract_countries_from_resource(
+    resource: dict, code_type: Literal["alpha_2", "alpha_3", "numeric"] = "alpha_2"
+) -> List[str]:
+    """
+    Given a FHIR resource, build a list containing all of the counries
+    found in the addresses associated with that resource in a standardized
+    form sepcified by code_type. If the resource is not of a supported
+    type, no countries will be contained in the returned list. Currently
+    supported resource types are:
+
+    - Patient
+
+    :param resource: A FHIR-formatted JSON dictionary
+    :param code_type: A string equal to 'alpha_2', 'alpha_3', or 'numeric'
+      to specify which type of standard country identifier to generate
+    """
+    countries = []
+    resource_type = resource.get("resourceType")
+    if resource_type == "Patient":
+        for address in resource.get("address"):
+            country = address.get("country")
+            countries.append(standardize_country_code(country, code_type))
+    return countries
---
+++ phdi/harmonization/hl7.py
@@ -0,0 +1,291 @@
+import logging
+import re
+import hl7
+
+from typing import List
+
+
+def standardize_hl7_datetimes(message: str) -> str:
+    """
+    Prepare an HL7 message for conversion by normalizing / sanitizing
+    fields that are known to contain data in problematic formats. This
+    function helps to-be-converted messages conform to expectations.
+    Cleaning operations include:
+
+    * Convert segment terminators from ``\\n`` to ``\\r``
+    * Normalize datetime fields
+    * Convert segment terminators back from ``\\r`` to ``\\n``
+
+    :param message: The raw HL7 message to sanitize
+    :return: The HL7 message with potential problem formats resolved,
+      if it was possible. If not, the original message.
+    """
+    parsed_message: hl7.Message = None
+    try:
+        # The hl7 module requires \n characters be replaced with \r
+        parsed_message = hl7.parse(message.replace("\n", "\r"))
+
+        # MSH-7 - Message date/time
+        _normalize_hl7_datetime_segment(parsed_message, "MSH", [7])
+
+        # PID-7 - Date of Birth
+        # PID-29 - Date of Death
+        # PID-33 - Last update date/time
+        _normalize_hl7_datetime_segment(parsed_message, "PID", [7, 29, 33])
+
+        # PV1-44 - Admission Date
+        # PV1-45 - Discharge Date
+        _normalize_hl7_datetime_segment(parsed_message, "PV1", [44, 45])
+
+        # ORC-9 Date/time of transaction
+        # ORC-15 Order effective date/time
+        # ORC-27 Filler's expected availability date/time
+        _normalize_hl7_datetime_segment(parsed_message, "ORC", [9, 15, 27])
+
+        # OBR-7 Observation date/time
+        # OBR-8 Observation end date/time
+        # OBR-22 Status change date/time
+        # OBR-36 Scheduled date/time
+        _normalize_hl7_datetime_segment(parsed_message, "OBR", [7, 8, 22, 36])
+
+        # OBX-12 Effective date/time of reference range
+        # OBX-14 Date/time of observation
+        # OBX-19 Date/time of analysis
+        _normalize_hl7_datetime_segment(parsed_message, "OBX", [12, 14, 19])
+
+        # TQ1-7 Start date/time
+        # TQ1-8 End date/time
+        _normalize_hl7_datetime_segment(parsed_message, "TQ1", [7, 8])
+
+        # SPM-18 Specimen received date/time
+        # SPM-19 Specimen expiration date/time
+        _normalize_hl7_datetime_segment(parsed_message, "SPM", [18, 19])
+
+        # RXA-3 Date/time start of administration
+        # RXA-4 Date/time end of administration
+        # RXA-16 Substance expiration date
+        # RXA-22 System entry date/time
+        _normalize_hl7_datetime_segment(parsed_message, "RXA", [3, 4, 16, 22])
+
+    # @TODO: Eliminate logging, raise an exception, document the exception
+    # in the docstring, and make this fit into our new structure of allowing
+    # the caller to implement more robust error handling
+    except Exception:
+        logging.exception(
+            "Exception occurred while cleaning message.  "
+            + "Passing through original message."
+        )
+
+        return message
+
+    return str(parsed_message).replace("\r", "\n")
+
+
+def convert_hl7_batch_messages_to_list(
+    content: str, delimiter: str = "\n"
+) -> List[str]:
+    """
+    Convert a batch file of messages into a list of strings
+    representing parts of the message. This function is based
+    on the following header/tail segments:
+
+    * FHS is a "File Header Segment", which is used to head a file
+        (group of batches)
+    * FTS is a "File Trailer Segment", which defines the end of a file
+    * BHS is "Batch Header Segment", which defines the start of a batch
+    * BTS is "Batch Trailer Segment", which defines the end of a batch
+
+    The structure of an HL7 Batch looks like this:
+    [FHS] (file header segment) { [BHS] (batch header segment)
+    { [MSH (zero or more HL7 messages)
+    ....
+    ....
+    ....
+    ] }
+    [BTS] (batch trailer segment)
+    }
+    [FTS] (file trailer segment)
+
+    We ignore lines that start with these since we don't want to include
+    them in a message.
+
+    :param content: the batch content to turn into a list
+    :param delimiter: the character delimiting messages in the batch
+    :return: A list of separated, cleaned HL7 messages
+    """
+
+    cleaned_batch = _clean_hl7_batch(content)
+    message_lines = cleaned_batch.split(delimiter)
+    next_message = ""
+    output = []
+
+    for line in message_lines:
+        if line.startswith(("FHS", "BHS", "BTS", "FTS")):
+            continue
+
+        # If we reach a line that starts with MSH and we have
+        # content in nextMessage, then by definition we have
+        # a full message in next_message and need to append it
+        # to output. This will not trigger the first time we
+        # see a line with MSH since next_message will be empty
+        # at that time.
+        if next_message != "" and line.startswith("MSH"):
+            output.append(next_message)
+            next_message = ""
+
+        # Otherwise, continue to add the line of text to next_message
+        if line != "":
+            next_message += f"{line}\r"
+
+    # Since our loop only adds messages to output when it finds
+    # a line that starts with MSH, the last message would never
+    # be added. So we explicitly add it here.
+    if next_message != "":
+        output.append(next_message)
+
+    return output
+
+
+def _default_hl7_value(
+    message: str, segment_id: str, field_num: int, default_value: str
+):
+    """
+    Default a field value in an HL7 message.
+
+    :param message: String representing the HL7 message use to modify
+      a value
+    :param segment_id: Segment type (MSH, PID, etc)
+    :param field_num: Field to replace in the HL7 message
+    :param default_value: If the selected field is blank, set the
+      field's value to this
+    :return: The HL7 message with default value inserted at the
+      specified segment location, if possible. If not, then the
+      original message.
+    """
+
+    parsed_message: hl7.Message = None
+    try:
+        # The hl7 module requires \n characters be replaced with \r
+        parsed_message = hl7.parse(message.replace("\n", "\r"))
+
+        segment: hl7.Segment = None
+        try:
+            segment = parsed_message.segment(segment_id=segment_id)
+        except KeyError:
+            # If the segment is not found, there is nothing to do
+            return message
+
+        if (
+            segment.extract_field(field_num=field_num) is None
+            or segment.extract_field(field_num=field_num) == ""
+        ):
+            segment.assign_field(value=default_value, field_num=field_num)
+
+    # @TODO: Eliminate logging, raise an exception, document the exception
+    # in the docstring, and make this fit into our new structure of allowing
+    # the caller to implement more robust error handling
+    except Exception:
+        logging.exception(
+            "Exception occurred while cleaning message.  "
+            + "Passing through original message."
+        )
+
+        return message
+
+    return str(parsed_message).replace("\r", "\n")
+
+
+def _normalize_hl7_datetime_segment(
+    message: hl7.Message, segment_id: str, field_list: list
+) -> None:
+    """
+    Utility function used to apply datetime normalization
+    to multiple fields in a segment.
+
+    :param message: The HL7 message, represented as a list
+      of indexable component strings (which is how the HL7 library
+      processes and returns messages)
+    :param segment_id: The HL7-specified segment (ex. PID) to
+      normalize the fields of
+    :param field_list: The list of fields contained in the
+      indexed message component, which are themselves indices to
+      data strings
+    :return: None (overwrites the input segment)
+    """
+    try:
+        for segment in message.segments(segment_id):
+            for field_num in field_list:
+                # Datetime value is always in first component
+                component_num = 0
+                if len(segment) > field_num and segment[field_num][component_num] != "":
+                    cleaned_datetime = _normalize_hl7_datetime(segment[field_num][0])
+                    segment[field_num][0] = cleaned_datetime
+
+    # @TODO: Eliminate logging, raise an exception, document the exception
+    # in the docstring, and make this fit into our new structure of allowing
+    # the caller to implement more robust error handling
+    except KeyError:
+        logging.debug(f"Segment {segment_id} not found in message.")
+
+
+def _normalize_hl7_datetime(hl7_datetime: str) -> str:
+    """
+    Split HL7 datetime-formatted fields into the following parts:
+    <integer 8+ digits>[.<integer 1+ digits>][+/-<integer 4+ digits>]
+
+    Each group of integers is truncated to conform to the HL7
+    specification:
+    - first integer group: max 14 digits
+    - following decimal point: max 4 digits
+    - following +/- (timezone): 4 digits
+
+    This normalization facilitates downstream processing using
+    cloud providers that have particular requirements for dates.
+
+    :param hl7_datetime: The raw datetime string to clean
+    :return: The datetime string with normalizing substitions
+      performed, or the original HL7 datetime if no matching
+      format could be found
+    """
+
+    hl7_datetime_match = re.match(r"(\d{8}\d*)(\.\d+)?([+-]\d+)?", hl7_datetime)
+
+    if not hl7_datetime_match:
+        return hl7_datetime
+
+    hl7_datetime_parts = hl7_datetime_match.groups()
+
+    # Start with date base
+    normalized_datetime = hl7_datetime_parts[0][:14]  # First 14 digits
+
+    # Add date decimal if present
+    if hl7_datetime_parts[1]:
+        normalized_datetime += hl7_datetime_parts[1][:5]  # . plus first 4 digits
+
+    # Add timezone information if present
+    if hl7_datetime_parts[2] and len(hl7_datetime_parts[2]) >= 5:
+        normalized_datetime += hl7_datetime_parts[2][:5]  # +/- plus 4 digits
+
+    return normalized_datetime
+
+
+def _clean_hl7_batch(batch: str, delimiter: str = "\n") -> str:
+    """
+    Clean a batch file by replacing Windows (CR-LF) newlines with
+    the specified newline delimiter (LF by default). Also, strip
+    vertical tab and file separator characters which can appear
+    in input batch file data.
+
+    :param batch: The batch file data to clean
+    :param delimiter: The newline character to standardize
+    :return: The batch file with newline characters standardized
+     and file separators removed
+    """
+    cleaned_batch = re.sub("[\r\n]+", delimiter, batch)
+
+    # These are unicode for vertical tab and file separator, respectively
+    # \u000b appears before every MSH segment, and \u001c appears at the
+    # end of the message in some of the data we've been receiving, so
+    # we're explicitly removing them here.
+    cleaned_batch = re.sub("[\u000b\u001c]", "", cleaned_batch).strip()
+    return cleaned_batch
