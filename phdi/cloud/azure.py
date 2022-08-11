ddcd882985450dd1dc6644a5df6978c540ed451a
Spencer Kathol <spencer@skylight.digital>
2022-08-11 11:36:59 -0600

    Implement Azure credential provider and supporting base class

---
+++ phdi/cloud/azure.py
@@ -0,0 +1,42 @@
+from core import BaseCredentialManager
+from azure.identity import DefaultAzureCredential
+
+
+class AzureCredentialManager(BaseCredentialManager):
+    """
+    This class provides an Azure-specific credential manager.
+    """
+
+    def __init__(self, resource_location: str, scope: str):
+        """
+        Create a new BaseCredentialManager object.
+        This object type is not intended to be called directly, but the constructor
+        may be called from base classes.
+
+        :param resource_location: URL or other location of the requested resource.
+        :param scope: A space-delimited list of scopes to limit access to resource.
+        """
+        self.resource_location = resource_location
+        self.scope = scope
+
+    def get_credential_object(self):
+        """
+        Returns a cloud-specific credential object
+        """
+        return DefaultAzureCredential()
+
+    def get_access_token(self, scope=None) -> str:
+        """
+        Obtain an access token from the Azure identity provider.
+
+        :param scope: OAuth2 scope to request for the access token
+        """
+        creds = self.get_credential_object()
+
+        if scope is None:
+            # This is the value Azure expects in order to retrieve the default scope
+            # from the identity provider
+            scope = f"{self.resource_location}/.default"
+
+        self.access_token = creds.get_token(scope)
+        return self.access_token
--- phdi/cloud/core.py
+++ phdi/cloud/core.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Any, Union, IO
 
 
 class BaseCredentialManager(ABC):
@@ -32,42 +31,4 @@ class BaseCredentialManager(ABC):
         """
         Returns an access token for the respective scope
         """
-        pass
-
-
-class CloudContainerConnection(ABC):
-    def __init__(self, resource_location: str, cred_manager: BaseCredentialManager):
-        """
-        Create a new BaseCredentialManager object.
-        This object type is not intended to be called directly, but the constructor
-        may be called from base classes.
-
-        :param resource_location: URL or other location of the requested resource.
-        :param scope: A space-delimited list of scopes to limit access to resource.
-        """
-        self.resource_location = resource_location
-        self.cred_manager = cred_manager
-
-    @abstractmethod
-    def download_object(
-        self, container_name: str, filename: str, cred_manager: BaseCredentialManager
-    ) -> Any:
-        pass
-
-    @abstractmethod
-    def upload_object(
-        self,
-        data: Union[str, dict, IO],
-        container_name: str,
-        filename: str,
-        cred_manager: BaseCredentialManager,
-    ) -> None:
-        pass
-
-    @abstractmethod
-    def list_containers(self):
-        pass
-
-    @abstractmethod
-    def list_objects(self):
         pass
