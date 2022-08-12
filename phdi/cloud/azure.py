bea886c864ff06186a4b20ec73daa7e774948f49
Spencer Kathol <spencer@skylight.digital>
2022-08-12 13:34:05 -0600

    Make get_access_token return a string, allow token reuse

--- phdi/cloud/azure.py
+++ phdi/cloud/azure.py
@@ -1,6 +1,7 @@
 from .core import BaseCredentialManager
 from azure.core.credentials import AccessToken
 from azure.identity import DefaultAzureCredential
+from datetime import datetime, timezone
 
 
 class AzureCredentialManager(BaseCredentialManager):
@@ -16,6 +17,10 @@ class AzureCredentialManager(BaseCredentialManager):
     def scope(self) -> str:
         return self.__scope
 
+    @property
+    def access_token(self) -> AccessToken:
+        return self.__access_token
+
     def __init__(self, resource_location: str, scope: str = None):
         """
         Create a new AzureCredentialManager object.
@@ -25,6 +30,7 @@ class AzureCredentialManager(BaseCredentialManager):
         """
         self.__resource_location = resource_location
         self.__scope = scope
+        self.__access_token = None
 
         if self.scope is None:
             self.__scope = f"{self.resource_location}/.default"
@@ -38,11 +44,31 @@ class AzureCredentialManager(BaseCredentialManager):
         """
         return DefaultAzureCredential()
 
-    def get_access_token(self) -> AccessToken:
+    def get_access_token(self, force_refresh: bool = False) -> str:
         """
         Obtain an access token from the Azure identity provider.
-        """
-        creds = self.get_credential_object()
 
-        access_token = creds.get_token(self.scope)
-        return access_token
+        :param force_refresh: force token refresh even if the current token is
+          still valid
+        :return: The access token, refreshed if necessary
+        """
+
+        if force_refresh or (self.access_token is None) or self._need_new_token():
+            creds = self.get_credential_object()
+            self.__access_token = creds.get_token(self.scope)
+
+        return self.access_token.token
+
+    def _need_new_token(self) -> bool:
+        """
+        Determine whether the token already stored for this object can be reused,
+        or if it needs to be re-requested.
+
+        :return: Whether we need a new token (True means we do)
+        """
+        try:
+            current_time_utc = datetime.now(timezone.utc).timestamp()
+            return self.access_token.expires_on < current_time_utc
+        except AttributeError:
+            # access_token not set
+            return True
