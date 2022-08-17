a7211d04fe05290eae3f7cf200a098a18ea6e1b5
bamader <49412165+bamader@users.noreply.github.com>
2022-08-17 17:42:27 -0400

    Convert to fhir (#15)
    * Stashing conversion
    
    * Stashing
    
    * Test case for conversion settings
    
    * Last FHIR test case
    
    * Pacify flake
    
    * FLAKE SHUT UP
    
    * Clean up xml parse and exceptions
    
    * Move to init, MSH3 test
    
    * Try this all formatting

---
+++ phdi/fhir/conversion/__init__.py
@@ -0,0 +1,3 @@
+from phdi.fhir.conversion.convert import convert_to_fhir
+
+__all__ = ("convert_to_fhir",)
---
+++ tests/fhir/test_utils.py
@@ -0,0 +1,32 @@
+import json
+import pathlib
+
+from phdi.fhir.utils import (
+    find_resource_by_type,
+    get_field,
+)
+
+
+def test_find_resource_by_type():
+    bundle = json.load(
+        open(pathlib.Path(__file__).parent.parent / "assets" / "patient_bundle.json")
+    )
+    found_patients = find_resource_by_type(bundle, "Patient")
+    assert len(found_patients) == 1
+    assert found_patients[0].get("resource").get("resourceType") == "Patient"
+
+
+def test_get_field():
+    bundle = json.load(
+        open(pathlib.Path(__file__).parent.parent / "assets" / "patient_bundle.json")
+    )
+    patient = bundle["entry"][1]["resource"]
+    assert get_field(patient, "telecom", "home", 0) == {
+        "use": "home",
+        "system": "phone",
+        "value": "123-456-7890",
+    }
+    assert get_field(patient, "telecom", "mobile", 1) == {
+        "value": "johndanger@doe.net",
+        "system": "email",
+    }
