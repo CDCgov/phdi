0262ec5d72d88df5603b80009b51c93091855451
Spencer Kathol <spencer@skylight.digital>
2022-08-19 14:13:25 -0600

    unit tests, module-level imports

---
+++ phdi/fhir/cloud/__init__.py
@@ -0,0 +1,3 @@
+from .azure import download_from_fhir_export_response
+
+__all__ = ["download_from_fhir_export_response"]
--- phdi/fhir/transport/__init__.py
+++ phdi/fhir/transport/__init__.py
@@ -4,8 +4,11 @@ from .http import (
     upload_bundle_to_fhir_server,
 )
 
+from .export import export_from_fhir_server
+
 __all__ = [
     "http_request_with_reauth",
     "fhir_server_get",
     "upload_bundle_to_fhir_server",
+    "export_from_fhir_server",
 ]
