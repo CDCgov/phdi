e09fe5f59f5c587c498351f493f824e36e09fbf2
Bryan <bryan@skylight.digital>
2022-08-24 08:59:11 -0400

    Adds information on exceptions explicitly raised to the docstring

--- phdi/transport/http.py
+++ phdi/transport/http.py
@@ -28,6 +28,8 @@ def http_request_with_retry(
       including Authorization and content-type
     :param data: JSON data in the case that the request requires data to be
       posted. Defaults to none.
+    :raises ValueError: An unsupported HTTP method (e.g. PATCH, DELETE, etc) was passed
+    to the request_type parameter.
     """
 
     request_type = request_type.upper()
