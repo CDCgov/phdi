500df371051c7d6da6b111c23696eb50f1803590
Bryan <97973748+bryan-skylight@users.noreply.github.com>
2022-08-29 14:31:23 -0400

    Add fhir tabulation module (#29)
    * Porting functionality over to phdi.fhir.tables
    
    * Porting tests over to tests/fhir/tabulation
    
    * Adds asset files for tabulation tests
    
    * Updating tests to reflect new function names and address errors
    
    * Implements styling changes around function names, imports, docstrings, etc as requested during the PR process
    
    * Removes test.py, which shouldn't have been included
    
    * Formatting the import statements

---
+++ phdi/fhir/tabulation/__init__.py
@@ -0,0 +1,11 @@
+from phdi.fhir.tabulation.tables import (
+    generate_table,
+    generate_all_tables_in_schema,
+    apply_schema_to_resource,
+)
+
+__all__ = [
+    "generate_table",
+    "generate_all_tables_in_schema",
+    "apply_schema_to_resource",
+]
