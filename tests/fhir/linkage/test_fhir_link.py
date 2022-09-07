e68eee8366e4b36f71c346acded139412070d7a6
KennethSkylight <107214304+KennethSkylight@users.noreply.github.com>
2022-09-07 07:53:52 -0700

    Linkage split (#31)
    * separating bundle from the individual
    
    * Add patient identifier by bundle
    
    * add by bundle
    
    * Change names
    
    * Fixed naming, added new test
    
    * Resolving PR comments
    
    * fixing patient identifier test

--- tests/fhir/linkage/test_fhir_link.py
+++ tests/fhir/linkage/test_fhir_link.py
@@ -1,7 +1,10 @@
 import json
 import pathlib
 from phdi.linkage.link import generate_hash_str
-from phdi.fhir.linkage.link import add_patient_identifier
+from phdi.fhir.linkage.link import (
+    add_patient_identifier,
+    add_patient_identifier_in_bundle,
+)
 
 
 def test_missing_address():
@@ -17,13 +20,13 @@ def test_missing_address():
         ]
     }
 
-    add_patient_identifier(bundle, "some-salt")
+    add_patient_identifier_in_bundle(bundle, "some-salt")
     expected = generate_hash_str("doe-19900101-", "some-salt")
     actual = bundle["entry"][0]["resource"]["identifier"][0]["value"]
     assert actual == expected
 
 
-def test_add_patient_identifier():
+def test_add_patient_identifier_by_bundle():
     salt_str = "super-legit-salt"
 
     incoming_bundle = json.load(
@@ -45,9 +48,40 @@ def test_add_patient_identifier():
         "use": "temp",
     }
 
-    add_patient_identifier(incoming_bundle, salt_str)
+    add_patient_identifier_in_bundle(incoming_bundle, salt_str)
     assert len(incoming_bundle["entry"]) == 3
     for resource in incoming_bundle["entry"]:
         if resource["resource"]["resourceType"] == "Patient":
             assert len(resource["resource"]["identifier"]) == 2
             assert resource["resource"]["identifier"][-1] == expected_new_identifier
+
+
+def test_patient_identifier():
+    salt_str = "super-legit-salt"
+
+    incoming_bundle = json.load(
+        open(
+            pathlib.Path(__file__).parent.parent.parent
+            / "assets"
+            / "patient_with_linking_id_bundle.json"
+        )
+    )
+
+    plaintext = (
+        "John-Tiberius-Shepard-2053-11-07-"
+        + "1234 Silversun Strip Zakera Ward, Citadel 99999"
+    )
+
+    expected_new_identifier = {
+        "value": generate_hash_str(plaintext, salt_str),
+        "system": "urn:ietf:rfc:3986",
+        "use": "temp",
+    }
+
+    for entry in incoming_bundle["entry"]:
+        if entry["resource"]["resourceType"] == "Patient":
+            resource = entry.get("resource")
+            add_patient_identifier(resource, salt_str, True)
+            assert len(resource["identifier"]) == 2
+            assert resource["identifier"][-1] == expected_new_identifier
+            break
