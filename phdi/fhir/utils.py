468e9b56e7ea3c8fe8f4e00311b076c0ce5f9401
bamader <brandon@skylight.digital>
2022-09-14 16:25:47 -0400

    Make utils more robust, add tests

--- phdi/fhir/utils.py
+++ phdi/fhir/utils.py
@@ -13,39 +13,51 @@ def find_entries_by_resource_type(bundle: dict, resource_type: str) -> List[dict
     """
     return [
         entry
-        for entry in bundle.get("entry")
-        if entry.get("resource", {}).get("resourceType") == resource_type
+        for entry in bundle.get("entry", [])
+        if entry.get("resource", {}).get("resourceType", "") == resource_type
     ]
 
 
-# @TODO: Improve this function for more general use, since it's user-
-# facing (i.e. make it more robust, less fragile, etc.)
-def get_field(resource: dict, field: str, use: str, default_field: int) -> str:
+def get_field(resource: dict, field: str, use: str, default_field: int = 0) -> str:
     """
-    Find the first-occuring instance of the field in a given FHIR-formatted JSON dict,
-    such that the instance is associated with a particular "use" case of a given field
-    (such as name or address). Use case here refers to the FHIR-based usage of
-    classifying how a value is used in reporting. For example, find the first name for a
-    patient that has a "use" of "official" (meaning the name is used for official
-    reports). If no instance of a field with the requested use case can be found,
-    instead return a specified default field.
+    Find the first-occuring instance of the field in a given FHIR-
+    formatted JSON dict, such that the instance is associated with
+    a particular "use" case of a given field (such as name or address).
+    Use case here refers to the FHIR-based usage of classifying how
+    a value is used in reporting. For example, find the first name
+    for a patient that has a "use" of "official" (meaning the name
+    is used for official reports). If no instance of a field with
+    the requested use case can be found, instead return a specified
+    default value for the field.
 
-    :param resource: Resource from a FHIR bundle
+    :param resource: A FHIR-formatted resource
     :param field: The field to extract
-    :param use: The use the field must have to qualify
-    :param default_field: The index of the field type to treat as
+    :param use: The 'use' the field must have to qualify
+    :param default_field: (optional) The index of the field type to treat as
       the default return type if no field with the requested use case is
-      found
+      found; if not supplied, use the first data available
     :return: The first instance of the field value matching the desired
       use, or a default field value if a match couldn't be found
     """
+    if field == "":
+        raise ValueError("Field must be a defined, non-empty string")
+    if use == "":
+        raise ValueError("Use must be a defined, non-empty string")
+    if field not in resource:
+        raise KeyError(f"Given resource does not contain a key called {field}")
+
     # The next function returns the "next" (in our case first) item from an
     # iterator that meets a given condition; if non exist, we index the
     # field for a default value
-    return next(
-        (item for item in resource[field] if item.get("use") == use),
-        resource[field][default_field],
-    )
+    try:
+        return next(
+            (item for item in resource.get(field, []) if item.get("use") == use),
+            resource.get(field)[default_field],
+        )
+    except:
+        raise IndexError(
+            "Index of provided field default is beyond length of field array"
+        )
 
 
 def get_one_line_address(address: dict) -> str:
@@ -56,8 +68,10 @@ def get_one_line_address(address: dict) -> str:
     :param address: The address bundle
     :return: A one-line string representation of an address
     """
+    if len(address) == 0:
+        return ""
     raw_one_line = " ".join(address.get("line", []))
-    raw_one_line += f" {address.get('city')}, {address.get('state')}"
-    if "postalCode" in address and address["postalCode"]:
+    raw_one_line += f" {address.get('city', '')}, {address.get('state', '')}"
+    if address.get("postalCode", "") != "":
         raw_one_line += f" {address['postalCode']}"
     return raw_one_line
