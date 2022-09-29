f98a6350fc5633b7cb32415a8a96122d3a20984d
m-goggins <53578688+m-goggins@users.noreply.github.com>
2022-09-29 15:13:52 -0700

    Census geocoding (#62)
    * initial commit of census.py
    
    * test case for census call with full address
    
    * add census-specific results
    
    * add geocode_from_dict()
    
    * add tests for census geocoding
    
    * add assert statement for ambiguous address
    
    * asserts None return
    
    * Fix exceptions, tests, and dict returns
    
    * fix valueerror messages
    
    * use http_request_with_retry method to call Census API
    
    * modify _call_census_api return value
    
    * Updated docstrings
    
    * update GeoCoding Pilot Partnership docs with Census info
    
    * update README to census
    
    * add census to geospatial tutorial
    
    * update geospatial tutorial with real address & census documentation
    
    * updated docstrings
    
    * update smarty raise
    
    * remove params for future considerations
    
    * removed erroneous Error
    
    * add test for missing zip
    
    * add detail to return for geocoded address
    
    * fix text edits
    
    * standardize testing error msgs
    
    * add test for failed api request
    
    * add mocked API call test
    
    * modify assertion

--- phdi/geospatial/__init__.py
+++ phdi/geospatial/__init__.py
@@ -1,4 +1,10 @@
 from phdi.geospatial.core import GeocodeResult, BaseGeocodeClient
 from phdi.geospatial.smarty import SmartyGeocodeClient
+from phdi.geospatial.census import CensusGeocodeClient
 
-__all__ = ("GeocodeResult", "BaseGeocodeClient", "SmartyGeocodeClient")
+__all__ = (
+    "GeocodeResult",
+    "BaseGeocodeClient",
+    "SmartyGeocodeClient",
+    "CensusGeocodeClient",
+)
