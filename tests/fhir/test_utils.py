ec8c45201990e3f621cd77901fbf916cc31e1cf9
Bryan <bryan@skylight.digital>
2022-10-06 11:39:41 -0400

    Updates tests to use pytest.raises and incorporates changes made to utils.py

--- tests/fhir/test_utils.py
+++ tests/fhir/test_utils.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+import pytest
 
 from phdi.fhir.utils import (
     find_entries_by_resource_type,
@@ -40,39 +41,24 @@ def test_get_field():
         "value": "123-456-7890",
     }
     assert get_field(
-        patient, field="telecom", use="mobile", require_use=False, index=1
+        patient, field="telecom", use="mobile", require_use=False, index=2
     ) == {
         "value": "johndanger@doe.net",
         "system": "email",
     }
 
     # Failure cases: undefined and mismatched inputs inputs
-    try:
-        get_field(patient, "", "home")
-    except Exception as e:
-        assert repr(e) == "ValueError('Field must be a defined, non-empty string')"
+    err_msg = "The field parameter must be a defined, non-empty string."
+    with pytest.raises(ValueError, match=err_msg):
+        get_field(patient, field="", use="home")
 
-    try:
-        get_field(patient, "telecom", "")
-    except Exception as e:
-        assert repr(e) == "ValueError('Use must be a defined, non-empty string')"
+    err_msg = "The use parameter should be a defined, non-empty string. If you don't want to include a use, set the parameter to None."  # noqa
+    with pytest.raises(ValueError, match=err_msg):
+        get_field(patient, field="telecom", use="")
 
-    try:
-        get_field({}, "telecom", "home")
-    except Exception as e:
-        assert (
-            repr(e)
-            == "KeyError('Given resource does not contain a key called telecom')"
-        )
-
-    # Failure case: default field out of range
-    try:
-        get_field(patient, "telecom", "office", 8)
-    except Exception as e:
-        assert (
-            repr(e)
-            == "IndexError('Index of provided field default is out of range for field array')"  # noqa
-        )
+    err_msg = "This resource does not contain a field called telecom."
+    with pytest.raises(KeyError, match=err_msg):
+        get_field({}, field="telecom", use="home")
 
 
 def test_get_one_line_address():
