5313e8c68a0131e07642ab00fc16f129ee5db523
Marcelle Goggins <marcelle.goggins@gmail.com>
2022-10-17 14:13:26 -0700

    remove unnecessary lines

--- phdi/fhir/tabulation/tables.py
+++ phdi/fhir/tabulation/tables.py
@@ -176,7 +176,6 @@ def drop_null(response: list, schema: dict):
     fields where include_nulls is False, as specified in the schema.
 
     :param response: List of resources returned from FHIR API.
-    :raises ValueError: If field  does not include street number and name.
     :param return: List of resources with removed nulls.
     """
 
@@ -190,10 +189,7 @@ def drop_null(response: list, schema: dict):
     # Identify indices in List of Lists to check for nulls
     indices_of_nulls = []
     for field in nulls_to_drop:
-        if field in response[0]:
-            indices_of_nulls.append(response[0].index(field))
-        else:
-            raise ValueError("Field not in FHIR response.")
+        indices_of_nulls.append(response[0].index(field))
 
     # Check if resource contains nulls to be dropped
     for resource in response[1:]:
