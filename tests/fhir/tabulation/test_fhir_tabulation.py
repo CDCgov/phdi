e88054e8e4ed868e00e0872e35e9702602830ef3
m-goggins <53578688+m-goggins@users.noreply.github.com>
2022-10-18 15:39:14 -0700

    Add drop_null function and tests (#87)
    * add drop_null function
    
    * fix test
    
    * remove unnecessary lines
    
    * changed null to look for empty strings
    
    * clean up
    
    * fixed test to match null search
    
    * add break
    
    * minor tweaks & manual merging

--- tests/fhir/tabulation/test_fhir_tabulation.py
+++ tests/fhir/tabulation/test_fhir_tabulation.py
@@ -9,6 +9,7 @@ from unittest import mock
 from phdi.fhir.tabulation.tables import (
     _apply_selection_criteria,
     apply_schema_to_resource,
+    drop_null,
     generate_all_tables_in_schema,
     generate_table,
     _generate_search_url,
@@ -344,3 +345,38 @@ def test_generate_search_urls_invalid():
 
     with pytest.raises(ValueError):
         _generate_search_urls(schema)
+
+
+def test_drop_null():
+
+    schema = yaml.safe_load(
+        open(
+            pathlib.Path(__file__).parent.parent.parent / "assets" / "test_schema.yaml"
+        )
+    )
+
+    fhir_server_responses_no_nulls = [
+        ["patient_id", "first_name", "last_name", "phone_number"],
+        ["some-uuid", "John", "Doe", "123-456-7890"],
+        ["some-uuid2", "First", "Last", "123-456-7890"],
+    ]
+
+    # Keeps all resources because include_nulls all False
+    responses_no_nulls = drop_null(
+        fhir_server_responses_no_nulls, schema["my_table"]["Patient"]
+    )
+    assert len(responses_no_nulls) == 3
+    assert responses_no_nulls[1][3] == fhir_server_responses_no_nulls[1][3]
+
+    # Drop null resource
+    fhir_server_responses_1_null = [
+        ["patient_id", "first_name", "last_name", "phone_number"],
+        ["some-uuid", "John", "Doe", "123-456-7890"],
+        ["some-uuid2", "Firstname", "Lastname", ""],
+    ]
+
+    responses_1_null = drop_null(
+        fhir_server_responses_1_null, schema["my_table"]["Patient"]
+    )
+    assert len(responses_1_null) == 2
+    assert responses_1_null[1][0] == fhir_server_responses_1_null[1][0]
