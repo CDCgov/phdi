7df0e6f6077d3302570d8306fc88f1b3a4d3526d
Brady Fausett at Skylight <109990072+BradySkylight@users.noreply.github.com>
2022-10-31 14:40:08 -0600

    Create upload blob to cloud storage function and container (#98)
    * Moved Credential Manager functionality out of FHIR Upload BB into Utils
    
    Added tests and fixed other tests
    
    * Initial commit
    
    Still working on tests
    
    * Added the cloud_write_to_storage route to main and config
    
    * Started to write tests
    
    * Flake8 fixes and update to name
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Fixed failing test issue
    
    * Added tests and fixed additional tests and general cleanup
    
    * More cleanup
    
    * fixed a slight bug and test
    
    * Fixed formatting issues
    
    * Change to a 'cloud' router.
    
    * Simplified the router and fixed tests
    
    Also added unique naming to the file name
    
    * Resolved merge conflicts
    
    * removed env variable for file_name - fixed tests
    
    * removed env setup in the config.py and updated tests
    
    * Update test_config.py
    
    Co-authored-by: DanPaseltiner <dan@skylight.digital>

---
+++ containers/phdi-ingestion/app/routers/cloud_storage.py
@@ -0,0 +1,61 @@
+import time
+from fastapi import APIRouter, Response, status
+from pydantic import BaseModel
+from typing import Literal, Optional
+
+from app.utils import search_for_required_values, get_cloud_provider_storage_connection
+
+
+router = APIRouter(
+    prefix="/cloud/storage",
+    tags=["cloud/storage"],
+)
+
+
+class WriteBlobToStorageInput(BaseModel):
+    blob: dict
+    cloud_provider: Optional[Literal["azure", "gcp"]]
+    bucket_name: Optional[str]
+    file_name: str
+
+
+@router.post("/write_blob_to_storage", status_code=200)
+def write_blob_to_cloud_storage_endpoint(
+    input: WriteBlobToStorageInput, response: Response
+) -> dict:
+    """
+    Upload the information from a blob into a specified cloud providers storage
+    organizing it by a bucket name as well as a file name.
+
+    :param input: A JSON formated request body (blob) with schema specified by the
+        WriteBlobToStorageInput model.
+    :return: A dictionary containing the status code and body of the response received
+        from the cloud provider.
+    """
+    input = dict(input)
+    required_values = ["cloud_provider", "bucket_name"]
+    search_result = search_for_required_values(input, required_values)
+    if search_result != "All values were found.":
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return search_result
+
+    cloud_provider_connection = get_cloud_provider_storage_connection(
+        cloud_provider=input["cloud_provider"]
+    )
+
+    full_file_name = input["file_name"] + str(int(time.time()))
+    cloud_provider_connection.upload_object(
+        message=input,
+        container_name=input["bucket_name"],
+        filename=full_file_name,
+    )
+
+    response.status_code = status.HTTP_201_CREATED
+    return {
+        "message": (
+            "The data has successfully been stored "
+            "in the {} cloud in {} container with the name {}.".format(
+                input["cloud_provider"], input["bucket_name"], full_file_name
+            )
+        )
+    }
