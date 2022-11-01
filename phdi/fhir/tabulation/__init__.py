ed2f3dbaa1dd593f2a5168cc2bb10e20f8865d03
bamader <49412165+bamader@users.noreply.github.com>
2022-11-01 13:20:22 -0400

    Add tabulate function and tests (#85)
    * Add tabulate function and tests
    
    * Formatting checks are so stupid
    
    * Change to LoL
    
    * Flake appeasement
    
    * Refactor for new schema
    
    * Flake happy
    
    * Add test, grab all tables for resource
    
    * SHUT UP FLAKE
    
    * Overhaul with new schema
    
    * Stupid flake
    
    * Address PR comments
    
    * Remove duplicated function
    
    * Adjust valid_schema file for generate urls test

--- phdi/fhir/tabulation/__init__.py
+++ phdi/fhir/tabulation/__init__.py
@@ -2,10 +2,12 @@ from phdi.fhir.tabulation.tables import (
     generate_table,
     generate_all_tables_in_schema,
     apply_schema_to_resource,
+    tabulate_data,
 )
 
 __all__ = [
     "generate_table",
     "generate_all_tables_in_schema",
     "apply_schema_to_resource",
+    "tabulate_data",
 ]
