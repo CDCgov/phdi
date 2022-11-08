d322b71c1f3684fe9831d56f8b42e6c54eb33ad9
m-goggins <53578688+m-goggins@users.noreply.github.com>
2022-11-08 13:37:43 -0800

    Schema validation (#115)
    * remove old schema formats
    
    * update valid schema path and valid_schema.json
    
    * first pass validation schema
    
    * add validate_schema function
    
    * add validate_schema tests
    
    * add jsonschema to poetry lock
    
    * add "all" to selection_criteria options
    
    * fix test to include 'all'

--- phdi/tabulation/__init__.py
+++ phdi/tabulation/__init__.py
@@ -1,3 +1,8 @@
-from phdi.tabulation.tables import load_schema, write_table, print_schema_summary
+from phdi.tabulation.tables import (
+    load_schema,
+    write_table,
+    print_schema_summary,
+    validate_schema,
+)
 
-__all__ = ("load_schema", "write_table", "print_schema_summary")
+__all__ = ("load_schema", "write_table", "print_schema_summary", "validate_schema")
