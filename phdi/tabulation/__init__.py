369ae3bcfda17b0eac42644ef2bfe5214a981c56
bamader <brandon@skylight.digital>
2022-11-10 13:37:17 -0500

    Delete old tabulate code

--- phdi/tabulation/__init__.py
+++ phdi/tabulation/__init__.py
@@ -1,8 +1,3 @@
-from phdi.tabulation.tables import (
-    load_schema,
-    write_table,
-    print_schema_summary,
-    validate_schema,
-)
+from phdi.tabulation.tables import load_schema, validate_schema, write_data
 
-__all__ = ("load_schema", "write_table", "print_schema_summary", "validate_schema")
+__all__ = ("load_schema", "validate_schema", write_data)
--- phdi/tabulation/tables.py
+++ phdi/tabulation/tables.py
@@ -7,7 +7,6 @@ import pyarrow.parquet as pq
 import sqlite3 as sql
 import yaml
 
-from pathlib import Path
 from typing import Literal, List, Union
 from jsonschema import validate
 import importlib.resources
@@ -44,6 +43,25 @@ def load_schema(path: pathlib.Path) -> dict:
         raise json.decoder.JSONDecodeError(
             "The specified file is not valid JSON.", e.doc, e.pos
         )
+
+
+def validate_schema(schema: dict):
+    """
+    Validates the schema structure, ensuring all required schema elements are present
+    and all schema elements are of the expected data type.
+
+    :param schema: A user-defined schema describing, for one or more
+      tables, the indexing FHIR resource type used to define rows, as
+      well as some number of columns specifying what values to include.
+    :raises jsonschema.exception.ValidationError: If the schema is invalid.
+    """
+    # Load validation schema
+    with importlib.resources.open_text(
+        "phdi.tabulation", "validation_schema.json"
+    ) as file:
+        validation_schema = json.load(file)
+
+    validate(schema=validation_schema, instance=schema)
 
 
 def write_data(
@@ -137,102 +155,3 @@ def write_data(
 
         conn.commit()
         conn.close()
-
-
-# @TODO: REMOVE THIS FUNCTION ALONG WITH THE OLD GENERATE ALL TABLES CODE
-# ONCE THE NEW TABULATION WORK IS COMPLETE
-def validate_schema(schema: dict):
-    """
-    Validates the schema structure, ensuring all required schema elements are present
-    and all schema elements are of the expected data type.
-
-    :param schema: A user-defined schema describing, for one or more
-      tables, the indexing FHIR resource type used to define rows, as
-      well as some number of columns specifying what values to include.
-    :raises jsonschema.exception.ValidationError: If the schema is invalid.
-    """
-    # Load validation schema
-    with importlib.resources.open_text(
-        "phdi.tabulation", "validation_schema.json"
-    ) as file:
-        validation_schema = json.load(file)
-
-    validate(schema=validation_schema, instance=schema)
-
-
-def write_table(
-    data: List[dict],
-    output_file_name: pathlib.Path,
-    file_format: Literal["parquet", "csv"],
-    writer: pq.ParquetWriter = None,
-) -> Union[None, pq.ParquetWriter]:
-    """
-    Given data stored as a list of dictionaries, where all dictionaries
-    have a common set of keys, writes the set of data to an output
-    file of a particular type.
-
-    :param data: A list of dictionaries representing the table's data.
-      Each dictionary represents one row in the resulting table. The
-      keys serve as the table's columns, and the values represent the
-      entry for that column in the row given by a particular dict.
-    :param output_file_name: The full name for the file where the table
-      is to be written.
-    :param output_format: The file format of the table to be written.
-    :param writer: A writer object that can be maintained
-      between different calls of this function to support file formats
-      that cannot be appended to after being written (e.g. parquet). Default: `None`
-    :return: An instance of `pq.ParquetWriter` if file_format is parquet,
-      otherwise `None`
-    """
-
-    if file_format == "parquet":
-        table = pa.Table.from_pylist(data)
-        if writer is None:
-            writer = pq.ParquetWriter(output_file_name, table.schema)
-        writer.write_table(table=table)
-        return writer
-
-    if file_format == "csv":
-        keys = data[0].keys()
-        new_file = False if os.path.isfile(output_file_name) else True
-        with open(output_file_name, "a", newline="") as output_file:
-            dict_writer = csv.DictWriter(output_file, keys)
-            if new_file:
-                dict_writer.writeheader()
-            dict_writer.writerows(data)
-
-
-def print_schema_summary(
-    directory: pathlib.Path,
-    display_head: bool = False,
-) -> None:
-    """
-    Prints a summary of each CSV of Parquet formatted table in a given directory of
-    tables.
-
-    :param directory: The path to a direct holding table files.
-    :param display_head: If true, print the first few rows of each table;
-      if false, only print table metadata. Default: `False`
-
-      Note: depending on the file format, this may require
-      reading large amounts of data into memory.
-    """
-    for (directory_path, _, file_names) in os.walk(directory):
-        for file_name in file_names:
-            if file_name.endswith("parquet"):
-
-                # Read metadata from parquet file without loading the actual data.
-                parquet_file = pq.ParquetFile(Path(directory_path) / file_name)
-                print(parquet_file.metadata)
-
-                # Read data from parquet and convert to pandas data frame.
-                if display_head is True:
-                    parquet_table = pq.read_table(Path(directory_path) / file_name)
-                    df = parquet_table.to_pandas()
-                    print(df.head())
-                    print(df.info())
-
-            if file_name.endswith("csv"):
-                with open(file_name, "r") as csv_file:
-                    reader = csv.reader(csv_file, dialect="excel")
-                    print(next(reader))
