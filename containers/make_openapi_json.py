875f42eac4491d3ec9617b3f13305b779071fe85
DanPaseltiner <dan@skylight.digital>
2022-12-02 11:27:48 -0500

    Remove trailing spaces for Flake8.

--- containers/make_openapi_json.py
+++ containers/make_openapi_json.py
@@ -3,9 +3,9 @@ from app.main import app
 import json
 
 """
-This is a simple script that writes the OpenAPI schema for a FastAPI application to 
-a JSON file. This JSON can then be used with a tool like redoc-cli to generate a static 
-HTML version of the API documentation served by a FastAPI applications at the /docs 
+This is a simple script that writes the OpenAPI schema for a FastAPI application to
+a JSON file. This JSON can then be used with a tool like redoc-cli to generate a static
+HTML version of the API documentation served by a FastAPI applications at the /docs
 endpoint.
 """
 
