66b9d94d83a0f9172afeeb0572abc9498928ebfd
DanPaseltiner <dan@skylight.digital>
2022-12-07 17:54:59 -0500

    Add metadata.

--- containers/phdi-ingestion/app/main.py
+++ containers/phdi-ingestion/app/main.py
@@ -7,13 +7,29 @@ from app.routers import (
     cloud_storage,
 )
 from app.config import get_settings
+from pathlib import Path
 
 # Read settings immediately to fail fast in case there are invalid values.
 get_settings()
 
-# Start the API
-app = FastAPI()
+# Start the API and set service metadata.
+description = Path('getting-started.md').read_text()
+app = FastAPI(
+    title="PHDI Ingestion Service",
+    description=description,
+    version="0.0.1",
+    contact={
+        "name": "CDC Public Health Data Infrastructure",
+        "url": "https://cdcgov.github.io/phdi-site/",
+        "email": "dmibuildingblocks@cdc.gov",
+    },
+    license_info={
+        "name": "Creative Commons Zero v1.0 Universal",
+        "url": "https://creativecommons.org/publicdomain/zero/1.0/",
+    },
+)
 
+# Include endpoints.
 app.include_router(fhir_harmonization_standardization.router)
 app.include_router(fhir_geospatial.router)
 app.include_router(fhir_linkage_link.router)
