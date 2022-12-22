d6d2506b3f9d14217ff3a67adc2fa65543536d3f
Gordon Farrell <gordon@skylight.digital>
2022-12-22 14:18:57 -0700

    Add schema validation endpoint and tests

--- containers/phdi-tabulation/main.py
+++ containers/phdi-tabulation/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import jsonschema
 
+from phdi.tabulation import validate_schema
 
 api = FastAPI()
 
@@ -10,12 +12,32 @@ class TabulationInput(BaseModel):
     Input parameters for the tabulation service.
     """
 
-    table_schema: dict
+    table_schema: dict = Field(alias="schema", description="A JSON formatted PHDI schema.")
 
 
 @api.get("/")
 async def health_check():
     return {"status": "OK"}
+
+
+@api.post("/validate-schema", status_code=200)
+async def validate_schema_endpoint(input: TabulationInput, response: Response):
+    try:
+        validate_schema(input.table_schema)
+        return {"success": True,
+                "isValid": True,
+                "message": "Valid Schema"}
+    except jsonschema.exceptions.ValidationError as e:
+        print('Error: ', e)
+        return {"success": True,
+                "isValid": False,
+                "message": "Invalid schema: Validation exception"}
+    except jsonschema.exceptions.SchemaError as e:
+        print('Error: ', e)
+        return {"success": True,
+                "isValid": False,
+                "message": "Invalid schema: Schema error"}
+
 
 
 @api.post("/tabulate", status_code=200)
--- containers/phdi-tabulation/test_tabulation.py
+++ containers/phdi-tabulation/test_tabulation.py
@@ -1,14 +1,21 @@
 # flake8: noqa
 from unittest import mock
 from fastapi.testclient import TestClient
+from pathlib import Path
+import json
+
 
 from main import api
 
 client = TestClient(api)
 
+schema_text = json.loads(Path("./valid_schema.json").read_text())
+
 valid_request = {
-    "table_schema": {},
+    "schema": schema_text
 }
+
+invalid_request = {"schema": {}}
 
 valid_response = {}
 
@@ -17,6 +24,22 @@ def test_health_check():
     actual_response = client.get("/")
     assert actual_response.status_code == 200
     assert actual_response.json() == {"status": "OK"}
+
+
+def test_validate_schema_pass():
+    actual_response = client.post("/validate-schema", json=valid_request)
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {"success": True,
+                                      "isValid": True,
+                                      "message": "Valid Schema"}
+
+
+def test_validate_validation_fail():
+    actual_response = client.post("/validate-schema", json=invalid_request)
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {"success": True,
+                                      "isValid": False,
+                                      "message": "Invalid schema: Validation exception"}
 
 
 def test_tabulate():
