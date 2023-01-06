fef1a7f66fe914d541d21a1c0ecdcf16dc94edad
Daniel Paseltiner <99684231+DanPaseltiner@users.noreply.github.com>
2023-01-06 13:13:29 -0500

    Add /tabulate and /validate endpoints to the tabulation service (#145)
    * Progress on tabulate endpoint.
    
    * Initial working endpoint.
    
    * Remove /tables
    
    * handle file paths with os
    
    * removed colons from datetime; reverted back to mkdir
    
    * minor tweaks
    
    * Get containerization working.
    
    * Remove unused util functions.
    
    * package resolution
    
    * Additional work
    
    * Removed functions added for debugging purposes.
    
    * Add schema validation to the tabulate endpoint.
    
    * minor tweaks
    
    * auto formatting
    
    * Merge schema validation endpoint work into branch
    
    * Create tests directory and add tests for util functions.
    
    * Add test for schema validator.
    
    * Add tabulate endpoint tests.
    
    * Additional unit test for the tabulate endpoint.
    
    * black
    
    * Add unit test for the tabulate function.
    
    * Fix failing validate test
    
    * Tweaks for green tests
    
    * Address review comments.
    
    * black
    
    * Address flake8
    
    * Add Power Shell virtual env command.
    
    Co-authored-by: Marcelle Goggins <marcelle.goggins@gmail.com>
    Co-authored-by: Gordon Farrell <gordon@skylight.digital>

---
+++ containers/tabulation/description.md
@@ -0,0 +1,45 @@
+## Getting Started with the PHDI Tabulation Service
+
+### Introduction
+The PHDI tabulation service offers a REST API to extract and tabulate data from a FHIR server according to a user-defined schema, and then persist the data in one of serveral supported formats. More information about this process and writing schemas can be found [here](https://github.com/CDCgov/phdi/blob/main/tutorials/tabulation-tutorial.md)
+
+### Running the Tabulation Service
+
+The tabulation service can be run using Docker (or any other OCI container runtime e.g. Podman), or directly from the Python sorce code.
+
+#### Running with Docker (Recommended)
+
+To run the tabulation service with Docker follow these steps.
+1. Confirm that you have Docker installed by running `docker -v`. If you do not see a response similar what is shown below, follow [these instructions](https://docs.docker.com/get-docker/) to install Docker.
+```
+❯ docker -v
+Docker version 20.10.21, build baeda1f
+``` 
+2. Download a copy of the Docker image from the PHDI repository by running `docker pull ghcr.io/cdcgov/phdi/tabulation:main`.
+3. Run the service with ` docker run -p 8080:8080 -v $(pwd):/code tabulation:main`.
+
+Congradulations the FHIR Converter should now be running on `localhost:8080`!
+
+#### Running from Python Source Code
+
+We recommend running the tabulation service from a container, but if that is not feasible for a given use-case, it may also be run directly from Python using the steps below.
+
+1. Ensure that both Git and Python 3.10 or higher are installed.
+2. Clone the PHDI repository with `git clone https://github.com/CDCgov/phdi`.
+3. Navigate to `/phdi/containers/tabulation/`.
+4. Make a fresh virtual environment with `python -m venv .venv`.
+5. Activate the virtual environement with `source .venv/bin/activate` (MacOS and Linux), `venv\Scripts\activate` (Windows Command Prompt), or `.venv\Scripts\Activate.ps1` (Windows Power Shell).
+5. Install all of the Python dependencies for the tabulation service with `pip install -r requirements.txt` into your virtual environment.
+6. Run the FHIR Converter on `localhost:8080` with `python -m uvicorn app.main:app --host 0.0.0.0 --port 8080`. 
+
+### Building the Docker Image
+
+To build the Docker image for the tabulation service from source instead of downloading it from the PHDI repository follow these steps.
+1. Ensure that both [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) and [Docker](https://docs.docker.com/get-docker/) are installed.
+2. Clone the PHDI repository with `git clone https://github.com/CDCgov/phdi`.
+3. Navigate to `/phdi/containers/tabulation/`.
+4. Run `docker build -t tabulation .`.
+
+### The API 
+
+When viewing these docs from the `/redoc` endpoint on a running instance of the tabulation service or the the PHDI website detailed documentation on the API will be avaiable below. 
---
+++ containers/tabulation/tests/test_tabulation.py
@@ -0,0 +1,255 @@
+# flake8: noqa
+import os
+from fastapi.testclient import TestClient
+import json
+from unittest import mock
+import pathlib
+import copy
+import urllib
+import datetime
+from app.main import app, tabulate
+
+valid_schema_path = (
+    pathlib.Path(__file__).parent.parent.parent.parent
+    / "tests"
+    / "assets"
+    / "valid_schema.json"
+)
+
+valid_schema = json.load(open(valid_schema_path))
+
+valid_tabulate_request = {
+    "output_type": "csv",
+    "schema_name": "new_schema",
+    "fhir_url": "http://localhost:8000/fhir/",
+    "schema": valid_schema,
+}
+
+client = TestClient(app)
+
+valid_validate_request = {"schema": valid_schema}
+
+invalid_validate_request = {"schema": {}}
+
+valid_response = {}
+
+
+def test_health_check():
+    actual_response = client.get("/")
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {"status": "OK"}
+
+
+def test_validate_schema_pass():
+    actual_response = client.post("/validate-schema", json=valid_validate_request)
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {
+        "success": True,
+        "isValid": True,
+        "message": "Valid Schema",
+    }
+
+
+def test_validate_validation_fail():
+    actual_response = client.post("/validate-schema", json=invalid_validate_request)
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {
+        "success": True,
+        "isValid": False,
+        "message": "Invalid schema: Validation exception",
+    }
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_valid_request(patched_tabulate):
+    valid_request = copy.deepcopy(valid_tabulate_request)
+    actual_response = client.post("/tabulate", json=valid_request)
+    valid_request["cred_manager"] = None
+    valid_request["schema_"] = valid_tabulate_request["schema"]
+    valid_request.pop("schema")
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {}
+    patched_tabulate.assert_called_with(**valid_request)
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_missing_schema(patched_tabulate):
+    invalid_tabulate_request = copy.deepcopy(valid_tabulate_request)
+    invalid_tabulate_request.pop("schema")
+    actual_response = client.post("/tabulate", json=invalid_tabulate_request)
+    assert actual_response.status_code == 422
+    assert actual_response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "schema"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_invalid_schema(patched_tabulate):
+    invalid_tabulate_request = copy.deepcopy(valid_tabulate_request)
+    invalid_tabulate_request["schema"] = {}
+    actual_response = client.post("/tabulate", json=invalid_tabulate_request)
+    assert actual_response.status_code == 422
+    assert actual_response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "schema"],
+                "msg": "Must provide a valid schema.",
+                "type": "assertion_error",
+            }
+        ]
+    }
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_missing_output_type(patched_tabulate):
+    invalid_tabulate_request = copy.deepcopy(valid_tabulate_request)
+    invalid_tabulate_request.pop("output_type")
+    actual_response = client.post("/tabulate", json=invalid_tabulate_request)
+    assert actual_response.status_code == 422
+    assert actual_response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "output_type"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_invalid_output_type(patched_tabulate):
+    invalid_tabulate_request = copy.deepcopy(valid_tabulate_request)
+    invalid_tabulate_request["output_type"] = "blah"
+    actual_response = client.post("/tabulate", json=invalid_tabulate_request)
+    assert actual_response.status_code == 422
+    assert actual_response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "output_type"],
+                "msg": "unexpected value; permitted: 'parquet', 'csv', 'sql'",
+                "type": "value_error.const",
+                "ctx": {"given": "blah", "permitted": ["parquet", "csv", "sql"]},
+            }
+        ]
+    }
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_invalid_cred_manager(patched_tabulate):
+    invalid_tabulate_request = copy.deepcopy(valid_tabulate_request)
+    invalid_tabulate_request["cred_manager"] = "blah"
+    actual_response = client.post("/tabulate", json=invalid_tabulate_request)
+    assert actual_response.status_code == 422
+    assert actual_response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "cred_manager"],
+                "msg": "unexpected value; permitted: 'azure', 'gcp'",
+                "type": "value_error.const",
+                "ctx": {"given": "blah", "permitted": ["azure", "gcp"]},
+            }
+        ]
+    }
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_missing_schema_name(patched_tabulate):
+    invalid_tabulate_request = copy.deepcopy(valid_tabulate_request)
+    invalid_tabulate_request.pop("schema_name")
+    actual_response = client.post("/tabulate", json=invalid_tabulate_request)
+    assert actual_response.status_code == 400
+    assert (
+        actual_response.json()
+        == "A value for schema_name could not be found. A value for schema_name must be provided using the 'schema_name' key in the request body, or within the metadata section of the schema."
+    )  # noqa
+
+
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_missing_fhirl_url(patched_tabulate):
+    invalid_tabulate_request = copy.deepcopy(valid_tabulate_request)
+    invalid_tabulate_request.pop("fhir_url")
+    actual_response = client.post("/tabulate", json=invalid_tabulate_request)
+    assert actual_response.status_code == 400
+    assert (
+        actual_response.json()
+        == "The following values are required, but were not included in the request and could not be read from the environment. Please resubmit the request including these values or add them as environment variables to this service. missing values: fhir_url."
+    )  # noqa
+
+
+@mock.patch("app.main.get_cred_manager")
+@mock.patch("app.main.tabulate")
+def test_tabulate_endpoint_instantiate_cred_manager(
+    patched_tabulate, patched_get_cred_manager
+):
+    tabulate_request = copy.deepcopy(valid_tabulate_request)
+    tabulate_request["cred_manager"] = "azure"
+    actual_response = client.post("/tabulate", json=tabulate_request)
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {}
+    patched_get_cred_manager.assert_called_with(
+        cred_manager=tabulate_request["cred_manager"],
+        location_url=tabulate_request["fhir_url"],
+    )
+
+
+@mock.patch("app.main.write_data")
+@mock.patch("app.main.tabulate_data")
+@mock.patch("app.main.extract_data_from_fhir_search_incremental")
+@mock.patch("app.main._generate_search_urls")
+def test_tabulate(
+    patched_generate_search_urls,
+    patched_extract_data_from_fhir_search_incremental,
+    patched_tabulate_data,
+    patched_write_data,
+):
+    tabulate_request = copy.deepcopy(valid_tabulate_request)
+    tabulate_request["schema_"] = tabulate_request["schema"]
+    tabulate_request.pop("schema")
+
+    search_urls = {"my-table": "my-table-search-url"}
+    patched_generate_search_urls.return_value = search_urls
+
+    incremental_results = ("some-incremental-results", None)
+    patched_extract_data_from_fhir_search_incremental.return_value = incremental_results
+
+    patched_tabulate_data.return_value = "some-tabulated-incremental-results"
+
+    pq_writer = mock.Mock()
+    patched_write_data.return_value = pq_writer
+
+    directory = (
+        pathlib.Path()
+        / "tables"
+        / tabulate_request["schema_name"]
+        / datetime.datetime.now().strftime("%m-%d-%YT%H%M%S")
+    )
+
+    tabulate(**tabulate_request)
+
+    patched_generate_search_urls.assert_called_with(schema=tabulate_request["schema_"])
+    patched_extract_data_from_fhir_search_incremental.assert_called_with(
+        search_url=urllib.parse.urljoin(
+            tabulate_request["fhir_url"], search_urls["my-table"]
+        ),
+        cred_manager=None,
+    )
+    patched_tabulate_data.assert_called_with(
+        incremental_results[0], tabulate_request["schema_"], list(search_urls.keys())[0]
+    )
+    patched_write_data.assert_called_with(
+        tabulated_data=patched_tabulate_data(),
+        directory=str(directory),
+        filename=list(search_urls.keys())[0],
+        output_type=tabulate_request["output_type"],
+        db_file=tabulate_request["schema_name"],
+        db_tablename=list(search_urls.keys())[0],
+        pq_writer=None,
+    )
+    assert pq_writer.close.called
