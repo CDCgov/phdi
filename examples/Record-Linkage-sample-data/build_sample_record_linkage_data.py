d36055eefe76593d320cdc00a740d9e74852c8ee
m-goggins <marcelle.goggins@gmail.com>
2023-02-03 15:03:42 -0800

    initial

---
+++ examples/Record-Linkage-sample-data/build_sample_record_linkage_data.py
@@ -0,0 +1,197 @@
+# This file builds a dataset of synthetic patient data to simulate incoming fields
+# derived from ELR/ECR/ADT data that could be used to link records. Names, addresses,
+# and DOBs have been intentionally scrambled and misspelled.
+
+import sqlite3
+import pandas as pd
+import random
+from random import shuffle
+import numpy as np
+
+
+# Get nicknames
+names_to_nicknames = {}
+with open("./phdi/harmonization/phdi_nicknames.csv", "r") as fp:
+    for line in fp:
+        if line.strip() != "":
+            name, nicks = line.strip().split(":", 1)
+            names_to_nicknames[name] = nicks.split(",")
+
+# Get source data
+conn = sqlite3.connect(
+    "C://Repos/phdi/examples/MPI-sample-data/synthetic_patient_mpi.db"
+)
+
+
+df = pd.read_sql_query("SELECT * from synthetic_patient_mpi", conn)
+conn.commit()
+conn.close()
+
+# Functions
+def scramble_dob(dob: str) -> str:
+
+    """
+    Scrambles a date of birth (DOB) that is in the form YYYY-MM-DD. DOBs can be
+    scrambled by year (last two digits are swapped), month (two digits are swapped),
+    day (digits are swapped), or diff. For diff, the year, month, or day are randomly
+    increased or decreased by a value of 1, e.g. 1984 could become 1983 or 1984.
+
+    :param dob: Date of birth string in the format YYYY-MM-DD.
+    :return: Scrambled date of birth string in the format YYYY-MM-DD.
+
+    """
+    # Randomly select how DOB will be scrambled.
+    method = random.choice(["year", "month", "day"])
+
+    # Swap last two digits of the year
+    if method == "year":
+        scrambled_dob = dob[:2] + dob[3] + dob[2] + dob[4:]
+    # Swap the two digits of the month
+    elif method == "month":
+        scrambled_dob = dob[:5] + dob[6] + dob[5] + dob[7:]
+    # Swap the two digits of the day
+    elif method == "day":
+        scrambled_dob = dob[:-2] + dob[-1] + dob[-2]
+    # Add or subtract 1 from a DOB's year, month, or day value
+    elif method == "diff":
+        time = random.choice(["year", "month", "day"])
+        plus_minus = random.choice([-1, 1])
+        if time == "year":
+            scrambled_dob = str(int(dob.split("-")[0]) + plus_minus) + dob[4:]
+        elif time == "month":
+            scrambled_dob = (
+                dob[0:5] + str(int(dob.split("-")[1]) + plus_minus).zfill(2) + dob[7:]
+            )
+        elif time == "day":
+            scrambled_dob = dob[0:8] + str(int(dob.split("-")[2]) + plus_minus).zfill(2)
+
+    return scrambled_dob
+
+
+def scramble_name(name: str) -> str:
+    """
+    Scrambles a single name by randomly adding an existing letter, removing an
+    existing letter, or swapping two existing letters.
+
+    :param name: Name, as a string, to be scrambled.
+    :return: Scrambled name, as a string.
+
+    """
+    # Randomly select scrambling method
+    method = random.choice(["add", "remove", "swap"])
+    # Randomly select non-first character in name
+    char = random.choice(range(1, len(name)))
+
+    if method == "add":
+        scrambled_name = name[0:char] + name[char] + name[char:]
+    elif method == "remove":
+        scrambled_name = name[0:char] + name[char + 1 :]
+    elif method == "swap":
+        scrambled_name = (
+            name[0 : char - 1] + name[char] + name[char - 1] + name[char + 1 :]
+        )
+
+    return scrambled_name
+
+
+def scramble_zip(zip: str) -> str:
+    """
+    Scrambles all digits of a zip code except for the first one.
+
+    :param zip: Zip code containing only numbers, as a string.
+    :return: Zip code with the last [1:] digits scrambled.
+
+    """
+    zip_list = list(zip[1:])
+    shuffle(zip_list)
+    scrambled_zip = zip[0] + "".join(zip_list)
+    return scrambled_zip
+
+
+def swap_name_for_nickname(name: str) -> str:
+    """
+    Swaps in a random, associated nickname for a given name, if the name has any
+    associated nicknames, e.g., 'Bill' or 'Will' could be randomly swapped for
+    'William' but 'Eddie' could not be because it is not an associated nickname for
+    'William'.
+
+    :param name: Single name
+    :return: Randomly chosen nickname that corresponds to input name. If no nicknames
+    correspond to the input name, the original name is returned instead.
+
+    """
+    if name.upper() in names_to_nicknames.keys():
+        swapped_name = random.choice(names_to_nicknames[name.upper()]).title()
+        return swapped_name
+    else:
+        return name
+
+
+def add_missing_values(data, column: str, perc_missing: float):
+    """
+    Randomly changes values in a column to missing (nan).
+
+    :param data: A DataFrame object.
+    :param column: Column name that you wish to add missing values to.
+    :param perc_missing: The percent missingness you wish to introduce.
+    :return: DataFrame with randomly missing data from the input column.
+
+    """
+    data[column] = data[column].sample(frac=(1 - perc_missing))
+    return data
+
+
+def add_copies(data, num_copies: int):
+    """
+    Adds duplicate rows to a DataFrame.
+
+    :param data: A DataFrame object.
+    :param num_copies: The number of duplicate rows to add for each existing row.
+    :return: A DataFrame object with duplicate rows.
+
+    """
+    data_with_copies = pd.DataFrame(np.repeat(data.values, num_copies, axis=0))
+    data_with_copies.columns = data.columns
+
+    return data_with_copies
+
+
+# Workflow
+seed = 123
+source_data = df.copy()
+source_data["ZIP"] = source_data["ZIP"].astype(str).str.split(".").str[0]
+source_data_with_copies = add_copies(source_data, num_copies=3)
+
+good_data = source_data_with_copies.sample(frac=0.7, random_state=seed)
+
+# Scramble DOB in subsample
+bad_dob = source_data_with_copies.sample(frac=0.05, random_state=seed)
+bad_dob["BIRTHDATE"] = bad_dob["BIRTHDATE"].apply(lambda x: scramble_dob(x))
+bad_dob["bad_dob"] = 1
+
+# Scramble zip in subsample
+bad_zip = source_data_with_copies.sample(frac=0.1, random_state=seed)
+bad_zip["bad_zip"] = 1
+bad_zip["ZIP"] = bad_zip["ZIP"].apply(lambda x: scramble_zip(x))
+
+# Assign nicknames in subsample
+bad_name_nickname = source_data_with_copies.sample(frac=0.1, random_state=seed)
+bad_name_nickname["bad_name_nickname"] = 1
+bad_name_nickname["FIRST"] = bad_name_nickname["FIRST"].apply(
+    lambda x: swap_name_for_nickname(x)
+)
+
+# Scramble names in subsample
+bad_name_scramble = source_data_with_copies.sample(frac=0.05, random_state=seed)
+bad_name_scramble["bad_name_scramble"] = 1
+bad_name_scramble["FIRST"] = bad_name_scramble["FIRST"].apply(
+    lambda x: scramble_name(x)
+)
+bad_name_scramble["FIRST4"] = bad_name_scramble["FIRST"].str[0:4]
+
+# Compile data
+data = pd.concat(
+    [good_data, bad_dob, bad_zip, bad_name_scramble, bad_name_nickname],
+    ignore_index=True,
+).sort_values(by="Id")
+data = data.fillna(0)
