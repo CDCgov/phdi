1efba056bd54891f6964d98537d0e8660b136c69
DanPaseltiner <dan@skylight.digital>
2023-02-10 10:21:12 -0500

    progress on unit tests

--- containers/validation/tests/test_validation.py
+++ containers/validation/tests/test_validation.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from unittest import mock
 from app.main import app
 
 
@@ -10,9 +11,33 @@ def test_health_check():
     assert actual_response.status_code == 200
     assert actual_response.json() == {"status": "OK"}
 
-
-def test_validate_valid_ecr():
+@mock.patch("app.main.validate_ecr")
+def test_validate_endpoint_valid_ecr():
     request_body = {"message_type": "ecr", "message": "my ecr"}
+    actual_response = client.post("/validate", json=request_body)
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {
+        "message_valid": True,
+        "validation_results": {
+            "details": "No validation was actually preformed. This endpoint only has "
+            "stubbed functionality"
+        },
+    }
+
+def test_validate_endpoint_valid_elr():
+    request_body = {"message_type": "elr", "message": "my ELR"}
+    actual_response = client.post("/validate", json=request_body)
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {
+        "message_valid": True,
+        "validation_results": {
+            "details": "No validation was actually preformed. This endpoint only has "
+            "stubbed functionality"
+        },
+    }
+@mock.patch("app.main.validate_ecr")
+def test_validate_endpoint_valid_vxu():
+    request_body = {"message_type": "vxu", "message": "my VXU"}
     actual_response = client.post("/validate", json=request_body)
     assert actual_response.status_code == 200
     assert actual_response.json() == {
