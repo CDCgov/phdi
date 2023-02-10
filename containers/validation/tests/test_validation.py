b1901cb19c8fbd66189dda207aa020c93ad3c056
DanPaseltiner <dan@skylight.digital>
2023-02-10 14:25:28 -0500

    New and improved unit tests

--- containers/validation/tests/test_validation.py
+++ containers/validation/tests/test_validation.py
@@ -1,7 +1,6 @@
 from fastapi.testclient import TestClient
 from unittest import mock
-from app.main import app
-
+from app.main import app, validate_ecr, validate_elr, validate_vxu, message_validators
 
 client = TestClient(app)
 
@@ -11,12 +10,8 @@ def test_health_check():
     assert actual_response.status_code == 200
     assert actual_response.json() == {"status": "OK"}
 
-@mock.patch("app.main.validate_ecr")
-def test_validate_endpoint_valid_ecr():
-    request_body = {"message_type": "ecr", "message": "my ecr"}
-    actual_response = client.post("/validate", json=request_body)
-    assert actual_response.status_code == 200
-    assert actual_response.json() == {
+def test_validate_ecr():
+    assert validate_ecr("my ecr contents") == {
         "message_valid": True,
         "validation_results": {
             "details": "No validation was actually preformed. This endpoint only has "
@@ -24,26 +19,38 @@ def test_validate_endpoint_valid_ecr():
         },
     }
 
-def test_validate_endpoint_valid_elr():
-    request_body = {"message_type": "elr", "message": "my ELR"}
-    actual_response = client.post("/validate", json=request_body)
-    assert actual_response.status_code == 200
-    assert actual_response.json() == {
+def test_validate_elr():
+    assert validate_elr("my elr contents") == {
         "message_valid": True,
         "validation_results": {
             "details": "No validation was actually preformed. This endpoint only has "
             "stubbed functionality"
         },
     }
-@mock.patch("app.main.validate_ecr")
-def test_validate_endpoint_valid_vxu():
-    request_body = {"message_type": "vxu", "message": "my VXU"}
-    actual_response = client.post("/validate", json=request_body)
-    assert actual_response.status_code == 200
-    assert actual_response.json() == {
+
+def test_validate_vxu():
+    assert validate_vxu("my vxu contents") == {
         "message_valid": True,
         "validation_results": {
             "details": "No validation was actually preformed. This endpoint only has "
             "stubbed functionality"
         },
     }
+
+@mock.patch("app.main.message_validators")
+def test_validate_endpoint_valid_vxu(patched_message_validators):
+    for message_type in message_validators:
+        # Prepare mocked validator function
+        validation_response = {"message_valid": True, "validation_results": {}}
+        mocked_validator = mock.Mock()
+        mocked_validator.return_value = validation_response
+        message_validators_dict = {message_type: mocked_validator}
+        patched_message_validators.__getitem__.side_effect = message_validators_dict.__getitem__
+
+        # Send request to test client
+        request_body = {"message_type": message_type, "message": "message contents"}
+        actual_response = client.post("/validate", json=request_body)
+
+        # Check that the correct validator was selected and used properly.
+        assert actual_response.status_code == 200
+        message_validators_dict[message_type].assert_called_with(request_body["message"])
