4b0aae6f0a8c9ee4ec326e224a981efb7f1fcf94
Kenneth Chow <kenneth@skylight.digital>
2023-02-14 10:37:22 -0800

    Validate.py changes

--- phdi/validator/validate.py
+++ phdi/validator/validate.py
@@ -4,11 +4,11 @@ import re
 
 config = {
     "requiredFields": [
-        # {
-        #     "fieldName": "Status",
-        #     "cdaPath": "//hl7:ClinicalDocument/hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:entry/hl7:act/hl7:code",
-        #     "attributes": [{"attributeName": "code"}],
-        # },
+        {
+            "fieldName": "Status",
+            "cdaPath": "//hl7:ClinicalDocument/hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:entry/hl7:act/hl7:code",
+            "attributes": [{"attributeName": "code"}],
+        },
         # {
         #     "fieldName": "Conditions",
         #     "cdaPath": "//hl7:ClinicalDocument/hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:entry/hl7:organizer/hl7:component/hl7:observation/hl7:value/hl7:code",
@@ -19,22 +19,22 @@ config = {
             "cdaPath": "//hl7:ClinicalDocument/hl7:id",
             "attributes": [{"attributeName": "root"}],
         },
-        # {
-        #     "fieldName": "eICR Version Number",
-        #     "cdaPath": "//hl7:ClinicalDocument/hl7:versionNumber",
-        #     "attributes": [{"attributeName": "value"}],
-        # },
-        # {
-        #     "fieldName": "Authoring date/hl7:time",
-        #     "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:author/hl7:time",
-        #     "attributes": [{"attributeName": "value"}],
-        # },
         {
-            "fieldName": "First Name",
-            "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:patientRole/hl7:patient/hl7:name",
-            "textRequired": True,
-            "attributes": [{"attributeName": "use", "regEx": "/L/"}],
+            "fieldName": "eICR Version Number",
+            "cdaPath": "//hl7:ClinicalDocument/hl7:versionNumber",
+            "attributes": [{"attributeName": "value"}],
         },
+        {
+            "fieldName": "Authoring date/hl7:time",
+            "cdaPath": "//hl7:ClinicalDocument/hl7:author/hl7:time",
+            "attributes": [{"attributeName": "value"}],
+        },
+        # {
+        #     "fieldName": "First Name",
+        #     "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:patientRole/hl7:patient/hl7:name",
+        #     "textRequired": True,
+        #     "attributes": [{"attributeName": "use", "regEx": "^L$"}],
+        # },
         # {
         #     "fieldName": "Middle Name",
         #     "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:patientRole/hl7:patient/hl7:name/hl7:given",
@@ -55,11 +55,11 @@ config = {
         #     "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:patientRole/hl7:patient/hl7:birthTime",
         #     "textRequired": True,
         # },
-        # {
-        #     "fieldName": "MRN",
-        #     "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:patientRole/hl7:id/hl7:",
-        #     "attributes": [{"attributeName": "extension"}, {"attributeName": "root"}],
-        # },
+        {
+            "fieldName": "MRN",
+            "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:patientRole/hl7:id",
+            "attributes": [{"attributeName": "extension"}, {"attributeName": "root"}],
+        },
         # {
         #     "fieldName": "Sex",
         #     "cdaPath": "//hl7:ClinicalDocument/hl7:recordTarget/hl7:patientRole/hl7:patient/hl7:administrativeGenderCode",
@@ -92,11 +92,11 @@ config = {
         #     "textRequired": True,
         #     "regEx": "/[0-9]{5}(?:-[0-9]{4})?/",
         # },
-        # {
-        #     "fieldName": "Provider ID",
-        #     "cdaPath": "//hl7:ClinicalDocument/hl7:componentOf/hl7:encompassingEncounter/hl7:responsibleParty/hl7:assignedEntity/hl7:id",
-        #     "attributes": [{"attributeName": "extension"}, {"attributeName": "root"}],
-        # },
+        {
+            "fieldName": "Provider ID",
+            "cdaPath": "//hl7:ClinicalDocument/hl7:componentOf/hl7:encompassingEncounter/hl7:responsibleParty/hl7:assignedEntity/hl7:id",
+            "attributes": [{"attributeName": "extension"}, {"attributeName": "root"}],
+        },
     ]
 }
 
@@ -119,24 +119,45 @@ def validate(file_name, config):
     tree = get_parsed_file(file_name)
     for field in config.get("requiredFields"):
         path = field.get("cdaPath")
-        matched_node = tree.xpath(path, namespaces=namespaces)
-        # attributes check
-        validate_attribute(field, matched_node)
-        # text check
-        validate_text
+        matched_nodes = tree.xpath(path, namespaces=namespaces)
+        for node in matched_nodes:
+            # attributes check
+            validate_attribute(field, node)
+            # text check
+            validate_text()
 
 
-def validate_attribute(field, matched_node):
+def validate_attribute(field, node):
+    """
+    Validates a node by checking if attribute exists or matches regex pattern.
+    If the node does not pass a test as described in the config, an error message is
+    appended. All fields are valid if the error message list is blank
+
+    :param field: A dictionary entry that describes what fields need to be
+        validated and how
+    :param node: A dictionary entry that includes the attribute name key and
+        value.
+    """
     attribute_value = ""
+    error_message = []
     for attribute in field.get("attributes"):
         if "attributeName" in attribute:
             attribute_name = attribute.get("attributeName")
-            attribute_value = matched_node[0].get(attribute_name)
-            print(attribute_value)
+            attribute_value = node.get(attribute_name)
+            if not attribute_value:
+                error_message.append(f"Could not find attribute {attribute_name}")
         if "regEx" in attribute:
             pattern = re.compile(attribute.get("regEx"))
-            print(pattern)
-            print(pattern.match(attribute_value))
+            if not pattern.match(attribute_value):
+                error_message.append(
+                    f"Attribute '{attribute_name}' not in expected format"
+                )
+    return error_message
+
+
+def _print_nodes(nodes):
+    for node in nodes:
+        print(node)
 
 
 def validate_text():
