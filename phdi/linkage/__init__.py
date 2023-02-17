a6968fb71c8b453e31cd20efeb949baed1a65cd0
bamader <49412165+bamader@users.noreply.github.com>
2023-02-17 16:26:01 -0500

    Implement validation of LAC model (#169)
    * Turns out I actually did all the LAC infra
    
    * Create evaluation script to play with
    
    * Appease Flake
    
    * Add phdi algorithm func, metaphone feature comparison
    
    * Miss profiler
    
    * Update modularity in testing script
    
    * Put branch in working order
    
    * Remove unused import
    
    * Update import logic, comments
    
    * Finalize LAC algo, v0 PHDI algo

--- phdi/linkage/__init__.py
+++ phdi/linkage/__init__.py
@@ -1,21 +1,29 @@
 from phdi.linkage.link import (
     generate_hash_str,
-    block_parquet_data,
+    block_data,
     match_within_block,
     feature_match_exact,
     feature_match_fuzzy_string,
     eval_perfect_match,
-    block,
+    compile_match_lists,
+    feature_match_four_char,
+    perform_linkage_pass,
+    score_linkage_vs_truth,
+    block_data_from_db,
     _generate_block_query,
 )
 
 __all__ = [
     "generate_hash_str",
-    "block_parquet_data",
+    "block_data",
     "match_within_block",
     "feature_match_exact",
     "feature_match_fuzzy_string",
     "eval_perfect_match",
-    "block",
+    "compile_match_lists",
+    "feature_match_four_char",
+    "perform_linkage_pass",
+    "score_linkage_vs_truth",
+    "block_data_from_db",
     "_generate_block_query",
 ]
