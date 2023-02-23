328bc7bae2f1247bd5298a54de7931bf4c957e04
Brady Fausett at Skylight <brady@skylight.digital>
2023-02-23 01:06:46 -0700

    Update __init__.py

--- phdi/fhir/validation/__init__.py
+++ phdi/fhir/validation/__init__.py
@@ -2,7 +2,4 @@ from phdi.fhir.validation.validation import validate_ecr_payload
 from phdi.validation.validation import validate_ecr
 
 
-__all__ = [
-    "validate_ecr_payload",
-    "validate_ecr"
-]
+__all__ = ["validate_ecr_payload", "validate_ecr"]
