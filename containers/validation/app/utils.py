75207fc5471e8f4fa1eb278ac040d2d4821b039e
KennethSkylight <107214304+KennethSkylight@users.noreply.github.com>
2023-02-24 13:42:24 -0800

    Xpath ecr validator (#172)
    * Add lxml and test file
    
    * One element query
    
    * separate functions
    
    * Validate.py changes
    
    * Initial text validation of ecr messages
    
    * Working tests
    
    * More tests
    
    * Add comments
    
    * added test
    
    * Added error test
    
    * Fixed test
    
    * Moved files, renamed, fix byte error
    
    * Fixed test and too long strings
    
    * Add pth file parent
    
    * Adding new tests for codecov
    
    * Refactor to make ecr validation work with REST API payload instead of ecr file (#178)
    
    * Add workflow to run tests then tag a new release if tests are green (#177)
    
    * Add workflow to test then tag a new release
    
    * Initial commit - still need to fix test cases
    
    * Update validation.py
    
    * Restructuring, fixing code and fixing tests - WIP need more tests
    
    * black and flak8 resolutions
    
    * fixed test case folder/file definitions
    
    * resolved file paths - fixed formatting errors
    
    * fixed format issues
    
    * fixed more formatting and file open errors
    
    * point to current branch
    
    * fixed tests and format issues
    
    * Update __init__.py
    
    * point to current branch commit
    
    * removed FHIR layer as validation occurs prior
    
    * remove print statement
    
    * update requirements for different containers
    
    * Update requirements.txt
    
    * updated tests and moved things around - still WIP
    
    * fixing more tests and errrors
    
    * fixed formatting and tests
    
    * fixed additional tests and format issues
    
    ---------
    
    Co-authored-by: gordonfarrell <93161643+gordonfarrell@users.noreply.github.com>
    
    * WIP
    
    * WIP
    
    * wip
    
    * Update validation.py
    
    * Update validation.py
    
    ---------
    
    Co-authored-by: nickbristow <richard.nick.bristow@gmail.com>
    Co-authored-by: Brady Fausett at Skylight <brady@skylight.digital>
    Co-authored-by: gordonfarrell <93161643+gordonfarrell@users.noreply.github.com>

---
+++ containers/validation/app/utils.py
@@ -0,0 +1,80 @@
+import pathlib
+import yaml
+
+VALID_ERROR_TYPES = ["error", "warn", "info"]
+
+
+# TODO: Determine where/when this configuration should be loaded (as we
+# will only want to load this once or after it has been updated instead
+# of loading it each time we validate an eCR)
+# we may also need to move this to a different location depending upon where/when
+# the loading occurs
+def load_config(path: pathlib.Path) -> dict:
+    """
+    Given the path to a local YAML file containing a validation
+    configuration, loads the file and returns the resulting validation
+    configuration as a dictionary. If the file can't be found, raises an error.
+
+    :param path: The file path to a YAML file holding a validation configuration.
+    :raises ValueError: If the provided path points to an unsupported file type.
+    :raises FileNotFoundError: If the file to be loaded could not be found.
+    :return: A dict representing a validation configuration read
+        from the given path.
+    """
+    try:
+        with open(path, "r") as file:
+            if path.suffix == ".yaml":
+                config = yaml.safe_load(file)
+            else:
+                ftype = path.suffix.replace(".", "").upper()
+                raise ValueError(f"Unsupported file type provided: {ftype}")
+        # TODO:
+        # Create a file that validates the validation configuration created
+        # by the client
+        # validate_config(config)
+        return config
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            "The specified file does not exist at the path provided."
+        )
+
+
+# def validate_config(config: dict):
+#     """
+#     Validates the validation configuration structure, ensuring
+#     all required validation elements are present and all configuration
+#     elements are of the expected data type.
+
+#     :param config: A declarative, user-defined configuration for validating
+#         data fields within a message (ecr, elr, vxu).
+#     :raises jsonschema.exception.ValidationError: If the schema is invalid.
+#     """
+#     # TODO:
+#     # Create a file that validates the validation configuration created
+#     # by the client
+#     with importlib.resources.open_text(
+#         "phdi.tabulation", "validation_schema.json"
+#     ) as file:
+#         validation_schema = json.load(file)
+
+#     validate(schema=validation_schema, instance=config)
+
+
+def validate_error_types(error_types: str) -> str:
+    """
+    Given a string of comma separated of error types ensure they are valid.
+    If they aren't, remove them from the string.
+
+    :param error_types: A comma separated string of error types.
+    :return: A valid comma separate list of error types in a string.
+    """
+    if error_types is None or error_types == "":
+        return ""
+
+    validated_error_types = []
+
+    for et in error_types.split(","):
+        if et in VALID_ERROR_TYPES:
+            validated_error_types.append(et)
+
+    return ",".join(validated_error_types)
---
+++ phdi/validation/__init__.py
@@ -0,0 +1,15 @@
+from .validation import (
+    validate_ecr,
+    _field_matches,
+    _validate_attribute,
+    _validate_text,
+    _organize_messages,
+)
+
+__all__ = [
+    "validate_ecr",
+    "_field_matches",
+    "_validate_attribute",
+    "_validate_text",
+    "_organize_messages",
+]
