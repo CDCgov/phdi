e6075b75c2a346d0b4ad2eff2b5e5b8cdee46fa7
Brady Fausett at Skylight <brady@skylight.digital>
2023-03-10 14:54:28 -0700

    Fix container test

--- containers/validation/tests/test_validation.py
+++ containers/validation/tests/test_validation.py
@@ -86,9 +86,9 @@ def test_validate_ecr_invalid():
                 "Could not find field. Field name: 'eICR Version Number' Attributes:"
                 + " name: 'value'",
                 "Could not find field. Field name: 'First Name' Parent element: 'name'"
-                + " Parent attributes name: 'use' RegEx: 'L'",
+                + " Parent attributes: name: 'use' RegEx: 'L'",
                 "Could not find field. Field name: 'City' Parent element: 'addr' Parent"
-                + " attributes name: 'use' RegEx: 'H'",
+                + " attributes: name: 'use' RegEx: 'H'",
                 "Field does not match regEx: [0-9]{5}(?:-[0-9]{4})?. Field name:"
                 + " 'Zip' value: '9999'",
             ],
@@ -112,6 +112,8 @@ def test_validate_ecr_invalid():
     actual_result3 = validate_ecr_msg(
         message=sample_file_bad, include_error_types=test_error_types
     )
+    print(actual_result3["validation_results"])
+    print(expected_result3["validation_results"])
     assert actual_result3 == expected_result3
 
 
