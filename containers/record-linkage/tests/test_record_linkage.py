a8ea7ebbbc35507885ec305e61e36f3ec60fba1d
Gordon Farrell <gordon@skylight.digital>
2023-03-16 10:59:56 -0600

    test for health check endpoint

---
+++ containers/record-linkage/tests/test_record_linkage.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_health_check():
+    actual_response = client.get("/")
+    assert actual_response.status_code == 200
+    assert actual_response.json() == {"status": "OK"}
