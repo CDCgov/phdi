443c2dfcc8c4096bf04c2d3be982c6def91457f5
emmastephenson <emma.stephenson.24@gmail.com>
2023-04-06 09:48:30 -0700

    good change

--- phdi/linkage/postgres.py
+++ phdi/linkage/postgres.py
@@ -4,6 +4,7 @@ import psycopg2
 import json
 
 
+# test change
 class DIBBsConnectorClient(BaseMPIConnectorClient):
     """
     Represents a Postgres-specific Master Patient Index (MPI) connector client for the
