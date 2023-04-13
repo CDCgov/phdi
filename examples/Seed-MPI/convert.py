0ebdb60c33289a4271c99207ac100052ac656ec9
m-goggins <marcelle.goggins@gmail.com>
2023-04-13 13:43:25 -0700

    add script for Synapse job

---
+++ examples/Seed-MPI/convert.py
@@ -0,0 +1,17 @@
+# This file converts a parquet file of patient data into tuples. Each row of patient
+# data is returned a tuple of (patient_id, patient_resource)
+import sys
+from phdi.linkage.seed import convert_to_patient_fhir_resources
+import pyarrow.parquet as pq
+
+
+def convert():
+    file_path = sys.argv[1]
+    parquet_file = pq.ParquetFile(file_path)
+    for row in parquet_file.iter_batches(batch_size=1):
+        data = row.to_pylist()[0]
+        patient_id, patient_resource = convert_to_patient_fhir_resources(data)
+
+
+if __name__ == "__main__":
+    convert()
