3fc5d9e82cd59d8529e72cfc50562f2cc2ccc244
m-goggins <marcelle.goggins@gmail.com>
2023-04-17 15:01:43 -0700

    update script for Synapse

--- examples/Seed-MPI/convert.py
+++ examples/Seed-MPI/convert.py
@@ -1,20 +1,35 @@
 # This file converts a parquet file of patient data into tuples. Each row of patient
 # data is returned a tuple of (iris_id, fhir_bundle)
-import sys
+
 from phdi.linkage.seed import convert_to_patient_fhir_resources
 import pyarrow.parquet as pq
+import pyarrow as pa
 
 
-file_path = "C://Users/Marcelle/Desktop/synthetic_patient_mpi_seed_data.parquet"
-
-
-def convert():
-    file_path = sys.argv[1]
-    parquet_file = pq.ParquetFile(file_path)
+def convert(bytes):
+    buffer = pa.BufferReader(bytes)
+    parquet_file = pq.ParquetFile(buffer)
+    converted_data = {}
     for row in parquet_file.iter_batches(batch_size=1):
         data = row.to_pylist()[0]
         iris_id, fhir_bundle = convert_to_patient_fhir_resources(data)
+        converted_data[iris_id] = fhir_bundle
+
+    return converted_data
 
 
-if __name__ == "__main__":
-    convert()
+# Uncomment for Synapse script
+# def read_file(file_path):
+#     file_client = DataLakeFileClient(
+#         account_url=f"https://{account_name}.dfs.core.windows.net",
+#         file_system_name=file_system_name,
+#         file_path=file_path,
+#         credential=credential,
+#     )
+#     download = file_client.download_file()
+#     bytes = download.readall()
+#     return bytes
+
+
+# bytes_data = read_file(file_path)
+# converted_data = convert(bytes_data)
