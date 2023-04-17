ab208ba4ad75c3feed2e608a2b63994886ec8363
m-goggins <53578688+m-goggins@users.noreply.github.com>
2023-04-17 10:23:21 -0700

    Convert parquet to fhir (#425)
    * script for building synthetic MPI data
    
    * add seed
    
    * funcs for converting parquet to fhir
    
    * refactored for 1 row & output tuple
    
    * add script for Synapse job
    
    * returns iris_id, NOT patient_id
    
    * update to return a fhir bundle, not patient resource

---
+++ tests/linkage/test_seed.py
@@ -0,0 +1,22 @@
+from phdi.linkage.seed import convert_to_patient_fhir_resources
+import pathlib
+import pyarrow.parquet as pq
+
+mpi_test_file_path = (
+    pathlib.Path(__file__).parent.parent.parent
+    / "tests"
+    / "assets"
+    / "synthetic_patient_mpi_seed_data.gzip"
+)
+
+
+def test_convert_to_patient_fhir_resources():
+    parquet_file = pq.ParquetFile(mpi_test_file_path)
+    for row in parquet_file.iter_batches(batch_size=1):
+        data = row.to_pylist()[0]
+        (iris_id, returned_fhir_bundle) = convert_to_patient_fhir_resources(data)
+        assert type(returned_fhir_bundle) == dict
+        assert returned_fhir_bundle["resourceType"] == "Bundle"
+        assert returned_fhir_bundle["entry"][0]["resourceType"] == "Patient"
+        assert returned_fhir_bundle["id"] != ""
+        assert iris_id == data["iris_id"]
