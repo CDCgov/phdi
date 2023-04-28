e48ee49380f6dad99e4dcbf2382ed725d7ae6eb2
Gordon Farrell <gordon@skylight.digital>
2023-04-28 12:47:56 -0600

    Move migration function call to its own endpoint for use in deployment workflow

--- containers/record-linkage/app/main.py
+++ containers/record-linkage/app/main.py
@@ -143,12 +143,22 @@ async def health_check() -> HealthCheckResponse:
     linkage service is available and running properly. The mpi_connection_status field
     contains a description of the connection health to the MPI database.
     """
-    run_migrations()
+
     try:
         connect_to_mpi_with_env_vars()
     except Exception as err:
         return {"status": "OK", "mpi_connection_status": str(err)}
     return {"status": "OK", "mpi_connection_status": "OK"}
+
+
+@app.get("/run-mpi-migrations")
+async def run_mpi_migrations() -> HealthCheckResponse:
+
+    try:
+        run_migrations()
+    except Exception as err:
+        return {"status": "OK", "Error": str(err)}
+    return {"status": "OK"}
 
 
 @app.post("/link-record", status_code=200)
@@ -166,7 +176,6 @@ async def link_record(input: LinkRecordInput, response: Response) -> LinkRecordR
 
     input = dict(input)
     input_bundle = input.get("bundle", {})
-    run_migrations()
 
     # Check that DB type is appropriately set up as Postgres so
     # we can fail fast if it's not
