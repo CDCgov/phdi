e9fe0c22e8d26f2f7b749a4aaf0eaa2bed2a3a11
robertmitchellv <robert.beaty.mitchell@gmail.com>
2023-05-02 14:01:54 -0700

    Added optional license argument with default value for BaseService; added a test for passing license dict to FastAPI

--- tests/containers/test_base_service.py
+++ tests/containers/test_base_service.py
@@ -2,25 +2,48 @@ from phdi.containers.base_service import BaseService
 from fastapi.testclient import TestClient
 from pathlib import Path
 
+default_app_version = "0.0.1"
+default_app_contact = {
+    "name": "CDC Public Health Data Infrastructure",
+    "url": "https://cdcgov.github.io/phdi-site/",
+    "email": "dmibuildingblocks@cdc.gov",
+}
+default_app_license = {
+    "name": "Creative Commons Zero v1.0 Universal",
+    "url": "https://creativecommons.org/publicdomain/zero/1.0/",
+}
+alternate_app_license = {"name": "The MIT License", "url": "https://mit-license.org/"}
+
 
 def test_base_service():
     service = BaseService(
-        "test_service", Path(__file__).parent.parent / "assets" / "test_description.md"
+        service_name="test_service",
+        description_path=Path(__file__).parent.parent
+        / "assets"
+        / "test_description.md",
     )
     assert service.app.title == "test_service"
-    assert service.app.version == "0.0.1"
-    assert service.app.contact == {
-        "name": "CDC Public Health Data Infrastructure",
-        "url": "https://cdcgov.github.io/phdi-site/",
-        "email": "dmibuildingblocks@cdc.gov",
-    }
-    assert service.app.license_info == {
-        "name": "Creative Commons Zero v1.0 Universal",
-        "url": "https://creativecommons.org/publicdomain/zero/1.0/",
-    }
+    assert service.app.version == default_app_version
+    assert service.app.contact == default_app_contact
+    assert service.app.license_info == default_app_license
     assert service.app.description == "This is a test description."
 
     client = TestClient(service.start())
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
+
+
+def test_base_service_alternate_license():
+    service = BaseService(
+        service_name="test_service",
+        description_path=Path(__file__).parent.parent
+        / "assets"
+        / "test_description.md",
+        license_info=alternate_app_license,
+    )
+    assert service.app.title == "test_service"
+    assert service.app.version == "0.0.1"
+    assert service.app.contact == default_app_contact
+    assert service.app.license_info == alternate_app_license
+    assert service.app.description == "This is a test description."
