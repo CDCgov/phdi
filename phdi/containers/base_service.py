1645ff63ee2083e71cea94b12805acaa6d2bed6d
DanPaseltiner <dan@skylight.digital>
2023-05-12 16:58:24 -0400

    Update unit testing and black

--- phdi/containers/base_service.py
+++ phdi/containers/base_service.py
@@ -7,18 +7,20 @@ from typing import Literal
 
 # create a class with the DIBBs default Creative Commons Zero v1.0 and
 # MIT license to be used by the BaseService class
-LICENSES = {"CreativeCommonsZero":{
+LICENSES = {
+    "CreativeCommonsZero": {
         "name": "Creative Commons Zero v1.0 Universal",
         "url": "https://creativecommons.org/publicdomain/zero/1.0/",
     },
-    "MIT": {"name": "The MIT License", "url": "https://mit-license.org/"}
+    "MIT": {"name": "The MIT License", "url": "https://mit-license.org/"},
 }
 
 DIBBS_CONTACT = {
-        "name": "CDC Public Health Data Infrastructure",
-        "url": "https://cdcgov.github.io/phdi-site/",
-        "email": "dmibuildingblocks@cdc.gov",
-    }
+    "name": "CDC Public Health Data Infrastructure",
+    "url": "https://cdcgov.github.io/phdi-site/",
+    "email": "dmibuildingblocks@cdc.gov",
+}
+
 
 class BaseService:
     def __init__(
--- tests/containers/test_base_service.py
+++ tests/containers/test_base_service.py
@@ -1,12 +1,9 @@
-from phdi.containers.base_service import LicenseType, BaseService
+from phdi.containers.base_service import LICENSES, DIBBS_CONTACT, BaseService
 from fastapi.testclient import TestClient
 from pathlib import Path
 from importlib import metadata
 
 default_app_version = metadata.version("phdi")
-default_app_contact = BaseService.DIBBS_CONTACT
-default_app_license = LicenseType.CreativeCommonsZero
-alternate_app_license = LicenseType.MIT
 
 
 def test_base_service():
@@ -16,14 +13,17 @@ def test_base_service():
     )
     assert service.app.title == "test_service"
     assert service.app.version == default_app_version
-    assert service.app.contact == default_app_contact
-    assert service.app.license_info == default_app_license
+    assert service.app.contact == DIBBS_CONTACT
+    assert service.app.license_info == LICENSES["CreativeCommonsZero"]
     assert service.app.description == "This is a test description."
 
     client = TestClient(service.start())
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
+
+    response = client.get("/redoc")
+    assert response.status_code == 200
 
 
 def test_base_service_alternate_license():
@@ -33,10 +33,18 @@ def test_base_service_alternate_license():
         / "assets"
         / "containers"
         / "test_description.md",
-        license_info=alternate_app_license,
+        license_info="MIT",
     )
     assert service.app.title == "test_service"
     assert service.app.version == default_app_version
-    assert service.app.contact == default_app_contact
-    assert service.app.license_info == alternate_app_license
+    assert service.app.contact == DIBBS_CONTACT
+    assert service.app.license_info == LICENSES["MIT"]
     assert service.app.description == "This is a test description."
+
+    client = TestClient(service.start())
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK"}
+
+    response = client.get("/redoc")
+    assert response.status_code == 200
