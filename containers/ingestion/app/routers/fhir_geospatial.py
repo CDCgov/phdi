4bf19b35f5aa52e29c987c7a396169612f6a5bf3
emmastephenson <80282552+emmastephenson@users.noreply.github.com>
2023-05-15 08:01:52 -0700

    Update geocoding docs (#574)
    Add sample requests and responses; tweak param descriptions.

--- containers/ingestion/app/routers/fhir_geospatial.py
+++ containers/ingestion/app/routers/fhir_geospatial.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Response, status
+from typing import Annotated
+from fastapi import APIRouter, Response, status, Body
 from pydantic import BaseModel, validator, Field
 from typing import Optional, Literal
 from phdi.fhir.geospatial import SmartyFhirGeocodeClient, CensusFhirGeocodeClient
@@ -7,14 +8,13 @@ from app.utils import (
     search_for_required_values,
     check_for_fhir_bundle,
     StandardResponse,
+    read_json_from_assets,
 )
-
 
 router = APIRouter(
     prefix="/fhir/geospatial/geocode",
     tags=["fhir/geospatial"],
 )
-
 
 license_types = Literal[
     "us-standard-cloud",
@@ -25,27 +25,39 @@ license_types = Literal[
     "international-global-plus-cloud",
 ]
 
+# Sample request/response for the geocode endpoint
+geocode_request_examples = read_json_from_assets("sample_geocode_request_data.json")
+
+raw_geocode_response_data = read_json_from_assets("sample_geocode_responses.json")
+
+sample_geocode_response = {200: raw_geocode_response_data}
+
 
 class GeocodeAddressInBundleInput(BaseModel):
-    bundle: dict = Field(description="A FHIR bundle")
+    bundle: dict = Field(description="A FHIR resource or bundle in JSON format.")
     geocode_method: Literal["smarty", "census"] = Field(
         description="The geocoding service to be used."
     )
     auth_id: Optional[str] = Field(
         description="Authentication ID for the geocoding service. Must be provided in "
-        "the request body or set as an environment variable of the service if "
-        "'geocode_method' is 'smarty'.",
+        "the request body or set as an environment variable of the "
+        "service if "
+        "`geocode_method` is `smarty`.",
         default="",
     )
     auth_token: Optional[str] = Field(
         description="Authentication Token for the geocoding service. Must be provided "
-        "in the request body or set as an environment variable of the service if "
-        "'geocode_method' is 'smarty'.",
+        "in the request body or set as an environment variable of the "
+        "service if "
+        "`geocode_method` is `smarty`.",
         default="",
     )
     license_type: Optional[license_types] = Field(
-        description="The license type for the geocoding service.",
-        default="",
+        description="License type for the geocoding service. Must be provided "
+        "in the request body or set as an environment variable of the "
+        "service if "
+        "`geocode_method` is `smarty`.",
+        default="us-rooftop-geocoding-enterprise-cloud",
     )
     overwrite: Optional[bool] = Field(
         description="If true, `data` is modified in-place; if false, a copy of `data` "
@@ -56,26 +68,25 @@ class GeocodeAddressInBundleInput(BaseModel):
     _check_for_fhir = validator("bundle", allow_reuse=True)(check_for_fhir_bundle)
 
 
-@router.post("/geocode_bundle", status_code=200)
+@router.post("/geocode_bundle", status_code=200, responses=sample_geocode_response)
 def geocode_bundle_endpoint(
-    input: GeocodeAddressInBundleInput, response: Response
+    input: Annotated[
+        GeocodeAddressInBundleInput, Body(examples=geocode_request_examples)
+    ],
+    response: Response,
 ) -> StandardResponse:
     """
-    Given a FHIR bundle and a specified geocode method, with any required
-    subsequent credentials (ie.. SmartyStreets auth id and auth token),
-    geocode all patient addresses across all patient resources in the bundle.
+    Given a FHIR bundle and a specified geocode method, geocode all patient addresses
+    across all patient resources in the bundle.
 
-    If the geocode method is smarty then the auth_id and auth_token parameter
-    values will be used.  If they are not provided in the request then the values
-    will be obtained via environment variables.  In the case where smarty is the geocode
-    method and auth_id and/or auth_token are not supplied then an HTTP 400 status
-    code will be returned.
-    :param input: A JSON formated request body with schema specified by the
-        GeocodeAddressInBundleInput model.
-    :return: A FHIR bundle where every patient resource address will now
-    contain a geocoded value.
+    Two geocode methods are currently supported - Smarty and the U.S. Census.
+
+    If using the Smarty provider, an auth_id, auth_token and license_type must be
+    provided. If they are not provided as request parameters, then the service will
+    attempt to obtain them through environment variables. If they cannot be found in
+    either the request parameters or environment variables, an HTTP 400 status will be
+    returned.
     """
-
     input = dict(input)
 
     if input.get("geocode_method") == "smarty":
