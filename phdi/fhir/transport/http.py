0c3790747bc04f63924086831205ae82a119d462
Brady Fausett at Skylight <brady@skylight.digital>
2023-05-26 14:51:30 -0600

    Update phdi/fhir/transport/http.py
    Co-authored-by: Daniel Paseltiner <99684231+DanPaseltiner@users.noreply.github.com>

--- phdi/fhir/transport/http.py
+++ phdi/fhir/transport/http.py
@@ -115,9 +115,7 @@ def upload_bundle_to_fhir_server(
 
                 # FHIR bundle.entry.response.status is string type - integer status code
                 # plus may inlude a message
-                if entry_response and not entry_response.get("status", "").startswith(
-                    "200"
-                ):
+                if entry_response and entry_response.get("status", "") not in ["200 OK", "201 Created", "200", "201"]:
                     _log_fhir_server_error(
                         status_code=int(entry_response["status"][0:3]),
                         batch_entry_index=entry_index,
