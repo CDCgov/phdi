2b3d695bc05085c6db204277c26a756d999f69a6
nickbristow <richard.nick.bristow@gmail.com>
2023-06-09 17:16:09 -0400

    test

--- containers/ingestion/tests/test_config.py
+++ containers/ingestion/tests/test_config.py
@@ -8,8 +8,8 @@ def test_get_settings_success():
     os.environ["CRED_MANAGER"] = "azure"
     os.environ["FHIR_URL"] = "some-FHIR-server-URL"
     os.environ["SALT_STR"] = "my-salt"
-    os.environ["AUTH_ID"] = "test_id"
-    os.environ["AUTH_TOKEN"] = "test_token"
+    os.environ["SMARTY_AUTH_ID"] = "test_id"
+    os.environ["SMARTY_AUTH_TOKEN"] = "test_token"
     os.environ["LICENSE_TYPE"] = "us-standard-cloud"
     os.environ["CLOUD_PROVIDER"] = "azure"
     os.environ["BUCKET_NAME"] = "my_bucket"
@@ -19,8 +19,8 @@ def test_get_settings_success():
         "cred_manager": "azure",
         "fhir_url": "some-FHIR-server-URL",
         "salt_str": "my-salt",
-        "auth_id": "test_id",
-        "auth_token": "test_token",
+        "smarty_auth_id": "test_id",
+        "smarty_auth_token": "test_token",
         "license_type": "us-standard-cloud",
         "cloud_provider": "azure",
         "bucket_name": "my_bucket",
@@ -28,8 +28,8 @@ def test_get_settings_success():
     }
     os.environ.pop("CRED_MANAGER", None)
     os.environ.pop("CLOUD_PROVIDER", None)
-    os.environ.pop("AUTH_ID", None)
-    os.environ.pop("AUTH_TOKEN", None)
+    os.environ.pop("SMARTY_AUTH_ID", None)
+    os.environ.pop("SMARTY_AUTH_TOKEN", None)
     os.environ.pop("LICENSE_TYPE", None)
     os.environ.pop("BUCKET_NAME", None)
     os.environ.pop("STORAGE_ACCOUNT_URL", None)
