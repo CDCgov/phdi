c79be16e25852efaa8a9cf2043e9f58bae2062f8
Brady Fausett at Skylight <brady@skylight.digital>
2023-07-28 11:28:38 -0600

    Add Additional ECR Fields into the ECR to FHIR Conversion (#728)
    * Initial changes/updates to templates to include new fields in ecr conversion process
    
    * Additional changes
    
    * Added Pregnancy Section with observations
    
    * Update to sample ecr messages
    
    * Fixed test
    
    * Update test_fhir_transport.py
    
    * Added missing communication preference to test files
    
    * Fixed issue with missing work/industry related observations in the test files
    
    * Fixed issue with missing work/industry related observations in the test files
    
    * Update test_fhir_transport.py
    
    * Update test_fhir_transport.py
    
    * Update test_fhir_transport.py fixed one last broken test
    
    * Fixed observation status from active to final
    
    * added all the relevant FHIR Paths
    
    * Fixed issue with duplicating looping through social history more than once and include all proper patient extensions
    
    * Fixed duplicate IF statement in all OBservation resources

--- tests/fhir/transport/test_fhir_transport.py
+++ tests/fhir/transport/test_fhir_transport.py
@@ -509,7 +509,7 @@ def test_split_bundle_resources():
     )
 
     my_count = len(_split_bundle_resources(bundle=bundle)[0].get("entry"))
-    assert my_count == 45
+    assert my_count == 52
 
     single_resource = bundle.get("entry")[0]
     # add 500 resources to bundle and then pass to function
@@ -517,27 +517,27 @@ def test_split_bundle_resources():
         bundle["entry"].append(single_resource)
 
     my_count = len(bundle.get("entry"))
-    assert my_count == 545
+    assert my_count == 552
 
     split_bundles = _split_bundle_resources(bundle=bundle)
     assert len(split_bundles) == 2
 
     assert len(split_bundles[0].get("entry")) == 500
-    assert len(split_bundles[1].get("entry")) == 45
+    assert len(split_bundles[1].get("entry")) == 52
 
     # add an additional 500 resources to bundle and then pass to function
     for x in range(500):
         bundle["entry"].append(single_resource)
 
     my_count = len(bundle.get("entry"))
-    assert my_count == 1045
+    assert my_count == 1052
 
     split_bundles = _split_bundle_resources(bundle=bundle)
     assert len(split_bundles) == 3
 
     assert len(split_bundles[0].get("entry")) == 500
     assert len(split_bundles[1].get("entry")) == 500
-    assert len(split_bundles[2].get("entry")) == 45
+    assert len(split_bundles[2].get("entry")) == 52
 
     bundle["entry"] = []
     split_bundles = _split_bundle_resources(bundle=bundle)
