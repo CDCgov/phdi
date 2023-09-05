8849bfc4adc2a83061455e569206a64a3a0f2bba
emmastephenson <emma.stephenson.24@gmail.com>
2023-09-05 16:44:22 -0700

    wip - dotnet issues

--- containers/fhir-converter/app/service.py
+++ containers/fhir-converter/app/service.py
@@ -59,7 +59,10 @@ def convert_to_fhir(
     :param root_template: Name of the liquid template within to be used for conversion.
         Options are listed in the FHIR-Converter README.md.
     """
-
+    print("made it into conversion")
+    print(input_data)
+    print(input_type)
+    print(root_template)
     # Setup path variables
     converter_project_path = (
         "/build/FHIR-Converter/output/Microsoft.Health.Fhir.Liquid.Converter.Tool.dll"
