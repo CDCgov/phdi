e7bfd97f4fe662ccffcfe3b7e99054bf645f0152
Nick Clyde <nick@clyde.tech>
2023-09-08 14:26:09 -0700

    Add stubbed HTML Insights Building Block

---
+++ containers/html-insights/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+app.use(express.json())
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.json({ status: "OK" });
+});
+
+app.post('/generate-html', (req, res) => {
+    const { data } = req.body
+    res.render('index', { data }, function (err, html) {
+        if (err) {
+            res.status(500).json({ error: err });
+        } else {
+            res.send(html);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`HTML Insights app listening on port ${port}`);
+});
