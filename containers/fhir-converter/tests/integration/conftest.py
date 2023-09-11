89c0864dfa77572dd75b50d66fd9d7df43a97118
emmastephenson <emma.stephenson.24@gmail.com>
2023-09-11 15:35:08 -0700

    add integration tests for FHIR converter

---
+++ containers/fhir-converter/tests/integration/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+from testcontainers.compose import DockerCompose
+
+
+@pytest.fixture(scope="session")
+def setup(request):
+    print("Setting up tests...")
+    compose_path = os.path.join(os.path.dirname(__file__), "./")
+    compose_file_name = "docker-compose.yaml"
+    print(compose_path)
+    fhir_converter = DockerCompose(compose_path, compose_file_name=compose_file_name)
+    converter_url = "http://0.0.0.0:8080"
+
+    fhir_converter.start()
+    fhir_converter.wait_for(converter_url)
+    print("FHIR Converter ready to test!")
+
+    def teardown():
+        print("Tests finished! Tearing down.")
+        fhir_converter.stop()
+
+    request.addfinalizer(teardown)
