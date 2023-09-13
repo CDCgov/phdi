31a9460166c485c6435a559149913fb722527e83
emmastephenson <emma.stephenson.24@gmail.com>
2023-09-13 12:13:46 -0700

    clean up

--- containers/fhir-converter/tests/test_FHIR-Converter.py
+++ containers/fhir-converter/tests/test_FHIR-Converter.py
@@ -225,8 +225,6 @@ def test_convert_conversion_failure(
     )
     assert actual_response.status_code == 400
     assert actual_response.json().get("response") == conversion_failure_response
-    # assert "blue" == "red"
-    # let's make sure a failure here stops integration tests from running
 
 
 @mock.patch("app.service.subprocess.run")
