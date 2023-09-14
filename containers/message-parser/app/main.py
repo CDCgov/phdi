4c5cf2dc5fdb549784eccb561af15c3b6b735db0
Brady Fausett at Skylight <brady@skylight.digital>
2023-09-14 16:46:56 -0600

    fixed issue with message parser out-puting "" instead of null

--- containers/message-parser/app/main.py
+++ containers/message-parser/app/main.py
@@ -179,8 +179,8 @@ async def parse_message_endpoint(
     for field, parser in parsers.items():
         if "secondary_parsers" not in parser:
             value = parser["primary_parser"](input.message)
-            if value is None or value == "":
-                value = "null,"
+            if len(value) == 0:
+                value = None
             else:
                 value = ",".join(value)
             parsed_values[field] = value
@@ -188,13 +188,16 @@ async def parse_message_endpoint(
             inital_values = parser["primary_parser"](input.message)
             values = []
             for initial_value in inital_values:
-                if initial_value is None or initial_value == "":
-                    initial_value = "null,"
                 value = {}
                 for secondary_field, secondary_parser in parser[
                     "secondary_parsers"
                 ].items():
-                    value[secondary_field] = ",".join(secondary_parser(initial_value))
+                    if len(secondary_parser(initial_value)) == 0:
+                        value[secondary_field] = None
+                    else:
+                        value[secondary_field] = ",".join(
+                            secondary_parser(initial_value)
+                        )
                 values.append(value)
             parsed_values[field] = values
 
