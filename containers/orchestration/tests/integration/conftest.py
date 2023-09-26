9aace7a1ca2052735bee5cbc48e51a57dabec0eb
emmastephenson <emma.stephenson.24@gmail.com>
2023-09-26 15:38:53 -0700

    updated docker-compose back to a single file

--- containers/orchestration/tests/integration/conftest.py
+++ containers/orchestration/tests/integration/conftest.py
@@ -8,20 +8,20 @@ def setup(request):
     print("Setting up tests...")
     compose_path = os.path.join(os.path.dirname(__file__), "./")
     compose_file_name = "docker-compose.yaml"
-    compose_validation_file_name = "docker-compose-validation.yaml"
+    # compose_validation_file_name = "docker-compose-validation.yaml"
     orchestration_service = DockerCompose(
         compose_path, compose_file_name=compose_file_name
     )
-    validation_service = DockerCompose(
-        compose_path, compose_file_name=compose_validation_file_name
-    )
+    # validation_service = DockerCompose(
+    #     compose_path, compose_file_name=compose_validation_file_name
+    # )
 
     orchestration_url = "http://0.0.0.0:8080"
-    validation_url = "http://0.0.0.0:8081"
+    # validation_url = "http://0.0.0.0:8081"
 
-    validation_service.start()
+    # validation_service.start()
     orchestration_service.start()
-    validation_service.wait_for(validation_url)
+    # validation_service.wait_for(validation_url)
     orchestration_service.wait_for(orchestration_url)
     print("Orchestration etc. services ready to test!")
 
@@ -30,6 +30,6 @@ def setup(request):
         print(orchestration_service.get_logs())
         print("Tests finished! Tearing down.")
         orchestration_service.stop()
-        validation_service.stop()
+        # validation_service.stop()
 
     request.addfinalizer(teardown)
