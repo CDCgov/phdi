2f9509dbcb77efc27d219643edc8912916eaa6ef
emmastephenson <80282552+emmastephenson@users.noreply.github.com>
2023-09-29 10:04:29 -0700

    Add exception handling for XML parsing to FHIR Converter (#839)

--- containers/fhir-converter/app/main.py
+++ containers/fhir-converter/app/main.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-from fastapi import FastAPI, Response, status
-
+from fastapi import FastAPI, Response, status, HTTPException
 from phdi.fhir.conversion import add_rr_data_to_eicr
 from app.constants import (
     sample_response,
     FhirConverterInput,
 )
 from app.service import convert_to_fhir
+from lxml.etree import XMLSyntaxError
 
 description = (Path(__file__).parent.parent / "description.md").read_text(
     encoding="utf-8"
@@ -67,8 +67,15 @@ async def convert(input: FhirConverterInput, response: Response):
             }
             return result
 
-        merged_ecr = add_rr_data_to_eicr(input.rr_data, input.input_data)
-        fhir_converter_input.update({"input_data": merged_ecr})
+        try:
+            merged_ecr = add_rr_data_to_eicr(input.rr_data, input.input_data)
+            fhir_converter_input.update({"input_data": merged_ecr})
+        except XMLSyntaxError:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="Reportability Response and eICR message both "
+                "must be valid XML messages.",
+            )
 
     result = convert_to_fhir(**fhir_converter_input)
     if "fhir_conversion_failed" in result.get("response"):
