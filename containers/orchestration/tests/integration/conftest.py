197214bcb07aa06dda91b44623579173f3ba120e
Gordon Farrell <gordon@skylight.digital>
2023-10-02 14:56:08 -0600

    wip

--- containers/orchestration/tests/integration/conftest.py
+++ containers/orchestration/tests/integration/conftest.py
@@ -22,6 +22,6 @@ def setup(request):
         print("\nContainer output: ")
         print(orchestration_service.get_logs())
         print("Tests finished! Tearing down.")
-        orchestration_service.stop()
+        # orchestration_service.stop()
 
     request.addfinalizer(teardown)
