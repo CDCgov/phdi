c735fef0b1cd55f2b5290f866d2802f44affb8c5
emmastephenson <emma.stephenson.24@gmail.com>
2023-10-03 13:23:36 -0700

    add the rest of the tests back

--- containers/orchestration/tests/integration/test_orchestration.py
+++ containers/orchestration/tests/integration/test_orchestration.py
@@ -1,7 +1,6 @@
 import httpx
 import pytest
-
-# from pathlib import Path
+from pathlib import Path
 
 
 ORCHESTRATION_URL = "http://localhost:8080"
@@ -30,43 +29,43 @@ def test_health_check(setup):
     assert message_parser_response.status_code == 200
 
 
-# @pytest.mark.integration
-# def test_process_endpoint_with_message(setup):
-#     message = open(
-#         Path(__file__).parent.parent.parent.parent.parent
-#         / "tests"
-#         / "assets"
-#         / "orchestration"
-#         / "CDA_eICR.xml"
-#     ).read()
-#     request = {
-#         "message_type": "ecr",
-#         "include_error_types": "errors",
-#         "message": message,
-#     }
-#     orchestration_response = httpx.post(PROCESS_ENDPOINT, json=request)
-#     print(f"orchestration_response: {orchestration_response}")
-#     assert orchestration_response.status_code == 200
-#     assert orchestration_response.json()["message"] == "Processing succeeded!"
-#
-#
-# @pytest.mark.integration
-# def test_process_endpoint_with_zip(setup):
-#     with open(
-#         Path(__file__).parent.parent.parent.parent.parent
-#         / "tests"
-#         / "assets"
-#         / "orchestration"
-#         / "test_zip.zip",
-#         "rb",
-#     ) as file:
-#         form_data = {
-#             "message_type": "ecr",
-#             "include_error_types": "errors",
-#         }
-#         files = {"upload_file": ("file.zip", file)}
-#         orchestration_response = httpx.post(
-#             PROCESS_ENDPOINT, data=form_data, files=files
-#         )
-#         assert orchestration_response.status_code == 200
-#         assert orchestration_response.json()["message"] == "Processing succeeded!"
+@pytest.mark.integration
+def test_process_endpoint_with_message(setup):
+    message = open(
+        Path(__file__).parent.parent.parent.parent.parent
+        / "tests"
+        / "assets"
+        / "orchestration"
+        / "CDA_eICR.xml"
+    ).read()
+    request = {
+        "message_type": "ecr",
+        "include_error_types": "errors",
+        "message": message,
+    }
+    orchestration_response = httpx.post(PROCESS_ENDPOINT, json=request)
+    print(f"orchestration_response: {orchestration_response}")
+    assert orchestration_response.status_code == 200
+    assert orchestration_response.json()["message"] == "Processing succeeded!"
+
+
+@pytest.mark.integration
+def test_process_endpoint_with_zip(setup):
+    with open(
+        Path(__file__).parent.parent.parent.parent.parent
+        / "tests"
+        / "assets"
+        / "orchestration"
+        / "test_zip.zip",
+        "rb",
+    ) as file:
+        form_data = {
+            "message_type": "ecr",
+            "include_error_types": "errors",
+        }
+        files = {"upload_file": ("file.zip", file)}
+        orchestration_response = httpx.post(
+            PROCESS_ENDPOINT, data=form_data, files=files
+        )
+        assert orchestration_response.status_code == 200
+        assert orchestration_response.json()["message"] == "Processing succeeded!"
