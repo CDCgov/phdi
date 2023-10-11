aa96e7d796c8d818abcefe6f18ad0aff023cb8ab
robertmitchellv <robert.beaty.mitchell@gmail.com>
2023-10-11 14:46:19 -0700

    added a param to include time in the output with tests

--- phdi/linkage/link.py
+++ phdi/linkage/link.py
@@ -29,27 +29,36 @@ LINKING_FIELDS_TO_FHIRPATHS = {
 }
 
 
-def datetime_to_str(input_date: Union[str, date, datetime]) -> str:
+def datetime_to_str(
+    input_date: Union[str, date, datetime], include_time: bool = False
+) -> str:
     """
     Convert a date or datetime object to a string; if a string is provided,
-    check that it follows the 'YYYY-MM-DD' format.
+    check that it follows the appropriate format.
 
     :param input_date: The input date to convert, which can be of type
         datetime.date, datetime.datetime, or str.
-    :return: The formatted date as a string in for format 'YYYY-MM-DD'.
+    :param include_time: Whether to include the time in the output string.
+    :return: The formatted date as a string. If include_time is True, the
+        format is 'YYYY-MM-DD HH:MM:SS', otherwise it's 'YYYY-MM-DD'.
     """
     # if input is str make sure it follows the expected format
     if isinstance(input_date, str):
         try:
-            datetime.strptime(input_date, "%Y-%m-%d")
+            expected_format = "%Y-%m-%d %H:%M:%S" if include_time else "%Y-%m-%d"
+            datetime.strptime(input_date, expected_format)
             return input_date
         except ValueError:
+            format_msg = " 'YYYY-MM-DD HH:MM:SS' " if include_time else " 'YYYY-MM-DD' "
             raise ValueError(
-                f"Input date {input_date} is not in the format 'YYYY-MM-DD'."
+                f"Input date {input_date} is not in the format" + format_msg
             )
     # if input is a date or datetime then convert in the expected format
     elif isinstance(input_date, (date, datetime)):
-        return input_date.strftime("%Y-%m-%d")
+        if include_time:
+            return input_date.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            return input_date.strftime("%Y-%m-%d")
     # if input isn't any of the accepted formats, then return a type error
     else:
         raise TypeError(
--- tests/linkage/test_linkage.py
+++ tests/linkage/test_linkage.py
@@ -146,6 +146,18 @@ def test_valid_datetime_to_str(input_value, expected_output):
     assert datetime_to_str(input_value) == expected_output
 
 
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    [
+        (date(2023, 10, 10), "2023-10-10 00:00:00"),
+        (datetime(2023, 10, 10, 15, 30), "2023-10-10 15:30:00"),
+        ("2023-10-10 15:30:00", "2023-10-10 15:30:00"),
+    ],
+)
+def test_valid_datetime_to_str_with_time(input_value, expected_output):
+    assert datetime_to_str(input_value, include_time=True) == expected_output
+
+
 def test_invalid_datetime_to_str():
     invalid_string = "10-10-2023"
     with pytest.raises(
@@ -162,6 +174,15 @@ def test_unsupported_datetime_to_str():
         match=f"Input date {unsupported_input} is not of type date, datetime, or str.",
     ):
         datetime_to_str(unsupported_input)
+
+
+def test_invalid_datetime_to_str_with_time():
+    invalid_string_with_time = "2023-10-10 15:30"
+    with pytest.raises(
+        ValueError,
+        match=f"Input date {invalid_string_with_time} is not in the format 'YYYY-MM-DD HH:MM:SS'",
+    ):
+        datetime_to_str(invalid_string_with_time, include_time=True)
 
 
 def test_extract_blocking_values_from_record():
