ba6dca9022c9b998b08b5eeb5e137b24b4826238
Robert Mitchell <rbmv@roberts-air.lan>
2023-10-12 22:17:09 -0700

    moved the function to a utils file within phdi.linkage

--- containers/record-linkage/app/utils.py
+++ containers/record-linkage/app/utils.py
@@ -2,8 +2,7 @@ import json
 import logging
 import pathlib
 import subprocess
-from datetime import date, datetime
-from typing import Literal, Union
+from typing import Literal
 
 from app.config import get_settings
 from phdi.linkage import DIBBsConnectorClient
@@ -127,40 +126,3 @@ def run_migrations():
     else:
         logger.error("MPI database schema validations failed.")
         raise Exception(validation_response.stderr.decode("utf-8"))
-
-
-def datetime_to_str(
-    input_date: Union[str, date, datetime], include_time: bool = False
-) -> str:
-    """
-    Convert a date or datetime object to a string; if a string is provided,
-    check that it follows the appropriate format.
-
-    :param input_date: The input date to convert, which can be of type
-        datetime.date, datetime.datetime, or str.
-    :param include_time: Whether to include the time in the output string.
-    :return: The formatted date as a string. If include_time is True, the
-        format is 'YYYY-MM-DD HH:MM:SS', otherwise it's 'YYYY-MM-DD'.
-    """
-    # if input is str make sure it follows the expected format
-    if isinstance(input_date, str):
-        try:
-            expected_format = "%Y-%m-%d %H:%M:%S" if include_time else "%Y-%m-%d"
-            datetime.strptime(input_date, expected_format)
-            return input_date
-        except ValueError:
-            format_msg = " 'YYYY-MM-DD HH:MM:SS' " if include_time else " 'YYYY-MM-DD' "
-            raise ValueError(
-                f"Input date {input_date} is not in the format" + format_msg
-            )
-    # if input is a date or datetime then convert in the expected format
-    elif isinstance(input_date, (date, datetime)):
-        if include_time:
-            return input_date.strftime("%Y-%m-%d %H:%M:%S")
-        else:
-            return input_date.strftime("%Y-%m-%d")
-    # if input isn't any of the accepted formats, then return a type error
-    else:
-        raise TypeError(
-            f"Input date {input_date} is not of type date, datetime, or str."
-        )
---
+++ tests/linkage/test_utils.py
@@ -0,0 +1,60 @@
+import pytest
+from datetime import date, datetime
+
+from phdi.linkage import (
+    datetime_to_str
+)
+
+
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    [
+        (date(2023, 10, 10), "2023-10-10"),
+        (datetime(2023, 10, 10, 15, 30), "2023-10-10"),
+        ("2023-10-10", "2023-10-10"),
+    ],
+)
+def test_valid_datetime_to_str(input_value, expected_output):
+    assert datetime_to_str(input_value) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    [
+        (date(2023, 10, 10), "2023-10-10 00:00:00"),
+        (datetime(2023, 10, 10, 15, 30), "2023-10-10 15:30:00"),
+        ("2023-10-10 15:30:00", "2023-10-10 15:30:00"),
+    ],
+)
+def test_valid_datetime_to_str_with_time(input_value, expected_output):
+    assert datetime_to_str(input_value, include_time=True) == expected_output
+
+
+def test_invalid_datetime_to_str():
+    invalid_string = "10-10-2023"
+    with pytest.raises(
+        ValueError,
+        match=f"Input date {invalid_string} is not in the format 'YYYY-MM-DD'.",
+    ):
+        datetime_to_str(invalid_string)
+
+
+def test_unsupported_datetime_to_str():
+    unsupported_input = 20231010
+    with pytest.raises(
+        TypeError,
+        match=f"Input date {unsupported_input} is not of type date, datetime, or str.",
+    ):
+        datetime_to_str(unsupported_input)
+
+
+def test_invalid_datetime_to_str_with_time():
+    invalid_string_with_time = "2023-10-10 15:30"
+    with pytest.raises(
+        ValueError,
+        match=(
+            f"Input date {invalid_string_with_time} "
+            "is not in the format 'YYYY-MM-DD HH:MM:SS'"
+        ),
+    ):
+        datetime_to_str(invalid_string_with_time, include_time=True)
