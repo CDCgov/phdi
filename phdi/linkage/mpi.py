5c8a140dbd9340123dc7e5f5cd597793d628d7e9
Brady Fausett at Skylight <brady@skylight.digital>
2023-11-03 15:13:43 -0600

    Update mpi.py

--- phdi/linkage/mpi.py
+++ phdi/linkage/mpi.py
@@ -271,7 +271,7 @@ class DIBBsMPIConnectorClient(BaseMPIConnectorClient):
                     print(f"fk_table name {fk_table.name}")
                     print(f"sub query name {sub_query.name}")
                     cte_query = (
-                        select(fk_table).join(
+                        select(fk_table.c.patient_id).join(
                             sub_query,
                             text(
                                 f"{fk_table.name}.{fk_column.name} = "
