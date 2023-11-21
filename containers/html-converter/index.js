7643031008ebe9ea91b57dfbdbdc12de262bcbf6
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-11-21 17:00:31 -0500

    verify file is JSON

--- containers/html-converter/index.js
+++ containers/html-converter/index.js
@@ -22,6 +22,11 @@ app.post('/generate-html', upload.single('file'), (req, res) => {
             if (err) {
                 res.status(500).json({error: err});
             } else {
+                try {
+                    JSON.parse(data.toString());
+                }catch (e) {
+                    res.status(500).json({error: 'Error: File is not JSON'});
+                }
                 res.send(data);
             }
         });
