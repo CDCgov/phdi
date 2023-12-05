ede8f2fb620eeac237604190c68760c23e892016
m-goggins <marcelle.goggins@gmail.com>
2023-12-05 13:09:29 -0500

    drop fk constraint in name

--- containers/record-linkage/migrations/V01_03__add_fk_constraints.sql
+++ containers/record-linkage/migrations/V01_03__add_fk_constraints.sql
@@ -2,36 +2,45 @@ BEGIN;
 
 /* 
 
+REMOVE 1 FOREIGN KEY AT A TIME:
+- name
+
+*/
+ALTER TABLE name DROP IF EXISTS CONSTRAINT fk_name_to_patient;
+
+
+/* 
+
 ADD FOREIGN KEY CONSTRAINTS BACK
 
 */
-ALTER TABLE name ADD CONSTRAINT fk_name_to_patient 
-FOREIGN KEY (patient_id)
-REFERENCES patient(patient_id);
+-- ALTER TABLE name ADD CONSTRAINT fk_name_to_patient 
+-- FOREIGN KEY (patient_id)
+-- REFERENCES patient(patient_id);
 
-ALTER TABLE identifier ADD CONSTRAINT fk_ident_to_patient 
-FOREIGN KEY (patient_id)
-REFERENCES patient(patient_id);
+-- ALTER TABLE identifier ADD CONSTRAINT fk_ident_to_patient 
+-- FOREIGN KEY (patient_id)
+-- REFERENCES patient(patient_id);
 
-ALTER TABLE phone_number ADD CONSTRAINT fk_phone_to_patient 
-FOREIGN KEY (patient_id)
-REFERENCES patient(patient_id);
+-- ALTER TABLE phone_number ADD CONSTRAINT fk_phone_to_patient 
+-- FOREIGN KEY (patient_id)
+-- REFERENCES patient(patient_id);
 
-ALTER TABLE address ADD CONSTRAINT fk_addr_to_patient 
-FOREIGN KEY (patient_id)
-REFERENCES patient(patient_id);
+-- ALTER TABLE address ADD CONSTRAINT fk_addr_to_patient 
+-- FOREIGN KEY (patient_id)
+-- REFERENCES patient(patient_id);
 
-ALTER TABLE given_name ADD CONSTRAINT fk_given_to_name 
-FOREIGN KEY (name_id)
-REFERENCES name(name_id);
+-- ALTER TABLE given_name ADD CONSTRAINT fk_given_to_name 
+-- FOREIGN KEY (name_id)
+-- REFERENCES name(name_id);
 
-ALTER TABLE external_person ADD CONSTRAINT fk_ext_person_to_person 
-FOREIGN KEY (person_id)
-REFERENCES person(person_id);
+-- ALTER TABLE external_person ADD CONSTRAINT fk_ext_person_to_person 
+-- FOREIGN KEY (person_id)
+-- REFERENCES person(person_id);
 
-ALTER TABLE external_person ADD CONSTRAINT fk_ext_person_to_source 
-FOREIGN KEY (external_source_id)
-REFERENCES external_source(external_source_id);
+-- ALTER TABLE external_person ADD CONSTRAINT fk_ext_person_to_source 
+-- FOREIGN KEY (external_source_id)
+-- REFERENCES external_source(external_source_id);
 
 
 
