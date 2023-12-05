247d007ae04199d919df0a778b06e261ba7ba736
m-goggins <marcelle.goggins@gmail.com>
2023-12-05 10:43:01 -0500

    if contraint exists

--- containers/record-linkage/migrations/V01_03__drop_fk_constraints.sql
+++ containers/record-linkage/migrations/V01_03__drop_fk_constraints.sql
@@ -3,18 +3,18 @@ BEGIN;
 /* 
 REMOVE FOREIGN KEY CONSTRAINTS
 */
-ALTER TABLE address DROP CONSTRAINT fk_addr_to_patient;
+ALTER TABLE address DROP CONSTRAINT IF EXISTS fk_addr_to_patient;
 
-ALTER TABLE phone_number DROP CONSTRAINT fk_phone_to_patient;
+ALTER TABLE phone_number DROP CONSTRAINT IF EXISTS fk_phone_to_patient;
 
-ALTER TABLE identifier DROP CONSTRAINT fk_ident_to_patient;
+ALTER TABLE identifier DROP CONSTRAINT IF EXISTS fk_ident_to_patient;
 
-ALTER TABLE name DROP CONSTRAINT fk_name_to_patient;
+ALTER TABLE name DROP CONSTRAINT IF EXISTS fk_name_to_patient;
 
-ALTER TABLE given_name DROP CONSTRAINT fk_given_to_name;
+ALTER TABLE given_name DROP CONSTRAINT IF EXISTS fk_given_to_name;
 
-ALTER TABLE external_person DROP CONSTRAINT fk_ext_person_to_source;
+ALTER TABLE external_person DROP CONSTRAINT IF EXISTS fk_ext_person_to_source;
 
-ALTER TABLE external_person DROP CONSTRAINT fk_ext_person_to_person;
+ALTER TABLE external_person DROP CONSTRAINT IF EXISTS fk_ext_person_to_person;
 
 COMMIT;
