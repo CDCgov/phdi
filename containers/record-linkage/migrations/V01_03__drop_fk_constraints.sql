773c0133a5a472cd9beaac8a16ada861af41cb88
m-goggins <marcelle.goggins@gmail.com>
2023-12-05 10:31:53 -0500

    remove fk constraints

---
+++ containers/record-linkage/migrations/V01_03__drop_fk_constraints.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+/* 
+REMOVE FOREIGN KEY CONSTRAINTS
+*/
+ALTER TABLE address DROP CONSTRAINT fk_addr_to_patient;
+
+ALTER TABLE phone_number DROP CONSTRAINT fk_phone_to_patient;
+
+ALTER TABLE identifier DROP CONSTRAINT fk_ident_to_patient;
+
+ALTER TABLE name DROP CONSTRAINT fk_name_to_patient;
+
+ALTER TABLE given_name CONSTRAINT fk_given_to_name;
+
+ALTER TABLE external_person CONSTRAINT fk_ext_person_to_source;
+
+ALTER TABLE external_person CONSTRAINT fk_ext_person_to_person;
