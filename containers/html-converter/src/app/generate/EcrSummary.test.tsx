dbec7df54a10157d87a243c31c01d1dcfaa17b8c
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-12-11 17:14:58 -0500

    move dependencies to dev dependenices

---
+++ containers/html-converter/src/app/generate/EcrSummary.test.tsx
@@ -0,0 +1,15 @@
+import {render} from "@testing-library/react";
+import EcrSummary from "@/app/generate/EcrSummary";
+import fs from "fs";
+import {Bundle} from "fhir/r4";
+import YAML from 'yaml'
+
+describe("EcrSummary", () => {
+    it("should display properly", () => {
+        const fhirPathFile = fs.readFileSync('./src/app/generate/fhirPath.yml', 'utf8').toString();
+        const fhirPathMappings = YAML.parse(fhirPathFile);
+        const fhirBundle: Bundle = JSON.parse(fs.readFileSync('./src/app/generate/exampleBundle.json', 'utf8').toString());
+
+        const {container} = render(<EcrSummary fhirPathMappings={null} fhirBundle={fhirBundle} />);
+    });
+})
