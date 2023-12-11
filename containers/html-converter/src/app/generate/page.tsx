02f03edefa071b7a4b710359fbc3fbe7d3eb453b
Boban <BobanL@users.noreply.github.com>
2023-12-11 13:39:52 -0500

    rough ecr summary layout (#981)
    * ignore .next folder
    
    * install fhirpath and fhir types
    
    * add fhirPath.yml
    
    * add temporary files to display page
    
    * install uswds
    
    * setup first iteration of summary info box
    
    * rename to ecrSummary
    
    * pull out ecr summary to its own file
    
    * import react
    
    * change quick ecr summary to be h2 to match order
    
    * move fhir bundle and fhir path mappings into page.tsx

---
+++ containers/html-converter/src/app/generate/page.tsx
@@ -0,0 +1,16 @@
+import EcrSummary from "@/app/generate/EcrSummary";
+import fs from "fs";
+import {Bundle} from "fhir/r4";
+import {parse} from "yaml";
+
+const EcrViewer = () =>{
+    const file = fs.readFileSync('./src/app/generate/fhirPath.yml', 'utf8').toString();
+    const fhirBundle: Bundle = JSON.parse(fs.readFileSync('./src/app/generate/exampleBundle.json', 'utf8').toString());
+    const fhirPathMappings = parse(file);
+
+    return <div>
+        <EcrSummary fhirPathMappings={fhirPathMappings} fhirBundle={fhirBundle} />
+    </div>
+}
+
+export default  EcrViewer;
