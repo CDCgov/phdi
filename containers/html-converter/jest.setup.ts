8047f0a0f7e3034a7ed366e48de5e0fc6f3aff10
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-12-12 14:30:14 -0500

    add accessibility test

--- containers/html-converter/jest.setup.ts
+++ containers/html-converter/jest.setup.ts
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom'
 import {toHaveNoViolations} from 'jest-axe'
-import {expect} from "@jest/globals";
 
 expect.extend(toHaveNoViolations);
--- containers/html-converter/src/app/generate/EcrSummary.test.tsx
+++ containers/html-converter/src/app/generate/EcrSummary.test.tsx
@@ -3,15 +3,21 @@ import EcrSummary from "@/app/generate/EcrSummary";
 import fs from "fs";
 import {Bundle} from "fhir/r4";
 import YAML from 'yaml'
-import {expect} from "@jest/globals";
+import {axe} from "jest-axe";
 
 describe("EcrSummary", () => {
-    it("should display properly", () => {
+    let container: HTMLElement
+    beforeAll(() =>{
         const fhirPathFile = fs.readFileSync('./src/app/generate/fhirPath.yml', 'utf8').toString();
         const fhirPathMappings = YAML.parse(fhirPathFile);
         const fhirBundle: Bundle = JSON.parse(fs.readFileSync('./src/app/generate/exampleBundle.json', 'utf8').toString());
 
-        const {container} = render(<EcrSummary fhirPathMappings={fhirPathMappings} fhirBundle={fhirBundle} />);
+         container = render(<EcrSummary fhirPathMappings={fhirPathMappings} fhirBundle={fhirBundle} />).container;
+    })
+    it("should match snapshot", () => {
         expect(container).toMatchSnapshot();
     });
+    it("should pass accessibility test", async () => {
+        expect(await axe(container)).toHaveNoViolations()
+    })
 })
