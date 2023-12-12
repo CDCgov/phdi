84cc7eec2fc62bb2d903284a82d733a57a682277
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-12-12 12:01:52 -0500

    setup jest for unit tests

--- containers/html-converter/src/app/generate/EcrSummary.test.tsx
+++ containers/html-converter/src/app/generate/EcrSummary.test.tsx
@@ -3,6 +3,7 @@ import EcrSummary from "@/app/generate/EcrSummary";
 import fs from "fs";
 import {Bundle} from "fhir/r4";
 import YAML from 'yaml'
+import {expect} from "@jest/globals";
 
 describe("EcrSummary", () => {
     it("should display properly", () => {
@@ -10,6 +11,7 @@ describe("EcrSummary", () => {
         const fhirPathMappings = YAML.parse(fhirPathFile);
         const fhirBundle: Bundle = JSON.parse(fs.readFileSync('./src/app/generate/exampleBundle.json', 'utf8').toString());
 
-        const {container} = render(<EcrSummary fhirPathMappings={null} fhirBundle={fhirBundle} />);
+        const {container} = render(<EcrSummary fhirPathMappings={fhirPathMappings} fhirBundle={fhirBundle} />);
+        expect(container).toMatchSnapshot();
     });
 })
