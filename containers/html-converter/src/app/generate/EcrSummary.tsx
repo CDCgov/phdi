4c2862ebf062f4f903d73ad0578b23b33ac9a528
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-12-12 13:15:25 -0500

    refactor path mappings into type and pull out evaluates into functions

--- containers/html-converter/src/app/generate/EcrSummary.tsx
+++ containers/html-converter/src/app/generate/EcrSummary.tsx
@@ -2,21 +2,45 @@ import {evaluate} from "fhirpath";
 import {Bundle} from "fhir/r4";
 
 interface EcrViewerProps {
-    fhirPathMappings: any
+    fhirPathMappings: PathMappings
     fhirBundle: Bundle
 }
+
+interface PathMappings {
+    [key: string]: string;
+}
+
+const patientAddress = (fhirBundle: Bundle, fhirPathMappings: PathMappings) => {
+    const streetAddresses = evaluate(fhirBundle, fhirPathMappings.patientStreetAddress).join("\n");
+    const city = evaluate(fhirBundle, fhirPathMappings.patientCity);
+    const state = evaluate(fhirBundle, fhirPathMappings.patientState);
+    const zipCode = evaluate(fhirBundle, fhirPathMappings.patientZipCode);
+
+    return(
+    `${streetAddresses}
+    ${city} ${state}
+    ${zipCode}, USA`);
+}
+
+const patientName = (fhirBundle: Bundle, fhirPathMappings: PathMappings) => {
+    const givenNames = evaluate(fhirBundle, fhirPathMappings.patientGivenName).join(" ");
+    const familyName = evaluate(fhirBundle, fhirPathMappings.patientFamilyName);
+
+    return `${givenNames} ${familyName}`;
+}
+
+const patientContactInfo = (fhirBundle: Bundle, fhirPathMappings: PathMappings) => {
+    const phoneNumbers = evaluate(fhirBundle, fhirPathMappings.patientPhoneNumbers).map(phoneNumber => `tel: (${phoneNumber.use}) ${phoneNumber.value}`).join("\n");
+    const emails = evaluate(fhirBundle, fhirPathMappings.patientEmails).map(email => `email: ${email.value}`).join("\n");
+
+    return `${phoneNumbers}
+    ${emails}`
+}
+
 
 const EcrSummary = (
     {fhirPathMappings, fhirBundle}: EcrViewerProps
 ) => {
-    let givenNames = evaluate(fhirBundle, fhirPathMappings.patientGivenName).join(" ");
-    let familyName = evaluate(fhirBundle, fhirPathMappings.patientFamilyName);
-    let streetAddresses = evaluate(fhirBundle, fhirPathMappings.patientStreetAddress).join("\n");
-    let city = evaluate(fhirBundle, fhirPathMappings.patientCity);
-    let state = evaluate(fhirBundle, fhirPathMappings.patientState);
-    let zipCode = evaluate(fhirBundle, fhirPathMappings.patientZipCode);
-    let phoneNumbers = evaluate(fhirBundle, fhirPathMappings.patientPhoneNumbers).map(phoneNumber => `tel: (${phoneNumber.use}) ${phoneNumber.value}`).join("\n");
-    let emails = evaluate(fhirBundle, fhirPathMappings.patientEmails).map(email => `email: ${email.value}`).join("\n");
     return (
         <div>
             <h2>Quick eCR Summary</h2>
@@ -35,25 +59,21 @@ const EcrSummary = (
                         <div className="grid-row">
                             <div className="grid-col-2 text-bold">Patient Name</div>
                             <div className="grid-col-auto">
-                                {givenNames} {familyName}
+                                {patientName(fhirBundle, fhirPathMappings)}
                             </div>
                         </div>
                         <div className={"section__line"} />
                         <div className="grid-row">
                             <div className="grid-col-2 text-bold">Patient Address</div>
                             <div className="grid-col-auto text-pre-line">
-                                {streetAddresses} {'\n'}
-                                {city}, {state} {'\n'}
-                                {zipCode}, USA
+                                {patientAddress(fhirBundle, fhirPathMappings)}
                             </div>
                         </div>
                         <div className={"section__line"}/>
                         <div className="grid-row">
                         <div className="grid-col-2 text-bold">Patient Contact</div>
                             <div className="grid-col-auto text-pre-line">
-                                {phoneNumbers}
-                                {'\n'}
-                                {emails}
+                                {patientContactInfo(fhirBundle, fhirPathMappings)}
                             </div>
                         </div>
                         <div className={"section__line"} />
