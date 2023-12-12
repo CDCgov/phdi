d0248a2ff63aaac1d8c7986e8d5fb24e991c161d
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-12-12 16:16:02 -0500

    setup next lint and prettier

--- containers/html-converter/next.config.js
+++ containers/html-converter/next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 
 const nextConfig = {
-    sassOptions: {
-        includePaths: [
-            path.join(__dirname, "node_modules", "@uswds", "uswds", "packages"),
-        ],
-    },
-    transpilePackages: ['yaml']
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, "node_modules", "@uswds", "uswds", "packages"),
+    ],
+  },
+  transpilePackages: ["yaml"],
+  eslint: {
+    dirs: ["src"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 };
 
 module.exports = nextConfig;
