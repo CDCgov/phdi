95de4347223a866ba5d2d87bf03d63f3edba377e
m-goggins <marcelle.goggins@gmail.com>
2023-12-13 09:17:08 -0800

    add standardize_name and tests to container

--- containers/standardization/tests/test_standardization.py
+++ containers/standardization/tests/test_standardization.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.utils import standardize_name
 
 client = TestClient(app)
 
@@ -11,3 +12,27 @@ def test_health_check():
     assert actual_response.json() == {
         "status": "OK",
     }
+
+
+def test_standardize_name():
+    # Basic case of input string
+    raw_text = " 12 dIBbs is ReaLLy KEWL !@#$ 34"
+    assert (
+        standardize_name(raw_text, trim=True, case="lower", remove_numbers=False)
+        == "12 dibbs is really kewl  34"
+    )
+    assert (
+        standardize_name(raw_text, trim=True, remove_numbers=True, case="title")
+        == "Dibbs Is Really Kewl"
+    )
+    assert (
+        standardize_name(raw_text, trim=False, remove_numbers=True, case="title")
+        == "  Dibbs Is Really Kewl  "
+    )
+    # Now check that it handles list inputs
+    names = ["Johnny T. Walker", " Paul bunYAN", "J;R;R;tOlK.iE87n 999"]
+    assert standardize_name(names, trim=True, remove_numbers=False) == [
+        "JOHNNY T WALKER",
+        "PAUL BUNYAN",
+        "JRRTOLKIE87N 999",
+    ]
