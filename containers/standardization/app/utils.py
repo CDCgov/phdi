c062c115b2ed6742f4a1c2ab06a1c36ebda03519
Marcelle <53578688+m-goggins@users.noreply.github.com>
2023-12-14 09:14:11 -0800

    Add standardize_name and tests to standardization service (#987)
    add standardize_name and tests to container
    
    Co-authored-by: Robert Mitchell <rmitchell@skylight.digital>

--- containers/standardization/app/utils.py
+++ containers/standardization/app/utils.py
@@ -1,6 +1,62 @@
 import json
 import pathlib
+from typing import Literal, List, Union
 
 
 def read_json_from_assets(filename: str):
     return json.load(open((pathlib.Path(__file__).parent.parent / "assets" / filename)))
+
+
+def standardize_name(
+    raw_name: Union[str, List[str]],
+    trim: bool = True,
+    case: Literal["upper", "lower", "title"] = "upper",
+    remove_numbers: bool = True,
+) -> Union[str, List[str]]:
+    """
+    Performs basic standardization (described below) on each given name. Removes
+    punctuation characters and performs a variety of additional cleaning operations.
+    Other options can be toggled on or off using the relevant parameter.
+
+    All options specified will be applied uniformly to each input name,
+    i.e., specifying case = "lower" will make all given names lower case.
+
+    :param raw_name: Either a single string name or a list of strings,
+      each representing a name.
+    :param trim: If true, strips leading/trailing whitespace;
+      if false, retains whitespace. Default: `True`
+    :param case: What case to enforce on each name.
+
+      * `upper`: All upper case
+      * `lower`: All lower case
+      * `title`: Title case
+
+      Default: `upper`
+    :remove_numbers: If true, removes numeric characters from inputs;
+      if false, retains numeric characters. Default `True`
+    :return: Either a string or a list of strings, depending on the
+      input of raw_name, holding the cleaned name(s).
+    """
+    names_to_clean = raw_name
+    if isinstance(raw_name, str):
+        names_to_clean = [raw_name]
+    outputs = []
+
+    for name in names_to_clean:
+        # Remove all punctuation
+        cleaned_name = "".join([ltr for ltr in name if ltr.isalnum() or ltr == " "])
+        if remove_numbers:
+            cleaned_name = "".join([ltr for ltr in cleaned_name if not ltr.isnumeric()])
+        if trim:
+            cleaned_name = cleaned_name.strip()
+        if case == "upper":
+            cleaned_name = cleaned_name.upper()
+        if case == "lower":
+            cleaned_name = cleaned_name.lower()
+        if case == "title":
+            cleaned_name = cleaned_name.title()
+        outputs.append(cleaned_name)
+
+    if isinstance(raw_name, str):
+        return outputs[0]
+    return outputs
