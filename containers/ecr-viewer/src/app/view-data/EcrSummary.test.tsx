5fd49490beb7654c6318f052e917b4c745251b99
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-12-13 22:22:04 -0500

    move and fix ecr summary tests

--- containers/ecr-viewer/src/app/view-data/EcrSummary.test.tsx
+++ containers/ecr-viewer/src/app/view-data/EcrSummary.test.tsx
@@ -1,16 +1,16 @@
 import {render} from "@testing-library/react";
-import EcrSummary from "@/app/generate/EcrSummary";
 import fs from "fs";
 import {Bundle} from "fhir/r4";
 import YAML from 'yaml'
 import {axe} from "jest-axe";
+import EcrSummary from "@/app/view-data/EcrSummary";
 
 describe("EcrSummary", () => {
     let container: HTMLElement
     beforeAll(() =>{
-        const fhirPathFile = fs.readFileSync('./src/app/generate/fhirPath.yml', 'utf8').toString();
+        const fhirPathFile = fs.readFileSync('./public/fhirPath.yml', 'utf8').toString();
         const fhirPathMappings = YAML.parse(fhirPathFile);
-        const fhirBundle: Bundle = JSON.parse(fs.readFileSync('./src/app/generate/exampleBundle.json', 'utf8').toString());
+        const fhirBundle: Bundle = JSON.parse(fs.readFileSync('./seed-scripts/fhir_data/1dd10047-2207-4eac-a993-0f706c88be5d.json', 'utf8').toString());
 
          container = render(<EcrSummary fhirPathMappings={fhirPathMappings} fhirBundle={fhirBundle} />).container;
     })
