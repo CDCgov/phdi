ac51f4cf47a925b62c94223b30d618f888802ec7
robertmitchellv <robert.beaty.mitchell@gmail.com>
2023-12-15 16:34:14 -0800

    added tests for the validate and standardize pieces of the code

--- containers/standardization/tests/test_standardization.py
+++ containers/standardization/tests/test_standardization.py
@@ -1,9 +1,22 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
-from app.utils import standardize_name
+from app.utils import (
+    read_json_from_assets,
+    standardize_name,
+    standardize_dob,
+    standardize_dob_fhir,
+    _standardize_date,
+    _validate_date,
+)
 
 client = TestClient(app)
+
+
+@pytest.fixture
+def single_patient_bundle():
+    return read_json_from_assets("single_patient_bundle.json")
 
 
 def test_health_check():
@@ -36,3 +49,51 @@ def test_standardize_name():
         "PAUL BUNYAN",
         "JRRTOLKIE87N 999",
     ]
+
+
+@pytest.mark.parametrize(
+    "input_date, format_string, future, expected",
+    [
+        ("1977-11-21", None, False, "1977-11-21"),
+        ("1980-01-31", None, False, "1980-01-31"),
+        ("1977/11/21", "%Y/%m/%d", False, "1977-11-21"),
+        ("1980/01/31", "%Y/%m/%d", False, "1980-01-31"),
+        ("01/1980/31", "%m/%Y/%d", False, "1980-01-31"),
+        ("11-1977-21", "%m-%Y-%d", False, "1977-11-21"),
+    ],
+)
+def test_standardize_date(input_date, format_string, future, expected):
+    if format_string:
+        assert _standardize_date(input_date, format_string, future) == expected
+    else:
+        assert _standardize_date(input_date, future=future) == expected
+
+
+def test_standardize_date_invalid():
+    with pytest.raises(ValueError) as e:
+        _standardize_date("blah")
+    assert "Invalid date format or missing components in date: blah" in str(e.value)
+
+
+def test_standardize_date_format_mismatch():
+    with pytest.raises(ValueError) as e:
+        _standardize_date("abc-def-ghi", "%Y-%m-%d")
+    assert "Invalid date format supplied:" in str(e.value)
+
+    with pytest.raises(ValueError) as e:
+        _standardize_date("1980-01-31", "%H:%M:%S")
+    assert "Invalid date format or missing components in date:" in str(e.value)
+
+
+@pytest.mark.parametrize(
+    "year, month, day, allow_future, expected",
+    [
+        ("1980", "10", "15", False, True),
+        ("3030", "10", "15", False, True),
+        ("3030", "10", "15", True, False),
+        ("2005", "15", "10", False, False),
+        ("2005", "02", "30", False, False),
+    ],
+)
+def test_validate_date(year, month, day, allow_future, expected):
+    assert _validate_date(year, month, day, allow_future) == expected
