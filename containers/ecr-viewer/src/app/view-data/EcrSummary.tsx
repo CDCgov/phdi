3793f5fe09c89540e933a9043824713e5601a6c2
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-12-20 15:15:01 -0500

    add temp tag around all placefiller data

--- containers/ecr-viewer/src/app/view-data/EcrSummary.tsx
+++ containers/ecr-viewer/src/app/view-data/EcrSummary.tsx
@@ -116,7 +116,7 @@ const EcrSummary = (
                             <div className="usa-card__container margin-left-0 margin-right-0 margin-top-1">
                                 <div className="usa-card__header padding-top-2 padding-bottom-2 padding-left-3">
                                     <p>
-                                        Hepatitis C IgG w/Rfx PCR<span
+                                        Hepatitis C IgG w/Rfx PCR (temp)<span
                                         className="usa-tag margin-left-1 bg-error-dark text-white">Abnormal</span>
                                     </p>
                                 </div>
@@ -133,10 +133,10 @@ const EcrSummary = (
                                         </thead>
                                         <tbody>
                                         <tr>
-                                            <td>Hepatitis C Antibody</td>
-                                            <td>Positive (A)</td>
-                                            <td>Not Detected</td>
-                                            <td>Blood</td>
+                                            <td>Hepatitis C Antibody (temp)</td>
+                                            <td>Positive (A) (temp)</td>
+                                            <td>Not Detected (temp)</td>
+                                            <td>Blood (temp)</td>
                                         </tr>
                                         </tbody>
                                     </table>
@@ -147,7 +147,7 @@ const EcrSummary = (
                                         <div className="grid-row">
                                             <div className="data-title"><h4>Analysis time</h4></div>
                                             <div className="grid-col-auto text-pre-line">
-                                                {evaluate(fhirBundle, fhirPathMappings.patientDOB)}
+                                                N/A (temp)
                                             </div>
                                         </div>
                                         {/*todo: replace with grey line*/}
@@ -155,7 +155,7 @@ const EcrSummary = (
                                         <div className="grid-row">
                                             <div className="data-title"><h4>Collection time</h4></div>
                                             <div className="grid-col-auto text-pre-line">
-                                                {evaluate(fhirBundle, fhirPathMappings.patientDOB)}
+                                                05/12/2022 6:00 AM CDT (temp)
                                             </div>
                                         </div>
                                         {/*todo: replace with grey line*/}
@@ -163,7 +163,7 @@ const EcrSummary = (
                                         <div className="grid-row">
                                             <div className="data-title"><h4>Received time</h4></div>
                                             <div className="grid-col-auto text-pre-line">
-                                                {evaluate(fhirBundle, fhirPathMappings.patientDOB)}
+                                                05/12/2022 11:30 AM CDT (temp)
                                             </div>
                                         </div>
                                         {/*todo: replace with grey line*/}
@@ -171,7 +171,7 @@ const EcrSummary = (
                                         <div className="grid-row">
                                             <div className="data-title"><h4>Notes</h4></div>
                                             <div className="grid-col-auto text-pre-line">
-                                                {evaluate(fhirBundle, fhirPathMappings.patientDOB)}
+                                                A detected result is positive and indicates the presence of the virus in the sample. A not detected result indicates that the test did not detect the virus in the sample. This test was performed using a multiplexed nucleic acid amplification test that is labeled Emergence Use Only by the U.S. FDA. Results of this test should not be used as the sole basis for diagnosis, treatment, or other management decisions. (temp)
                                             </div>
                                         </div>
                                     </div>
@@ -181,7 +181,7 @@ const EcrSummary = (
                         <div className={"section__line"}/>
                         <div className="grid-row">
                             <h4>Clinical sections relevant to reportable condition</h4>
-                            <div className={"padding-top-05"}>No matching clinical data found in this eCR</div>
+                            <div className={"padding-top-05"}>No matching clinical data found in this eCR (temp) </div>
                         </div>
                     </div>
                 </div>
