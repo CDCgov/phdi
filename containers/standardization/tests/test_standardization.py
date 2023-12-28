bf9026de7a886f999e948444ca1d0c5c524dbd87
robertmitchellv <robert.beaty.mitchell@gmail.com>
2023-12-28 15:36:00 -0800

    added fixtures for repeating data, streamlined tests

--- containers/standardization/tests/test_standardization.py
+++ containers/standardization/tests/test_standardization.py
@@ -233,166 +233,144 @@ def test_extract_countries_from_resource():
     ] == ["840"] * 3
 
 
-def test_parse_smarty_result_success():
-    candidate = Candidate({})
-    candidate.delivery_line_1 = "123 FAKE ST"
-    candidate.delivery_line_2 = "UNIT 221B"
-    candidate.metadata = Metadata(
-        {
-            "latitude": 45.123,
-            "longitude": -70.234,
-            "county_fips": "36061",
-            "county_name": "New York",
-            "precision": "Zip9",
-        }
+@pytest.fixture
+def expected_geocode_result():
+    return GeocodeResult(
+        line=["1428 Post Aly"],
+        city="Seattle",
+        state="WA",
+        lat=47.608479,
+        lng=-122.340202,
+        county_fips="53033",
+        county_name="King",
+        postal_code="98101",
+        precision="Rooftop",
     )
 
+
+@pytest.fixture
+def smarty_geocode_client():
+    smarty_auth_id = mock.Mock()
+    smarty_auth_token = mock.Mock()
+    return SmartyGeocodeClient(smarty_auth_id, smarty_auth_token)
+
+
+@pytest.fixture
+def mock_smarty_candidate():
+    candidate = Candidate({})
+    candidate.delivery_line_1 = "1428 Post Aly"
     candidate.components = Components(
-        {"zipcode": "10001", "city_name": "New York", "state_abbreviation": "NY"}
+        {
+            "primary_number": "1428",
+            "street_name": "Post",
+            "street_suffix": "Aly",
+            "city_name": "Seattle",
+            "state_abbreviation": "WA",
+            "zipcode": "98101",
+            "plus4_code": "2034",
+        }
     )
+    candidate.metadata = Metadata(
+        {
+            "latitude": 47.608479,
+            "longitude": -122.340202,
+            "county_fips": "53033",
+            "county_name": "King",
+            "precision": "Rooftop",
+        }
+    )
+    return candidate
+
+
+def test_parse_smarty_result_success(mock_smarty_candidate):
+    candidate = mock_smarty_candidate
 
     lookup = mock.Mock()
     lookup.result = [candidate]
     encoded_result = SmartyGeocodeClient._parse_smarty_result(lookup)
 
-    assert encoded_result.line == ["123 FAKE ST", "UNIT 221B"]
-    assert encoded_result.city == "New York"
-    assert encoded_result.state == "NY"
-    assert encoded_result.lat == 45.123
-    assert encoded_result.lng == -70.234
-    assert encoded_result.county_fips == "36061"
-    assert encoded_result.county_name == "New York"
-    assert encoded_result.postal_code == "10001"
-    assert encoded_result.precision == "Zip9"
+    assert encoded_result.line == ["1428 Post Aly"]
+    assert encoded_result.city == "Seattle"
+    assert encoded_result.state == "WA"
+    assert encoded_result.lat == 47.608479
+    assert encoded_result.lng == -122.340202
+    assert encoded_result.county_fips == "53033"
+    assert encoded_result.county_name == "King"
+    assert encoded_result.postal_code == "98101"
+    assert encoded_result.precision == "Rooftop"
 
 
 def test_parse_smarty_result_failure():
-    lookup = mock.Mock()
-    lookup.result = None
-    assert SmartyGeocodeClient._parse_smarty_result(lookup) is None
+    # no result from the API
+    lookup_no_result = mock.Mock()
+    lookup_no_result.result = None
+    assert SmartyGeocodeClient._parse_smarty_result(lookup_no_result) is None
 
-    candidate = Candidate({})
-    candidate.delivery_line_1 = "123 FAKE ST"
-    candidate.metadata = Metadata(
-        {
-            "county_fips": "36061",
-            "county_name": "New York",
-            "precision": "Zip9",
-        }
-    )
-    lookup.result = [candidate]
-    assert SmartyGeocodeClient._parse_smarty_result(lookup) is None
+    # incomplete data in the result
+    lookup_incomplete = mock.Mock()
+    candidate_incomplete = Candidate({})
+    candidate_incomplete.delivery_line_1 = "1428 Post Aly"
+    lookup_incomplete.result = [candidate_incomplete]
+
+    assert SmartyGeocodeClient._parse_smarty_result(lookup_incomplete) is None
 
 
-def test_geocode_from_str():
-    smarty_auth_id = mock.Mock()
-    smarty_auth_token = mock.Mock()
-    smarty_client = SmartyGeocodeClient(smarty_auth_id, smarty_auth_token)
-    assert smarty_client.client is not None
+def test_geocode_from_str(
+    smarty_geocode_client, mock_smarty_candidate, expected_geocode_result
+):
+    assert smarty_geocode_client.client is not None
 
-    candidate = Candidate({})
-    candidate.delivery_line_1 = "123 FAKE ST"
-    candidate.metadata = Metadata(
-        {
-            "latitude": 45.123,
-            "longitude": -70.234,
-            "county_fips": "36061",
-            "county_name": "New York",
-            "precision": "Zip9",
-        }
-    )
-    candidate.components = Components(
-        {"zipcode": "10001", "city_name": "New York", "state_abbreviation": "NY"}
-    )
-
-    with mock.patch.object(smarty_client.client, "send_lookup") as mock_send_lookup:
-
+    with mock.patch.object(
+        smarty_geocode_client.client, "send_lookup"
+    ) as mock_send_lookup:
+        # provide a function that adds results to the existing object
         def fill_in_result(*args, **kwargs):
-            args[0].result = [candidate]
+            args[0].result = [mock_smarty_candidate]
 
         mock_send_lookup.side_effect = fill_in_result
 
-        geocoded_response = GeocodeResult(
-            line=["123 FAKE ST"],
-            city="New York",
-            state="NY",
-            lat=45.123,
-            lng=-70.234,
-            county_fips="36061",
-            county_name="New York",
-            postal_code="10001",
-            precision="Zip9",
-        )
-
-        assert geocoded_response == smarty_client.geocode_from_str(
-            "123 FAKE ST New York NY 10001"
+        assert expected_geocode_result == smarty_geocode_client.geocode_from_str(
+            "1428 Post Aly Seattle WA 98101"
         )
         mock_send_lookup.assert_called()
 
 
-def test_geocode_from_dict():
-    smarty_auth_id = mock.Mock()
-    smarty_auth_token = mock.Mock()
-    smarty_client = SmartyGeocodeClient(smarty_auth_id, smarty_auth_token)
-    assert smarty_client.client is not None
+def test_geocode_from_dict(
+    smarty_geocode_client, mock_smarty_candidate, expected_geocode_result
+):
+    assert smarty_geocode_client.client is not None
 
-    candidate = Candidate({})
-    candidate.delivery_line_1 = "123 FAKE ST"
-    candidate.metadata = Metadata(
-        {
-            "latitude": 45.123,
-            "longitude": -70.234,
-            "county_fips": "36061",
-            "county_name": "New York",
-            "precision": "Zip9",
+    with mock.patch.object(
+        smarty_geocode_client.client, "send_lookup"
+    ) as mock_send_lookup:
+        # provide a function that adds results to the existing object
+        def fill_in_result(*args, **kwargs):
+            args[0].result = [mock_smarty_candidate]
+
+        mock_send_lookup.side_effect = fill_in_result
+
+        input_dict = {
+            "street": "1428 Post Aly",
+            "city": "Seattle",
+            "state": "WA",
+            "zip": "98101",
         }
-    )
-    candidate.components = Components(
-        {"zipcode": "10001", "city_name": "New York", "state_abbreviation": "NY"}
-    )
 
-    # Provide a function that adds results to the existing object
-    def fill_in_result(*args, **kwargs):
-        args[0].result = [candidate]
-
-    smarty_client.client.send_lookup = mock.Mock()
-    smarty_client.client.send_lookup.side_effect = fill_in_result
-
-    geocoded_response = GeocodeResult(
-        line=["123 FAKE ST"],
-        city="New York",
-        state="NY",
-        lat=45.123,
-        lng=-70.234,
-        county_fips="36061",
-        county_name="New York",
-        postal_code="10001",
-        precision="Zip9",
-    )
-
-    input_dict = {
-        "street": "123 FAKE ST",
-        "city": "New York",
-        "state": "NY",
-        "zip": "10001",
-    }
-    assert geocoded_response == smarty_client.geocode_from_dict(input_dict)
-    smarty_client.client.send_lookup.assert_called()
+        assert expected_geocode_result == smarty_geocode_client.geocode_from_dict(
+            input_dict
+        )
+        mock_send_lookup.assert_called()
 
 
-def test_blank_geocode_inputs():
-    smarty_auth_id = mock.Mock()
-    smarty_auth_token = mock.Mock()
-    smarty_client = SmartyGeocodeClient(smarty_auth_id, smarty_auth_token)
-    assert smarty_client.client is not None
+def test_blank_geocode_inputs(smarty_geocode_client):
+    assert smarty_geocode_client.client is not None
 
-    geocoded_response = None
+    # test geocode_from_str with empty string
     with pytest.raises(ValueError) as e:
-        geocoded_response = smarty_client.geocode_from_str("")
+        smarty_geocode_client.geocode_from_str("")
     assert "Address must include street number and name at a minimum" in str(e.value)
-    assert geocoded_response is None
 
+    # test geocode_from_dict with empty dict
     with pytest.raises(ValueError) as e:
-        geocoded_response = smarty_client.geocode_from_dict({})
+        smarty_geocode_client.geocode_from_dict({})
     assert "Address must include street number and name at a minimum" in str(e.value)
-    assert geocoded_response is None
