57c1c01c30861647f20e39b1d1fa2e8ac5abaff5
Josh Nygaard <jnygaard@skylight.digital>
2024-01-03 16:20:26 -0500

    rebasing about the condition work

--- containers/ecr-viewer/src/app/view-data/components/EcrSummary.tsx
+++ containers/ecr-viewer/src/app/view-data/components/EcrSummary.tsx
@@ -1,6 +1,13 @@
 import {evaluate} from "fhirpath";
 import {Bundle} from "fhir/r4";
-import {PathMappings, formatPatientName, formatPatientAddress, formatPatientContactInfo} from "../../utils";
+import {
+    extractFacilityAddress,
+    extractPatientAddress,
+    formatEncounterDate,
+    formatPatientContactInfo,
+    formatPatientName,
+    PathMappings
+} from "../../utils";
 
 interface EcrViewerProps {
     fhirPathMappings: PathMappings
@@ -15,40 +22,75 @@ const EcrSummary = (
         <div className={"info-container"}>
             <div
                 className="usa-summary-box padding-3"
+                aria-labelledby="summary-box-key-information"
             >
-                <div>
-                    <h3>
+                <div className="usa-summary-box__body">
+                    <h3 id="summary-box-key-information">
                         About the Patient
                     </h3>
                     <div className="usa-summary-box__text">
                         <div className="grid-row">
                             <div className="data-title"><h4>Patient Name</h4></div>
-                            <div className="grid-col-fill">
+                            <div className="grid-col-auto">
                                 {formatPatientName(fhirBundle, fhirPathMappings)}
                             </div>
                         </div>
                         <div className={"section__line"}/>
                         <div className="grid-row">
                             <div className="data-title"><h4>DOB</h4></div>
-                            <div className="grid-col-fill text-pre-line">
+                            <div className="grid-col-auto text-pre-line">
                                 {evaluate(fhirBundle, fhirPathMappings.patientDOB)}
                             </div>
                         </div>
                         <div className={"section__line"}/>
                         <div className="grid-row">
                             <div className="data-title"><h4>Patient Address</h4></div>
-                            <div className="grid-col-fill text-pre-line">
-                                {formatPatientAddress(fhirBundle, fhirPathMappings)}
+                            <div className="grid-col-auto text-pre-line">
+                                {extractPatientAddress(fhirBundle, fhirPathMappings)}
                             </div>
                         </div>
                         <div className={"section__line"}/>
                         <div className="grid-row">
                             <div className="data-title"><h4>Patient Contact</h4></div>
-                            <div className="grid-col-fill text-pre-line">
+                            <div className="grid-col-auto text-pre-line">
                                 {formatPatientContactInfo(fhirBundle, fhirPathMappings)}
                             </div>
                         </div>
                         <div className={"section__line"}/>
+                    </div>
+                </div>
+                <div className="usa-summary-box__body">
+                    <h3 id="summary-box-key-information">
+                        About the Encounter
+                    </h3>
+                    <div className="usa-summary-box__text">
+                        <div className="grid-row">
+                            <div className="data-title"><h4>Facility Name</h4></div>
+                            <div className="grid-col-auto">
+                                {evaluate(fhirBundle, fhirPathMappings.facilityName)}
+                            </div>
+                        </div>
+                        <div className={"section__line"}/>
+                        <div className="grid-row">
+                            <div className="data-title"><h4>Facility Address</h4></div>
+                            <div className="grid-col-auto text-pre-line">
+                                {extractFacilityAddress(fhirBundle, fhirPathMappings)}
+                            </div>
+                        </div>
+                        <div className={"section__line"}/>
+                        <div className="grid-row">
+                            <div className="data-title"><h4>Encounter Type</h4></div>
+                            <div className="grid-col-auto">
+                                {evaluate(fhirBundle, fhirPathMappings.encounterType)}
+                            </div>
+                        </div>
+                        <div className={"section__line"}/>
+                        <div className="grid-row">
+                            <div className="data-title"><h4>Encounter Date</h4></div>
+                            <div className="grid-col-auto">
+                                {formatEncounterDate(fhirBundle, fhirPathMappings)}
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="usa-summary-box__body">
@@ -71,7 +113,7 @@ const EcrSummary = (
                         </div>
                         <div className={"section__line"}/>
                         <div className="grid-row">
-                                <h4>Lab results relevant to reportable condition</h4>
+                            <h4>Lab results relevant to reportable condition</h4>
                             <div className="usa-card__container margin-left-0 margin-right-0 margin-top-1">
                                 <div className="usa-card__header padding-top-2 padding-bottom-2 padding-left-3">
                                     <p>
@@ -141,7 +183,7 @@ const EcrSummary = (
                         <div className={"section__line"}/>
                         <div className="grid-row">
                             <h4>Clinical sections relevant to reportable condition</h4>
-                            <div className={"padding-top-05"}>No matching clinical data found in this eCR (temp) </div>
+                            <div className={"padding-top-05"}>No matching clinical data found in this eCR (temp)</div>
                         </div>
                     </div>
                 </div>
