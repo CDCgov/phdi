0d684008c82bf622fe33adeeeb243911c9c77658
emmastephenson <emma.stephenson.24@gmail.com>
2024-01-02 16:09:00 -0800

    add prettier

--- containers/ecr-viewer/src/app/view-data/page.tsx
+++ containers/ecr-viewer/src/app/view-data/page.tsx
@@ -1,15 +1,14 @@
 // pages/index.js
-"use client"
+"use client";
 import EcrSummary from "@/app/view-data/components/EcrSummary";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Bundle } from "fhir/r4";
 import Demographics from "./components/Demographics";
 import SocialHistory from "./components/SocialHistory";
-import { Accordion } from '@trussworks/react-uswds'
+import { Accordion } from "@trussworks/react-uswds";
 import UnavailableInfo from "./components/UnavailableInfo";
-import {PathMappings, evaluateSocialData} from "../utils";
-
+import { PathMappings, evaluateSocialData } from "../utils";
 
 const ECRViewerPage = () => {
   const [fhirBundle, setFhirBundle] = useState<Bundle>();
@@ -21,9 +20,9 @@ const ECRViewerPage = () => {
   type FhirMappings = { [key: string]: string };
 
   type ApiResponse = {
-    fhirBundle: Bundle,
-    fhirPathMappings: FhirMappings
-  }
+    fhirBundle: Bundle;
+    fhirPathMappings: FhirMappings;
+  };
 
   useEffect(() => {
     // Fetch the appropriate bundle from Postgres database
@@ -32,76 +31,81 @@ const ECRViewerPage = () => {
         const response = await fetch(`/api/data?id=${fhirId}`);
         if (!response.ok) {
           const errorData = await response.json();
-          throw new Error( errorData.message || 'Internal Server Error');
-        } else{
-          const bundle: ApiResponse = (await response.json());
-          setFhirBundle(bundle.fhirBundle)
-          setMappings(bundle.fhirPathMappings)
+          throw new Error(errorData.message || "Internal Server Error");
+        } else {
+          const bundle: ApiResponse = await response.json();
+          setFhirBundle(bundle.fhirBundle);
+          setMappings(bundle.fhirPathMappings);
         }
       } catch (error) {
-        setErrors(error)
-        console.error('Error fetching data:', error);
+        setErrors(error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
   }, []);
 
   const renderAccordion = () => {
-    const social_data = evaluateSocialData(fhirBundle, mappings)
+    const social_data = evaluateSocialData(fhirBundle, mappings);
     const accordionItems: any[] = [
       {
-        title: 'Patient Info',
+        title: "Patient Info",
         content: (
           <div>
             <Demographics fhirPathMappings={mappings} fhirBundle={fhirBundle} />
-            {social_data.available_data && <SocialHistory socialData={social_data.available_data} />}
+            {social_data.available_data && (
+              <SocialHistory socialData={social_data.available_data} />
+            )}
           </div>
         ),
         expanded: true,
-        id: '1',
-        headingLevel: 'h2',
+        id: "1",
+        headingLevel: "h2",
       },
       {
-        title: 'Unavailable Info',
+        title: "Unavailable Info",
         content: (
           <div>
             <UnavailableInfo unavailableData={social_data.unavailable_data} />
           </div>
         ),
         expanded: true,
-        id: '2',
-        headingLevel: 'h2',
+        id: "2",
+        headingLevel: "h2",
       },
-    ]
+    ];
 
     return (
-      <Accordion className="info-container" items={accordionItems} multiselectable={true} />
-    )
-  }
+      <Accordion
+        className="info-container"
+        items={accordionItems}
+        multiselectable={true}
+      />
+    );
+  };
 
   if (errors) {
+    return <div>{`${errors}`}</div>;
+  } else if (fhirBundle && mappings) {
     return (
       <div>
-        {`${errors}`}
-      </div>
-    )
-  }
-  else if (fhirBundle && mappings) {
-
-    return (
-      <div>
-        <header><h1 className={"page-title"}>EZ eCR Viewer</h1></header>
+        <header>
+          <h1 className={"page-title"}>EZ eCR Viewer</h1>
+        </header>
         <div className={"ecr-viewer-container"}>
           <h2>eCR Summary</h2>
           <EcrSummary fhirPathMappings={mappings} fhirBundle={fhirBundle} />
           <h2>Additional Details</h2>
           {renderAccordion()}
         </div>
-      </div>)
+      </div>
+    );
   } else {
-    return <div>
-      <h1>Loading...</h1>
-    </div>
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
 };
 
