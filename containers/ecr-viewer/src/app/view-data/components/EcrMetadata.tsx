43b88fe7b98680e49276839d82d54089cea11f9b
Gordon Farrell <gordon@skylight.digital>
2024-01-03 13:46:31 -0700

    wip

--- containers/ecr-viewer/src/app/view-data/components/EcrMetadata.tsx
+++ containers/ecr-viewer/src/app/view-data/components/EcrMetadata.tsx
@@ -21,69 +21,61 @@ const EcrMetadata = (
                         className="usa-summary-box__heading padding-y-105"
                         id="summary-box-key-information"
                     >
-                        eCR Metadatagit
+                        eICR Details
                     </h3>
                     <div className="usa-summary-box__text">
                         <div className="grid-row">
-                            <div className="data-title"><h4>Patient Name</h4></div>
+                            <div className="data-title"><h4>eICR Identifier</h4></div>
                             <div className="grid-col-auto">
-                                {formatPatientName(fhirBundle, fhirPathMappings)}
+                                {}
                             </div>
                         </div>
                         <div className={"section__line_gray"} />
                         <div className="grid-row">
-                            <div className="data-title"><h4>Patient ID</h4></div>
-                            <div className="grid-col-auto"> 
-                                {evaluate(fhirBundle, fhirPathMappings.patientId)}
-                            </div>
+                <h3
+                    className="usa-summary-box__heading padding-y-105"
+                    id="summary-box-key-information"
+                >
+                    eCR Sender Details
+                </h3>
+                        <div className="data-title"><h4>Date/Time eCR Created</h4></div>
+                        <div className="grid-col-auto">
+                            {evaluate(fhirBundle, fhirPathMappings.patientId)}
+                        </div>
                         </div>
                         <div className={"section__line_gray"} />
                         <div className="grid-row">
-                            <div className="data-title"><h4>DOB</h4></div>
+                            <div className="data-title"><h4>Sender Software</h4></div>
                             <div className="grid-col-auto">
                                 {evaluate(fhirBundle, fhirPathMappings.patientDOB)}
                             </div>
                         </div>
                         <div className={"section__line_gray"} />
                         <div className="grid-row">
-                            <div className="data-title"><h4>Sex</h4></div>
+                            <div className="data-title"><h4>Sender Facility Name</h4></div>
                             <div className="grid-col-auto">
                                 {evaluate(fhirBundle, fhirPathMappings.patientGender)}
                             </div>
                         </div>
                         <div className={"section__line_gray"} />
                         <div className="grid-row">
-                            <div className="data-title"><h4>Race</h4></div>
+                            <div className="data-title"><h4>Facility Address</h4></div>
                             <div className="grid-col-auto">
                                 {evaluate(fhirBundle, fhirPathMappings.patientRace)}
                             </div>
                         </div>
                         <div className={"section__line_gray"} />
                         <div className="grid-row">
-                            <div className="data-title"><h4>Ethnicity</h4></div>
+                            <div className="data-title"><h4>Facility Contact</h4></div>
                             <div className="grid-col-auto">
                                 {evaluate(fhirBundle, fhirPathMappings.patientEthnicity)}
                             </div>
                         </div>
                         <div className={"section__line_gray"} />
                         <div className="grid-row">
-                            <div className="data-title"><h4>Preferred Language</h4></div>
+                            <div className="data-title"><h4>Facility ID</h4></div>
                             <div className="grid-col-auto">
                                 {evaluate(fhirBundle, fhirPathMappings.patientLanguage)}
-                            </div>
-                        </div>
-                        <div className={"section__line_gray"} />
-                        <div className="grid-row">
-                            <div className="data-title"><h4>Patient Address</h4></div>
-                            <div className="grid-col-auto text-pre-line">
-                                {formatPatientAddress(fhirBundle, fhirPathMappings)}
-                            </div>
-                        </div>
-                        <div className={"section__line_gray"} />
-                        <div className="grid-row">
-                            <div className="data-title"><h4>Contact</h4></div>
-                            <div className="grid-col-auto text-pre-line">
-                                {formatPatientContactInfo(fhirBundle, fhirPathMappings)}
                             </div>
                         </div>
                         <div className={"section__line_gray"} />
