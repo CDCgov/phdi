8f7b762a234c910304192096b6b76a89bee6d557
KennethSkylight <107214304+KennethSkylight@users.noreply.github.com>
2024-01-04 11:54:18 -0600

    Adding encounter info (#1014)
    * Adding encounter info
    
    * Adding encounter info header
    
    * updating tests
    
    * Fixing the date/time

--- containers/ecr-viewer/src/app/utils.tsx
+++ containers/ecr-viewer/src/app/utils.tsx
@@ -1,53 +1,15 @@
-import {Bundle} from "fhir/r4";
-import {evaluate} from "fhirpath";
+import { Bundle } from "fhir/r4";
+import { evaluate } from "fhirpath";
 
 export interface DisplayData {
     title: string,
-    value: string
+    value: string | undefined
 }
 
 export interface PathMappings {
     [key: string]: string;
 }
 
-export const socialData = [
-    {
-        'title': 'Occupation',
-        'value': 'patientCurrentJobTitle'
-    },
-    {
-        'title': 'Tobacco Use',
-        'value': 'patientTobaccoUse',
-    },
-    {
-        'title': 'Travel History',
-        'value': 'patientTravelHistory',
-    },
-    {
-        'title': 'Homeless Status',
-        'value': 'patientHomelessStatus',
-    },
-    {
-        'title': 'Pregnancy Status',
-        'value': 'patientPregnancyStatus',
-    },
-    {
-        'title': 'Alcohol Use',
-        'value': 'patientAlcoholUse',
-    },
-    {
-        'title': 'Sexual Orientation',
-        'value': 'patientSexualOrientation',
-    },
-    {
-        'title': 'Gender Identity',
-        'value': 'patientGenderIdentity',
-    },
-    {
-        'title': 'Occupation',
-        'value': 'patientCurrentJobTitle',
-    },
-]
 
 export const formatPatientName = (fhirBundle: Bundle | undefined, fhirPathMappings: PathMappings) => {
     const givenNames = evaluate(fhirBundle, fhirPathMappings.patientGivenName).join(" ");
@@ -56,16 +18,32 @@ export const formatPatientName = (fhirBundle: Bundle | undefined, fhirPathMappin
     return `${givenNames} ${familyName}`;
 }
 
+const formatName = (firstName: string, lastName: string) => {
+    if (firstName != undefined) {
+        return `${firstName} ${lastName}`
+    }
+    else {
+        return undefined
+    }
+}
+
 export const formatPatientAddress = (fhirBundle: Bundle | undefined, fhirPathMappings: PathMappings) => {
     const streetAddresses = evaluate(fhirBundle, fhirPathMappings.patientStreetAddress).join("\n");
     const city = evaluate(fhirBundle, fhirPathMappings.patientCity);
     const state = evaluate(fhirBundle, fhirPathMappings.patientState);
     const zipCode = evaluate(fhirBundle, fhirPathMappings.patientZipCode);
     const country = evaluate(fhirBundle, fhirPathMappings.patientCountry);
-    return(
+    return (
         `${streetAddresses}
     ${city}, ${state}
     ${zipCode}${country && `, ${country}`}`);
+}
+
+const formatAddress = (streetAddresses: string[], city: string, state: string, zipCode: string) => {
+    return (
+        `${streetAddresses} 
+        ${city}, ${state} ${zipCode}`
+    );
 }
 
 export const formatPatientContactInfo = (fhirBundle: Bundle | undefined, fhirPathMappings: PathMappings) => {
@@ -76,18 +54,146 @@ export const formatPatientContactInfo = (fhirBundle: Bundle | undefined, fhirPat
     ${emails}`
 }
 
-export const evaluateSocialData = (fhirBundle: Bundle | undefined, mappings: PathMappings) => {
-    let socialArray: DisplayData[] = []
-    let unavailableArray: DisplayData[] = []
-    socialData.forEach((item) => {
-        const evaluatedFhirPath = evaluate(fhirBundle, mappings[item.value])
-        const evaluatedItem: DisplayData = { 'title': item.title, 'value': evaluatedFhirPath[0] }
+const formatPhoneNumber = (phoneNumber: string) => {
+    try {
+        const formattedPhoneNumber = phoneNumber.replace(/\D/g, "").replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+        return formattedPhoneNumber
+    } catch {
+        return undefined
+    }
 
-        if (evaluatedFhirPath.length > 0) {
-            socialArray.push(evaluatedItem)
+}
+
+const formatStartEndDateTime = (startDateTime: 'string', endDateTime: 'string') => {
+    const startDateObject = new Date(startDateTime);
+    const endDateObject = new Date(endDateTime)
+
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    };
+
+    const startFormattedDate = startDateObject.toLocaleString('en-US', options).replace(',', '');
+    const endFormattedDate = endDateObject.toLocaleString('en-us', options).replace(',', '')
+
+    return (
+        `Start: ${startFormattedDate}
+        End: ${endFormattedDate}`
+    )
+}
+
+export const evaluateSocialData = (fhirBundle: Bundle | undefined, mappings: PathMappings) => {
+    const socialData = [
+        {
+            'title': 'Occupation',
+            'value': evaluate(fhirBundle, mappings['patientCurrentJobTitle'])[0]
+        },
+        {
+            'title': 'Tobacco Use',
+            'value': evaluate(fhirBundle, mappings['patientTobaccoUse'])[0]
+        },
+        {
+            'title': 'Travel History',
+            'value': evaluate(fhirBundle, mappings['patientTravelHistory'])[0]
+        },
+        {
+            'title': 'Homeless Status',
+            'value': evaluate(fhirBundle, mappings['patientHomelessStatus'])[0]
+        },
+        {
+            'title': 'Pregnancy Status',
+            'value': evaluate(fhirBundle, mappings['patientPregnancyStatus'])[0]
+        },
+        {
+            'title': 'Alcohol Use',
+            'value': evaluate(fhirBundle, mappings['patientAlcoholUse'])[0]
+        },
+        {
+            'title': 'Sexual Orientation',
+            'value': evaluate(fhirBundle, mappings['patientSexualOrientation'])[0]
+        },
+        {
+            'title': 'Gender Identity',
+            'value': evaluate(fhirBundle, mappings['patientGenderIdentity'])[0]
+        },
+        {
+            'title': 'Occupation',
+            'value': evaluate(fhirBundle, mappings['patientCurrentJobTitle'])[0]
+        },
+    ]
+    return evaluateData(socialData)
+}
+
+export const evaluateEncounterData = (fhirBundle: Bundle | undefined, mappings: PathMappings) => {
+    const encounterData = [
+        {
+            'title': 'Encounter Date/Time',
+            'value': formatStartEndDateTime(evaluate(fhirBundle, mappings['encounterStartDate'])[0], evaluate(fhirBundle, mappings['encounterEndDate'])[0])
+        },
+        {
+            'title': 'Encounter Type',
+            'value': evaluate(fhirBundle, mappings['encounterType'])[0]
+        },
+        {
+            'title': 'Facility Name',
+            'value': evaluate(fhirBundle, mappings['facilityName'])[0]
+        },
+        {
+            'title': 'Facility Address',
+            'value': formatAddress(
+                evaluate(fhirBundle, mappings['facilityStreetAddress']),
+                evaluate(fhirBundle, mappings['facilityCity'])[0],
+                evaluate(fhirBundle, mappings['facilityState'])[0],
+                evaluate(fhirBundle, mappings['facilityZipCode'])[0]
+            )
+        },
+        {
+            'title': 'Facility Contact',
+            'value': formatPhoneNumber(evaluate(fhirBundle, mappings['facilityContact'])[0])
+        },
+        {
+            'title': 'Facility Type',
+            'value': evaluate(fhirBundle, mappings['facilityType'])[0]
+        },
+        {
+            'title': 'Facility ID',
+            'value': evaluate(fhirBundle, mappings['facilityID'])[0]
+        },
+
+    ]
+    return evaluateData(encounterData)
+}
+
+export const evaluateProviderData = (fhirBundle: Bundle | undefined, mappings: PathMappings) => {
+    const providerData = [
+        {
+            'title': 'Provider Name',
+            'value': formatName(
+                evaluate(fhirBundle, mappings['providerGivenName'])[0],
+                evaluate(fhirBundle, mappings['providerFamilyName'])[0]
+            )
+        },
+        {
+            'title': 'Provider Contact',
+            'value': formatPhoneNumber(evaluate(fhirBundle, mappings['providerContact'])[0])
+        },
+    ]
+    return evaluateData(providerData)
+}
+
+const evaluateData = (data: DisplayData[]) => {
+    let availableArray: DisplayData[] = []
+    let unavailableArray: DisplayData[] = []
+    data.forEach((item) => {
+        if (item.value != undefined) {
+            availableArray.push(item)
         } else {
-            unavailableArray.push(evaluatedItem)
+            unavailableArray.push(item)
         }
     })
-    return { 'available_data': socialArray, 'unavailable_data': unavailableArray }
+    return { 'available_data': availableArray, 'unavailable_data': unavailableArray }
 }
