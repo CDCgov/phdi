44d7ba32d9f604bf312c51ce4f0947b2c102049d
m-goggins <marcelle.goggins@gmail.com>
2024-01-11 12:03:30 -0800

    add labels for test cases

--- containers/message-parser/tests/test_phdc.py
+++ containers/message-parser/tests/test_phdc.py
@@ -6,16 +6,19 @@ from lxml import etree as ET
 @pytest.mark.parametrize(
     "build_telecom_test_data, expected_result",
     [
+        # Success with `use`
         (
             {"phone": "+1-800-555-1234", "use": "WP"},
             '<telecom use="WP" value="+1-800-555-1234"/>',
         ),
+        # Success without `use`
         (
             {
                 "phone": "+1-800-555-1234",
             },
             '<telecom value="+1-800-555-1234"/>',
         ),
+        # Success with `use` as None
         (
             {"phone": "+1-800-555-1234", "use": None},
             '<telecom value="+1-800-555-1234"/>',
