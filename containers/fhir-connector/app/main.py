db335d3fc3f6cd64585a2ca1ee9dcff9d8298925
DanPaseltiner <dan@skylight.digital>
2024-01-16 14:00:34 -0500

    Add syphilis

--- containers/fhir-connector/app/main.py
+++ containers/fhir-connector/app/main.py
@@ -6,7 +6,28 @@ import json
 from pydantic import BaseModel
 from typing import Optional, Literal
 
-USE_CASES = Literal["social-determinants", "new-born-screening"]
+USE_CASES = Literal["social-determinants", "newborn-screening", "syphilis"]
+
+FHIR_SERVERS = {
+    "meld": {"hostname": "https://gw.interop.community/HeliosConnectathonSa/open"},
+    "ehealthexchange": {
+        "hostname": "https://concept01.ehealthexchange.org:52780/fhirproxy/r4/",
+        "username": "svc_eHxFHIRSandbox",
+        "password": "willfulStrongStandurd7",
+        "headers": {
+            "Accept": "application/json, application/*+json, */*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Content-Type": "application/fhir+json; charset=UTF-8",
+            "X-DESTINATION": "CernerHelios",
+            "X-POU": "TREAT",
+            "X-Request-Id": str(uuid.uuid4()),
+            "prefer": "return=representation",
+            "Cache-Control": "no-cache",
+            "OAUTHSCOPES": "system/Condition.read system/Encounter.read system/Immunization.read system/MedicationRequest.read system/Observation.read system/Patient.read system/Procedure.read system/MedicationAdministration.read system/DiagnosticReport.read system/RelatedPerson.read",
+        },
+    },
+}
+
 
 # Instantiate FastAPI via PHDI's BaseService class
 app = BaseService(
@@ -16,6 +37,7 @@ app = BaseService(
 
 
 class UseCaseQueryRequest(BaseModel):
+    fhir_server: Literal["meld", "ehealthexchange"]
     first_name: str
     last_name: str
     dob: str
@@ -29,12 +51,25 @@ class UseCaseQueryRequest(BaseModel):
 
 @app.post("/use-case-query/{use_case}")
 async def use_case_query(use_case: USE_CASES, input: UseCaseQueryRequest):
-    fhir_host = "https://gw.interop.community/HeliosConnectathonSa/open"
+    
+    # Connect to FHIR Server
+    fhir_host = FHIR_SERVERS[input.fhir_server]["hostname"]
+    session = requests.Session()
+    fhir_server_config = FHIR_SERVERS[input.fhir_server]
+    if fhir_server_config.get("username") and fhir_server_config.get("password"):
+        session.auth = (
+            fhir_server_config["username"],
+            fhir_server_config["password"],
+        )
+    if fhir_server_config.get("headers"):
+        session.headers = fhir_server_config["headers"]
+    session.verify = False   
+    
 
     # Find Patient
 
     patient_query = f"{fhir_host}/Patient?given={input.first_name}&family={input.last_name}&birthdate={input.dob}"
-    response = requests.get(patient_query)
+    response = session.get(patient_query)
 
     if response.status_code != 200:
         return {
@@ -55,13 +90,13 @@ async def use_case_query(use_case: USE_CASES, input: UseCaseQueryRequest):
 
     print(patient_id)
     # Use Case Query
-
     if use_case == "social-determinants":
         use_case_query = (
             f"{fhir_host}/Observation?subject=Patient/{patient_id}&category=survey"
         )
+        use_case_response = session.get(use_case_query).json()
 
-    elif use_case == "new-born-screening":
+    elif use_case == "newborn-screening":
         use_case_query = (
             f"{fhir_host}/Observation?subject=Patient/{patient_id}&category=laboratory"
         )
@@ -79,85 +114,24 @@ async def use_case_query(use_case: USE_CASES, input: UseCaseQueryRequest):
         ]
         loinc_fitler = "http://loinc.org|" + ",".join(screening_loinc_codes)
         use_case_query = f"{use_case_query}&code={loinc_fitler}"
+        use_case_response = session.get(use_case_query).json()
 
-    print(use_case_query)
-
-    use_case_response = requests.get(use_case_query)
-
-    return use_case_response.json()
-
-
-@app.get(
-    "/new-born-screening/{earliest_dob}",
-    status_code=200,
-)
-async def new_born_screening(earliest_dob: str):
-    fhir_host = "https://concept01.ehealthexchange.org:52780/fhirproxy/r4/"
-    newborn_query = f"{fhir_host}Patient?birthdate=ge{earliest_dob}"
-    username = "svc_skylight"
-    password = "TendingTired7Leaves"
-
-    headers = {
-        "Accept": "application/json, application/*+json, */*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Content-Type": "application/fhir+json; charset=UTF-8",
-        "X-DESTINATION": "CernerHelios",
-        "X-POU": "TREAT",
-        "X-Request-Id": str(uuid.uuid4()),
-        "prefer": "return=representation",
-        "Cache-Control": "no-cache",
-        "OAUTHSCOPES": "system/Condition.read system/Encounter.read system/Immunization.read system/MedicationRequest.read system/Observation.read system/Patient.read system/Procedure.read system/MedicationAdministration.read system/DiagnosticReport.read system/RelatedPerson.read",
-    }
-
-    session = requests.Session()
-    session.auth = (username, password)
-    session.verify = False
-    session.headers = headers
-    newborn_response = session.get(newborn_query)
-    print(newborn_response.text)
-    print(newborn_response.status_code)
-    # newborn_bundle = json.loads(newborn_response.text)
-
-    # newborn_screening_results = []
-    # for newborn in newborn_bundle["entry"]:
-    #     screening_loinc_codes = ["73700-7", "73698-3", "54108-6", "54109-4", "58232-0", "57700-7", "73739-5", "73742-9", "2708-6", "8336-0"]
-    #     loinc_fitler = "http://loinc.org|" + ','.join(screening_loinc_codes)
-    #     newborn_id = newborn["resource"]["id"]
-    #     screening_query = f"{fhir_host}Observation?patient={newborn_id}&code={loinc_fitler}"
-    #     screening_response = session.get(screening_query)
-    #     screening_bundle = json.loads(screening_response.text)
-
-    #     if "entry" in screening_bundle:
-    #         screening_bundle["entry"].insert(0,newborn)
-    #         screening_bundle["total"] = len(screening_bundle["entry"])
-
-    #     else:
-    #         screening_bundle = {"resourceType": "Bundle",
-    #                             "id": str(uuid.uuid4()),
-    #                             "type": "searchset",
-    #                             "total": 1,
-    #                             "entry": [newborn]}
-    #     newborn_screening_results.append(screening_bundle)
-
-    return {
-        "query": newborn_query,
-        "headers": headers,
-        "username": username,
-        "password": password,
-        "response_text": newborn_response.text,
-        "response_status_code": newborn_response.status_code,
-    }
-
-
-# async def make_fhir_request(url :str, session: aiohttp.ClientSession):
-#     response = await session.get(url=url)
-#     fhir_bundle = await json.loads(response.text)
-#     return fhir_bundle
-
-# async def main(urls):
-#     async with aiohttp.ClientSession() as session:
-#    	 tasks = []
-#    	 for url in urls:
-#    		 tasks.append(get_url_data(url=url, session=session))
-#    	 results = await asyncio.gather(*tasks, return_exceptions=True)
-#     return results
+    elif use_case == "syphilis":
+        syphilis_loincs = ["LP70657-9", "98212-4"]
+        syphilis_loincs = ",".join(syphilis_loincs)
+        conditon_query = f"{fhir_host}/Condition?subject={patient_id}&code=76272004"
+        labs_query = f"{fhir_host}/Observation?subject={patient_id}&code={syphilis_loincs}"
+        diagnositic_report_query = f"{fhir_host}/DiagnosticReport?subject={patient_id}&code={syphilis_loincs}"
+        
+        queries = [conditon_query, labs_query, diagnositic_report_query]
+        
+        use_case_response = None
+        for query in queries:
+            partial_response = session.get(query)
+            print(query)
+            if use_case_response is None:
+                use_case_response = partial_response.json()
+            else:
+                use_case_response["entry"].extend(partial_response.json()["entry"])
+            use_case_response["total"] = len(use_case_response["entry"])
+    return use_case_response
