b9bfe061489b990b700c115d28c449d9e7cf80fb
nickbristow <richard.nick.bristow@gmail.com>
2024-01-22 16:16:32 -0500

    Refactors to reduce code reuse

--- containers/ecr-viewer/src/app/view-data/page.tsx
+++ containers/ecr-viewer/src/app/view-data/page.tsx
@@ -3,11 +3,7 @@ import EcrSummary from "@/app/view-data/components/EcrSummary";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Bundle } from "fhir/r4";
-import Demographics from "./components/Demographics";
-import SocialHistory from "./components/SocialHistory";
 import { Accordion } from "@trussworks/react-uswds";
-import UnavailableInfo from "./components/UnavailableInfo";
-import EcrMetadata from "./components/EcrMetadata";
 import {
   PathMappings,
   evaluateSocialData,
@@ -15,6 +11,10 @@ import {
   evaluateProviderData,
   evaluateDemographicsData,
 } from "../utils";
+import Demographics from "./components/Demographics";
+import SocialHistory from "./components/SocialHistory";
+import UnavailableInfo from "./components/UnavailableInfo";
+import EcrMetadata from "./components/EcrMetadata";
 import EncounterDetails from "./components/Encounter";
 
 const ECRViewerPage = () => {
