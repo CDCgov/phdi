46cbf2e1d53d9c55ab06ed387cb0c15660f8f77c
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-01-24 10:23:37 -0500

    add clinical info section

---
+++ containers/ecr-viewer/src/app/view-data/components/ClinicalInfo.tsx
@@ -0,0 +1,19 @@
+import { DisplayData, renderData } from "../../utils";
+import { AccordianDiv, AccordianH3, AccordianSection } from "@/app/view-data/component-utils";
+
+interface ClinicalInfoProps {
+  clinicalNotes: DisplayData[];
+}
+
+const ClinicalInfo = ({ clinicalNotes }: ClinicalInfoProps) => {
+  return(
+    <AccordianSection>
+      <AccordianH3>Clinical Info</AccordianH3>
+      <AccordianDiv>
+        {renderData(clinicalNotes)}
+      </AccordianDiv>
+    </AccordianSection>
+  )
+}
+
+export default ClinicalInfo;
