f877f2ae188c8b451675b82c45527ec46c086080
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-01-24 15:52:02 -0500

    fix line wrap

--- containers/ecr-viewer/src/app/view-data/components/EcrMetadata.tsx
+++ containers/ecr-viewer/src/app/view-data/components/EcrMetadata.tsx
@@ -19,54 +19,60 @@ const EcrMetadata = ({
   eCRSenderDetails,
 }: EcrMetadataProps) => {
   return (
-    <AccordianSection>
-      <AccordianH3>RR Details</AccordianH3>
-      <AccordianDiv>
-        {rrDetails.map(({ title, value }) => {
-          return (
-            <Fragment key={title}>
-              <div className="grid-row">
-                <div className="data-title">
-                  <h4>{title}</h4>
-                </div>
-                <div className="grid-col-fill">{value}</div>
-              </div>
-              <div className={"section__line_gray"} />
-            </Fragment>
-          );
-        })}
-        <div className={"padding-bottom-1"} />
-        <AccordianH3>eICR Details</AccordianH3>
-        {eicrDetails.map(({ title, value }) => {
-          return (
-            <Fragment key={title}>
-              <div className="grid-row">
-                <div className="data-title">
-                  <h4>{title}</h4>
-                </div>
-                <div className="grid-col-auto">{value}</div>
-              </div>
-              <div className={"section__line_gray"} />
-            </Fragment>
-          );
-        })}
-        <div className={"padding-bottom-1"} />
-        <AccordianH3>eCR Sender Details</AccordianH3>
-        {eCRSenderDetails.map(({ title, value }) => {
-          return (
-            <Fragment key={title}>
-              <div className="grid-row">
-                <div className="data-title">
-                  <h4>{title}</h4>
-                </div>
-                <div className="grid-col-auto">{value}</div>
-              </div>
-              <div className={"section__line_gray"} />
-            </Fragment>
-          );
-        })}
-      </AccordianDiv>
-    </AccordianSection>
+      <AccordianSection>
+        <AccordianH3>
+          RR Details
+        </AccordianH3>
+          <AccordianDiv>
+            {rrDetails.map(({ title, value }) => {
+              return (
+                <Fragment key={title}>
+                  <div className="grid-row">
+                    <div className="data-title">
+                      <h4>{title}</h4>
+                    </div>
+                    <div className="grid-col-fill text-pre-line">{value}</div>
+                  </div>
+                  <div className={"section__line_gray"} />
+                </Fragment>
+              );
+            })}
+            <div className={"padding-bottom-1"} />
+            <AccordianH3>
+              eICR Details
+            </AccordianH3>
+            {eicrDetails.map(({ title, value }) => {
+              return (
+                <Fragment key={title}>
+                  <div className="grid-row">
+                    <div className="data-title">
+                      <h4>{title}</h4>
+                    </div>
+                    <div className="grid-col-auto text-pre-line">{value}</div>
+                  </div>
+                  <div className={"section__line_gray"} />
+                </Fragment>
+              );
+            })}
+            <div className={"padding-bottom-1"} />
+            <AccordianH3>
+              eCR Sender Details
+            </AccordianH3>
+            {eCRSenderDetails.map(({ title, value }) => {
+              return (
+                <Fragment key={title}>
+                  <div className="grid-row">
+                    <div className="data-title">
+                      <h4>{title}</h4>
+                    </div>
+                    <div className="grid-col-auto text-pre-line">{value}</div>
+                  </div>
+                  <div className={"section__line_gray"} />
+                </Fragment>
+              );
+            })}
+        </AccordianDiv>
+      </AccordianSection>
   );
 };
 
