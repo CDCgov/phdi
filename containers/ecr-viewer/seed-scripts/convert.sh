aa2d6aab6ba45f943c7d7ec2d5a78ea76f93fa0d
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-01-25 14:33:19 -0500

    save json to proper folder

--- containers/ecr-viewer/seed-scripts/convert.sh
+++ containers/ecr-viewer/seed-scripts/convert.sh
@@ -13,9 +13,10 @@ docker run --rm -d -it -p 8080:8080 $(docker build -q ../../fhir-converter/ )
 
 echo "Looping through folders in baseECR"
 for d in baseECR/* ; do
+    #first escape ", then /, and finally remove all new lines
     rr=$(sed -e 's/"/\\"/g ; s=/=\\\/=g ; $!s/$/\\n/' "$d/CDA_RR.xml" | tr -d '\n')
     eicr=$(sed 's/"/\\"/g ; s=/=\\\/=g ; $!s/$/\\n/'  "$d/CDA_eICR.xml" | tr -d '\n')
     resp=$(curl -l 'http://localhost:8080/convert-to-fhir' --header 'Content-Type: application/json' --data-raw '{"input_type":"ecr","root_template":"EICR","input_data": "'"$eicr"'","rr_data": "'"$rr"'"}')
-    echo $resp | jq '.response.FhirResource' > "./fhir_data/a.json"
+    echo $resp | jq '.response.FhirResource' > "./fhir_data/$(basename $d).json"
 
 done
