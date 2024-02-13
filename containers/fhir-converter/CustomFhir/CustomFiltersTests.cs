d32a67e9dcee67c26ac9b981688daa877b9b439f
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-02-13 11:15:34 -0500

    add complicated test

--- containers/fhir-converter/CustomFhir/CustomFiltersTests.cs
+++ containers/fhir-converter/CustomFhir/CustomFiltersTests.cs
@@ -15,7 +15,7 @@ public class CustomFilterTests
     [Fact]
     public void ToHtmlString_ObjectList_ReturnsStrings()
     {
-        var strList = new List<object>(){"Race", "car"};
+        var strList = new List<object>() { "Race", "car" };
         var actual = Filters.ToHtmlString(strList);
         Assert.Equal("Racecar", actual);
     }
@@ -59,12 +59,98 @@ public class CustomFilterTests
     [Fact]
     public void ToHtmlString_StringObjectDictionaryList_ReturnsEachItemWithTags()
     {
-        var strList = new List<object>(){"Race", "car"};
+        var strList = new List<object>() { "Race", "car" };
         var dictDict = new Dictionary<string, object>(){
             {"span", strList},
             {"/nSun", "flower"}
         };
         var actual = Filters.ToHtmlString(dictDict);
         Assert.Equal("<span>Race</span><span>car</span>", actual);
+    }
+
+    // <text>
+    // <table>
+    // <colgroup>
+    //     <col width="75%"/>
+    //     <col width="25%"/>
+    // </colgroup>
+    // <thead>
+    //     <tr>
+    //         <th>Active Problems</th>
+    //         <th>Noted Date</th>
+    //     </tr>
+    // </thead>
+    // <tbody>
+    //     <tr ID="problem13" styleCode="xRowNormal">
+    //         <td ID="problem13name">Parkinson's syndrome</td>
+    //         <td>7/25/22</td>
+    //     </tr>
+    //     <tr ID="problem12" styleCode="xRowAlt">
+    //         <td ID="problem12name">Essential hypertension</td>
+    //         <td>7/21/22</td>
+    //     </tr>
+    // </tbody>
+    // </table>
+    // <footnote ID="subTitle11" styleCode="xSectionSubTitle">documented as of this encounter (statuses as of 07/25/2022)</footnote>
+    // </text>
+    [Fact]
+    public void ToHtmlString_ComplicatedExample_ReturnsString()
+    {
+        var footnote = new Dictionary<string, object>(){
+            {"ID", "subTitle11"},
+            {"styleCode", "xSectionSubTitle"},
+            {"_", "documented as of this encounter (statuses as of 07/25/2022)"}
+        };
+        var tbodyTrTd12Column1 = new Dictionary<string, object>(){
+            {"ID", "problem12name"},
+            {"_", "Essential hypertension"},
+        };
+        var tbodyTrTd12 = new List<object>() { tbodyTrTd12Column1, "7/21/22" };
+        var tbodyTr12 = new Dictionary<string, object>(){
+            {"ID", "problem12"},
+            {"styleCode", "xRowAlt"},
+            {"td", tbodyTrTd12},
+        };
+        var tbodyTrTd13Column1 = new Dictionary<string, object>(){
+            {"ID", "problem13name"},
+            {"_", "Parkinson's syndrome"},
+        };
+        var tbodyTrTd13 = new List<object>() { tbodyTrTd13Column1, "7/25/22" };
+        var tbodyTr13 = new Dictionary<string, object>(){
+            {"ID", "problem13"},
+            {"styleCode", "xRowNormal"},
+            {"td", tbodyTrTd13},
+        };
+        var tbodyTrList = new List<object>() { tbodyTr13, tbodyTr12 };
+        var tbody = new Dictionary<string, object>(){
+            {"tr", tbodyTrList},
+        };
+        var theadTrTh = new List<object>() { "Active Problems", "Noted Date" };
+        var theadTr = new Dictionary<string, object>(){
+            {"th", theadTrTh},
+        };
+        var thead = new Dictionary<string, object>(){
+            {"tr", theadTr},
+        };
+        var col = new Dictionary<string, object>(){
+            {"width", "50%"},
+        };
+        var colList = new List<object>() {col, col};
+        var colGroup = new Dictionary<string, object>(){
+            {"col", colList},
+        };
+        var table = new Dictionary<string, object>(){
+            {"colgroup", colGroup},
+            {"thead", thead},
+            {"tbody", tbody}
+        };
+        var completeDict = new Dictionary<string, object>(){
+            {"table", table},
+            {"footnote", footnote}
+        };
+
+
+        var actual = Filters.ToHtmlString(completeDict);
+        Assert.Equal("<table><thead><tr><th>Active Problems</th><th>Noted Date</th></tr></thead><tbody><tr><td>Parkinson's syndrome</td><td>7/25/22</td></tr><tr><td>Essential hypertension</td><td>7/21/22</td></tr></tbody></table>documented as of this encounter (statuses as of 07/25/2022)", actual);
     }
 }
