a60ef84e419f311e3fa92cf8ddabcfb08ef8e047
Kenneth Chow <kenneth@skylight.digital>
2024-02-21 11:19:27 -0600

    Adding s3 route and environment variables

---
+++ containers/ecr-viewer/src/app/api/s3/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { loadYamlConfig, streamToJson } from "../utils";
+
+// Initialize the S3 client outside of your API route handler to reuse the connection
+const s3Client = new S3Client({ region: process.env.AWS_REGION });
+
+export async function GET(request: NextRequest) {
+  const params = request.nextUrl.searchParams;
+  const ecr_id = params.get("id") ? params.get("id") : null;
+
+  const bucketName = process.env.ECR_BUCKET_NAME;
+  const objectKey = `${ecr_id}.json`; // This could also come from the request, e.g., req.query.key
+
+  try {
+    const command = new GetObjectCommand({
+      Bucket: bucketName,
+      Key: objectKey,
+    });
+
+    const { Body } = await s3Client.send(command);
+    const content = await streamToJson(Body);
+
+    return NextResponse.json(
+      { fhirBundle: content, fhirPathMappings: loadYamlConfig() },
+      { status: 200 },
+    );
+  } catch (error: any) {
+    console.error("S3 GetObject error:", error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
--- containers/ecr-viewer/src/app/view-data/page.tsx
+++ containers/ecr-viewer/src/app/view-data/page.tsx
@@ -14,6 +14,18 @@ import { clinicalInfoConfig } from "./components/ClinicalInfo";
 import { PathMappings } from "../utils";
 import SideNav, { SectionConfig } from "./components/SideNav";
 import { processSnomedCode } from "./service";
+// string constants to match with possible .env values
+const S3_SOURCE = "s3";
+const POSTGRES_SOURCE = "postgres";
+
+const assignApiPath = () => {
+  console.log(process.env.NEXT_PUBLIC_SOURCE);
+  if (process.env.NEXT_PUBLIC_SOURCE === S3_SOURCE) {
+    return "s3";
+  } else if (process.env.NEXT_PUBLIC_SOURCE === POSTGRES_SOURCE) {
+    return "fhir-data";
+  }
+};
 
 const ECRViewerPage = () => {
   const [fhirBundle, setFhirBundle] = useState<Bundle>();
@@ -21,6 +33,7 @@ const ECRViewerPage = () => {
   const [errors, setErrors] = useState<Error | unknown>(null);
   const searchParams = useSearchParams();
   const fhirId = searchParams.get("id") ?? "";
+  const apiPath = assignApiPath();
   const snomedCode = searchParams.get("snomed-code") ?? "";
 
   type ApiResponse = {
@@ -32,7 +45,7 @@ const ECRViewerPage = () => {
     // Fetch the appropriate bundle from Postgres database
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/fhir-data?id=${fhirId}`);
+        const response = await fetch(`/api/${apiPath}?id=${fhirId}`);
         if (!response.ok) {
           const errorData = await response.json();
           throw new Error(errorData.message || "Internal Server Error");
