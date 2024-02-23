02fa366a4424c58998e61ffdfd92b2522250968a
Boban <BobanL@users.noreply.github.com>
2024-02-23 13:44:31 -0500

    set ecr viewer's auth parameter as cookie (#1366)
    * add type to snomed code
    
    * add middleware to strip the token out of the URL and save it as a cookie
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ containers/ecr-viewer/src/middleware.test.ts
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment node
+ */
+import { middleware } from "@/middleware";
+import { NextRequest, NextResponse } from "next/server";
+import { NextURL } from "next/dist/server/web/next-url";
+
+describe("Middleware", () => {
+  const redirectSpy = jest.spyOn(NextResponse, "redirect");
+
+  afterEach(() => {
+    redirectSpy.mockReset();
+  });
+  it("should strip the auth query param and set the token", async () => {
+    const req = new NextRequest(
+      "https://www.example.com/view-data?id=1234&auth=abcd",
+    );
+
+    const resp = middleware(req);
+    expect(resp.cookies.get("auth-token")).toEqual({
+      name: "auth-token",
+      path: "/",
+      value: "abcd",
+      httpOnly: true,
+    });
+    expect(redirectSpy).toHaveBeenCalledTimes(1);
+    expect(redirectSpy as unknown as NextURL).toHaveBeenCalledWith(
+      new NextURL("view-data?id=1234", req.url, {
+        headers: {},
+        nextConfig: undefined,
+      }),
+    );
+  });
+});
