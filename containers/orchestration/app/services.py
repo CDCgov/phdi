a18a2d0922049663c98023b3786c8b8c67b943fb
Lina Roth <lina.roth.ctr@teambespin.us>
2024-02-27 13:37:18 -0500

    Remove IC

--- containers/orchestration/app/services.py
+++ containers/orchestration/app/services.py
@@ -26,7 +26,6 @@ from fastapi import HTTPException
 from fastapi import Response
 from fastapi import WebSocket
 from fastapi.encoders import jsonable_encoder
-from icecream import ic
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
@@ -192,7 +191,6 @@ def save_to_db(**kwargs) -> dict:
     endpoint. Once this endpoint exists in the eCR viewer, we can
     write full-fledged request and response handlers for this service.
     """
-    ic(kwargs)
     ecr_id = kwargs["payload"]["ecr_id"]
     payload_data = kwargs["payload"]["data"]
     url = kwargs["url"]
@@ -312,7 +310,6 @@ async def call_apis(
     :return: A tuple holding the concluding status code of the orchestration
       service, as well as each step's response along the way.
     """
-    ic("hello")
     workflow = config.get("workflow", [])
     current_message = input.get("message")
     response = current_message
@@ -323,7 +320,6 @@ async def call_apis(
     for step in workflow:
         service = step["service"]
         endpoint = step["endpoint"]
-        ic(service)
         endpoint_name = endpoint.split("/")[-1]
         params = step.get("params", None)
 
