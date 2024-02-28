0ca088558e1bfbdca052d8aa69c4e947e1acda7b
bamader <brandon@skylight.digital>
2024-02-28 16:44:54 -0500

    Clean up tests and config model

--- containers/orchestration/tests/test_services.py
+++ containers/orchestration/tests/test_services.py
@@ -4,6 +4,9 @@ from fastapi import HTTPException
 from requests.models import Response
 
 
+# TODO: Update this test once save_to_db is created as an actual
+# endpoint, but for now, don't mess with the structure as this is
+# needed for compatibility with the demo UI.
 def test_save_to_db_payload():
     response = Response()
     response.status_code = 200
@@ -17,6 +20,9 @@ def test_save_to_db_payload():
     assert result == expected_result
 
 
+# TODO: Update this test once save_to_db is created as an actual
+# endpoint, but for now, don't mess with the structure as this is
+# needed for compatibility with the demo UI.
 def test_save_to_db_failure_missing_eicr_id():
     response = Response()
     response.status_code = 200
