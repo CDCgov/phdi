832d13c1ede1fd53550994219c90efb3f228acfc
David McClatchey <david@skylight.digital>
2024-03-01 10:05:02 -0500

    unused function cleanup

--- containers/message-parser/tests/test_phdc.py
+++ containers/message-parser/tests/test_phdc.py
@@ -19,17 +19,6 @@ from lxml import etree as ET
 from xmldiff import formatting
 from xmldiff import main as xmldiff
 
-
-def read_json_from_test_assets(filename: str) -> dict:
-    """
-    Reads a JSON file from the test assets directory.
-
-    :param filename: The name of the file to read.
-    :return: A dictionary containing the contents of the file.
-    """
-    return json.load(open((pathlib.Path(__file__).parent.parent / "assets" / filename)))
-
-
 def read_file_from_test_assets(filename: str) -> str:
     """
     Reads a file from the test assets directory.
