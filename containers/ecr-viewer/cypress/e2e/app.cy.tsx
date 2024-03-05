f0e5319d378f744a89144a9f7fe8b24de0f3a724
Kenneth Chow <kenneth@skylight.digital>
2024-03-05 16:39:23 -0600

    removing test for redirect

--- containers/ecr-viewer/cypress/e2e/app.cy.tsx
+++
@@ -1,9 +0,0 @@
-describe("Navigation", () => {
-  it("should redirect when url contains /ecr-viewer", () => {
-    // Visit bad url
-    cy.visit("http://localhost:3000/ecr-viewer/view-data");
-
-    // The new url should NOT include "/ecr-viewer"
-    cy.url().should("not.include", "/ecr-viewer");
-  });
-});
