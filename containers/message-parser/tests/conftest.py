677cb3c0cc646751ced9406a52ee2d2f11808721
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-03-06 15:34:55 -0800

    conftest is magic; DRY'd up the validate_xml function in the two validation tests

---
+++ containers/message-parser/tests/conftest.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import lxml.etree as ET
+import pytest
+from rich.console import Console
+from rich.table import Table
+
+
+@pytest.fixture
+def validate_xml():
+    def _validate(xml_input: ET.ElementTree) -> bool:
+        console = Console()
+
+        # adjust the path to locate the XSD file relative to this conftest.py file
+        xsd_path = (
+            Path(__file__).parents[1]
+            / "schema"
+            / "extensions"
+            / "SDTC"
+            / "infrastructure"
+            / "cda"
+            / "CDA_SDTC.xsd"
+        )
+
+        with open(xsd_path, "rb") as xsd_file:
+            xsd_tree = ET.XMLSchema(ET.parse(xsd_file))
+            console.print("XSD schema loaded successfully", style="bold green")
+
+        is_valid = xsd_tree.validate(xml_input)
+
+        if is_valid:
+            console.print(
+                "The XML file is valid according to the XSD schema", style="bold green"
+            )
+            return True
+        else:
+            console.print("The XML file is not valid", style="bold red")
+            table = Table(
+                title="Validation Errors", show_header=True, header_style="bold magenta"
+            )
+            table.add_column("Line", style="dim", width=6, justify="right")
+            table.add_column("Column", style="dim", width=6, justify="right")
+            table.add_column("Message", overflow="fold")
+            for error in xsd_tree.error_log:
+                table.add_row(str(error.line), str(error.column), error.message)
+            console.print(table)
+            return False
+
+    return _validate
