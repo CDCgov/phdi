d43c80f8fba7ceb172fd91d708a68c06495d81c6
angelathe <angela.the@skylight.digital>
2024-03-06 11:59:17 -0800

    add cypress test, unsure if using

---
+++ containers/ecr-viewer/cypress/e2e/api/save-fhir-data.cy.tsx
@@ -0,0 +1,80 @@
+const testBody = {
+  fhirBundle: {
+    resourceType: "Bundle",
+    type: "batch",
+    entry: [
+      {
+        fullUrl: "urn:uuid:12345",
+        resource: {
+          resourceType: "Composition",
+          id: "12345",
+        },
+      },
+    ],
+  },
+};
+
+describe("POST /api/save-fhir-data", () => {
+  it("should successfully save FHIR Bundle to the database", () => {
+    cy.intercept("POST", "/api/save-fhir-data", (req) => {
+      // Mock response
+      req.reply({
+        statusCode: 200,
+        body: {
+          message: "Success. Saved FHIR Bundle to database: test_reply",
+        },
+        headers: {
+          "content-type": "text/html",
+        },
+      });
+    }).as("successPostRequest");
+
+    cy.visit({
+      method: "POST",
+      url: "http://localhost:3000/api/save-fhir-data",
+      body: testBody,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    cy.wait("@successPostRequest").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      cy.contains("Success. Saved FHIR Bundle to database: test_reply").should(
+        "exist",
+      );
+    });
+  });
+
+  it("should fail to save FHIR Bundle to the database", () => {
+    cy.intercept("POST", "/api/save-fhir-data", (req) => {
+      // Mock response
+      req.reply({
+        statusCode: 400,
+        body: {
+          message: "Failed to insert data to database. test_reply",
+        },
+        headers: {
+          "content-type": "text/html",
+        },
+      });
+    }).as("failPostRequest");
+
+    cy.visit({
+      method: "POST",
+      url: "http://localhost:3000/api/save-fhir-data",
+      body: testBody,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      failOnStatusCode: false,
+    });
+
+    cy.wait("@failPostRequest").then((interception) => {
+      expect(interception.response.statusCode).to.eq(400);
+      cy.contains("Failed to insert data to database. test_reply").should(
+        "exist",
+      );
+    });
+  });
+});
