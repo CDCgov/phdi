593271611ac65f5bc5bfa018023424eab006fbaa
angelathe <angela.the@skylight.digital>
2024-03-07 13:44:51 -0800

    reverting changes made when attempting jest test

--- containers/ecr-viewer/jest.setup.ts
+++ containers/ecr-viewer/jest.setup.ts
@@ -1,10 +1,6 @@
 import "@testing-library/jest-dom";
-import "cross-fetch/polyfill";
 import { toHaveNoViolations } from "jest-axe";
-import { TextEncoder, TextDecoder } from "util";
 import * as matchers from "jest-extended";
 
 expect.extend(toHaveNoViolations);
 expect.extend(matchers);
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
