b54f5bd3e2ee9a1959fc9c2fcbbef407c75f804f
Angela The <angela.the@skylight.digital>
2024-03-12 10:37:15 -0700

    Save FHIR data to S3 in ecr-viewer (#1456)
    * refactor route to prep for adding functionality to save to s3
    
    * wip. add saveToS3, incomplete
    
    * first pass at saving to s3
    
    * rename s3_route test, add placeholder for aws keys
    
    * add local env script
    
    * wip - first pass at unit test
    
    * tests and types
    
    * add tests
    
    * add jsdoc
    
    * Replace new NextResponses with NextResponse.json
    
    * Make error message consistent
    
    * refactor save api helper functions to separate service file
    
    * refactor view api helper functions to separate service file
    
    * Updated JSDocs
    
    ---------
    
    Co-authored-by: nickbristow <richard.nick.bristow@gmail.com>

--- containers/ecr-viewer/src/app/api/save-fhir-data/route.ts
+++ containers/ecr-viewer/src/app/api/save-fhir-data/route.ts
@@ -1,13 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import pgPromise from "pg-promise";
+import { saveToS3, saveToPostgres } from "./save-fhir-data-service";
+
+const S3_SOURCE = "s3";
+const POSTGRES_SOURCE = "postgres";
 
 /**
  * Handles POST requests and saves the FHIR Bundle to the database.
  *
- * @param request - The incoming request object. Expected to have a JSON body in the format `{"fhirBundle":{}, "saveSource": "postgres|s3""}`.
+ * @param request - The incoming request object. Expected to have a JSON body in the format `{"fhirBundle":{}, "saveSource": "postgres|s3""}`. FHIR bundle must include the ecr ID under entry[0].resource.id.
  * @returns A `NextResponse` object with a JSON payload indicating the success message and the status code set to 200. The response content type is set to `application/json`.
  *
- * @example - Saving to Postgres
+ * @example - Request body
  * ```typescript
  * const body = {
     "fhirBundle": {
@@ -25,9 +28,6 @@ import pgPromise from "pg-promise";
     },
     "saveSource": "postgres"
   }
- * const request = new NextRequest({ body: JSON.stringify(body) });
- * const response = await POST(request);
- * console.log(response);
  * ```
  */
 
@@ -44,58 +44,43 @@ export async function POST(request: NextRequest) {
     ecrId = requestBody.fhirBundle.entry[0].resource.id;
   } catch (error: any) {
     console.error("Error reading request body:", error);
-    return new NextResponse(
-      JSON.stringify({ error: "Error reading request body. " + error.message }),
-      { status: 400, headers: { "content-type": "application/json" } },
+    return NextResponse.json(
+      { message: "Error reading request body. " + error.message },
+      { status: 400 },
+    );
+  }
+
+  if (!fhirBundle || !ecrId) {
+    return NextResponse.json(
+      {
+        message:
+          "Error reading request body. Body must include a FHIR bundle with an ID.",
+      },
+      { status: 400 },
     );
   }
 
   if (!saveSource) {
-    return new NextResponse(
-      JSON.stringify({
+    return NextResponse.json(
+      {
         message:
           "Save location is undefined. Please provide a valid value for 'saveSource' (postgres or s3).",
-      }),
-      { status: 400, headers: { "content-type": "application/json" } },
+      },
+      { status: 400 },
     );
   }
 
-  const db_url = process.env.DATABASE_URL || "";
-  const db = pgPromise();
-  const database = db(db_url);
-
-  if (fhirBundle && ecrId) {
-    const { ParameterizedQuery: PQ } = pgPromise;
-    const addFhir = new PQ({
-      text: "INSERT INTO fhir VALUES ($1, $2) RETURNING ecr_id",
-      values: [ecrId, fhirBundle],
-    });
-
-    try {
-      const saveECR = await database.one(addFhir);
-
-      return new NextResponse(
-        JSON.stringify({
-          message: "Success. Saved FHIR Bundle to database: " + saveECR.ecr_id,
-        }),
-        { status: 200, headers: { "content-type": "application/json" } },
-      );
-    } catch (error: any) {
-      console.error("Error inserting data to database:", error);
-      return new NextResponse(
-        JSON.stringify({
-          message: "Failed to insert data to database." + error.message,
-        }),
-        { status: 400, headers: { "content-type": "application/json" } },
-      );
-    }
+  if (saveSource === S3_SOURCE) {
+    return saveToS3(fhirBundle, ecrId);
+  } else if (saveSource === POSTGRES_SOURCE) {
+    return await saveToPostgres(fhirBundle, ecrId);
   } else {
-    return new NextResponse(
-      JSON.stringify({
+    return NextResponse.json(
+      {
         message:
-          "Invalid request body. Body must include a FHIR bundle with an ID.",
-      }),
-      { status: 400, headers: { "content-type": "application/json" } },
+          "Invalid save source. Please provide a valid source (postgres or s3)",
+      },
+      { status: 400 },
     );
   }
 }
