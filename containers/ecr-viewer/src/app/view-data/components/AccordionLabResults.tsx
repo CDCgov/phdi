6554f7a8b12e63ebf2c51cb176c8c9769b4d2830
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-03-13 15:21:51 -0400

    add jsdocs for AccordionLabResults.tsx

--- containers/ecr-viewer/src/app/view-data/components/AccordionLabResults.tsx
+++ containers/ecr-viewer/src/app/view-data/components/AccordionLabResults.tsx
@@ -1,16 +1,25 @@
 import { Accordion, Icon, Tag } from "@trussworks/react-uswds";
 import React from "react";
 
-interface AccordionRRProps {
+interface AccordionLabResultsProps {
   title: string;
   abnormalTag: boolean;
   content: React.JSX.Element;
 }
-export const AccordionLabResults: React.FC<AccordionRRProps> = ({
+
+/**
+ * Accordion component for displaying lab results.
+ * @param {AccordionLabResultsProps} props - The props object.
+ * @param {string} props.title - The title of the lab result.
+ * @param {boolean} props.abnormalTag - Boolean value if the lab result is abnormal.
+ * @param {React.JSX.Element} props.content - The content within the accordian.
+ * @returns {React.JSX.Element} React element representing the AccordionLabResults component.
+ */
+export const AccordionLabResults: React.FC<AccordionLabResultsProps> = ({
   title,
   abnormalTag,
   content,
-}) => {
+}: AccordionLabResultsProps): React.JSX.Element => {
   return (
     <Accordion
       items={[
