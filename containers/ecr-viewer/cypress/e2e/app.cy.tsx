f72970450304274afbe204b7533907736e5d479f
emmastephenson <emma.stephenson.24@gmail.com>
2024-03-20 16:19:28 -0700

    add more e2e tests and update error messaging

---
+++ containers/ecr-viewer/cypress/e2e/app.cy.tsx
@@ -0,0 +1,21 @@
+describe("Happy Path", () => {
+  it("Should load the eCR Viewer for a known ID", () => {
+    // Visit a valid URL
+    cy.visit(
+      "http://localhost:3000/view-data?id=6100896d-b520-497c-b2fe-1c111c679274",
+    );
+
+    // Gets the expected content
+    cy.get("#about-the-patient").contains("About the Patient");
+    cy.contains("VICTORIA HUNTER");
+  });
+});
+
+describe("Failing Path", () => {
+  it("Should return an error when given an invalid ID", () => {
+    //Visit an invalid URL
+    cy.visit("http://localhost:3000/view-data?id=123");
+
+    cy.contains("Sorry, we couldn't find this eCR ID.");
+  });
+});
