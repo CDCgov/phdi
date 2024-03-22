81c8f5e6d3e7ae33d84d850961e66d0acc72ccfe
m-goggins <marcelle.goggins@gmail.com>
2024-03-22 14:46:36 -0700

    remove extra page

--- containers/tefca-viewer-nextjs/src/app/patient-search/page.tsx
+++ containers/tefca-viewer-nextjs/src/app/patient-search/page.tsx
@@ -1,26 +1,44 @@
+// "use client";
+
+// import { useFormState, useFormStatus } from "react-dom";
+// import { createTodo } from "@/app/utils.tsx";
+
+// const initialState = {
+//     message: "",
+// };
+
+// function SubmitButton() {
+//     const { pending } = useFormStatus();
+
+//     return (
+//         <button type="submit" aria-disabled={pending}>
+//             Add
+//         </button>
+//     );
+// }
+
+// export function AddForm() {
+//     const [state, formAction] = useFormState(createTodo, initialState);
+
+//     return (
+//         <form action={formAction}>
+//             <label htmlFor="todo">Enter Task</label>
+//             <input type="text" id="todo" name="todo" required />
+//             <SubmitButton />
+//             <p aria-live="polite" className="sr-only" role="status">
+//                 {state?.message}
+//             </p>
+//         </form>
+//     );
+// }
+
 "use client";
 import React, { useState } from "react";
 
-const Page: React.FC = () => {
+const PatientSearch: React.FC = () => {
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
   const [fhirServer, setFhirServer] = useState<string>("");
-
-  const handleFirstNameChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setFirstName(event.target.value);
-  };
-
-  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLastName(event.target.value);
-  };
-
-  const handleFhirServerChange = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    setFhirServer(event.target.value);
-  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -37,94 +55,49 @@ const Page: React.FC = () => {
         type="text/css"
         href="patient-search/style.css"
       ></link>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="fhirServer">FHIR Server:</label>
-          <select
-            id="fhirServer"
-            value={fhirServer}
-            onChange={handleFhirServerChange}
-            required
-          >
-            <option value="">Select FHIR Server</option>
-            <option value="meld">Meld</option>
-            <option value="ehealth-exchange">eHealth Exchange</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={handleFirstNameChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={handleLastNameChange}
-            required
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+
+      <div>
+        <label htmlFor="fhirServer">FHIR Server:</label>
+        <select
+          id="fhirServer"
+          value={fhirServer}
+          onChange={(event) => {
+            setFhirServer(event.target.value);
+          }}
+          required
+        >
+          <option value="">Select FHIR Server</option>
+          <option value="meld">Meld</option>
+          <option value="ehealth-exchange">eHealth Exchange</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(event) => {
+            setFirstName(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(event) => {
+            setLastName(event.target.value);
+          }}
+        />
+      </div>
+      <button type="button" onClick={handleSubmit}>
+        Submit
+      </button>
     </div>
   );
 };
 
-export default Page;
-
-// 'use client'
-// import { ProcessList, ProcessListItem, ProcessListHeading, Button, Link, Form } from '@trussworks/react-uswds'
-// import { useRouter } from 'next/navigation';
-// import { useState } from 'react';
-
-// const router = useRouter();
-
-// const MyForm = () => {
-//     const handleClick = () => {
-//         router.push('/data-viewer')
-//     }
-//     const [formData, setFormData] = useState({
-//         name: '',
-//         email: '',
-//         message: '',
-//     });
-
-//     const handleChange = (e) => {
-//         setFormData({ ...formData, [e.target.name]: e.target.value });
-//     };
-
-//     return (
-//         <form onSubmit={handleClick}>
-//             <input
-//                 type="text"
-//                 name="name"
-//                 value={formData.name}
-//                 onChange={handleChange}
-//                 placeholder="Your Name"
-//             />
-//             <input
-//                 type="email"
-//                 name="email"
-//                 value={formData.email}
-//                 onChange={handleChange}
-//                 placeholder="Your Email"
-//             />
-//             <textarea
-//                 name="message"
-//                 value={formData.message}
-//                 onChange={handleChange}
-//                 placeholder="Your Message"
-//             ></textarea>
-//             <Button type="button" onClick={handleClick}>Submit</Button>
-//         </form>
-//     );
-// };
-
-// export default MyForm;
+export default PatientSearch;
