4d8072c4ce7c8aff26040dbe809ed8c62580f42d
m-goggins <marcelle.goggins@gmail.com>
2024-04-03 08:29:03 -0700

    add ./integration/ to filepath

--- containers/message-parser/tests/conftest.py
+++ containers/message-parser/tests/conftest.py
@@ -87,7 +87,7 @@ def read_schema_from_default_schemas():
 @pytest.fixture(scope="session")
 def setup(request):
     print("Setting up tests...")
-    compose_path = os.path.join(os.path.dirname(__file__), "./")
+    compose_path = os.path.join(os.path.dirname(__file__), "./integration/")
     compose_file_name = "docker-compose.yaml"
     message_parser = DockerCompose(compose_path, compose_file_name=compose_file_name)
     parser_url = "http://0.0.0.0:8080"
