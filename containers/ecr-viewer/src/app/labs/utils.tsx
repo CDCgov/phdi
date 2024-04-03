5f2bbbf84cdd3d8da7b197facfb3d4e7438343c8
Lina Roth <lina@skylight.digital>
2024-04-03 17:22:06 -0400

    Add test method to lab info  (#1547)
    * add test method to lab results. add test. fix error
    
    * Fix casting
    
    * Fix tests
    
    * Fix build
    
    * remove unused import
    
    * Update snapshots and logic to fix build error
    
    * fix snapshots and tests and build
    
    * set timezone for jest watch
    
    * fix evalaute diagnosticreport tests
    
    ---------
    
    Co-authored-by: Boban <BobanL@users.noreply.github.com>
    Co-authored-by: angelathe <angela.the@skylight.digital>

--- containers/ecr-viewer/src/app/labs/utils.tsx
+++ containers/ecr-viewer/src/app/labs/utils.tsx
@@ -324,7 +324,15 @@ export const evaluateDiagnosticReportData = (
     { columnName: "Component", infoPath: "observationComponent" },
     { columnName: "Value", infoPath: "observationValue" },
     { columnName: "Ref Range", infoPath: "observationReferenceRange" },
-    { columnName: "Test Method", infoPath: "observationMethod" },
+    {
+      columnName: "Test Method",
+      value: returnFieldValueFromLabHtmlString(
+        report,
+        fhirBundle,
+        mappings,
+        "Test Method",
+      ) as string,
+    },
   ];
   return evaluateObservationTable(report, fhirBundle, mappings, columnInfo);
 };
