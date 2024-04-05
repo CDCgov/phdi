d7c7b4d2f688c4d0e1193c2fa0416fa9dde669c3
nickbristow <richard.nick.bristow@gmail.com>
2024-04-05 14:32:14 -0400

    wip

--- containers/ecr-viewer/src/app/view-data/service.tsx
+++ containers/ecr-viewer/src/app/view-data/service.tsx
@@ -1,3 +1,8 @@
+/**
+ * Processes a SNOMED code by performing placeholder operations and logging the code.
+ * @param {string} snomedCode - The SNOMED code to be processed.
+ * @returns {string} The processed SNOMED code.
+ */
 export const processSnomedCode = (snomedCode: string) => {
   //Placeholder
   console.log("SNOMED code: ", snomedCode);
