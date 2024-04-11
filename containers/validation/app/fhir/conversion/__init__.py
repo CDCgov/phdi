8ee80135cdf5b845efccb0ffb10da039b913a3f2
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-04-11 10:11:48 -0700

    removing code that doesn't need to be in validation

--- containers/validation/app/fhir/conversion/__init__.py
+++ containers/validation/app/fhir/conversion/__init__.py
@@ -1,7 +1,3 @@
 from app.fhir.conversion.convert import add_rr_data_to_eicr
-from app.fhir.conversion.convert import convert_to_fhir
 
-__all__ = (
-    "add_rr_data_to_eicr",
-    "convert_to_fhir",
-)
+__all__ = ("add_rr_data_to_eicr",)
--- containers/validation/app/transport/__init__.py
+++
@@ -1,3 +0,0 @@
-from .http import http_request_with_retry
-
-__all__ = ["http_request_with_retry"]
