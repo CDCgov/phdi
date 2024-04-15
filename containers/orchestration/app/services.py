5f631fc887e7d297e13747112f5481f610ffdb3b
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-04-15 09:49:51 -0400

    add handlers for save_bundle functionaility
    Co-authored-by: Josh Nygaard <jnygaard@skylight.digital>

--- containers/orchestration/app/services.py
+++ containers/orchestration/app/services.py
@@ -7,7 +7,6 @@ import requests
 from fastapi import HTTPException
 from fastapi import Response
 from fastapi import WebSocket
-from fastapi.encoders import jsonable_encoder
 
 from app.handlers import build_fhir_converter_request
 from app.handlers import build_geocoding_request
@@ -16,15 +15,16 @@ from app.handlers import build_ingestion_name_request
 from app.handlers import build_ingestion_phone_request
 from app.handlers import build_message_parser_message_request
 from app.handlers import build_message_parser_phdc_request
+from app.handlers import build_save_fhir_data_body
 from app.handlers import build_validation_request
 from app.handlers import ServiceHandlerResponse
 from app.handlers import unpack_fhir_converter_response
 from app.handlers import unpack_fhir_to_phdc_response
 from app.handlers import unpack_ingestion_standardization
 from app.handlers import unpack_parsed_message_response
+from app.handlers import unpack_save_fhir_data_response
 from app.handlers import unpack_validation_response
 from app.models import OrchestrationRequest
-from app.utils import CustomJSONResponse
 from app.utils import format_service_url
 
 # Locations of the various services the service will delegate
@@ -40,7 +40,7 @@ SERVICE_URLS = {
 # Mappings of endpoint names to the service input and output building
 # functions--lets the workflow config drive the API loop with no need
 # to change function signatures
-ENDPOINT_TO_REQUEST = {
+ENDPOINT_TO_REQUEST_BODY = {
     "validate": build_validation_request,
     "convert-to-fhir": build_fhir_converter_request,
     "geocode_bundle": build_geocoding_request,
@@ -49,6 +49,7 @@ ENDPOINT_TO_REQUEST = {
     "standardize_phones": build_ingestion_phone_request,
     "parse_message": build_message_parser_message_request,
     "fhir_to_phdc": build_message_parser_phdc_request,
+    "save-fhir-data": build_save_fhir_data_body,
 }
 ENDPOINT_TO_RESPONSE = {
     "validate": unpack_validation_response,
@@ -59,28 +60,8 @@ ENDPOINT_TO_RESPONSE = {
     "standardize_phones": unpack_ingestion_standardization,
     "parse_message": unpack_parsed_message_response,
     "fhir_to_phdc": unpack_fhir_to_phdc_response,
+    "save-fhir-data": unpack_save_fhir_data_response,
 }
-
-
-def send_to_ecr_viewer(bundle, source: str) -> dict:
-    """
-    Sends the bundle data to ecr_viewer. The source is either
-    postgres or S3
-
-    :param bundle: The fhir bundle data returned from fhir converter
-    :param source: Where the data will be saved postgres or s3
-    :return: A response object
-    """
-    fhir_bundle = bundle.json()["bundle"]
-    payload_body = {"fhirBundle": fhir_bundle, "saveSource": source}
-
-    try:
-        ecr_viewer_url_base = os.getenv("ECR_VIEWER_URL")
-        ecr_viewer_save_url = f"{ecr_viewer_url_base}/api/save-fhir-data"
-        msg = post_request(url=ecr_viewer_save_url, payload=payload_body)
-        return msg
-    except Exception as e:
-        return CustomJSONResponse(content=jsonable_encoder(e), status_code=500)
 
 
 def post_request(url: str, payload: dict) -> Response:
@@ -183,59 +164,37 @@ async def call_apis(
 
         service_url = format_service_url(SERVICE_URLS[service], endpoint)
 
-        # TODO: Once the save to DB functionality is registered as an actual
-        # service endpoint on the eCR viewer side, we can write real handlers
-        # for it and take out the if/else logic
-        if service not in ["save_to_s3", "save_to_db"]:
-            request_func = ENDPOINT_TO_REQUEST[endpoint_name]
-            response_func = ENDPOINT_TO_RESPONSE[endpoint_name]
-            service_request = request_func(current_message, input, params)
-            response = post_request(service_url, service_request)
-            bundle = save_bundle(response=response, bundle=bundle)
-            service_response = response_func(response)
+        request_body_func = ENDPOINT_TO_REQUEST_BODY[endpoint_name]
+        response_func = ENDPOINT_TO_RESPONSE[endpoint_name]
+        request_body = request_body_func(current_message, input, params)
+        response = post_request(service_url, request_body)
+        bundle = save_bundle(response=response, bundle=bundle)
+        service_response = response_func(response)
 
-            if websocket:
-                progress_dict = await _send_websocket_dump(
-                    endpoint_name,
-                    response,
-                    service_response,
-                    progress_dict,
-                    websocket,
-                )
+        if websocket:
+            progress_dict = await _send_websocket_dump(
+                endpoint_name,
+                response,
+                service_response,
+                progress_dict,
+                websocket,
+            )
 
-            if service_response.status_code != 200:
-                raise HTTPException(
-                    status_code=service_response.status_code,
-                    detail=f"Service {service} failed with error {service_response.msg_content}",  # noqa
-                )
+        if service_response.status_code != 200:
+            raise HTTPException(
+                status_code=service_response.status_code,
+                detail=f"Service {service} failed with error {service_response.msg_content}",  # noqa
+            )
 
-            if not service_response.should_continue:
-                raise HTTPException(
-                    status_code=400,
-                    detail=f"Service {service} completed, but orchestration cannot continue: "  # noqa
-                    + f"{service_response.msg_content}",
-                )
+        if not service_response.should_continue:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Service {service} completed, but orchestration cannot continue: "  # noqa
+                + f"{service_response.msg_content}",
+            )
 
-            # Validation reports only whether we should continue, with
-            # no updated data, so don't send error results to next
-            # service
-            if service != "validation":
-                current_message = service_response.msg_content
-            responses[service] = response
-        else:
-            if service == "save_to_s3":
-                response = send_to_ecr_viewer(bundle=bundle, source="s3")
-            elif service == "save_to_db":
-                response = send_to_ecr_viewer(bundle=bundle, source="postgres")
-
-            if websocket:
-                progress_dict = await _send_websocket_dump(
-                    endpoint_name,
-                    response,
-                    service_response,
-                    progress_dict,
-                    websocket,
-                )
-
-            responses[service] = response
+        # Validation and save_bundle do not contain any updates to the data
+        if service not in ["validation", "save_bundle"]:
+            current_message = service_response.msg_content
+        responses[service] = response
     return (response, responses)
