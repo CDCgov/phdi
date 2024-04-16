b77ac0d92bca78ab525b73476ecf0c923d9b3ed8
m-goggins <marcelle.goggins@gmail.com>
2024-04-16 14:54:21 -0700

    add placeholder test for /refine endpoint

--- containers/refiner/tests/test_refiner.py
+++ containers/refiner/tests/test_refiner.py
@@ -1,4 +1,5 @@
 from app.main import app
+from app.models import RefinerInput
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
@@ -10,3 +11,14 @@ def test_health_check():
     assert actual_response.json() == {
         "status": "OK",
     }
+
+
+def test_refiner(
+    input=RefinerInput(
+        message="test test",
+    ),
+    expected_successful_response={"refined_message": "test test", "status": "OK"},
+):
+    actual_response = client.post("/refine", json=input.dict())
+    assert actual_response.status_code == 200
+    assert actual_response.json() == expected_successful_response
