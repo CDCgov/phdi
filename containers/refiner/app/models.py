20d54d4c29a32c26f903676f90a91e51e474bed3
m-goggins <marcelle.goggins@gmail.com>
2024-04-16 11:16:48 -0700

    add refiner models

---
+++ containers/refiner/app/models.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from typing import List
+
+from pydantic import BaseModel
+from pydantic import Field
+from lxml import etree as ET
+
+
+class RefinerInput(BaseModel):
+    """
+    The schema for requests to the /refine endpoint.
+    """
+
+    include_headers: Optional[bool] = Field(
+        description="Include headers in the output."
+    )
+    fields_to_include: Optional[List[str]] = Field(
+        description="Fields to include in the refinement."
+    )
+    message: ET.ElementTree = Field(description="The XML to refine.")
+
+
+class RefinerResponse(BaseModel):
+    """
+    The schema for responses from the /refine endpoint.
+    """
+
+    status: str = Field(
+        description="A message describing the result of a request to "
+        "the /refine endpoint."
+    )
+    refined_message: ET.ElementTree = Field(description="The refined XML.")
