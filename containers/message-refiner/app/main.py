de6dbc172dc6caeb4ddaf51b432af390911382d0
Marcelle <53578688+m-goggins@users.noreply.github.com>
2024-04-18 09:46:41 -0700

    Implement `/refine` endpoint (#1656)
    * add refiner models
    
    * move reading files to dibbs/assets
    
    * sample requests/responses
    
    * update requirments + Dockerfile
    
    * add placeholder test for /refine endpoint
    
    * add refine-ecr placeholder
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * remove unsued imports
    
    * formatting
    
    * remove response status
    
    * add read from test assets
    
    * remove headers and fields from request body
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * update description to message refiner
    
    * remove custom models
    
    * parametrize test
    
    * add pytest import
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * move to message-refiner
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ containers/message-refiner/app/main.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from dibbs.base_service import BaseService
+from fastapi import Request
+from fastapi import Response
+
+# Instantiate FastAPI via DIBBs' BaseService class
+app = BaseService(
+    service_name="Message Refiner",
+    service_path="/message-refiner",
+    description_path=Path(__file__).parent.parent / "description.md",
+    include_health_check_endpoint=False,
+).start()
+
+
+@app.get("/")
+async def health_check():
+    """
+    Check service status. If an HTTP 200 status code is returned along with
+    '{"status": "OK"}' then the refiner service is available and running
+    properly.
+    """
+    return {"status": "OK"}
+
+
+@app.post("/ecr/")
+async def refine_ecr(
+    refiner_input: Request, sections_to_include: str | None = None
+) -> Response:
+    """
+    Refines an incoming XML message based on the fields to include and whether
+    to include headers.
+
+    :param request: The request object containing the XML input.
+    :param fields_to_include: The fields to include in the refined message.
+    :return: The RefinerResponse which includes the `refined_message`
+    """
+    data = await refiner_input.body()
+
+    if sections_to_include:
+        # TODO: add logic to filter the XML message based on the sections to include
+        refined_message = Response(content=data, media_type="application/xml")
+    else:
+        refined_message = Response(content=data, media_type="application/xml")
+
+    return refined_message
