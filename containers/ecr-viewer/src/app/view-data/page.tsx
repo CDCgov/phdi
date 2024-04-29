898ac59e3153914ee59ecd3035901cc6020e0572
KennethSkylight <107214304+KennethSkylight@users.noreply.github.com>
2024-04-29 11:24:59 -0500

    Make the ECR Wider and Fixing Random Styling Issues (#1692)
    * width
    
    * Fixing random issues
    
    * fixing test snapshots

--- containers/ecr-viewer/src/app/view-data/page.tsx
+++ containers/ecr-viewer/src/app/view-data/page.tsx
@@ -86,7 +86,7 @@ const ECRViewerPage: React.FC = () => {
                   fhirPathMappings={mappings}
                   fhirBundle={fhirBundle}
                 />
-                <div className="margin-top-6">
+                <div className="margin-top-10">
                   <GridContainer className={"padding-0 margin-bottom-3"}>
                     <Grid row>
                       <Grid>
