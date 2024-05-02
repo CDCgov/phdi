e7b4526deefd2b7a448bcebabf61c16c6846dff9
Kenneth Chow <kenneth@skylight.digital>
2024-05-02 11:53:46 -0500

    Adding the other two tests

--- containers/ingestion/tests/integration/test_ingestion.py
+++ containers/ingestion/tests/integration/test_ingestion.py
@@ -60,3 +60,89 @@ def test_standardize_names(setup, fhir_bundle):
 
     assert parsing_response.status_code == 200
     assert parsing_response.json() == expected_reference_response
+
+
+@pytest.mark.integration
+def test_standardize_phone(setup, fhir_bundle):
+    expected_reference_response = {
+        "status_code": "200",
+        "message": None,
+        "bundle": {
+            "resourceType": "Bundle",
+            "id": "bundle-transaction",
+            "meta": {"lastUpdated": "2018-03-11T11:22:16Z"},
+            "type": "transaction",
+            "entry": [
+                {
+                    "resource": {
+                        "resourceType": "Patient",
+                        "name": [
+                            {"family": "Smith", "given": ["DeeDee"], "use": "official"}
+                        ],
+                        "gender": "female",
+                        "telecom": [{"system": "phone", "value": "+18015557777"}],
+                        "address": [
+                            {
+                                "line": ["123 Main St."],
+                                "city": "Anycity",
+                                "state": "CA",
+                                "postalCode": "12345",
+                                "country": "USA",
+                            }
+                        ],
+                        "birthDate": "1955-11-05",
+                    },
+                    "request": {"method": "POST", "url": "Patient"},
+                }
+            ],
+        },
+    }
+
+    request = {"data": fhir_bundle}
+    parsing_response = httpx.post(PHONE_URL, json=request)
+
+    assert parsing_response.status_code == 200
+    assert parsing_response.json() == expected_reference_response
+
+
+@pytest.mark.integration
+def test_standardize_dob(setup, fhir_bundle):
+    expected_reference_response = {
+        "status_code": "200",
+        "message": None,
+        "bundle": {
+            "resourceType": "Bundle",
+            "id": "bundle-transaction",
+            "meta": {"lastUpdated": "2018-03-11T11:22:16Z"},
+            "type": "transaction",
+            "entry": [
+                {
+                    "resource": {
+                        "resourceType": "Patient",
+                        "name": [
+                            {"family": "Smith", "given": ["DeeDee"], "use": "official"}
+                        ],
+                        "gender": "female",
+                        "telecom": [{"system": "phone", "value": "8015557777"}],
+                        "address": [
+                            {
+                                "line": ["123 Main St."],
+                                "city": "Anycity",
+                                "state": "CA",
+                                "postalCode": "12345",
+                                "country": "USA",
+                            }
+                        ],
+                        "birthDate": "1955-11-05",
+                    },
+                    "request": {"method": "POST", "url": "Patient"},
+                }
+            ],
+        },
+    }
+
+    request = {"data": fhir_bundle}
+    parsing_response = httpx.post(DOB_URL, json=request)
+
+    assert parsing_response.status_code == 200
+    assert parsing_response.json() == expected_reference_response
