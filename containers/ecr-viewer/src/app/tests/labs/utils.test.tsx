58ff39b1226c376db3e13e739b96e2de04cab6b5
angelathe <angela.the@skylight.digital>
2024-05-06 12:20:43 -0700

    update use of getLabJsonObject and fix tests

--- containers/ecr-viewer/src/app/tests/labs/utils.test.tsx
+++ containers/ecr-viewer/src/app/tests/labs/utils.test.tsx
@@ -12,10 +12,6 @@ import { Bundle, Observation } from "fhir/r4";
 import { evaluate } from "fhirpath";
 import { render, screen } from "@testing-library/react";
 
-const pathLabReportAbnormal =
-  "Bundle.entry.resource.where(resourceType = 'DiagnosticReport').where(id = '68477c03-5689-f9e5-c267-a3c7bdff6fe0')";
-const labReportAbnormal = evaluate(BundleLab, pathLabReportAbnormal)[0];
-
 const pathLabReportNormal =
   "Bundle.entry.resource.where(resourceType = 'DiagnosticReport').where(id = 'c090d379-9aea-f26e-4ddc-378223841e3b')";
 const labReportNormal = evaluate(BundleLab, pathLabReportNormal)[0];
@@ -141,6 +137,152 @@ const labReportNormalJsonObject = {
     ],
   ],
 };
+const labReportAbnormalJsonObject = {
+  resultId: "Result.1.2.840.114350.1.13.297.3.7.2.798268.1670844",
+  resultName:
+    "(ABNORMAL) Stool Pathogens, NAAT, 12 to 25 Targets (09/28/2022 1:51 PM PDT)",
+  tables: [
+    [
+      {
+        Component: {
+          value: "Campylobacter, NAAT",
+          metadata: {
+            "data-id":
+              "Result.1.2.840.114350.1.13.297.3.7.2.798268.1670844.Comp1Name",
+          },
+        },
+        Value: {
+          value: "Not Detected",
+          metadata: {},
+        },
+        "Ref Range": {
+          value: "Not Detected",
+          metadata: {},
+        },
+        "Test Method": {
+          value: "LAB DEVICE: BIOFIRE® FILMARRAY® 2.0 SYSTEM",
+          metadata: {},
+        },
+        "Analysis Time": {
+          value: "09/28/2022 2:00 PM PDT",
+          metadata: {},
+        },
+        "Performed At": {
+          value:
+            "PROVIDENCE ST. JOSEPH MEDICAL CENTER LABORATORY (CLIA 05D0672675)",
+          metadata: {},
+        },
+        "Pathologist Signature": {
+          value: "",
+          metadata: {
+            "data-id":
+              "Result.1.2.840.114350.1.13.297.3.7.2.798268.1670844.Comp1Signature",
+          },
+        },
+      },
+      {
+        Component: {
+          value: "Plesiomonas shigelloides, NAAT",
+          metadata: {
+            "data-id":
+              "Result.1.2.840.114350.1.13.297.3.7.2.798268.1670844.Comp2Name",
+          },
+        },
+        Value: {
+          value: "Not Detected",
+          metadata: {},
+        },
+        "Ref Range": {
+          value: "Not Detected",
+          metadata: {},
+        },
+        "Test Method": {
+          value: "LAB DEVICE: BIOFIRE® FILMARRAY® 2.0 SYSTEM",
+          metadata: {},
+        },
+        "Analysis Time": {
+          value: "09/28/2022 2:00 PM PDT",
+          metadata: {},
+        },
+        "Performed At": {
+          value:
+            "PROVIDENCE ST. JOSEPH MEDICAL CENTER LABORATORY (CLIA 05D0672675)",
+          metadata: {},
+        },
+        "Pathologist Signature": {
+          value: "",
+          metadata: {
+            "data-id":
+              "Result.1.2.840.114350.1.13.297.3.7.2.798268.1670844.Comp2Signature",
+          },
+        },
+      },
+    ],
+    [
+      {
+        "Specimen (Source)": {
+          value: "Stool",
+          metadata: {
+            "data-id":
+              "Result.1.2.840.114350.1.13.297.3.7.2.798268.1670844.Specimen",
+          },
+        },
+        "Anatomical Location / Laterality": {
+          value: "STOOL SPECIMEN / Unknown",
+          metadata: {},
+        },
+        "Collection Method / Volume": {
+          value: "",
+          metadata: {},
+        },
+        "Collection Time": {
+          value: "09/28/2022 1:51 PM PDT",
+          metadata: {},
+        },
+        "Received Time": {
+          value: "09/28/2022 1:51 PM PDT",
+          metadata: {},
+        },
+      },
+    ],
+    [
+      {
+        "Authorizing Provider": {
+          value: "Ambhp1 Test MD",
+          metadata: {},
+        },
+        "Result Type": {
+          value: "MICROBIOLOGY - GENERAL ORDERABLES",
+          metadata: {},
+        },
+      },
+    ],
+    [
+      {
+        "Performing Organization": {
+          value:
+            "PROVIDENCE ST. JOSEPH MEDICAL CENTER LABORATORY (CLIA 05D0672675)",
+          metadata: {
+            "data-id":
+              "Result.1.2.840.114350.1.13.297.3.7.2.798268.1670844.PerformingLab",
+          },
+        },
+        Address: {
+          value: "501 S. Buena Vista Street",
+          metadata: {},
+        },
+        "City/State/ZIP Code": {
+          value: "Burbank, CA 91505",
+          metadata: {},
+        },
+        "Phone Number": {
+          value: "818-847-6000",
+          metadata: {},
+        },
+      },
+    ],
+  ],
+};
 
 const pathLabOrganismsTableAndNarr =
   "Bundle.entry.resource.where(resourceType = 'DiagnosticReport').where(id = 'b0f590a6-4bf5-7add-9716-2bd3ba6defb2')";
@@ -208,22 +350,14 @@ describe("Labs Utils", () => {
   describe("checkAbnormalTag", () => {
     it("should return true if lab report has abnormal tag", () => {
       const expectedResult = true;
-      const result = checkAbnormalTag(
-        labReportAbnormal,
-        BundleLab as unknown as Bundle,
-        mappings,
-      );
+      const result = checkAbnormalTag(labReportAbnormalJsonObject);
 
       expect(result).toStrictEqual(expectedResult);
     });
 
     it("should return false if lab report does not have abnormal tag", () => {
       const expectedResult = false;
-      const result = checkAbnormalTag(
-        labReportNormal,
-        BundleLab as unknown as Bundle,
-        mappings,
-      );
+      const result = checkAbnormalTag(labReportNormalJsonObject);
 
       expect(result).toStrictEqual(expectedResult);
     });
@@ -257,9 +391,7 @@ describe("Labs Utils", () => {
       const expectedResult = "09/28/2022 1:59 PM PDT";
 
       const result = returnFieldValueFromLabHtmlString(
-        labReportNormal,
-        BundleLab as unknown as Bundle,
-        mappings,
+        labReportNormalJsonObject,
         fieldName,
       );
 
@@ -273,9 +405,7 @@ describe("Labs Utils", () => {
       );
 
       const result = returnFieldValueFromLabHtmlString(
-        labReportNormal,
-        BundleLab as unknown as Bundle,
-        mappings,
+        labReportNormalJsonObject,
         invalidFieldName,
       );
 
