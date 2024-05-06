8de718b3ab0d771fab88c0e91aa3f2e24f1b36e8
Kenneth Chow <kenneth@skylight.digital>
2024-05-06 11:35:07 -0500

    fixing pr comments

--- containers/ecr-viewer/src/instrumentation.ts
+++ containers/ecr-viewer/src/instrumentation.ts
@@ -1,7 +1,8 @@
 import { registerOTel } from "@vercel/otel";
 
 /**
- *
+ * Registers the ecr-viewer service for OpenTelemetry
+ * When listening for logs, it will show up as ecr-viewer
  */
 export function register() {
   registerOTel({ serviceName: "ecr-viewer" });
