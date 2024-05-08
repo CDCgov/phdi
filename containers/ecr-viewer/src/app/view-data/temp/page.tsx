d936ea9ff9860d5bd5baead55fa9d8e5704e4e02
Kenneth Chow <kenneth@skylight.digital>
2024-05-08 10:47:57 -0500

    Adding loading page basics

---
+++ containers/ecr-viewer/src/app/view-data/temp/page.tsx
@@ -0,0 +1,15 @@
+import { EcrLoading } from "../components/loading/LoadingComponent";
+
+/**
+ * temp
+ * @returns The main eCR Viewer JSX component.
+ */
+const TempPage = () => {
+  return (
+    <>
+      <EcrLoading />
+    </>
+  );
+};
+
+export default TempPage;
