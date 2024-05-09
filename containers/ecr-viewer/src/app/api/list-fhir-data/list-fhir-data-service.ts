fb85ce20d8d34194ba91dba3773ce2909396b617
angelathe <angela.the@skylight.digital>
2024-05-09 14:25:48 -0700

    create api endpoint for listing eCRs

---
+++ containers/ecr-viewer/src/app/api/list-fhir-data/list-fhir-data-service.ts
@@ -0,0 +1,56 @@
+import { NextResponse } from "next/server";
+import pgPromise from "pg-promise";
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { database } from "@/app/api/fhir-data/db";
+
+const s3Client = new S3Client({ region: process.env.AWS_REGION });
+
+/**
+ * Retrieves array of eCR IDs from PostgreSQL database.
+ * @returns A promise resolving to a NextResponse object.
+ */
+export const list_postgres = async () => {
+  // const params = request.nextUrl.searchParams;
+  // const ecr_id = params.get("id") ? params.get("id") : null;
+  // const mappings = loadYamlConfig();
+
+  const { ParameterizedQuery: PQ } = pgPromise;
+  const listFhir = new PQ({
+    text: "SELECT ecr_id FROM fhir",
+  });
+  try {
+    const entry = await database.manyOrNone(listFhir);
+    return NextResponse.json({ data: entry }, { status: 200 });
+  } catch (error: any) {
+    console.error("Error fetching data:", error);
+    if (error.message == "No data returned from the query.") {
+      return NextResponse.json(
+        { message: "eCR ID not found" },
+        { status: 404 },
+      );
+    } else {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+  }
+};
+
+/**
+ * Retrieves array of eCRs and their metadata from S3.
+ * @returns A promise resolving to a NextResponse object.
+ */
+export const list_s3 = async () => {
+  const bucketName = process.env.ECR_BUCKET_NAME;
+
+  try {
+    const command = new ListObjectsV2Command({
+      Bucket: bucketName,
+    });
+
+    const response = await s3Client.send(command);
+
+    return NextResponse.json({ data: response }, { status: 200 });
+  } catch (error: any) {
+    console.error("S3 GetObject error:", error);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+};
