23ec5b37790c2d1ee69a48a7ff236cf1fdb3d94c
AmyK <amyk@AmyKs-MacBook-Pro.local>
2024-05-14 16:59:31 -0700

    adds test for get-value-sets

--- containers/trigger-code-reference/tests/test_condition_endpoints.py
+++ containers/trigger-code-reference/tests/test_condition_endpoints.py
@@ -3,6 +3,12 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
+def mock_db():
+    with patch("sqlite3.connect", autospec=True) as mock_connect:
+        mock_conn = mock_connect.return_value
+        mock_conn.__enter__.return_value = mock_conn
+        mock_cursor = mock_conn.cursor.return_value
+        yield mock_cursor
 
 def test_health_check():
     actual_response = client.get("/")
@@ -11,10 +17,15 @@ def test_health_check():
         "status": "OK",
     }
 
-
 def test_get_value_sets_for_condition():
-    pass
-
+    snowmed_code = "276197005"
+    expected_result = [
+        ("dxtc", "A36.3|A36", "http://hl7.org/fhir/sid/icd-10-cm"),
+        ("sdtc", "772150003", "http://snomed.info/sct"),
+    ]
+    mock_db.fetchall.return_value = expected_result
+    result = get_clinical_services_list(snowmed_code)
+    assert result == expected_result
 
 def test_insert_condition_extensions():
     pass
