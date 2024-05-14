bba2cc783e3783427532bf9f46def7b511803929
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-05-14 10:17:35 -0400

    Add error and loading pages

---
+++ containers/ecr-viewer/src/app/view-data/loading.tsx
@@ -0,0 +1,8 @@
+/**
+ * Loading page for view-data
+ * @returns - loading page element
+ */
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div>Loading ....</div>;
+}
