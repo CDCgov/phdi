1eec39db1b2fe6470165f170629809daf10e475a
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2024-05-14 10:38:02 -0400

    pull out ecr viewer components into separate file

---
+++ containers/ecr-viewer/src/app/view-data/components/EcrViewer.tsx
@@ -0,0 +1,89 @@
+"use client";
+import { Bundle, FhirResource } from "fhir/r4";
+import { PathMappings } from "@/app/utils";
+import SideNav from "@/app/view-data/components/SideNav";
+import EcrSummary from "@/app/view-data/components/EcrSummary";
+import {
+  evaluateEcrSummaryAboutTheConditionDetails,
+  evaluateEcrSummaryEncounterDetails,
+  evaluateEcrSummaryPatientDetails,
+} from "@/app/services/ecrSummaryService";
+import { Grid, GridContainer } from "@trussworks/react-uswds";
+import { ExpandCollapseButtons } from "@/app/view-data/components/ExpandCollapseButtons";
+import AccordionContainer from "@/app/view-data/components/AccordionContainer";
+import React from "react";
+
+interface EcrViewerProps {
+  fhirBundle: Bundle<FhirResource>;
+  mappings: PathMappings;
+}
+
+/**
+ * Renders an eCR Viewer component.
+ * @param props - The props object.
+ * @param props.fhirBundle - The FHIR bundle containing data.
+ * @param props.mappings - The mappings object for FHIR paths.
+ * @returns - Returns the JSX element for the eCR Viewer.
+ */
+export default function EcrViewer({ fhirBundle, mappings }: EcrViewerProps) {
+  return (
+    <div>
+      <div className="main-container">
+        <div className="content-wrapper">
+          <div className="nav-wrapper">
+            <nav className="sticky-nav">
+              <SideNav />
+            </nav>
+          </div>
+          <div className={"ecr-viewer-container"}>
+            <div className="ecr-content">
+              <h2 className="margin-bottom-3" id="ecr-summary">
+                eCR Summary
+              </h2>
+              <EcrSummary
+                patientDetails={evaluateEcrSummaryPatientDetails(
+                  fhirBundle,
+                  mappings,
+                )}
+                encounterDetails={evaluateEcrSummaryEncounterDetails(
+                  fhirBundle,
+                  mappings,
+                )}
+                aboutTheCondition={evaluateEcrSummaryAboutTheConditionDetails(
+                  fhirBundle,
+                  mappings,
+                )}
+              />
+              <div className="margin-top-10">
+                <GridContainer className={"padding-0 margin-bottom-3"}>
+                  <Grid row>
+                    <Grid>
+                      <h2 className="margin-bottom-0" id="ecr-document">
+                        eCR Document
+                      </h2>
+                    </Grid>
+                    <Grid className={"flex-align-self-center margin-left-auto"}>
+                      <ExpandCollapseButtons
+                        id={"main"}
+                        buttonSelector={"h3 > .usa-accordion__button"}
+                        accordionSelector={
+                          ".info-container > .usa-accordion__content"
+                        }
+                        expandButtonText={"Expand all sections"}
+                        collapseButtonText={"Collapse all sections"}
+                      />
+                    </Grid>
+                  </Grid>
+                </GridContainer>
+                <AccordionContainer
+                  fhirPathMappings={mappings}
+                  fhirBundle={fhirBundle}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- containers/ecr-viewer/src/app/view-data/page.tsx
+++ containers/ecr-viewer/src/app/view-data/page.tsx
@@ -1,21 +1,13 @@
-"use client";
-import AccordionContainer from "@/app/view-data/components/AccordionContainer";
-import React, { useEffect, useState } from "react";
 import { Bundle } from "fhir/r4";
 import { PathMappings } from "../utils";
-import SideNav from "./components/SideNav";
-import { processSnomedCode } from "./service";
-import { Grid, GridContainer } from "@trussworks/react-uswds";
-import { ExpandCollapseButtons } from "@/app/view-data/components/ExpandCollapseButtons";
-import EcrSummary from "./components/EcrSummary";
-import {
-  evaluateEcrSummaryPatientDetails,
-  evaluateEcrSummaryEncounterDetails,
-  evaluateEcrSummaryAboutTheConditionDetails,
-} from "../services/ecrSummaryService";
+import EcrViewer from "@/app/view-data/components/EcrViewer";
+import { processSnomedCode } from "@/app/view-data/service";
 
 // string constants to match with possible .env values
-const basePath = process.env.NODE_ENV === "production" ? "/ecr-viewer" : "";
+const basePath =
+  process.env.NODE_ENV === "production"
+    ? "/ecr-viewer"
+    : "http://localhost:3000";
 
 /**
  * Functional component for rendering a page based on provided search parameters.
@@ -23,125 +15,21 @@ const basePath = process.env.NODE_ENV === "production" ? "/ecr-viewer" : "";
  * @param props.searchParams - Search parameters object.
  * @returns - functional component for view-data
  */
-export default function Page({
+export default async function Page({
   searchParams,
 }: Readonly<{ searchParams: { [key: string]: string | undefined } }>) {
-  const [fhirBundle, setFhirBundle] = useState<Bundle>();
-  const [mappings, setMappings] = useState<PathMappings>({});
-  const [errors, setErrors] = useState<Error>();
   const fhirId = searchParams["id"] ?? "";
   const snomedCode = searchParams["snomed-code"] ?? "";
+  processSnomedCode(snomedCode);
 
   type ApiResponse = {
     fhirBundle: Bundle;
     fhirPathMappings: PathMappings;
   };
+  const response = await fetch(`${basePath}/api/fhir-data?id=${fhirId}`);
+  const bundle: ApiResponse = await response.json();
+  const fhirBundle = bundle.fhirBundle;
+  const mappings = bundle.fhirPathMappings;
 
-  useEffect(() => {
-    // Fetch the appropriate bundle from Postgres database
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${basePath}/api/fhir-data?id=${fhirId}`);
-        if (!response.ok) {
-          if (response.status == 404) {
-            throw new Error(
-              "Sorry, we couldn't find this eCR ID. Please try again with a different ID.",
-            );
-          } else {
-            const errorData = response.statusText;
-            throw new Error(errorData || "Internal Server Error");
-          }
-        } else {
-          const bundle: ApiResponse = await response.json();
-          processSnomedCode(snomedCode);
-          setFhirBundle(bundle.fhirBundle);
-          setMappings(bundle.fhirPathMappings);
-        }
-      } catch (error: any) {
-        setErrors(error);
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (errors) {
-    return (
-      <div>
-        <pre>
-          <code>{`${errors}`}</code>
-        </pre>
-      </div>
-    );
-  } else if (fhirBundle && mappings) {
-    return (
-      <div>
-        <div className="main-container">
-          <div className="content-wrapper">
-            <div className="nav-wrapper">
-              <nav className="sticky-nav">
-                <SideNav />
-              </nav>
-            </div>
-            <div className={"ecr-viewer-container"}>
-              <div className="ecr-content">
-                <h2 className="margin-bottom-3" id="ecr-summary">
-                  eCR Summary
-                </h2>
-                <EcrSummary
-                  patientDetails={evaluateEcrSummaryPatientDetails(
-                    fhirBundle,
-                    mappings,
-                  )}
-                  encounterDetails={evaluateEcrSummaryEncounterDetails(
-                    fhirBundle,
-                    mappings,
-                  )}
-                  aboutTheCondition={evaluateEcrSummaryAboutTheConditionDetails(
-                    fhirBundle,
-                    mappings,
-                  )}
-                />
-                <div className="margin-top-10">
-                  <GridContainer className={"padding-0 margin-bottom-3"}>
-                    <Grid row>
-                      <Grid>
-                        <h2 className="margin-bottom-0" id="ecr-document">
-                          eCR Document
-                        </h2>
-                      </Grid>
-                      <Grid
-                        className={"flex-align-self-center margin-left-auto"}
-                      >
-                        <ExpandCollapseButtons
-                          id={"main"}
-                          buttonSelector={"h3 > .usa-accordion__button"}
-                          accordionSelector={
-                            ".info-container > .usa-accordion__content"
-                          }
-                          expandButtonText={"Expand all sections"}
-                          collapseButtonText={"Collapse all sections"}
-                        />
-                      </Grid>
-                    </Grid>
-                  </GridContainer>
-                  <AccordionContainer
-                    fhirPathMappings={mappings}
-                    fhirBundle={fhirBundle}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
+  return <EcrViewer fhirBundle={fhirBundle} mappings={mappings} />;
 }
