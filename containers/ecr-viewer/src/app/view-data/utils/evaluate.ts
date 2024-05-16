2d1d4447c328493587f61fb7e07a16562014cc82
Boban <BobanL@users.noreply.github.com>
2024-05-16 11:02:03 -0400

    cache fhir evaluate functions (#1806)
    * install uuid-by-string
    
    * create new evaluate function that has a cache
    
    Include comments for speed
    
    * replace imports to use the one with cache
    
    * remove uuid-by-string
    
    * remove logging
    
    * fix missing key
    
    * replace all evaluates with one
    
    * fix evaluate in tefca

---
+++ containers/ecr-viewer/src/app/view-data/utils/evaluate.ts
@@ -0,0 +1,39 @@
+import { evaluate as fhirPathEvaluate } from "fhirpath";
+
+let cache: { [key: string]: any } = {};
+
+/**
+ * Evaluates a FHIRPath expression on the provided FHIR data.
+ * @param fhirData - The FHIR data to evaluate the FHIRPath expression on.
+ * @param path - The FHIRPath expression to evaluate.
+ * @param [context] - Optional context object to provide additional data for evaluation.
+ * @param [model] - Optional model object to provide additional data for evaluation.
+ * @param [options] - Optional options object for additional configuration.
+ * @param [options.resolveInternalTypes] - Whether to resolve internal types in the evaluation.
+ * @param [options.traceFn] - Optional trace function for logging evaluation traces.
+ * @param [options.userInvocationTable] - Optional table for tracking user invocations.
+ * @returns - An array containing the result of the evaluation.
+ */
+export const evaluate = (
+  fhirData: any,
+  path: string | Path,
+  context?: Context,
+  model?: Model,
+  options?: {
+    resolveInternalTypes?: boolean;
+    traceFn?: (value: any, label: string) => void;
+    userInvocationTable?: UserInvocationTable;
+  },
+): any[] => {
+  let key =
+    JSON.stringify(fhirData) +
+    ",,,,," +
+    JSON.stringify(context) +
+    ",,,,," +
+    JSON.stringify(path);
+  if (cache.hasOwnProperty(key)) {
+    return cache[key];
+  }
+  cache[key] = fhirPathEvaluate(fhirData, path, context, model, options);
+  return cache[key];
+};
