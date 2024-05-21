83ccc4d3566610aecfde22e15b2f79a2af820027
Nick <nick@skylight.digital>
2024-05-21 10:39:07 -0400

    basic metrics collection (#1876)
    * basic metrics collection
    
    * Update package.json
    
    * use metrics
    
    * fix evaluate

--- containers/ecr-viewer/src/instrumentation.ts
+++ containers/ecr-viewer/src/instrumentation.ts
@@ -1,9 +1,9 @@
-import { registerOTel } from "@vercel/otel";
-
 /**
  * Registers the ecr-viewer service for OpenTelemetry
  * When listening for logs, it will show up as ecr-viewer
  */
-export function register() {
-  registerOTel({ serviceName: "ecr-viewer" });
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./app/services/instrumentation");
+  }
 }
