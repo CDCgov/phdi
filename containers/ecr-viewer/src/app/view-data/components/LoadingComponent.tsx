f9a6adf94557b4e5e8f8799e3d4084f2b2ace368
Kenneth Chow <kenneth@skylight.digital>
2024-05-22 10:57:34 -0500

    shimmer effect custom

--- containers/ecr-viewer/src/app/view-data/components/LoadingComponent.tsx
+++ containers/ecr-viewer/src/app/view-data/components/LoadingComponent.tsx
@@ -25,11 +25,15 @@ const renderLoadingBlobs = (numberOfRows: number, isGray: boolean = true) => {
         <div key={index}>
           <div className="grid-row">
             <div className="grid-col-4">
-              <div className={`${loadingBlobStyle} margin-right-1`}>&nbsp;</div>
+              <div
+                className={`${loadingBlobStyle} margin-right-1 loading-blob`}
+              >
+                &nbsp;
+              </div>
             </div>
             <div className={`grid-col-8 ${loadingBlobStyle}`}>&nbsp;</div>
           </div>
-          <div className={sectionLineStyle} />
+          <div className={`${sectionLineStyle}`} />
         </div>
       ))}
     </>
---
+++ containers/ecr-viewer/src/app/view-data/temp/page.tsx
@@ -0,0 +1,10 @@
+import { EcrLoadingSkeleton } from "../components/LoadingComponent";
+
+/**
+ * @returns something
+ */
+const ECRViewerPage: React.FC = () => {
+  return <EcrLoadingSkeleton />;
+};
+
+export default ECRViewerPage;
