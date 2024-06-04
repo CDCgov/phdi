4f35d6ce98a69dead220b2d44617dd6354ff120d
bamader <brandon@skylight.digital>
2024-06-04 14:15:55 -0400

    Parameterize openapi url

--- containers/message-refiner/app/main.py
+++ containers/message-refiner/app/main.py
@@ -18,6 +18,7 @@ app = BaseService(
     service_path="/message-refiner",
     description_path=Path(__file__).parent.parent / "description.md",
     include_health_check_endpoint=False,
+    openapi_url="/message-refiner/openapi.json",
 ).start()
 
 
