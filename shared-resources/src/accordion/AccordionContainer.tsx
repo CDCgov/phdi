a4b8e0d6d04c64b888c7246e01bb88274bc517d1
Nick <nick@skylight.digital>
2024-06-11 14:33:10 -0500

    Create shared resource (#1887)
    * working
    
    * add shared dir
    
    * moved shared resources folder
    
    * webpack test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Update docker-compose.yml
    
    * Update buildContainers.yaml
    
    * test
    
    * another test
    
    * remove symlinks if needed
    
    * tests
    
    * tests
    
    * fix lighthouse
    
    * remove symlinks from lighthouse
    
    * update readme
    
    * fix package
    
    * Update Dockerfile
    
    * Update package.json

---
+++ shared-resources/src/accordion/AccordionContainer.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from "react";
+import { Accordion } from "@trussworks/react-uswds";
+import { formatString } from "../services/formatService";
+import { AccordionItemProps } from "@trussworks/react-uswds/lib/components/Accordion/Accordion";
+
+type AccordionContainerProps = {
+  children?: ReactNode;
+  accordionItems: AccordionItemProps[];
+};
+
+/**
+ * Functional component for an accordion container displaying various sections of eCR information.
+ * @param props - Props containing FHIR bundle and path mappings.
+ * @param props.accordionItems - The list of accordion items.
+ * @returns The JSX element representing the accordion container.
+ */
+const AccordionContainer: React.FC<AccordionContainerProps> = ({
+  accordionItems,
+}) => {
+  //Add id, adjust title
+  const items: AccordionItemProps[] = accordionItems.map((item, index) => {
+    let formattedTitle = formatString(`${item["title"]}`);
+    return {
+      ...item,
+      id: `${formattedTitle}_${index + 1}`,
+      title: <span id={formattedTitle}>{item["title"]}</span>,
+    };
+  });
+
+  return (
+    <Accordion
+      className="info-container"
+      items={items}
+      multiselectable={true}
+    />
+  );
+};
+export default AccordionContainer;
