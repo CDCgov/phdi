dbe111126ab29b67478157074bf582c559d13f90
Nick <nick@skylight.digital>
2024-06-18 16:08:34 -0400

    Update the seed data file to pull meta data (#1970)
    * add platform for builds to work on macos
    
    * wip
    
    * pull first eicr id
    
    * Update package.json
    
    * Update package.json
    
    * Update seed-ecr-viewer-config.json
    
    * Update sample-orchestration-config.json
    
    ---------
    
    Co-authored-by: robertmitchellv <rmitchell@skylight.digital>

--- containers/ecr-viewer/seed-scripts/sql/01-init.sql
+++ containers/ecr-viewer/seed-scripts/sql/01-init.sql
@@ -11,8 +11,9 @@ CREATE TABLE IF NOT EXISTS fhir_metadata(
   ecr_id VARCHAR(200) NOT NULL,
   data_source VARCHAR(2) NOT NULL, -- S3 or DB
   data_link VARCHAR(500) NOT NULL, -- Link to the data
-  patient_name VARCHAR(100) NOT NULL,
-  patient_DOB DATE NOT NULL,
+  patient_name_first VARCHAR(100) NOT NULL,
+  patient_name_last VARCHAR(100) NOT NULL,
+  patient_birth_date DATE NOT NULL,
   reportable_condition VARCHAR(10000),
   rule_summary VARCHAR(10000),
   report_date DATE NOT NULL,
