e166d084a7e8d0c9cc5785188bfd2e4f24be2f1d
m-goggins <marcelle.goggins@gmail.com>
2024-06-25 14:56:47 -0700

    add QueryResponse to constants

--- containers/tefca-viewer/src/app/constants.ts
+++ containers/tefca-viewer/src/app/constants.ts
@@ -1,3 +1,15 @@
+import {
+  Patient,
+  Observation,
+  DiagnosticReport,
+  Condition,
+  Encounter,
+  Medication,
+  MedicationAdministration,
+  MedicationRequest,
+  Bundle,
+} from "fhir/r4";
+
 /**
  * The use cases that can be used in the app
  */
@@ -13,7 +25,7 @@ export type USE_CASES = (typeof UseCases)[number];
 
 /**
  * The FHIR servers that can be used in the app
- */
+ =*/
 export const FhirServers = [
   "HELIOS Meld: Direct",
   "HELIOS Meld: eHealthExchange",
@@ -24,3 +36,24 @@ export const FhirServers = [
   "CernerHelios: eHealthExchange",
 ] as const;
 export type FHIR_SERVERS = (typeof FhirServers)[number];
+
+/**
+ * The sources that a query request can come from. This is used to determine if the
+ * response should be of type ViewerQueryResponse or ApiQueryResponse.
+ */
+export const RequestSources = ["api", "viewer"] as const;
+export type REQUEST_SOURCES = (typeof RequestSources)[number];
+
+/**
+ * The query response when the request source is from the Viewer UI.
+ */
+export type QueryResponse = {
+  Patient?: Patient[];
+  Observation?: Observation[];
+  DiagnosticReport?: DiagnosticReport[];
+  Condition?: Condition[];
+  Encounter?: Encounter[];
+  Medication?: Medication[];
+  MedicationAdministration?: MedicationAdministration[];
+  MedicationRequest?: MedicationRequest[];
+};
