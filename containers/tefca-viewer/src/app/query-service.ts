4631d392e6c925a6f9a25eba60dd75fe54417f7a
m-goggins <marcelle.goggins@gmail.com>
2024-06-25 15:28:09 -0700

    remove request_source as element of UseCaseQueryRequest

--- containers/tefca-viewer/src/app/query-service.ts
+++ containers/tefca-viewer/src/app/query-service.ts
@@ -2,17 +2,11 @@
 import fetch from "node-fetch";
 
 import FHIRClient from "./fhir-servers";
-import {
-  USE_CASES,
-  FHIR_SERVERS,
-  REQUEST_SOURCES,
-  QueryResponse,
-} from "./constants";
+import { USE_CASES, FHIR_SERVERS, QueryResponse } from "./constants";
 
 export type UseCaseQueryRequest = {
   use_case: USE_CASES;
   fhir_server: FHIR_SERVERS;
-  RequestSource: REQUEST_SOURCES;
   first_name?: string;
   last_name?: string;
   dob?: string;
@@ -23,7 +17,7 @@ const UseCaseQueryMap: {
   [key in USE_CASES]: (
     patientId: string,
     fhirClient: FHIRClient,
-    queryResponse: QueryResponse,
+    queryResponse: QueryResponse
   ) => Promise<QueryResponse>;
 } = {
   "social-determinants": socialDeterminantsQuery,
@@ -48,7 +42,7 @@ export type UseCaseQueryResponse = Awaited<ReturnType<typeof UseCaseQuery>>;
 async function patientQuery(
   request: UseCaseQueryRequest,
   fhirClient: FHIRClient,
-  queryResponse: QueryResponse,
+  queryResponse: QueryResponse
 ): Promise<void> {
   // Query for patient
   let query = "Patient?";
@@ -73,8 +67,8 @@ async function patientQuery(
       `Patient search failed. Status: ${
         response.status
       } \n Body: ${await response.text} \n Headers: ${JSON.stringify(
-        response.headers.raw(),
-      )}`,
+        response.headers.raw()
+      )}`
     );
   }
   queryResponse = await parseFhirSearch(response, queryResponse);
@@ -89,7 +83,7 @@ async function patientQuery(
  */
 export async function UseCaseQuery(
   request: UseCaseQueryRequest,
-  queryResponse: QueryResponse = {},
+  queryResponse: QueryResponse = {}
 ): Promise<QueryResponse> {
   const fhirClient = new FHIRClient(request.fhir_server);
 
@@ -118,7 +112,7 @@ export async function UseCaseQuery(
 async function socialDeterminantsQuery(
   patientId: string,
   fhirClient: FHIRClient,
-  queryResponse: QueryResponse,
+  queryResponse: QueryResponse
 ): Promise<QueryResponse> {
   const query = `/Observation?subject=${patientId}&category=social-history`;
   const response = await fhirClient.get(query);
@@ -135,7 +129,7 @@ async function socialDeterminantsQuery(
 async function newbornScreeningQuery(
   patientId: string,
   fhirClient: FHIRClient,
-  queryResponse: QueryResponse,
+  queryResponse: QueryResponse
 ): Promise<QueryResponse> {
   const loincs: Array<string> = [
     "73700-7",
@@ -167,7 +161,7 @@ async function newbornScreeningQuery(
 async function syphilisQuery(
   patientId: string,
   fhirClient: FHIRClient,
-  queryResponse: QueryResponse,
+  queryResponse: QueryResponse
 ): Promise<QueryResponse> {
   const loincs: Array<string> = ["LP70657-9", "98212-4", "53605-2"];
   const snomed: Array<string> = ["76272004", "186847001"];
@@ -225,7 +219,7 @@ async function syphilisQuery(
 async function gonorrheaQuery(
   patientId: string,
   fhirClient: FHIRClient,
-  queryResponse: QueryResponse,
+  queryResponse: QueryResponse
 ): Promise<QueryResponse> {
   const loincs: Array<string> = [
     "24111-7", // Neisseria gonorrhoeae DNA [Presence] in Specimen by NAA with probe detection
@@ -297,7 +291,7 @@ async function gonorrheaQuery(
 async function chlamydiaQuery(
   patientId: string,
   fhirClient: FHIRClient,
-  queryResponse: QueryResponse,
+  queryResponse: QueryResponse
 ): Promise<QueryResponse> {
   const loincs: Array<string> = [
     "24111-7", // Neisseria gonorrhoeae DNA [Presence] in Specimen by NAA with probe detection
@@ -371,7 +365,7 @@ async function chlamydiaQuery(
 async function cancerQuery(
   patientId: string,
   fhirClient: FHIRClient,
-  queryResponse: QueryResponse,
+  queryResponse: QueryResponse
 ): Promise<QueryResponse> {
   const snomed: Array<string> = ["92814006"];
   const rxnorm: Array<string> = ["828265"]; // drug codes from NLM/NIH RxNorm
@@ -407,7 +401,7 @@ async function cancerQuery(
  */
 async function parseFhirSearch(
   response: fetch.Response | Array<fetch.Response>,
-  queryResponse: QueryResponse = {},
+  queryResponse: QueryResponse = {}
 ): Promise<QueryResponse> {
   let resourceArray: any[] = [];
 
