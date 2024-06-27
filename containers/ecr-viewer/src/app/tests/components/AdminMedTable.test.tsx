76c19d22845c28c0d710681383c4c1c02a48f830
Boban Ljuljdjurovic <boban@skylight.digital>
2024-06-27 11:52:37 -0400

    Add Administered Medication

---
+++ containers/ecr-viewer/src/app/tests/components/AdminMedTable.test.tsx
@@ -0,0 +1,39 @@
+import { AdministeredMedication } from "@/app/view-data/components/AdministeredMedication";
+import { render, screen } from "@testing-library/react";
+import fs from "fs";
+import YAML from "yaml";
+import BundleWithPatient from "@/app/tests/assets/BundlePatient.json";
+import BundleMedication from "@/app/tests/assets/BundleMedication.json";
+import { Bundle } from "fhir/r4";
+
+describe("AdminMedTable", () => {
+  const fhirPathFile = fs
+    .readFileSync("./src/app/api/fhirPath.yml", "utf8")
+    .toString();
+  const fhirPathMappings = YAML.parse(fhirPathFile);
+  it("should not render anything if there is no administered medications", () => {
+    render(
+      <AdministeredMedication
+        fhirBundle={BundleWithPatient as unknown as Bundle}
+        mappings={fhirPathMappings}
+      />,
+    );
+
+    expect(
+      screen.queryByText("Administered Medications"),
+    ).not.toBeInTheDocument();
+  });
+
+  it("should render administered medications", () => {
+    render(
+      <AdministeredMedication
+        fhirBundle={BundleMedication as unknown as Bundle}
+        mappings={fhirPathMappings}
+      />,
+    );
+
+    expect(screen.getByText("Administered Medications")).toBeVisible();
+    expect(screen.getByText("aspirin tablet 325 mg")).toBeVisible();
+    expect(screen.getByText("09/29/2022")).toBeVisible();
+  });
+});
