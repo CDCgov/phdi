1d79d6be369b7a9ef507f9bafedff8489747909e
Lina Roth <lina.roth.ctr@teambespin.us>
2024-07-08 14:06:22 -0400

    Fix test

--- containers/ecr-viewer/src/app/tests/HomePage.test.tsx
+++ containers/ecr-viewer/src/app/tests/HomePage.test.tsx
@@ -1,9 +1,16 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import HomePage from "@/app/page";
 import { any } from "prop-types";
+import { listEcrData } from "@/app/api/services/listEcrDataService";
+
+jest.mock("../../app/api/services/listEcrDataService");
 
 describe("Home Page", () => {
   let container: HTMLElement;
+  afterEach(() => {
+    delete process.env.STANDALONE_VIEWER;
+    jest.clearAllMocks();
+  });
   it("env false value, should not show the homepage", async () => {
     process.env.STANDALONE_VIEWER = "false";
     container = render(await HomePage(any, any)).container;
@@ -20,7 +27,12 @@ describe("Home Page", () => {
   });
   it("env true value, should show the homepage", async () => {
     process.env.STANDALONE_VIEWER = "true";
-    container = render(await HomePage(any, any)).container;
-    expect(container).not.toContainHTML("Sorry, this page is not available.");
+    const mockData = [{ id: 1, name: "Test Ecr" }];
+    (listEcrData as jest.Mock).mockResolvedValue(mockData);
+    container = render(await HomePage(mockData)).container;
+    await waitFor(() => {
+      expect(listEcrData).toHaveBeenCalled();
+      expect(container).not.toContainHTML("Sorry, this page is not available.");
+    });
   });
 });
