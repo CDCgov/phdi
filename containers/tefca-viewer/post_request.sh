b243e1a052a199d3a2d7e68632b1f9763c7b8599
m-goggins <marcelle.goggins@gmail.com>
2024-07-09 09:00:54 -0700

    add separate data loader service

--- containers/tefca-viewer/post_request.sh
+++ containers/tefca-viewer/post_request.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 120
+sleep 120 #TODO: Change to wait for positive response instead of hardcoding time
 
 curl -X POST -H "Content-Type: application/json" -d @/etc/BundleHAPIServer.json http://tefca-fhir-server:8080/fhir/
 
