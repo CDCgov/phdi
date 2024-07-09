23bb2ad335a2c9953dfbaa8fbe857100259ded75
m-goggins <marcelle.goggins@gmail.com>
2024-07-09 11:03:44 -0700

    add check for server readiness

--- containers/tefca-viewer/post_request.sh
+++ containers/tefca-viewer/post_request.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sleep 120 #TODO: Change to wait for positive response instead of hardcoding time
+# Check if tefca-fhir-server is up
+while ! curl -s http://tefca-fhir-server:8080/fhir/ > /dev/null
+
+# POST Bundle of synthetic data to spun up server
 
 curl -X POST -H "Content-Type: application/json" -d @/etc/BundleHAPIServer.json http://tefca-fhir-server:8080/fhir/
-
-
-
-# sleep 180
