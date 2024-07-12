b8e29c219e8135cfb22ac1e8ae9d5cfc6d59b47e
Boban Ljuljdjurovic <boban@skylight.digital>
2024-07-12 14:20:45 -0400

    add repo as a dependency to pre-commit

--- containers/ecr-viewer/src/instrumentation.ts
+++ containers/ecr-viewer/src/instrumentation.ts
@@ -2,6 +2,7 @@
  * Imports the otel registration
  */
 export async function register() {
+  let a = "a";
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./app/services/instrumentation");
   }
