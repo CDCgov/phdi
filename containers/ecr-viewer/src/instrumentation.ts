d4215a32a82905519a7272decc4a0543d7d14977
Boban Ljuljdjurovic <boban@skylight.digital>
2024-07-12 14:22:01 -0400

    revert change

--- containers/ecr-viewer/src/instrumentation.ts
+++ containers/ecr-viewer/src/instrumentation.ts
@@ -2,7 +2,6 @@
  * Imports the otel registration
  */
 export async function register() {
-  let a = "a";
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./app/services/instrumentation");
   }
