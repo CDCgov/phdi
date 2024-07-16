a6aa4303bbc1379b8c33af912df98455fabf7927
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-07-16 14:29:39 -0400

    updating copy of the header

--- containers/tefca-viewer/src/app/header.tsx
+++ containers/tefca-viewer/src/app/header.tsx
@@ -9,38 +9,40 @@ export default function HeaderComponent() {
     <header className="usa-header usa-header--basic bg-primary-darker">
       <div
         className="header-footer-content usa-nav-container"
-        style={{ display: "flex", alignItems: "center" }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
       >
-        <div className="grid-row grid-gap">
-          <div
-            className="
-                usa-footer__logo
-                grid-row
-                mobile-lg:grid-col-10 mobile-lg:grid-gap-1
-              "
-            style={{ width: "80%" }}
-          >
-            <div className="mobile-lg:grid-col-auto">
-              <Image
-                src="/tefca-viewer/CDC_logo.png"
-                alt="DIBBs Logo"
-                width={62}
-                height={36}
-                className="usa-footer__logo-img"
-              />
-            </div>
-            <div className="mobile-lg:grid-col-auto">
-              <em className="usa-logo__text text-base-lightest">
-                <a
-                  className="text-base-lightest font-sans-2xl text-bold"
-                  href="/tefca-viewer"
-                  title="Pipeline Demo Site"
-                >
-                  Pipeline Demo Site
-                </a>
-              </em>
-            </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Image
+            src="/tefca-viewer/DIBBs_logo.png"
+            alt="DIBBs Logo"
+            width={168}
+            height={34}
+            className="usa-footer__logo-img"
+          />
+          <div className="usa-logo" style={{ marginLeft: "16px" }}>
+            <em className="usa-logo__text text-base-lightest">
+              <a
+                className="text-base-lightest font-sans-lg text-bold"
+                href="/tefca-viewer"
+                title="TEFCA Viewer"
+              >
+                TEFCA Viewer
+              </a>
+            </em>
           </div>
+        </div>
+        <div
+          style={{
+            whiteSpace: "nowrap",
+            textAlign: "right",
+            marginLeft: "auto",
+          }}
+        >
+          <p className="text-base-lightest">Data Usage Policy</p>
         </div>
       </div>
     </header>
