a7113817c9abb778817b48fae62c8857b9ce13b3
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-07-16 16:51:45 -0400

    fixing client side rendering

--- containers/tefca-viewer/src/app/header.tsx
+++ containers/tefca-viewer/src/app/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Modal,
   ModalHeading,
@@ -16,6 +16,11 @@ import {
  */
 export default function HeaderComponent() {
   const modalRef = useRef(null);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <>
@@ -34,7 +39,7 @@ export default function HeaderComponent() {
               alt="DIBBs Logo"
               width={211}
               height={43}
-              className="usa-footer__logo-img"
+              className="usa-header__logo-img"
             />
             <div className="usa-logo" style={{ marginLeft: "16px" }}>
               <em className="usa-logo__text text-base-lightest">
@@ -55,53 +60,53 @@ export default function HeaderComponent() {
               marginLeft: "auto",
             }}
           >
-            <ModalToggleButton
-              modalRef={modalRef}
-              opener
-              className="text-base-lightest"
-              style={{
-                cursor: "pointer",
-                background: "none",
-                border: "none",
-                color: "inherit",
-                padding: 0,
-                font: "inherit",
-              }}
-              title="Data Usage Policy"
-            >
-              Data Usage Policy
-            </ModalToggleButton>
+            {isClient && (
+              <ModalToggleButton
+                modalRef={modalRef}
+                opener
+                className="text-base-lightest"
+                style={{
+                  cursor: "pointer",
+                  background: "none",
+                  border: "none",
+                  color: "inherit",
+                  padding: 0,
+                  font: "inherit",
+                }}
+                title="Data Usage Policy"
+              >
+                Data Usage Policy
+              </ModalToggleButton>
+            )}
           </div>
         </div>
       </header>
 
-      <Modal
-        ref={modalRef}
-        id="data-usage-policy-modal"
-        aria-labelledby="data-usage-policy-modal-heading"
-        aria-describedby="data-usage-policy-modal-description"
-        isInitiallyOpen={false}
-        placeholder={undefined}
-        onPointerEnterCapture={undefined}
-        onPointerLeaveCapture={undefined}
-      >
-        <ModalHeading id="data-usage-policy-modal-heading">
-          How is my data stored?
-        </ModalHeading>
-        <div className="usa-prose">
-          <p id="data-usage-policy-modal-description">
-            It's not! Data inputted into the TEFCA Query Connector is not
-            persisted or stored anywhere.
-          </p>
-        </div>
-        <ModalFooter>
-          <ButtonGroup>
-            <ModalToggleButton modalRef={modalRef} closer>
-              Close
-            </ModalToggleButton>
-          </ButtonGroup>
-        </ModalFooter>
-      </Modal>
+      {isClient && (
+        <Modal
+          ref={modalRef}
+          id="data-usage-policy-modal"
+          aria-labelledby="data-usage-policy-modal-heading"
+          aria-describedby="data-usage-policy-modal-description"
+        >
+          <ModalHeading id="data-usage-policy-modal-heading">
+            How is my data stored?
+          </ModalHeading>
+          <div className="usa-prose">
+            <p id="data-usage-policy-modal-description">
+              It's not! Data inputted into the TEFCA Query Connector is not
+              persisted or stored anywhere.
+            </p>
+          </div>
+          <ModalFooter>
+            <ButtonGroup>
+              <ModalToggleButton modalRef={modalRef} closer>
+                Close
+              </ModalToggleButton>
+            </ButtonGroup>
+          </ModalFooter>
+        </Modal>
+      )}
     </>
   );
 }
