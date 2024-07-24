92e2ede49e4328575c9a26e58a70705c1dcca02c
Boban Ljuljdjurovic <boban@skylight.digital>
2024-07-24 10:10:45 -0400

    remove old evaluateEcrSummaryAboutTheConditionDetails

--- containers/ecr-viewer/src/app/tests/services/ecrSummaryService.test.tsx
+++ containers/ecr-viewer/src/app/tests/services/ecrSummaryService.test.tsx
@@ -1,13 +1,11 @@
 import { loadYamlConfig } from "@/app/api/utils";
 import {
-  evaluateEcrSummaryAboutTheConditionDetails,
   evaluateEcrSummaryConditionSummary,
   evaluateEcrSummaryRelevantClinicalDetails,
 } from "@/app/services/ecrSummaryService";
 import BundleWithClinicalInfo from "@/app/tests/assets/BundleClinicalInfo.json";
 import { evaluateEcrSummaryRelevantLabResults } from "@/app/services/ecrSummaryService";
 import BundleLab from "@/app/tests/assets/BundleLab.json";
-import BundleEcrMetadata from "@/app/tests/assets/BundleEcrMetadata.json";
 import BundleEcrSummary from "@/app/tests/assets/BundleEcrSummary.json";
 import { Bundle } from "fhir/r4";
 import { render, screen } from "@testing-library/react";
@@ -102,60 +100,6 @@ describe("Evaluate eCR Summary Relevant Lab Results", () => {
 
     render(result[1].value);
     expect(screen.getByText("Cytogenomic SNP microarray")).toBeInTheDocument();
-  });
-});
-
-describe("Evaluate ecr Summary About the condition", () => {
-  it.each([
-    {
-      title: "should return all data when no SNOMED code is provided",
-      expectedCondition: [
-        "Disease caused by severe acute respiratory syndrome coronavirus 2 (disorder)",
-      ],
-      expectedSummary: [
-        "COVID-19 (as a diagnosis or active problem)",
-        "Detection of SARS-CoV-2 nucleic acid in a clinical or post-mortem specimen by any method",
-        "Detection of Hepatitis C virus antibody in a clinical specimen by any method",
-      ],
-    },
-    {
-      title: "should return all data when an unknown SNOMED code is provided",
-      expectedCondition: [
-        "Disease caused by severe acute respiratory syndrome coronavirus 2 (disorder)",
-        "Viral hepatitis type C (disorder)",
-      ],
-      expectedSummary: [
-        "COVID-19 (as a diagnosis or active problem)",
-        "Detection of SARS-CoV-2 nucleic acid in a clinical or post-mortem specimen by any method",
-        "Detection of Hepatitis C virus antibody in a clinical specimen by any method",
-      ],
-    },
-    {
-      title: "should return Reportable Condition and RCKMS Rule Summary",
-      expectedCondition: [
-        "Disease caused by severe acute respiratory syndrome coronavirus 2 (disorder)",
-      ],
-      expectedSummary: [
-        "COVID-19 (as a diagnosis or active problem)",
-        "Detection of SARS-CoV-2 nucleic acid in a clinical or post-mortem specimen by any method",
-      ],
-    },
-  ])("$title", ({ expectedCondition, expectedSummary }) => {
-    const actual = evaluateEcrSummaryAboutTheConditionDetails(
-      BundleEcrMetadata as unknown as Bundle,
-      mappings,
-    );
-
-    render(actual[0].value);
-    render(actual[1].value);
-
-    expect(actual).toHaveLength(2);
-    expectedCondition.forEach((value) => {
-      expect(screen.getByText(value)).toBeInTheDocument();
-    });
-    expectedSummary.forEach((value) => {
-      expect(screen.getByText(value)).toBeInTheDocument();
-    });
   });
 });
 
