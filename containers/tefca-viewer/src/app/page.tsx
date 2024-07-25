f8676ad5f7068c55e04b60f630eee94904757e41
m-goggins <marcelle.goggins@gmail.com>
2024-07-25 14:40:33 -0700

    merge fix

--- containers/tefca-viewer/src/app/page.tsx
+++ containers/tefca-viewer/src/app/page.tsx
@@ -9,11 +9,6 @@ import {
   ModalFooter,
   ModalToggleButton,
   ModalRef,
-  Modal,
-  ModalHeading,
-  ModalFooter,
-  ModalToggleButton,
-  ModalRef,
 } from "@trussworks/react-uswds";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
