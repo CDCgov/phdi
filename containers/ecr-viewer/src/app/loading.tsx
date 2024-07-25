be5d111aba53547f74d3bd2f25f40d2e6698d07e
Boban <BobanL@users.noreply.github.com>
2024-07-25 14:19:51 -0400

    update to nextjs 14.2.5 for ecr-viewer (#2188)
    * update nextjs and open telemetry
    
    * Add a loading to let nextjs create a suspense boundry around the whole page
    
    * remove axios and chai deps
    
    * remove testcontainers

---
+++ containers/ecr-viewer/src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { EcrLoadingSkeleton } from "@/app/view-data/components/LoadingComponent";
+
+/**
+ * Loading page
+ * @returns loading page
+ */
+export default function Loading() {
+  return <EcrLoadingSkeleton />;
+}
