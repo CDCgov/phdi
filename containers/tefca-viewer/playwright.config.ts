1919ba330851273362601abe2a5a6006dac10cdf
m-goggins <marcelle.goggins@gmail.com>
2024-07-31 11:01:56 -0700

    rebuild docker image for playwright

--- containers/tefca-viewer/playwright.config.ts
+++ containers/tefca-viewer/playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "docker compose up",
+    command: "docker compose build --no-cache && docker compose up",
     port: 3000,
     timeout: 120 * 1000,
   },
