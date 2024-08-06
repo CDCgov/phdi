758d770f9b1597b3b9082e9cdd5024a3282935a3
m-goggins <marcelle.goggins@gmail.com>
2024-08-06 13:30:22 -0700

    try different input type

--- containers/record-linkage/app/main.py
+++ containers/record-linkage/app/main.py
@@ -22,7 +22,7 @@ from app.utils import run_migrations
 # Ensure MPI is configured as expected.
 run_migrations()
 
-print("test")
+print("test 2")
 
 settings = get_settings()
 MPI_CLIENT = DIBBsMPIConnectorClient(
