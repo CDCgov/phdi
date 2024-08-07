44fb54e653506cd316596a957603026081dc62b5
m-goggins <marcelle.goggins@gmail.com>
2024-08-07 08:18:53 -0700

    try to trigger rl tests with normal linting

--- containers/record-linkage/app/main.py
+++ containers/record-linkage/app/main.py
@@ -22,6 +22,7 @@ from app.utils import run_migrations
 # Ensure MPI is configured as expected.
 run_migrations()
 
+print("test 8")
 settings = get_settings()
 MPI_CLIENT = DIBBsMPIConnectorClient(
     pool_size=settings["connection_pool_size"],
