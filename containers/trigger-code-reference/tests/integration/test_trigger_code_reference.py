be8018d706a7620dcc68dd692fb7c6b2531e961d
Josh Nygaard <jnygaard@skylight.digital>
2024-08-12 15:54:12 -0400

    Fix integration test

--- containers/trigger-code-reference/tests/integration/test_trigger_code_reference.py
+++ containers/trigger-code-reference/tests/integration/test_trigger_code_reference.py
@@ -24,7 +24,7 @@ def test_openapi():
 
 @pytest.mark.integration
 def test_tcr_stamping(setup, fhir_bundle):
-    reportable_condition_code = "64572001"
+    reportable_condition_code = "840539006"
     request = {"bundle": fhir_bundle}
     stamp_response = httpx.post(STAMP_ENDPOINT, json=request)
     assert stamp_response.status_code == 200
@@ -34,7 +34,6 @@ def test_tcr_stamping(setup, fhir_bundle):
     #   2. the observation containing a positive COVID test
     expected_stamped_ids = [
         "d90b3a5e-3328-48dd-bcbb-3c810a0ffb3d",
-        "a73bcca9-c9e9-7ea2-44c4-b271b9017284",
     ]
     stamped_resources = []
     stamped_bundle = stamp_response.json().get("extended_bundle")
