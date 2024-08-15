c81ca9aaad58ece57d3f6beb98aa20f8ea86f253
angelathe <angela.the@skylight.digital>
2024-08-15 14:15:01 -0500

    add rr conditions and rule summaries tables, change ecr_id to eICR_ID

--- containers/ecr-viewer/sql/core.sql
+++ containers/ecr-viewer/sql/core.sql
@@ -1,11 +1,25 @@
 CREATE TABLE IF NOT EXISTS ecr_data
 (
-  ecr_id VARCHAR(200) NOT NULL,
+  eICR_ID VARCHAR(200) NOT NULL,
   data_source VARCHAR(2) NOT NULL, -- S3 or DB
   data_link VARCHAR(500), -- Link to the data
   patient_name_first VARCHAR(100) NOT NULL,
   patient_name_last VARCHAR(100) NOT NULL,
   patient_birth_date DATE NOT NULL,
   report_date DATE,
-  PRIMARY KEY (ecr_id)
+  PRIMARY KEY (eICR_ID)
+);
+
+CREATE TABLE IF NOT EXISTS ecr_rr_conditions
+(
+    uuid UUID PRIMARY KEY,
+    eICR_ID UUID REFERENCES ecr_data(eICR_ID),
+    condition VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS ecr_rr_rule_summaries
+(
+    uuid UUID PRIMARY KEY,
+    ecr_rr_conditions_id UUID REFERENCES ecr_rr_conditions(uuid),
+    rule_summary TEXT
 );
