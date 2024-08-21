0ae92174595f782ade30ff153ba8300b82cd993a
Boban Ljuljdjurovic <boban@skylight.digital>
2024-08-21 14:03:00 -0400

    use VARCHAR instead of UUID
    general clean up

--- containers/ecr-viewer/sql/core.sql
+++ containers/ecr-viewer/sql/core.sql
@@ -1,22 +1,21 @@
 CREATE TABLE ecr_data (
-  eICR_ID VARCHAR(200) NOT NULL,
-  data_source VARCHAR(2) NOT NULL, -- S3 or DB
+  eICR_ID VARCHAR(200) PRIMARY KEY,
+  data_source VARCHAR(2), -- S3 or DB
   data_link VARCHAR(500), -- Link to the data
-  patient_name_first VARCHAR(100) NOT NULL,
-  patient_name_last VARCHAR(100) NOT NULL,
-  patient_birth_date DATE NOT NULL,
-  report_date DATE,
-  PRIMARY KEY (eICR_ID)
+  patient_name_first VARCHAR(100),
+  patient_name_last VARCHAR(100),
+  patient_birth_date DATE,
+  report_date DATE
 );
 
 CREATE TABLE ecr_rr_conditions (
-    uuid UUID PRIMARY KEY,
+    uuid VARCHAR(200) PRIMARY KEY,
     eICR_ID VARCHAR(200) NOT NULL REFERENCES ecr_data(eICR_ID),
     condition VARCHAR(255)
 );
 
 CREATE TABLE ecr_rr_rule_summaries (
-    uuid UUID PRIMARY KEY,
-    ecr_rr_conditions_id UUID REFERENCES ecr_rr_conditions(uuid),
+    uuid VARCHAR(200) PRIMARY KEY,
+    ecr_rr_conditions_id VARCHAR(200) REFERENCES ecr_rr_conditions(uuid),
     rule_summary TEXT
 );
