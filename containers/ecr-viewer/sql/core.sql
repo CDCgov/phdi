a1e9d3c6727332924af43b8d81276f1f7f61e0bd
Boban <BobanL@users.noreply.github.com>
2024-08-22 08:17:44 -0400

    add extended ecr data schema (#2385)
    * add metadata schema
    
    * update to use more generic types and only create table if not exists
    
    * Change UUID to varchar
    
    * replace DATETIME with date and time separately
    
    * remove IF NOT EXISTS since that doesn't work in SQL SERVER
    
    * use VARCHAR instead of UUID
    
    general clean up
    
    * use VARCHAR instead of defining size for variable fields
    
    * set condition to VARCHAR

--- containers/ecr-viewer/sql/core.sql
+++ containers/ecr-viewer/sql/core.sql
@@ -1,22 +1,21 @@
-CREATE TABLE IF NOT EXISTS ecr_data (
-  eICR_ID VARCHAR(200) NOT NULL,
-  data_source VARCHAR(2) NOT NULL, -- S3 or DB
+CREATE TABLE ecr_data (
+  eICR_ID VARCHAR(200) PRIMARY KEY,
+  data_source VARCHAR(2), -- S3 or DB
   data_link VARCHAR(500), -- Link to the data
-  patient_name_first VARCHAR(100) NOT NULL,
-  patient_name_last VARCHAR(100) NOT NULL,
-  patient_birth_date DATE NOT NULL,
-  report_date DATE,
-  PRIMARY KEY (eICR_ID)
+  patient_name_first VARCHAR(100),
+  patient_name_last VARCHAR(100),
+  patient_birth_date DATE,
+  report_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS ecr_rr_conditions (
-    uuid UUID PRIMARY KEY,
+CREATE TABLE ecr_rr_conditions (
+    uuid VARCHAR(200) PRIMARY KEY,
     eICR_ID VARCHAR(200) NOT NULL REFERENCES ecr_data(eICR_ID),
-    condition VARCHAR(255)
+    condition VARCHAR
 );
 
-CREATE TABLE IF NOT EXISTS ecr_rr_rule_summaries (
-    uuid UUID PRIMARY KEY,
-    ecr_rr_conditions_id UUID REFERENCES ecr_rr_conditions(uuid),
-    rule_summary TEXT
+CREATE TABLE ecr_rr_rule_summaries (
+    uuid VARCHAR(200) PRIMARY KEY,
+    ecr_rr_conditions_id VARCHAR(200) REFERENCES ecr_rr_conditions(uuid),
+    rule_summary VARCHAR
 );
---
+++ containers/ecr-viewer/sql/extended.sql
@@ -0,0 +1,86 @@
+CREATE TABLE ECR_DATA
+(
+    eICR_ID                  VARCHAR(200) PRIMARY KEY,
+    set_id                   VARCHAR(255),
+    fhir_reference_link      VARCHAR(255),
+    last_name                VARCHAR(255),
+    first_name               VARCHAR(255),
+    birth_date               DATE,
+    gender                   VARCHAR(50),
+    birth_sex                VARCHAR(50),
+    gender_identity          VARCHAR(50),
+    race                     VARCHAR(255),
+    ethnicity                VARCHAR(255),
+    street_address_1         VARCHAR(255),
+    street_address_2         VARCHAR(255),
+    state                    VARCHAR(50),
+    zip_code                 VARCHAR(20),
+    latitude                 FLOAT,
+    longitude                FLOAT,
+    homelessness_status      VARCHAR(255),
+    disabilities             VARCHAR(255),
+    tribal_affiliation       VARCHAR(255),
+    tribal_enrollment_status VARCHAR(255),
+    current_job_title        VARCHAR(255),
+    current_job_industry     VARCHAR(255),
+    usual_occupation         VARCHAR(255),
+    usual_industry           VARCHAR(255),
+    preferred_language       VARCHAR(255),
+    pregnancy_status         VARCHAR(255),
+    rr_id                    VARCHAR(255),
+    processing_status        VARCHAR(255),
+    eicr_version_number      VARCHAR(50),
+    authoring_date           DATE,
+    authoring_time           TIME,
+    authoring_provider       VARCHAR(255),
+    provider_id              VARCHAR(255),
+    facility_id              VARCHAR(255),
+    facility_name            VARCHAR(255),
+    encounter_type           VARCHAR(255),
+    encounter_start_date     DATE,
+    encounter_start_time     TIME,
+    encounter_end_date       DATE,
+    encounter_end_time       TIME,
+    reason_for_visit         VARCHAR,
+    active_problems          VARCHAR
+);
+
+CREATE TABLE ecr_rr_conditions
+(
+    UUID      VARCHAR(200) PRIMARY KEY,
+    eICR_ID   VARCHAR(200) NOT NULL REFERENCES ECR_DATA (eICR_ID),
+    condition VARCHAR
+);
+
+CREATE TABLE ecr_rr_rule_summaries
+(
+    UUID                 VARCHAR(200) PRIMARY KEY,
+    ECR_RR_CONDITIONS_ID VARCHAR(200) REFERENCES ecr_rr_conditions (UUID),
+    rule_summary         VARCHAR
+);
+
+
+CREATE TABLE ecr_labs
+(
+    UUID                                   VARCHAR(200) PRIMARY KEY,
+    eICR_ID                                VARCHAR(200) REFERENCES ECR_DATA (eICR_ID),
+    test_type                              VARCHAR(255),
+    test_type_code                         VARCHAR(50),
+    test_type_system                       VARCHAR(50),
+    test_result_qualitative                VARCHAR(255),
+    test_result_quantitative               FLOAT,
+    test_result_units                      VARCHAR(50),
+    test_result_code                       VARCHAR(50),
+    test_result_code_display               VARCHAR(255),
+    test_result_code_system                VARCHAR(50),
+    test_result_interpretation             VARCHAR(255),
+    test_result_interpretation_code        VARCHAR(50),
+    test_result_interpretation_system      VARCHAR(50),
+    test_result_reference_range_low_value  FLOAT,
+    test_result_reference_range_low_units  VARCHAR(50),
+    test_result_reference_range_high_value FLOAT,
+    test_result_reference_range_high_units VARCHAR(50),
+    specimen_type                          VARCHAR(255),
+    specimen_collection_date               DATE,
+    performing_lab                         VARCHAR(255)
+);
