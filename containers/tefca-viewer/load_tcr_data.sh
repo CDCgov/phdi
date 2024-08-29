3ac47cca61ef3799b65af8066554ae05025ab10d
m-goggins <marcelle.goggins@gmail.com>
2024-08-29 13:50:26 -0700

    update load script to add sqlite db

--- containers/tefca-viewer/load_tcr_data.sh
+++ containers/tefca-viewer/load_tcr_data.sh
@@ -5,18 +5,45 @@ source tefca.env
 # Define the database connection string using environment variables
 PG_CONN="postgresql://postgres:pw@db:5432/tefca_db"
 
-echo "PG_CONN: $PG_CONN"
+tables=("concepts" "valuesets" "conditions")
 
-# Function to check if the "concepts" table contains data
-check_concepts_table() {
-  psql "CREATE TABLE concepts (id TEXT PRIMARY KEY, name TEXT NOT NULL);"
-  count=$(psql "$PG_CONN" -c "SELECT COUNT(*) FROM concepts;" -tA)
-  if [ "$count" -eq 0 ]; then
-    return 1  # Table is empty
-  else
-    return 0  # Table contains data
-  fi
+### TEMP: Remove this once flyway is implemented
+for table in "${tables[@]}"; do
+    psql "$PG_CONN" -c "CREATE TABLE IF NOT EXISTS $table (id INT, name VARCHAR(80));" -tA
+    echo "Table $table created."
+    pgsl "$PG_CONN" -c "INSERT INTO $table (id, name) VALUES (1, item);"
+done
+### END TEMP
+
+# Function to check if tables contain data
+check_tables() {
+  for table in "${tables[@]}"; do
+    count=$(psql "$PG_CONN" -c "SELECT COUNT(*) FROM $table;" -tA)
+    if [ "$count" -eq 0 ]; then
+      return 1
+    fi
+  done
+  return 0
 }
 
-# Check if the "concepts" table contains data
-check_concepts_table
+# Check if the tables contain data
+check_tables
+if [ $? -ne 0 ]; then
+  echo "Tables are empty. Proceeding with data loading..."
+
+  # Install pgloader if not already installed
+  if ! command -v pgloader &> /dev/null; then
+    echo "Installing pgloader..."
+    apk add --no-cache pgloader
+  fi
+
+  # Download the ERSD SQLite file
+  echo "Downloading ERSD SQLite file..."
+  wget -O /tmp/ersd.db https://github.com/CDCgov/phdi/raw/main/containers/trigger-code-reference/seed-scripts/ersd.db
+
+  echo "Loading data into PostgreSQL..."
+  pgloader sqlite://tmp/ersd.db "$PG_CONN"
+
+  echo "Successfully loaded TCR data into tefca_db"
+
+fi
