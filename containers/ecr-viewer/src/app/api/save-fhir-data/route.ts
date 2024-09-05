dc9ff0ffa6cd587f25c68bc7224a56d32ea0b084
Josh Nygaard <jnygaard@skylight.digital>
2024-09-05 15:20:54 -0400

    Revert changes to save fhir data route

--- containers/ecr-viewer/src/app/api/save-fhir-data/route.ts
+++ containers/ecr-viewer/src/app/api/save-fhir-data/route.ts
@@ -1,142 +1,68 @@
-import { BlobServiceClient } from "@azure/storage-blob";
-import { NextResponse } from "next/server";
-import pgPromise from "pg-promise";
+import { NextRequest, NextResponse } from "next/server";
 import {
-  S3Client,
-  PutObjectCommand,
-  PutObjectCommandOutput,
-} from "@aws-sdk/client-s3";
-import { Bundle } from "fhir/r4";
-
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_CUSTOM_ENDPOINT,
-  forcePathStyle: process.env.AWS_CUSTOM_ENDPOINT !== undefined,
-});
+  saveToS3,
+  saveToAzure,
+  saveToPostgres,
+} from "./save-fhir-data-service";
+import { S3_SOURCE, AZURE_SOURCE, POSTGRES_SOURCE } from "@/app/api/utils";
 
 /**
- * Saves a FHIR bundle to a postgres database.
- * @async
- * @function saveToS3
- * @param fhirBundle - The FHIR bundle to be saved.
- * @param ecrId - The unique identifier for the Electronic Case Reporting (ECR) associated with the FHIR bundle.
- * @returns A promise that resolves when the FHIR bundle is successfully saved to postgres.
- * @throws {Error} Throws an error if the FHIR bundle cannot be saved to postgress.
+ * Handles POST requests and saves the FHIR Bundle to the database.
+ * @param request - The incoming request object. Expected to have a JSON body in the format `{"fhirBundle":{}, "saveSource": "postgres|s3""}`. FHIR bundle must include the ecr ID under entry[0].resource.id.
+ * @returns A `NextResponse` object with a JSON payload indicating the success message and the status code set to 200. The response content type is set to `application/json`.
  */
-export const saveToPostgres = async (fhirBundle: Bundle, ecrId: string) => {
-  const db_url = process.env.DATABASE_URL || "";
-  const db = pgPromise();
-  const database = db(db_url);
-
-  const { ParameterizedQuery: PQ } = pgPromise;
-  const addFhir = new PQ({
-    text: "INSERT INTO fhir VALUES ($1, $2) RETURNING ecr_id",
-    values: [ecrId, fhirBundle],
-  });
+export async function POST(request: NextRequest) {
+  let requestBody;
+  let fhirBundle;
+  let saveSource;
+  let ecrId;
 
   try {
-    const saveECR = await database.one(addFhir);
-
-    return NextResponse.json(
-      { message: "Success. Saved FHIR Bundle to database: " + saveECR.ecr_id },
-      { status: 200 },
-    );
+    requestBody = await request.json();
+    fhirBundle = requestBody.fhirBundle;
+    saveSource = requestBody.saveSource;
+    ecrId = requestBody.fhirBundle.entry[0].resource.id;
   } catch (error: any) {
-    console.error("Error inserting data to database:", error);
+    console.error("Error reading request body:", error);
     return NextResponse.json(
-      { message: "Failed to insert data to database. " + error.message },
+      { message: "Error reading request body. " + error.message },
       { status: 400 },
     );
   }
-};
 
-/**
- * Saves a FHIR bundle to an AWS S3 bucket.
- * @async
- * @function saveToS3
- * @param fhirBundle - The FHIR bundle to be saved.
- * @param ecrId - The unique identifier for the Electronic Case Reporting (ECR) associated with the FHIR bundle.
- * @returns A promise that resolves when the FHIR bundle is successfully saved to the S3 bucket.
- * @throws {Error} Throws an error if the FHIR bundle cannot be saved to the S3 bucket.
- */
-export const saveToS3 = async (fhirBundle: Bundle, ecrId: string) => {
-  const bucketName = process.env.ECR_BUCKET_NAME;
-  const objectKey = `${ecrId}.json`;
-  const body = JSON.stringify(fhirBundle);
-
-  try {
-    const input = {
-      Body: body,
-      Bucket: bucketName,
-      Key: objectKey,
-      ContentType: "application/json",
-    };
-
-    const command = new PutObjectCommand(input);
-    const response: PutObjectCommandOutput = await s3Client.send(command);
-    const httpStatusCode = response?.$metadata?.httpStatusCode;
-
-    if (httpStatusCode !== 200) {
-      throw new Error(`HTTP Status Code: ${httpStatusCode}`);
-    }
-    return NextResponse.json(
-      { message: "Success. Saved FHIR Bundle to S3: " + ecrId },
-      { status: 200 },
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      { message: "Failed to insert data to S3. " + error.message },
-      { status: 400 },
-    );
-  }
-};
-
-/**
- * Saves a FHIR bundle to Azure Blob Storage.
- * @async
- * @function saveToAzure
- * @param fhirBundle - The FHIR bundle to be saved.
- * @param ecrId - The unique ID for the eCR associated with the FHIR bundle.
- * @returns A promise that resolves when the FHIR bundle is successfully saved to Azure Blob Storage.
- * @throws {Error} Throws an error if the FHIR bundle cannot be saved to Azure Blob Storage.
- */
-export const saveToAzure = async (fhirBundle: Bundle, ecrId: string) => {
-  // TODO: Make this global after we get Azure access
-  const blobClient = BlobServiceClient.fromConnectionString(
-    process.env.AZURE_STORAGE_CONNECTION_STRING!,
-  );
-
-  if (!process.env.AZURE_CONTAINER_NAME)
-    throw Error("Azure container name not found");
-
-  const containerName = process.env.AZURE_CONTAINER_NAME;
-  const blobName = `${ecrId}.json`;
-  const body = JSON.stringify(fhirBundle);
-
-  try {
-    const containerClient = blobClient.getContainerClient(containerName);
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-    const response = await blockBlobClient.upload(body, body.length, {
-      blobHTTPHeaders: { blobContentType: "application/json" },
-    });
-
-    if (response._response.status !== 201) {
-      throw new Error(`HTTP Status Code: ${response._response.status}`);
-    }
-
-    return NextResponse.json(
-      { message: "Success. Saved FHIR bundle to Azure Blob Storage: " + ecrId },
-      { status: 200 },
-    );
-  } catch (error: any) {
+  if (!fhirBundle || !ecrId) {
     return NextResponse.json(
       {
         message:
-          "Failed to insert FHIR bundle to Azure Blob Storage. " +
-          error.message,
+          "Error reading request body. Body must include a FHIR bundle with an ID.",
       },
       { status: 400 },
     );
   }
-};
+
+  if (!saveSource) {
+    return NextResponse.json(
+      {
+        message:
+          "Save location is undefined. Please provide a valid value for 'saveSource' (postgres or s3).",
+      },
+      { status: 400 },
+    );
+  }
+
+  if (saveSource === S3_SOURCE) {
+    return saveToS3(fhirBundle, ecrId);
+  } else if (saveSource === AZURE_SOURCE) {
+    return saveToAzure(fhirBundle, ecrId);
+  } else if (saveSource === POSTGRES_SOURCE) {
+    return await saveToPostgres(fhirBundle, ecrId);
+  } else {
+    return NextResponse.json(
+      {
+        message:
+          "Invalid save source. Please provide a valid source (postgres or s3)",
+      },
+      { status: 400 },
+    );
+  }
+}
