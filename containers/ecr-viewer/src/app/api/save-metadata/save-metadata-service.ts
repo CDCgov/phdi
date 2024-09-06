bb592a8fdd22403e28dc732120d48af38340766d
Josh Nygaard <jnygaard@skylight.digital>
2024-09-06 12:18:46 -0400

    Remove S3, Azure saving for metadata.

--- containers/ecr-viewer/src/app/api/save-metadata/save-metadata-service.ts
+++ containers/ecr-viewer/src/app/api/save-metadata/save-metadata-service.ts
@@ -1,9 +1,3 @@
-import {
-  PutObjectCommand,
-  PutObjectCommandOutput,
-  S3Client,
-} from "@aws-sdk/client-s3";
-import { BlobServiceClient } from "@azure/storage-blob";
 import { NextResponse } from "next/server";
 import pgPromise from "pg-promise";
 
@@ -17,20 +11,14 @@ interface BundleMetadata {
   report_date: string;
 }
 
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_CUSTOM_ENDPOINT,
-  forcePathStyle: process.env.AWS_CUSTOM_ENDPOINT !== undefined,
-});
-
 /**
- * Saves a FHIR bundle to a postgres database.
+ * Saves a FHIR bundle metadata to a postgres database.
  * @async
  * @function saveToS3
- * @param metadata - The FHIR bundle to be saved.
+ * @param metadata - The FHIR bundle metadata to be saved.
  * @param ecrId - The unique identifier for the Electronic Case Reporting (ECR) associated with the FHIR bundle.
- * @returns A promise that resolves when the FHIR bundle is successfully saved to postgres.
- * @throws {Error} Throws an error if the FHIR bundle cannot be saved to postgress.
+ * @returns A promise that resolves when the FHIR bundle metadata is successfully saved to postgres.
+ * @throws {Error} Throws an error if the FHIR bundle metadata cannot be saved to postgress.
  */
 export const saveToPostgres = async (
   metadata: BundleMetadata,
@@ -66,96 +54,6 @@ export const saveToPostgres = async (
     console.error("Error inserting metadata to database:", error);
     return NextResponse.json(
       { message: "Failed to insert metadata to database. " + error.message },
-      { status: 400 },
-    );
-  }
-};
-
-/**
- * Saves a FHIR bundle to an AWS S3 bucket.
- * @async
- * @function saveToS3
- * @param metadata - The FHIR bundle to be saved.
- * @param ecrId - The unique identifier for the Electronic Case Reporting (ECR) associated with the FHIR bundle.
- * @returns A promise that resolves when the FHIR bundle is successfully saved to the S3 bucket.
- * @throws {Error} Throws an error if the FHIR bundle cannot be saved to the S3 bucket.
- */
-export const saveToS3 = async (metadata: BundleMetadata, ecrId: string) => {
-  const bucketName = process.env.ECR_BUCKET_NAME;
-  const objectKey = `${ecrId}.json`;
-  const body = JSON.stringify(metadata);
-
-  try {
-    const input = {
-      Body: body,
-      Bucket: bucketName,
-      Key: objectKey,
-      ContentType: "application/json",
-    };
-
-    const command = new PutObjectCommand(input);
-    const response: PutObjectCommandOutput = await s3Client.send(command);
-    const httpStatusCode = response?.$metadata?.httpStatusCode;
-
-    if (httpStatusCode !== 200) {
-      throw new Error(`HTTP Status Code: ${httpStatusCode}`);
-    }
-    return NextResponse.json(
-      { message: "Success. Saved metadata to S3: " + ecrId },
-      { status: 200 },
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      { message: "Failed to insert data to S3. " + error.message },
-      { status: 400 },
-    );
-  }
-};
-
-/**
- * Saves a FHIR bundle to Azure Blob Storage.
- * @async
- * @function saveToAzure
- * @param metadata - The FHIR bundle to be saved.
- * @param ecrId - The unique ID for the eCR associated with the FHIR bundle.
- * @returns A promise that resolves when the FHIR bundle is successfully saved to Azure Blob Storage.
- * @throws {Error} Throws an error if the FHIR bundle cannot be saved to Azure Blob Storage.
- */
-export const saveToAzure = async (metadata: BundleMetadata, ecrId: string) => {
-  // TODO: Make this global after we get Azure access
-  const blobClient = BlobServiceClient.fromConnectionString(
-    process.env.AZURE_STORAGE_CONNECTION_STRING!,
-  );
-
-  if (!process.env.AZURE_CONTAINER_NAME)
-    throw Error("Azure container name not found");
-
-  const containerName = process.env.AZURE_CONTAINER_NAME;
-  const blobName = `${ecrId}.json`;
-  const body = JSON.stringify(metadata);
-
-  try {
-    const containerClient = blobClient.getContainerClient(containerName);
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-    const response = await blockBlobClient.upload(body, body.length, {
-      blobHTTPHeaders: { blobContentType: "application/json" },
-    });
-
-    if (response._response.status !== 201) {
-      throw new Error(`HTTP Status Code: ${response._response.status}`);
-    }
-
-    return NextResponse.json(
-      { message: "Success. Saved metadata to Azure Blob Storage: " + ecrId },
-      { status: 200 },
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      {
-        message:
-          "Failed to insert metadata to Azure Blob Storage. " + error.message,
-      },
       { status: 400 },
     );
   }
