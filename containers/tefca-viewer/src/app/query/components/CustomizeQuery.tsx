239e4486200bc9af867f89df7fc2432681892d80
Rob Mitchell <40571882+robertandremitchell@users.noreply.github.com>
2024-09-09 13:58:55 -0400

    TEFCA query customization page UI (#2363)
    * Initializing TEFCA Custom Query Design Work
    
    * getting closer
    
    * fixing up formatting
    
    * trying to add author/system
    
    * continuing messing up styles...
    
    * saving changes
    
    * trying to fix the dang checkmark
    
    * fixing mode
    
    * Fix TEFCA Customize Query Nav Tab Styling (#2375)
    
    * TEFCA Customize Query Nav Tab Styling
    
    * fix spacing around include all
    
    * cleaning up comments
    
    * updating hr line
    
    * mode
    
    * mode 2
    
    * fixing github action errors
    
    * fixing spacing, updating include all styling
    
    * updating customize query padding
    
    * switching from <p> to <div>
    
    * fixing gap between side nav
    
    * Customized query accordion component (#2404)
    
    * add tester accordion component
    
    * customizedQueryAccordion generic
    
    * move to more generic accordion
    
    * change to ValueSet
    
    * genericize accordion
    
    * add labs to interfaces
    
    * minor beautification
    
    * organize components
    
    * add new custom styles
    
    * add accordion component as a CSS grid
    
    * modify linting to prevent us from using any
    
    * rename styles to be unique
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * temp remove linting for any
    
    * remove trailing comma
    
    * remove logging statement
    
    * add toggle to flip accordion directional expander
    
    * remove unused styles
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * design fixes
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * remove styling comments
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    * TEFCA Customize UI fix title and subtitle (#2394)
    
    * Fixing TEFCA Customize Query Header
    
    * fixing return to search nav
    
    * TEFCA add customize query button to query form (#2392)
    
    * TEFCA Query adding customize query button
    
    * updating customize page back option
    
    * adding back font size
    
    * standardizing /customize to work in both npm and docker
    
    * standardizing code with existing functions
    
    * trying to get setMode set up
    
    * reverting to try again...
    
    * hacky fix to the 'back' button
    
    * getting queryType, useCase to carry over
    
    * add dummy customize query mode
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * transitioning from /customize to a component page for customize query
    
    * fixing build
    
    * adding type
    
    * removing customize page, updating test params
    
    * removing setMode from CustomizeQuery
    
    * adding documentation
    
    * more docs
    
    * updating doc
    
    * fixing merge
    
    ---------
    
    Co-authored-by: m-goggins <marcelle.goggins@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    * Fix TEFCA CustomizeQuery Checkboxes (#2432)
    
    * Fixing TEFCA CustomizeQuery Checkboxes
    
    * comment
    
    * changing checkbox sizes
    
    * adding correct checkbox
    
    * add indeterminate state
    
    * fixing spacing of sub-checkboxes
    
    * fixing the none selected checkbox formatting
    
    * fixing isExpanded
    
    * updating background color, button color and copy
    
    * changing background color
    
    * fixing styling on hr line
    
    * TEFCA Customize Query Accordion Updates (#2459)
    
    * TEFCA CustomizeQuery UI Accordion Formatting
    
    * fixing row lines
    
    * fixing final row, reverting to original hr
    
    * making ExpandMore/Less button sync with accordion click
    
    * fixing height of expandmore/less to be flush with selected
    
    * fixing checkboxes to not expand/close accordion
    
    * Begin populating codes in the TEFCA query customization page (#2488)
    
    * saving tefca changes
    
    * fixing customize-query in searchform
    
    * removing console logs, fixing customize query redirect
    
    * building out loading view
    
    ---------
    
    Co-authored-by: Marcelle <53578688+m-goggins@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: m-goggins <marcelle.goggins@gmail.com>
    Co-authored-by: bamader <brandon@skylight.digital>

---
+++ containers/tefca-viewer/src/app/query/components/CustomizeQuery.tsx
@@ -0,0 +1,326 @@
+"use client";
+
+import React, { useMemo, useState, useEffect } from "react";
+import { Accordion, Button, Icon } from "@trussworks/react-uswds";
+import { AccordianSection } from "../../query/component-utils";
+import { ValueSet } from "../../constants";
+import { AccordionItemProps } from "@trussworks/react-uswds/lib/components/Accordion/Accordion";
+import { UseCaseQueryResponse } from "../../query-service";
+import LoadingView from "./LoadingView";
+
+interface CustomizeQueryProps {
+  useCaseQueryResponse: UseCaseQueryResponse;
+  queryType: string;
+  ValueSet: ValueSet;
+  goBack: () => void;
+}
+
+/**
+ * CustomizeQuery component for displaying and customizing query details.
+ * @param root0 - The properties object.
+ * @param root0.useCaseQueryResponse - The response from the query service.
+ * @param root0.queryType - The type of the query.
+ * @param root0.ValueSet - The value set of labs, conditions, and medications.
+ * @param root0.goBack - Back button to go from "customize-queries" to "search" component.
+ * @returns The CustomizeQuery component.
+ */
+const CustomizeQuery: React.FC<CustomizeQueryProps> = ({
+  useCaseQueryResponse,
+  queryType,
+  ValueSet,
+  goBack,
+}) => {
+  const [activeTab, setActiveTab] = useState("labs");
+
+  const [valueSetState, setValueSetState] = useState<ValueSet>(ValueSet);
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  if (!useCaseQueryResponse) {
+    return <LoadingView loading={true} />;
+  }
+
+  // Keeps track of whether the accordion is expanded to change the direction of the arrow
+  const handleToggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  // Keeps track of which side nav tab to display to users
+  const handleTabChange = (tab: keyof ValueSet) => {
+    setActiveTab(tab);
+  };
+
+  // Allows all items to be selected within an accordion section
+  const handleSelectAllChange = (
+    items: any[],
+    setItems: React.Dispatch<React.SetStateAction<any[]>>,
+    checked: boolean,
+  ) => {
+    const updatedItems = items.map((item) => ({ ...item, include: checked }));
+    setItems(updatedItems);
+  };
+
+  const handleIncludeAll = (
+    setValueSet: React.Dispatch<React.SetStateAction<ValueSet>>,
+    key: keyof ValueSet,
+    include: boolean,
+  ) => {
+    setValueSet((prevValueSet) => ({
+      ...prevValueSet,
+      [key]: prevValueSet[key].map((item) => ({ ...item, include })),
+    }));
+  };
+
+  // Handles the toggle of the 'include' state for individual items
+  const toggleInclude = (index: number) => {
+    const updatedItems = [...valueSetState[activeTab as keyof ValueSet]];
+    updatedItems[index].include = !updatedItems[index].include;
+    setValueSetState((prevState) => ({
+      ...prevState,
+      [activeTab]: updatedItems,
+    }));
+  };
+
+  // Will eventually be the json object storing the parsed data to return on the results page
+  const handleApplyChanges = () => {
+    const selectedItems = Object.keys(valueSetState).reduce((acc, key) => {
+      const items = valueSetState[key as keyof ValueSet];
+      acc[key as keyof ValueSet] = items.filter((item) => item.include);
+      return acc;
+    }, {} as ValueSet);
+    goBack();
+  };
+
+  useEffect(() => {
+    const items = valueSetState[activeTab as keyof ValueSet];
+    const selectedCount = items.filter((item) => item.include).length;
+    const topCheckbox = document.getElementById(
+      "select-all",
+    ) as HTMLInputElement;
+    if (topCheckbox) {
+      topCheckbox.indeterminate =
+        selectedCount > 0 && selectedCount < items.length;
+    }
+  }, [valueSetState, activeTab]);
+
+  const accordionItems: AccordionItemProps[] = useMemo(() => {
+    const items = valueSetState[activeTab as keyof ValueSet];
+    const selectedCount = items.filter((item) => item.include).length;
+    return items.length
+      ? [
+          {
+            title: (
+              <div
+                className="accordion-header display-flex flex-no-wrap flex-align-start"
+                onClick={handleToggleExpand}
+              >
+                <div
+                  id="select-all"
+                  className="hide-checkbox-label"
+                  style={{
+                    width: "36px",
+                    height: "36px",
+                    backgroundColor: selectedCount === 0 ? "#565C65" : "#fff",
+                    border:
+                      selectedCount === 0
+                        ? "3px white solid"
+                        : "1px solid #A9AEB1",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    cursor: "pointer",
+                    borderRadius: "4px",
+                  }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleSelectAllChange(
+                      items,
+                      (updatedItems) =>
+                        setValueSetState((prevState) => ({
+                          ...prevState,
+                          [activeTab]: updatedItems,
+                        })),
+                      selectedCount !== items.length,
+                    );
+                  }}
+                >
+                  {selectedCount === items.length && (
+                    <Icon.Check
+                      className="usa-icon"
+                      style={{ backgroundColor: "white" }}
+                      size={4}
+                      color="#565C65"
+                    />
+                  )}
+                  {selectedCount > 0 && selectedCount < items.length && (
+                    <Icon.Remove
+                      className="usa-icon"
+                      style={{ backgroundColor: "white" }}
+                      size={4}
+                      color="#565C65"
+                    />
+                  )}
+                </div>
+                <div>
+                  {`${items[0].display}`}
+
+                  <span className="accordion-subtitle margin-top-2">
+                    <strong>Author:</strong> {items[0].author}{" "}
+                    <strong style={{ marginLeft: "20px" }}>System:</strong>{" "}
+                    {items[0].system}
+                  </span>
+                </div>
+                <span className="margin-left-auto">{`${selectedCount} selected`}</span>
+                <div
+                  onClick={handleToggleExpand}
+                  style={{
+                    cursor: "pointer",
+                    alignItems: "center",
+                    display: "flex",
+                    margin: "-3px",
+                  }}
+                >
+                  {isExpanded ? (
+                    <Icon.ExpandLess size={4} />
+                  ) : (
+                    <Icon.ExpandMore size={4} />
+                  )}
+                </div>
+              </div>
+            ),
+            id: items[0].author + ":" + items[0].system,
+            className: "accordion-item",
+            content: (
+              <AccordianSection>
+                <div className="customize-query-grid-container customize-query-table">
+                  <div className="customize-query-grid-header margin-top-10">
+                    <div className="accordion-table-header">Include</div>
+                    <div className="accordion-table-header">Code</div>
+                    <div className="accordion-table-header">Display</div>
+                  </div>
+                  <div className="customize-query-grid-body">
+                    {items.map((item, index) => (
+                      <div
+                        className="customize-query-grid-row customize-query-striped-row"
+                        key={item.code}
+                      >
+                        <div
+                          className="hide-checkbox-label"
+                          style={{
+                            border: "1px solid #A9AEB1",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            cursor: "pointer",
+                            borderRadius: "4px",
+                            width: "36px",
+                            height: "36px",
+                            marginLeft: "30px",
+                            backgroundColor: "#fff",
+                          }}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            toggleInclude(index);
+                          }}
+                        >
+                          {item.include && (
+                            <Icon.Check
+                              className="usa-icon"
+                              style={{ backgroundColor: "white" }}
+                              size={4}
+                              color="#005EA2"
+                            />
+                          )}
+                        </div>
+                        <div>{item.code}</div>
+                        <div>{item.display}</div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </AccordianSection>
+            ),
+            expanded: true,
+            headingLevel: "h3",
+          },
+        ]
+      : [];
+  }, [valueSetState, activeTab, isExpanded]);
+
+  return (
+    <div className="customize-query-container">
+      <div style={{ paddingTop: "24px" }}>
+        <a
+          href="#"
+          onClick={() => goBack()}
+          className="text-bold"
+          style={{ fontSize: "16px" }}
+        >
+          <Icon.ArrowBack /> Return to patient search
+        </a>
+      </div>
+      <h1 className="font-sans-2xl text-bold" style={{ paddingBottom: "0px" }}>
+        Customize query
+      </h1>
+      <div
+        className="font-sans-lg text-light"
+        style={{ paddingBottom: "0px", paddingTop: "4px" }}
+      >
+        Query: {queryType}
+      </div>
+      <nav className="usa-nav custom-nav">
+        <li
+          className={`usa-nav__primary-item ${
+            activeTab === "labs" ? "usa-current" : ""
+          }`}
+        >
+          <a href="#labs" onClick={() => handleTabChange("labs")}>
+            Labs
+          </a>
+        </li>
+        <li
+          className={`usa-nav__primary-item ${
+            activeTab === "medications" ? "usa-current" : ""
+          }`}
+        >
+          <a href="#medications" onClick={() => handleTabChange("medications")}>
+            Medications
+          </a>
+        </li>
+        <li
+          className={`usa-nav__primary-item ${
+            activeTab === "conditions" ? "usa-current" : ""
+          }`}
+        >
+          <a href="#conditions" onClick={() => handleTabChange("conditions")}>
+            Conditions
+          </a>
+        </li>
+      </nav>
+      <ul className="usa-nav__primary usa-accordion"></ul>
+      <hr className="custom-hr"></hr>
+      <a
+        href="#"
+        type="button"
+        className="include-all-link"
+        onClick={() =>
+          handleIncludeAll(setValueSetState, activeTab as keyof ValueSet, true)
+        }
+      >
+        Include all {activeTab}
+      </a>
+      <div>
+        <Accordion items={accordionItems} multiselectable bordered />
+      </div>
+      <div className="button-container">
+        <Button type="button" onClick={handleApplyChanges}>
+          Apply changes
+        </Button>
+        <Button type="button" outline onClick={() => goBack()}>
+          Cancel
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CustomizeQuery;
