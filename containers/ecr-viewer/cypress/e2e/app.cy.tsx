adfa819d803bb2059df6fe613182c6070b2d30c4
angelathe <angela.the@skylight.digital>
2024-09-11 14:36:32 -0500

    revert changes to cypress & set NODE_ENV=test

--- containers/ecr-viewer/cypress/e2e/app.cy.tsx
+++ containers/ecr-viewer/cypress/e2e/app.cy.tsx
@@ -1,8 +1,9 @@
 describe("Happy Path", () => {
   it("Should load the eCR Viewer for a known ID", () => {
     // Visit a valid URL
-    const basePath = Cypress.env("BASE_PATH") || "";
-    cy.visit(`${basePath}/view-data?id=6100896d-b520-497c-b2fe-1c111c679274`);
+    cy.visit(
+      "http://localhost:3000/view-data?id=6100896d-b520-497c-b2fe-1c111c679274",
+    );
 
     // Gets the expected content
     cy.get("#patient-summary").contains("Patient Summary");
@@ -13,8 +14,7 @@ describe("Happy Path", () => {
 describe("Failing Path", () => {
   it("Should return an error when given an invalid ID", () => {
     // Visit an invalid URL
-    const basePath = Cypress.env("BASE_PATH") || "";
-    cy.visit(`${basePath}/view-data?id=123`);
+    cy.visit("http://localhost:3000/view-data?id=123");
 
     cy.contains("Sorry, we couldn't find this eCR ID.");
   });
