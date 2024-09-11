8d9fe2553213cd4e33ed31094aaa5b6de6960a17
angelathe <angela.the@skylight.digital>
2024-09-11 13:22:28 -0500

    update cypress test paths

--- containers/ecr-viewer/cypress.config.ts
+++ containers/ecr-viewer/cypress.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000",
+    env: {
+      BASE_PATH: "/ecr-viewer",
+    },
   },
 });
