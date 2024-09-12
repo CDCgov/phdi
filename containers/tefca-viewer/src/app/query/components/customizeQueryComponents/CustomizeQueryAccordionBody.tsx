b1b33a1434246979c4d25277c0ca51326f61d7fe
Bob Zhao <bob@skylight.digital>
2024-09-12 11:56:13 -0400

    try stuff and then break it

--- containers/tefca-viewer/src/app/query/components/customizeQueryComponents/CustomizeQueryAccordionBody.tsx
+++ containers/tefca-viewer/src/app/query/components/customizeQueryComponents/CustomizeQueryAccordionBody.tsx
@@ -11,45 +11,44 @@ type CustomizeQueryAccordionBodyProps = {
 const CustomizeQueryAccordionBody: React.FC<
   CustomizeQueryAccordionBodyProps
 > = ({ group, toggleInclude, groupIndex }) => {
+  console.log("in child body component", group.items);
   return (
-    <>
-      <div className="padding-bottom-3">
-        <div className="customize-query-grid-container customize-query-table">
-          <div className="customize-query-grid-header margin-top-10">
-            <div className="accordion-table-header">Include</div>
-            <div className="accordion-table-header">Code</div>
-            <div className="accordion-table-header">Display</div>
-          </div>
-          <div className="customize-query-grid-body">
-            {group.items.map((item, index) => (
+    <div className="padding-bottom-3">
+      <div className="customize-query-grid-container customize-query-table">
+        <div className="customize-query-grid-header margin-top-10">
+          <div className="accordion-table-header">Include</div>
+          <div className="accordion-table-header">Code</div>
+          <div className="accordion-table-header">Display</div>
+        </div>
+        <div className="customize-query-grid-body">
+          {group.items.map((item, index) => (
+            <div
+              className="customize-query-grid-row customize-query-striped-row"
+              key={item.code}
+            >
               <div
-                className="customize-query-grid-row customize-query-striped-row"
-                key={item.code}
+                className={`hide-checkbox-label margin-4 ${customAccordionStyles.checkboxLabel} `}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleInclude(groupIndex, index);
+                }}
               >
-                <div
-                  className={`hide-checkbox-label margin-4 ${customAccordionStyles.checkboxLabel} `}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    toggleInclude(groupIndex, index);
-                  }}
-                >
-                  {item.include && (
-                    <Icon.Check
-                      className="usa-icon"
-                      style={{ backgroundColor: "white" }}
-                      size={4}
-                      color="#005EA2"
-                    />
-                  )}
-                </div>
-                <div>{item.code}</div>
-                <div>{item.display}</div>
+                {item.include && (
+                  <Icon.Check
+                    className="usa-icon"
+                    style={{ backgroundColor: "white" }}
+                    size={4}
+                    color="#005EA2"
+                  />
+                )}
               </div>
-            ))}
-          </div>
+              <div>{item.code}</div>
+              <div>{item.display}</div>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
