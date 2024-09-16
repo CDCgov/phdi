5444f9727da76e06236e32cffdf68980bd180ec2
bamader <brandon@skylight.digital>
2024-09-16 17:09:19 -0400

    Update encoder interceptor

--- containers/tefca-viewer/jest.setup.ts
+++ containers/tefca-viewer/jest.setup.ts
@@ -2,5 +2,11 @@ import "@testing-library/jest-dom";
 import { toHaveNoViolations } from "jest-axe";
 import * as matchers from "jest-extended";
 
+import { TextEncoder } from "util";
+global.TextEncoder = TextEncoder;
+
+// Uncomment and import the TextDecoder if interceptor parsing becomes a problem
+// global.TextDecoder = TextDecoder;
+
 expect.extend(toHaveNoViolations);
 expect.extend(matchers);
