44043c4802cde742cda8d14c9fc1925dbb9c9ef0
bamader <brandon@skylight.digital>
2024-09-17 08:33:27 -0400

    Ummm what

--- containers/tefca-viewer/jest.setup.ts
+++ containers/tefca-viewer/jest.setup.ts
@@ -2,8 +2,8 @@ import "@testing-library/jest-dom";
 import { toHaveNoViolations } from "jest-axe";
 import * as matchers from "jest-extended";
 
-import { TextEncoder } from "util";
-global.TextEncoder = TextEncoder;
+// import { TextEncoder } from "util";
+// global.TextEncoder = TextEncoder;
 
 // Uncomment and import the TextDecoder if interceptor parsing becomes a problem
 // global.TextDecoder = TextDecoder;
