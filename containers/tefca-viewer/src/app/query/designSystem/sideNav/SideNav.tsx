a5b79ccbb1b84f0d23350bf77bddaac42c0dc8c7
Bob Zhao <bob@skylight.digital>
2024-09-23 11:08:59 -0400

    new folder

---
+++ containers/tefca-viewer/src/app/query/designSystem/sideNav/SideNav.tsx
@@ -0,0 +1,68 @@
+import { SideNav as UswdsSideNav } from "@trussworks/react-uswds";
+import { formatIdForAnchorTag } from "../../components/resultsViewAccordion/ResultsViewTable";
+import { ReactNode } from "react";
+import styles from "./sidenav.module.css";
+
+export type NavItem = {
+  title: string;
+  activeItem: boolean;
+  isSubNav?: boolean;
+  navItemClassName?: string;
+};
+
+export type SideNavProps = {
+  items: NavItem[];
+  containerClassName?: string;
+  sticky?: boolean;
+};
+
+const SideNav: React.FC<SideNavProps> = ({
+  items,
+  containerClassName,
+  sticky = false,
+}) => {
+  const sideNavItems: ReactNode[] = [];
+  for (let item of items) {
+    const sideNavItem = buildSectionMarkUp(
+      item.title,
+      item.activeItem,
+      item.navItemClassName
+    );
+    sideNavItems.push(
+      <div className={styles.subItem}>
+        <UswdsSideNav items={[sideNavItem]} isSubnav={item.isSubNav} />
+      </div>
+    );
+  }
+  return (
+    <nav
+      className={`${styles.container} ${containerClassName ?? ""} ${
+        sticky ? styles.sticky : ""
+      }`}
+    >
+      <UswdsSideNav items={sideNavItems} />
+    </nav>
+  );
+};
+
+function buildSectionMarkUp(
+  title: string,
+  activeItem: boolean,
+  className?: string
+): ReactNode {
+  let sectionId = formatIdForAnchorTag(title);
+  let sideNavItem = (
+    <a
+      key={sectionId}
+      href={"#" + sectionId}
+      className={`${styles.item} ${className} ${
+        activeItem ? `usa-current` : ""
+      }`}
+    >
+      {title}
+    </a>
+  );
+  return sideNavItem;
+}
+
+export default SideNav;
