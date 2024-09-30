d851a8493fc601a311320e2176b498feda910b4d
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-09-30 10:57:27 -0400

    Disable Pool Client SSL Configuration for tefca

--- containers/tefca-viewer/src/app/database-service.ts
+++ containers/tefca-viewer/src/app/database-service.ts
@@ -1,6 +1,6 @@
 "use server";
 import { Pool, PoolConfig, QueryResultRow } from "pg";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import { ValueSetItem, valueSetTypeToClincalServiceTypeMap } from "./constants";
 
 const getQuerybyNameSQL = `
@@ -14,14 +14,18 @@ select q.query_name, q.id, qtv.valueset_id, vs.name as valueset_name, vs.author 
 `;
 
 // Load environment variables from tefca.env and establish a Pool configuration
-// dotenv.config({ path: "tefca.env" });
+console.log("Database Configuration:");
+console.log("Connection String:", process.env.DATABASE_URL);
+dotenv.config({ path: "tefca.env" });
 const dbConfig: PoolConfig = {
   connectionString: process.env.DATABASE_URL,
+  ssl: false, // Disable SSL explicitly
   max: 10, // Maximum # of connections in the pool
   idleTimeoutMillis: 30000, // A client must sit idle this long before being released
   connectionTimeoutMillis: 2000, // Wait this long before timing out when connecting new client
 };
 const dbClient = new Pool(dbConfig);
+console.log("DB Config Object:", dbConfig);
 
 /**
  * Executes a search for a CustomQuery against the query-loaded Postgres
