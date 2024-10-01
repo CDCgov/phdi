37406f43005a72ac4bf66ab63309b29b4bf1bb6e
m-goggins <marcelle.goggins@gmail.com>
2024-10-01 15:46:46 -0400

    undo changes to migrations

--- containers/tefca-viewer/flyway/sql/V01_03__test_bash_script.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE OR REPLACE FUNCTION run_bash_script()
-RETURNS VOID AS $$
-BEGIN
-    SELECT plsh('bash /docker-entrypoint-initdb.d/setup-env.sh');
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT run_bash_script();
