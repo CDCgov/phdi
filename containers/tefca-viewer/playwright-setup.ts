4de324f79fc6084e351ea8a43155d21bf6ab1723
m-goggins <marcelle.goggins@gmail.com>
2024-10-02 14:43:31 -0400

    update url

--- containers/tefca-viewer/playwright-setup.ts
+++ containers/tefca-viewer/playwright-setup.ts
@@ -1,18 +1,8 @@
-// setup("Checking for 200 response from localhost", async ({}) => {
-//   const url = "http://localhost:3000/tefca-viewer";
-
-//   const response = await fetch(url);
-//   while (response.status !== 200) {
-//     console.log(`Failed to connect to ${url}`);
-//     const response = await fetch(url);
-//   }
-// });
-
 /**
  *
  */
 async function globalSetup() {
-  const url = "http://localhost:3000/";
+  const url = "http://localhost:3000/tefca-viewer";
 
   const response = await fetch(url);
   while (response.status !== 200) {
