864b46077f99c6e79ff08e816c9f8972b3b1e102
Boban Ljuljdjurovic <boban@skylight.digital>
2024-10-08 11:34:11 -0400

    Add eicr author details as its own section in unavailable information

--- containers/ecr-viewer/src/app/view-data/components/AccordionContent.tsx
+++ containers/ecr-viewer/src/app/view-data/components/AccordionContent.tsx
@@ -68,7 +68,7 @@ const AccordionContent: React.FC<AccordionContainerProps> = ({
       ...ecrMetadata.eicrDetails.unavailableData,
       ...ecrMetadata.ecrCustodianDetails.unavailableData,
       ecrMetadata.eRSDWarnings,
-      ...ecrMetadata.eicrAuthorDetails.unavailableData,
+      ecrMetadata.eicrAuthorDetails.map((details) => details.unavailableData),
     ];
     return unavailableDataArrays.some(
       (array) => Array.isArray(array) && array.length > 0,
@@ -165,7 +165,9 @@ const AccordionContent: React.FC<AccordionContainerProps> = ({
           {Object.keys(ecrMetadata.rrDetails).length > 0 ||
           ecrMetadata.eRSDWarnings.length > 0 ||
           ecrMetadata.eicrDetails.availableData.length > 0 ||
-          ecrMetadata.eicrAuthorDetails.availableData.length > 0 ||
+          ecrMetadata.eicrAuthorDetails.find(
+            (authorDetails) => authorDetails.availableData.length > 0,
+          ) ||
           ecrMetadata.ecrCustodianDetails.availableData.length > 0 ? (
             <EcrMetadata
               eicrDetails={ecrMetadata.eicrDetails.availableData}
@@ -174,7 +176,9 @@ const AccordionContent: React.FC<AccordionContainerProps> = ({
               }
               rrDetails={ecrMetadata.rrDetails}
               eRSDWarnings={ecrMetadata.eRSDWarnings}
-              eicrAuthorDetails={ecrMetadata.eicrAuthorDetails.availableData}
+              eicrAuthorDetails={ecrMetadata.eicrAuthorDetails.map(
+                (details) => details.availableData,
+              )}
             />
           ) : (
             <p className="text-italic padding-bottom-05">
@@ -213,8 +217,10 @@ const AccordionContent: React.FC<AccordionContainerProps> = ({
                   ? [{ title: "eRSD Warnings", value: "" }]
                   : []),
                 ...ecrMetadata.ecrCustodianDetails.unavailableData,
-                ...ecrMetadata.eicrAuthorDetails.unavailableData,
               ]}
+              eicrAuthorDetails={ecrMetadata.eicrAuthorDetails.map(
+                (authorDetails) => authorDetails.unavailableData,
+              )}
             />
           ) : (
             <p className="text-italic padding-bottom-105 margin-0">
