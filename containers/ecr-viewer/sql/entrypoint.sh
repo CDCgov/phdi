8e5a211dd1e33d672e877e86a258e53e4d530e46
Josh Nygaard <jnygaard@skylight.digital>
2024-10-15 10:14:38 -0400

    Move SQL Server scripts into a single directory

--- containers/ecr-viewer/sql/entrypoint.sh
+++ containers/ecr-viewer/sql/entrypoint.sh
@@ -4,7 +4,7 @@
 /opt/mssql/bin/sqlservr &
 
 # Wait for SQL Server to start (you can fine-tune the sleep time if needed)
-sleep 61
+sleep 120
 
 # Run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i /var/opt/mssql/scripts/extended.sql -C
