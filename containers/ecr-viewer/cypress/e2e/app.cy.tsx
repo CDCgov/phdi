2f9ba1cffb24e1f15fd3ff04f621f9a938ce3c90
Josh Nygaard <jnygaard@skylight.digital>
2024-10-16 16:00:05 -0400

    Cypress logs

--- containers/ecr-viewer/cypress/e2e/app.cy.tsx
+++ containers/ecr-viewer/cypress/e2e/app.cy.tsx
@@ -2,6 +2,9 @@ describe("Happy Path", () => {
   it("Should load the eCR Viewer for a known ID", () => {
     // Visit a valid URL
     const basePath = Cypress.env("BASE_PATH") || "";
+    cy.log(
+      `Visiting ${basePath}/view-data?id=6100896d-b520-497c-b2fe-1c111c679274`,
+    );
     cy.visit(`${basePath}/view-data?id=6100896d-b520-497c-b2fe-1c111c679274`);
 
     // Gets the expected content
@@ -14,6 +17,7 @@ describe("Failing Path", () => {
   it("Should return an error when given an invalid ID", () => {
     // Visit an invalid URL
     const basePath = Cypress.env("BASE_PATH") || "";
+    cy.log(`Visiting ${basePath}/view-data?id=123`);
     cy.visit(`${basePath}/view-data?id=123`);
 
     cy.contains("Sorry, we couldn't find this eCR ID.");
