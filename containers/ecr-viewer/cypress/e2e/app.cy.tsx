abfe40e0313f998739f7e35c345e379d82edfea2
Josh Nygaard <jnygaard@skylight.digital>
2024-10-16 16:56:44 -0400

    I just want logging

--- containers/ecr-viewer/cypress/e2e/app.cy.tsx
+++ containers/ecr-viewer/cypress/e2e/app.cy.tsx
@@ -2,14 +2,12 @@ describe("Happy Path", () => {
   it("Should load the eCR Viewer for a known ID", () => {
     // Visit a valid URL
     const basePath = Cypress.env("BASE_PATH") || "";
-    cy.log(
-      `Visiting ${basePath}/view-data?id=6100896d-b520-497c-b2fe-1c111c679274`,
-    );
     cy.visit(`${basePath}/view-data?id=6100896d-b520-497c-b2fe-1c111c679274`);
 
     // Gets the expected content
     cy.get("#patient-summary").contains("Patient Summary");
     cy.contains("VICTORIA HUNTER");
+    cy.contains(basePath);
   });
 });
 
@@ -17,9 +15,9 @@ describe("Failing Path", () => {
   it("Should return an error when given an invalid ID", () => {
     // Visit an invalid URL
     const basePath = Cypress.env("BASE_PATH") || "";
-    cy.log(`Visiting ${basePath}/view-data?id=123`);
     cy.visit(`${basePath}/view-data?id=123`);
 
     cy.contains("Sorry, we couldn't find this eCR ID.");
+    cy.contains(basePath);
   });
 });
