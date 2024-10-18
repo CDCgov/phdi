acb03193baaabe3c188f8d793e482407743e0c68
Josh Nygaard <jnygaard@skylight.digital>
2024-10-18 15:55:28 -0400

    Add tests

--- containers/fhir-converter/tests/integration/test_FHIR-Converter.py
+++ containers/fhir-converter/tests/integration/test_FHIR-Converter.py
@@ -118,3 +118,46 @@ def test_invalid_rr_format(setup):
         == "Reportability Response and eICR message both "
         "must be valid XML messages."
     )
+
+
+@pytest.mark.integration
+def test_single_administrated_medications():
+    input_data = open(
+        "tests/test_files/eICR_with_single_administrated_medication.xml"
+    ).read()
+    request = {"input_data": input_data, "input_type": "ecr", "root_template": "EICR"}
+    ecr_conversion_response = httpx.post(CONVERT_TO_FHIR, json=request)
+    assert ecr_conversion_response.status_code == 200
+
+    medication_administration = filter(
+        lambda x: x["fullUrl"] == "urn:uuid:620f71f8-1ab2-93c8-e0f5-44aec35c7aba",
+        ecr_conversion_response.json()["response"]["fhir_Resource"],
+    )
+    assert len(medication_administration) == 1
+
+
+@pytest.mark.integration
+def test_multiple_administrated_medications():
+    input_data = open(
+        "tests/test_files/eICR_with_single_administrated_medication.xml"
+    ).read()
+    request = {"input_data": input_data, "input_type": "ecr", "root_template": "EICR"}
+    ecr_conversion_response = httpx.post(CONVERT_TO_FHIR, json=request)
+    assert ecr_conversion_response.status_code == 200
+
+    medication_administration_references = [
+        x["fullUrl"]
+        for x in filter(
+            lambda x: x["resource"]["resourceType"] == "MedicationAdministration",
+            ecr_conversion_response.json()["response"]["fhir_Resource"],
+        )
+    ]
+    assert len(medication_administration_references) == 2
+    assert (
+        "urn:uuid:0a8a0aba-cf15-5ea8-f64f-3f635a582a6e"
+        in medication_administration_references
+    )
+    assert (
+        "urn:uuid:d0722cbe-d8ea-fd17-aa37-4afd9f630db1"
+        in medication_administration_references
+    )
