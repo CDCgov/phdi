4700972dc1581a29208963d6c05b1b9487a6ad5c
Boban Ljuljdjurovic <boban@skylight.digital>
2024-10-23 07:23:59 -0400

    set base url to localhost again ?

--- containers/ecr-viewer/cypress.config.ts
+++ containers/ecr-viewer/cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: `http://127.0.0.1:3000`,
+    baseUrl: `http://localhost:3000`,
     env: {
       BASE_PATH: `${isDev ? "/" : "/ecr-viewer"}`,
     },
